9441d7b934a9958bc5af1a7e1b5a6fd741d10994
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-09-08 23:24:17 +0000

    removed #pragma auto lib linking

--- trunk/delta3d/inc/rticonnection.h
+++ trunk/delta3d/inc/rticonnection.h
@@ -1106,36 +1106,4 @@ namespace dtHLA
    };
 };
 
-#undef _AUTOLIBNAME
-#undef _AUTOLIBNAME1
-#undef _AUTOLIBNAME2
-#undef _AUTOLIBNAME3
-
-//
-// Automatic library inclusion macros that use the #pragma/lib feature
-//
-#undef _AUTOLIBNAME
-#if defined(_DEBUG)
-   #define _AUTOLIBNAME  "libRTI-NGd.lib"
-#else
-   #define _AUTOLIBNAME  "libRTI-NG.lib"
-#endif
-
-#define _AUTOLIBNAME1  "tinyxml.lib"
-#define _AUTOLIBNAME3  "osgSim.lib"
-#define _AUTOLIBNAME2  "ws2_32.lib"
-
-/* You may turn off this include message by defining _NOAUTOLIB */
-#ifndef _NOAUTOLIBMSG
-#pragma message( "Will automatically link with " _AUTOLIBNAME )
-#pragma message( "Will automatically link with " _AUTOLIBNAME1 )
-#pragma message( "Will automatically link with " _AUTOLIBNAME2 )
-#pragma message( "Will automatically link with " _AUTOLIBNAME3 )
-#endif
-
-#pragma comment(lib, _AUTOLIBNAME)
-#pragma comment(lib, _AUTOLIBNAME1)
-#pragma comment(lib, _AUTOLIBNAME2)
-#pragma comment(lib, _AUTOLIBNAME3)
-
 #endif // DT_HLA_RTI_CONNECTION
