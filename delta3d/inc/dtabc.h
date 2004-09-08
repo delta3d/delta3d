1ef4492eb5d7e9a5c6f0fb08231566d9e9a0efa7
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-09-08 19:29:53 +0000

    Module-specific header files

---
+++ delta3d/inc/dtabc.h
@@ -0,0 +1,49 @@
+#ifndef DTABC_INCLUDE
+#define DTABC_INCLUDE
+
+#include "application.h"
+#include "baseabc.h"
+#include "weather.h"
+
+/* You may turn off this include message by defining _NOAUTOLIB */
+#undef _AUTOLIBNAME1
+#undef _AUTOLIBNAME2
+#undef _AUTOLIBNAME3
+#undef _AUTOLIBNAME4
+#undef _AUTOLIBNAME5
+#undef _AUTOLIBNAME6
+#undef _AUTOLIBNAME7
+
+#if defined(_DEBUG)
+   #define _AUTOLIBNAME1   "dtabcD.lib"
+   #define _AUTOLIBNAME2   "gui_fld.lib"
+#else 
+   #define _AUTOLIBNAME1   "dtabc.lib"  
+   #define _AUTOLIBNAME2   "gui_fl.lib"
+#endif
+
+#define _AUTOLIBNAME3 "tinyxml.lib"
+#define _AUTOLIBNAME4 "fltk.lib"
+#define _AUTOLIBNAME5 "wsock32.lib"
+#define _AUTOLIBNAME6 "comctl32.lib"
+#define _AUTOLIBNAME7 "winmm.lib"
+
+#ifndef _NOAUTOLIBMSG
+   #pragma message( "Will automatically link with " _AUTOLIBNAME1 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME2 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME3 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME4 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME5 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME6 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME7 )
+#endif
+
+#pragma  comment( lib, _AUTOLIBNAME1 )
+#pragma  comment( lib, _AUTOLIBNAME2 )
+#pragma  comment( lib, _AUTOLIBNAME3 )
+#pragma  comment( lib, _AUTOLIBNAME4 )
+#pragma  comment( lib, _AUTOLIBNAME5 )
+#pragma  comment( lib, _AUTOLIBNAME6 )
+#pragma  comment( lib, _AUTOLIBNAME7 )
+
+#endif //DEFINE DTABC_INCLUDE
