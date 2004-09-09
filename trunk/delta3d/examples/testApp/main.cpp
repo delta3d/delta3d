babb6f40d138576d844207c5928d6d81cd827bc9
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-09-09 01:31:33 +0000

    Made the example slightly more interesting.

--- trunk/delta3d/examples/testApp/main.cpp
+++ trunk/delta3d/examples/testApp/main.cpp
@@ -6,6 +6,23 @@
 int main()
 {
    dtABC::Application *app = new dtABC::Application("config.xml");
+
+   //load something interesting
+   dtCore::SetDataFilePathList("../../data/;../../../data/");
+   dtCore::Object *obj = new dtCore::Object("MyObject");
+   obj->LoadFile("ground.flt");
+   app->AddDrawable(obj);
+
+   //adjust the Camera position
+   dtCore::Transform camPos;
+   sgVec3 camXYZ={0.f, -50.f, 20.f};
+   sgVec3 lookAtXYZ = {0.f, 0.f, 0.f};
+   sgVec3 upVec = {0.f, 0.f, 1.f};
+   camPos.SetLookAt(camXYZ, lookAtXYZ, upVec);
+   app->GetCamera()->SetTransform(&camPos);
+
+   app->GetWindow()->SetWindowTitle("Hit escape to exit");
+
    app->Config();
 
    app->Run();
