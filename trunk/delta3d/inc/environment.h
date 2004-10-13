236fa0e3b4d97e05a21b821fe8987b30c23b6aae
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-10-13 01:53:32 +0000

    added handle to the parent Scene

--- trunk/delta3d/inc/environment.h
+++ trunk/delta3d/inc/environment.h
@@ -8,6 +8,7 @@
 #include "sunlightshader.h"
 #include "skydomeshader.h"
 #include "skydome.h"
+#include "scene.h"
 
 namespace dtCore
 {
@@ -163,6 +164,8 @@ namespace dtCore
       SunlightShader *mSunlightShader; ///<pixel shader for light scattering
       SkyDomeShader *mSkyDomeShader; ///<pixel shader for the skydome
       osg::ref_ptr<SkyDome> mSkyDome; ///<the added SkyDome (couuld be NULL)
+
+      osg::ref_ptr<Scene> mParentScene;///<The Scene the Environment was added to
 
       void UpdateSkyLight(void);
       void UpdateFogColor(void);
