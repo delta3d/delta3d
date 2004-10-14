5a1a0141a6501040b920b9200fc67b1e2f955e78
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-10-14 02:01:11 +0000

    used only for the API documentation

---
+++ trunk/delta3d/inc/mainpage.h
@@ -0,0 +1,8 @@
+/** \mainpage Delta3D API Documentation
+  * \section Intro
+  * Delta3D is an Open Source engine which can be used for games,
+  * simulations, or other graphical applications.
+  * \section API
+  * Welcome to the API Documentation.  Click on the above links for more 
+  * API info.
+  */
