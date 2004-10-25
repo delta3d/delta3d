268605844a48c8247042802c9778f8330f6cf554
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-10-25 23:30:13 +0000

    updated for new functions, structs

--- delta3d/src/python/deltawinbindings.cpp
+++ delta3d/src/python/deltawinbindings.cpp
@@ -10,12 +10,21 @@ using namespace dtCore;
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SC_overloads, ShowCursor, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SFSM_overloads, SetFullScreenMode, 0, 1)
-
+   
 void initDeltaWinBindings()
 {
    DeltaWin* (*DeltaWinGI1)(int) = &DeltaWin::GetInstance;
    DeltaWin* (*DeltaWinGI2)(std::string) = &DeltaWin::GetInstance;
 
+   bool (*DeltaWinCSR1)(int,int,int,int) = &DeltaWin::ChangeScreenResolution;
+   bool (*DeltaWinCSR2)(Resolution) = &DeltaWin::ChangeScreenResolution;
+
+   class_<Resolution>("Resolution")
+            .def_readwrite("width", &Resolution::width)
+            .def_readwrite("height", &Resolution::height)
+            .def_readwrite("bitDepth", &Resolution::bitDepth)
+            .def_readwrite("refresh", &Resolution::refresh);
+   
    class_<DeltaWin, bases<Base>, osg::ref_ptr<DeltaWin> >("DeltaWin", init<optional<std::string, int, int, int, int> >())
       .def("GetInstanceCount", &DeltaWin::GetInstanceCount)
       .staticmethod("GetInstanceCount")
@@ -34,5 +43,7 @@ void initDeltaWinBindings()
       .def("GetRenderSurface", &DeltaWin::GetRenderSurface, return_internal_reference<>())
       .def("GetKeyboard", &DeltaWin::GetKeyboard, return_internal_reference<>())
       .def("GetMouse", &DeltaWin::GetMouse, return_internal_reference<>())
-      .def("ChangeScreenResolution", &DeltaWin::ChangeScreenResolution);
+      .def("GetCurrentResolution", &DeltaWin::GetCurrentResolution)
+      .def("ChangeScreenResolution", DeltaWinCSR1)
+      .def("ChangeScreenResolution", DeltaWinCSR2);
 }
