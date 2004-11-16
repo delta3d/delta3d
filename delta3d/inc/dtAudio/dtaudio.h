fd7eecc30875d92e8eb94ed0b11c4932d76745f6
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-11-16 20:08:09 +0000

    Moved from /inc

---
+++ delta3d/inc/dtAudio/dtaudio.h
@@ -0,0 +1,55 @@
+#ifndef  DELTA_DTAUDIO
+#define  DELTA_DTAUDIO
+
+#include <dtaudio/sound.h>
+#include <dtaudio/listener.h>
+#include <dtaudio/audiomanager.h>
+#include <dtaudio/soundeffectbinder.h>
+
+
+
+/* You may turn off this include message by defining _NOAUTOLIB */
+#undef _AUTOLIBNAME1
+#undef _AUTOLIBNAME2
+#undef _AUTOLIBNAME3
+
+
+
+#if   defined(_DEBUG)
+   #ifndef  DT_LIBRARY
+      #define  _AUTOLIBNAME1  "dtaudiod.lib"
+   #endif
+#else
+   #ifndef  DT_LIBRARY
+      #define  _AUTOLIBNAME1  "dtaudio.lib"
+   #endif
+#endif
+
+
+
+#define  _AUTOLIBNAME2  "OpenAL32.lib" // Note: Not Debug
+#define  _AUTOLIBNAME3  "ALut.lib"     // Note: Not Debug
+
+
+
+#if   !  defined(_NOAUTOLIBMSG)
+   #ifndef  DT_LIBRARY
+      #pragma message( "Will automatically link with " _AUTOLIBNAME1 )
+   #endif
+
+   #pragma message( "Will automatically link with " _AUTOLIBNAME2 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME3 )
+#endif
+
+
+
+#ifndef  DT_LIBRARY
+   #pragma  comment( lib, _AUTOLIBNAME1 )
+#endif
+
+#pragma  comment( lib, _AUTOLIBNAME2 )
+#pragma  comment( lib, _AUTOLIBNAME3 )
+
+
+
+#endif   // DELTA_DTAUDIO
