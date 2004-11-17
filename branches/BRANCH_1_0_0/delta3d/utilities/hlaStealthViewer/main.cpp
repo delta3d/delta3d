d813feadace3e5bfd0cb8b8329053a4244b9a4e6
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-11-17 17:54:46 +0000

    added ../data and ../data/images to search path for running from exe

--- branches/BRANCH_1_0_0/delta3d/utilities/hlaStealthViewer/main.cpp
+++ branches/BRANCH_1_0_0/delta3d/utilities/hlaStealthViewer/main.cpp
@@ -41,7 +41,7 @@ class HLAStealthViewerApplication : public Application
       {
          GetWindow()->SetWindowTitle("HLA Stealth Viewer");
 
-         SetDataFilePathList("./data;../../data;./data/images;" + GetDeltaDataPathList());
+         SetDataFilePathList("./data;./data/images;../data;../data/images;" + GetDeltaDataPathList());
          
          mEffectManager = new EffectManager;
          
