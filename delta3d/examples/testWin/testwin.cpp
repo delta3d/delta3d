b17acda04ad7fd12d6b9a8f7a8a1b482cad80da2
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2004-11-30 20:21:21 +0000

    added ability to re-title window, position window

--- delta3d/examples/testWin/testwin.cpp
+++ delta3d/examples/testWin/testwin.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include <sstream>
 
 #include "testwin.h"
 #include "UI/CUI_ComboBox.h"
@@ -14,7 +15,15 @@ CUI_UI*       TestWinApp::mUI = NULL;
 DeltaWin*     TestWinApp::mWindow = NULL;
 ResolutionVec TestWinApp::mResVec;
 int           TestWinApp::mSelectedRes = -1;
-CUI_TextBox*  TestWinApp::mCurrentRexText = NULL;
+CUI_TextBox*  TestWinApp::mCurrentResText = NULL;
+
+template<class T>
+std::string ToString(const T& val)
+{
+   std::ostringstream strm;
+   strm << val;
+   return strm.str();
+} 
 
 TestWinApp::TestWinApp( std::string configFilename /*= "config.xml"*/ )
 :  Application(configFilename)
@@ -58,7 +67,7 @@ TestWinApp::Config()
    drawable->CreateShader( "button", "gui/button.tga" );
    drawable->CreateShader( "button_hover", "gui/button_hover.tga" );
    drawable->CreateShader( "button_click", "gui/button_click.tga" );
-   drawable->CreateShader( "cursor", "gui/cursor.rgba" );
+   //drawable->CreateShader( "cursor", "gui/cursor.rgba" );
    drawable->CreateShader( "edit_box", "gui/edit_box.rgb");
    drawable->CreateShader( "back_panel", "gui/back_panel.rgb");
 
@@ -70,10 +79,10 @@ TestWinApp::Config()
    drawable->AddRootFrame("testWin", mainFrame);
 
    CUI_TextBox* resTitle = new CUI_TextBox;
-   resTitle->Move( 0.1, 0.875, 0.4, 0.925 );
-   resTitle->SetSize(15, 1);
-   resTitle->SetTextScale( 1.0f );
-   resTitle->SetText( "Resolution:" );
+   resTitle->Move( 0.1, 0.8, 0.4, 0.885 );
+   resTitle->SetSize(12, 1);
+   resTitle->SetTextScale( 0.8f );
+   resTitle->SetText( "Resolution" );
    resTitle->SetFont(drawable->GetFont("raster8b") );
    resTitle->SetShader(drawable->GetShader("transparent"));
    mainFrame->AddChild( resTitle );
@@ -91,15 +100,15 @@ TestWinApp::Config()
 
    string currentString = string(wh) + " " + string(r) + " " + string(d);
 
-   mCurrentRexText = new CUI_TextBox;
-   mCurrentRexText->Move( 0.41, 0.835, 0.79, 0.915 );
-   mCurrentRexText->SetSize(25, 1);
-   mCurrentRexText->SetTextScale( 1.0f );
-   mCurrentRexText->SetText( currentString.c_str() );
-   mCurrentRexText->SetFont(drawable->GetFont("raster8w") );
-   mCurrentRexText->SetShader(drawable->GetShader("edit_box"));
-   mainFrame->AddChild( mCurrentRexText );
-   drawable->AddFrame( mCurrentRexText );
+   mCurrentResText = new CUI_TextBox;
+   mCurrentResText->Move( 0.41, 0.8, 0.79, 0.885 );
+   mCurrentResText->SetSize(25, 1);
+   mCurrentResText->SetTextScale( 0.8f );
+   mCurrentResText->SetText( currentString.c_str() );
+   mCurrentResText->SetFont(drawable->GetFont("raster8w") );
+   mCurrentResText->SetShader(drawable->GetShader("edit_box"));
+   mainFrame->AddChild( mCurrentResText );
+   drawable->AddFrame( mCurrentResText );
 
    // dump all valid resolutions into a vector
    mResVec = DeltaWin::GetResolutions();
@@ -135,13 +144,11 @@ TestWinApp::Config()
    resolutionMenu->SetHoverShader( drawable->GetShader("button") );
    resolutionMenu->SetFlag( UI_SQUEEZE, true );
    resolutionMenu->SetItemSize( 0.04 );
-   resolutionMenu->Move( 0.4, 0.725, 0.8, 0.825 );
+   resolutionMenu->Move( 0.4, 0.715, 0.8, 0.8 );
    resolutionMenu->SetFrameID( RES_MENU );
 
    drawable->AddFrame( resolutionMenu );
    mainFrame->AddChild( resolutionMenu );
-
-   
 
    resolutionMenu->AddItem( "Select resolution...", 0, NULL, UI_ITEM_SUBMENU, widthHeightMenu );
 
@@ -251,7 +258,7 @@ TestWinApp::Config()
 
    // "set" button
    CUI_Button* resButton = new CUI_Button;
-   resButton->Move(0.8, 0.825, 0.9, 0.925 );
+   resButton->Move(0.8, 0.8, 0.9, 0.885 );
    resButton->SetShader(drawable->GetShader("button"));
    resButton->SetHoverShader(drawable->GetShader("button_hover"));
    resButton->SetClickShader(drawable->GetShader("button_click"));
@@ -264,9 +271,9 @@ TestWinApp::Config()
    drawable->AddFrame( resButton );  
 
    CUI_TextBox* fullScreenTitle = new CUI_TextBox;
-   fullScreenTitle->Move( 0.1, 0.6, 0.4, 0.65 );
-   fullScreenTitle->SetSize(15, 1);
-   fullScreenTitle->SetTextScale( 1.0f );
+   fullScreenTitle->Move( 0.1, 0.565, 0.4, 0.65 );
+   fullScreenTitle->SetSize(12, 1);
+   fullScreenTitle->SetTextScale( 0.8f );
    fullScreenTitle->SetText( "FullScreen" );
    fullScreenTitle->SetFont(drawable->GetFont("raster8b") );
    fullScreenTitle->SetShader(drawable->GetShader("transparent"));
@@ -278,7 +285,7 @@ TestWinApp::Config()
 
    fullScreenCombo->SetFlag( UI_VERTICAL, true );  
    fullScreenCombo->SetFrameID( FULL_COMBO );
-   fullScreenCombo->Move( 0.4, 0.55, 0.8, 0.65 );
+   fullScreenCombo->Move( 0.4, 0.565, 0.8, 0.65 );
    fullScreenCombo->SetShader( drawable->GetShader( "transparent" ) );
    fullScreenCombo->SetItemShader( drawable->GetShader("button") );
    fullScreenCombo->SetSelectedShader( drawable->GetShader("button") );
@@ -293,7 +300,7 @@ TestWinApp::Config()
 
    // "set" button
    CUI_Button* fullScreenButton = new CUI_Button;
-   fullScreenButton->Move(0.8, 0.55, 0.9, 0.65 );
+   fullScreenButton->Move(0.8, 0.565, 0.9, 0.65 );
    fullScreenButton->SetShader(drawable->GetShader("button"));
    fullScreenButton->SetHoverShader(drawable->GetShader("button_hover"));
    fullScreenButton->SetClickShader(drawable->GetShader("button_click"));
@@ -302,20 +309,181 @@ TestWinApp::Config()
    fullScreenButton->SetTextScale(0.40f);
    fullScreenButton->SetFrameID( FULL_SET );
    mainFrame->AddChild( fullScreenButton );
-   drawable->AddFrame( fullScreenButton );    
+   drawable->AddFrame( fullScreenButton );
+
+   // window title manipulation
+   CUI_TextBox* windowNameTitle = new CUI_TextBox;
+   windowNameTitle->Move( 0.1, 0.415, 0.4, 0.5 );
+   windowNameTitle->SetSize(12, 1);
+   windowNameTitle->SetTextScale( 0.8f );
+   windowNameTitle->SetText( "Window Title" );
+   windowNameTitle->SetFont(drawable->GetFont("raster8b") );
+   windowNameTitle->SetShader(drawable->GetShader("transparent"));
+   mainFrame->AddChild( windowNameTitle );
+   drawable->AddFrame( windowNameTitle );
+
+   CUI_EditableTextBox* windowNameEdit = new CUI_EditableTextBox;
+   windowNameEdit->Move( 0.41, 0.415, 0.79, 0.5 );
+   windowNameEdit->SetSize(25, 1);
+   windowNameEdit->SetTextScale( 0.8f );
+   windowNameEdit->SetText( mWindow->GetWindowTitle().c_str() );
+   windowNameEdit->SetFont(drawable->GetFont("raster8w") );
+   windowNameEdit->SetShader(drawable->GetShader("edit_box"));
+   windowNameEdit->SetFrameID( TITLE_EDIT );
+   windowNameEdit->SetCursorType(UI_CURSOR_BAR);
+   windowNameEdit->MoveCursor( mWindow->GetWindowTitle().length() );
+   mainFrame->AddChild( windowNameEdit );
+   drawable->AddFrame( windowNameEdit );
+
+   // "set" button
+   CUI_Button* windowNameButton = new CUI_Button;
+   windowNameButton->Move(0.8, 0.415, 0.9, 0.5 );
+   windowNameButton->SetShader(drawable->GetShader("button"));
+   windowNameButton->SetHoverShader(drawable->GetShader("button_hover"));
+   windowNameButton->SetClickShader(drawable->GetShader("button_click"));
+   windowNameButton->SetFont( drawable->GetFont("raster8w") );
+   windowNameButton->SetText("Set");
+   windowNameButton->SetTextScale(0.40f);
+   windowNameButton->SetFrameID( TITLE_SET );
+   mainFrame->AddChild( windowNameButton );
+   drawable->AddFrame( windowNameButton );
+
+   // window title manipulation
+   CUI_TextBox* positionTitle = new CUI_TextBox;
+   positionTitle->Move( 0.1, 0.265, 0.4, 0.35 );
+   positionTitle->SetSize(12, 1);
+   positionTitle->SetTextScale( 0.8f );
+   positionTitle->SetText( "Position" );
+   positionTitle->SetFont(drawable->GetFont("raster8b") );
+   positionTitle->SetShader(drawable->GetShader("transparent"));
+   positionTitle->SetFrameID( POS_TITLE );
+   mainFrame->AddChild( positionTitle );
+   drawable->AddFrame( positionTitle );
+
+   CUI_EditableTextBox* positionXEdit = new CUI_EditableTextBox;
+   positionXEdit->Move( 0.41, 0.265, 0.5, 0.35 );
+   positionXEdit->SetSize(4, 1);
+   positionXEdit->SetTextScale( 0.8f );
+   positionXEdit->SetText( ToString(x).c_str() );
+   positionXEdit->SetFont(drawable->GetFont("raster8w") );
+   positionXEdit->SetShader(drawable->GetShader("edit_box"));
+   positionXEdit->SetFrameID( POS_X );
+   positionXEdit->SetCursorType(UI_CURSOR_BAR);
+   positionXEdit->MoveCursor( ToString(x).length()  );
+   mainFrame->AddChild( positionXEdit );
+   drawable->AddFrame( positionXEdit );
+
+   CUI_EditableTextBox* positionYEdit = new CUI_EditableTextBox;
+   positionYEdit->Move( 0.51, 0.265, 0.6, 0.35 );
+   positionYEdit->SetSize(4, 1);
+   positionYEdit->SetTextScale( 0.8f );
+   positionYEdit->SetText( ToString(y).c_str() );
+   positionYEdit->SetFont(drawable->GetFont("raster8w") );
+   positionYEdit->SetShader(drawable->GetShader("edit_box"));
+   positionYEdit->SetFrameID( POS_Y );
+   positionYEdit->SetCursorType(UI_CURSOR_BAR);
+   positionYEdit->MoveCursor( ToString(y).length() );
+   mainFrame->AddChild( positionYEdit );
+   drawable->AddFrame( positionYEdit );
+
+   CUI_EditableTextBox* positionWEdit = new CUI_EditableTextBox;
+   positionWEdit->Move( 0.61, 0.265, 0.7, 0.35 );
+   positionWEdit->SetSize(4, 1);
+   positionWEdit->SetTextScale( 0.8f );
+   positionWEdit->SetText( ToString(w).c_str() );
+   positionWEdit->SetFont(drawable->GetFont("raster8w") );
+   positionWEdit->SetShader(drawable->GetShader("edit_box"));
+   positionWEdit->SetFrameID( POS_W );
+   positionWEdit->SetCursorType(UI_CURSOR_BAR);
+   positionWEdit->MoveCursor( ToString(w).length() );
+   mainFrame->AddChild( positionWEdit );
+   drawable->AddFrame( positionWEdit );
+
+   CUI_EditableTextBox* positionHEdit = new CUI_EditableTextBox;
+   positionHEdit->Move( 0.71, 0.265, 0.8, 0.35 );
+   positionHEdit->SetSize(4, 1);
+   positionHEdit->SetTextScale( 0.8f );
+   positionHEdit->SetText( ToString(h).c_str() );
+   positionHEdit->SetFont(drawable->GetFont("raster8w") );
+   positionHEdit->SetShader(drawable->GetShader("edit_box"));
+   positionHEdit->SetFrameID( POS_H );
+   positionHEdit->SetCursorType(UI_CURSOR_BAR);
+   positionHEdit->MoveCursor( ToString(h).length() );
+   mainFrame->AddChild( positionHEdit );
+   drawable->AddFrame( positionHEdit );
+
+   CUI_TextBox* positionXText = new CUI_TextBox;
+   positionXText->Move( 0.41, 0.18, 0.5, 0.265 );
+   positionXText->SetSize(1, 1);
+   positionXText->SetTextScale( 0.8f );
+   positionXText->SetText( "X" );
+   positionXText->SetFont(drawable->GetFont("raster8b") );
+   positionXText->SetShader(drawable->GetShader("transparent"));
+   positionXText->SetFrameID( POS_X_TEXT );
+   mainFrame->AddChild( positionXText );
+   drawable->AddFrame( positionXText );
+
+   CUI_TextBox* positionYText = new CUI_TextBox;
+   positionYText->Move( 0.51, 0.18, 0.6, 0.265 );
+   positionYText->SetSize(1, 1);
+   positionYText->SetTextScale( 0.8f );
+   positionYText->SetText( "Y" );
+   positionYText->SetFont(drawable->GetFont("raster8b") );
+   positionYText->SetShader(drawable->GetShader("transparent"));
+   positionYText->SetFrameID( POS_Y_TEXT );
+   mainFrame->AddChild( positionYText );
+   drawable->AddFrame( positionYText );
+
+   CUI_TextBox* positionWText = new CUI_TextBox;
+   positionWText->Move( 0.61, 0.18, 0.7, 0.265 );
+   positionWText->SetSize(1, 1);
+   positionWText->SetTextScale( 0.8f );
+   positionWText->SetText( "W" );
+   positionWText->SetFont(drawable->GetFont("raster8b") );
+   positionWText->SetShader(drawable->GetShader("transparent"));
+   positionWText->SetFrameID( POS_W_TEXT );
+   mainFrame->AddChild( positionWText );
+   drawable->AddFrame( positionWText );
+
+   CUI_TextBox* positionHText = new CUI_TextBox;
+   positionHText->Move( 0.71, 0.18, 0.8, 0.265 );
+   positionHText->SetSize(1, 1);
+   positionHText->SetTextScale( 0.8f );
+   positionHText->SetText( "H" );
+   positionHText->SetFont(drawable->GetFont("raster8b") );
+   positionHText->SetShader(drawable->GetShader("transparent"));
+   positionHText->SetFrameID( POS_H_TEXT );
+   mainFrame->AddChild( positionHText );
+   drawable->AddFrame( positionHText );
+
+   // "set" button
+   CUI_Button* positionButton = new CUI_Button;
+   positionButton->Move(0.8, 0.265, 0.9, 0.35 );
+   positionButton->SetShader(drawable->GetShader("button"));
+   positionButton->SetHoverShader(drawable->GetShader("button_hover"));
+   positionButton->SetClickShader(drawable->GetShader("button_click"));
+   positionButton->SetFont( drawable->GetFont("raster8w") );
+   positionButton->SetText("Set");
+   positionButton->SetTextScale(0.40f);
+   positionButton->SetFrameID( POS_SET );
+   mainFrame->AddChild( positionButton );
+   drawable->AddFrame( positionButton );
 
    /// "exit" button
    CUI_Button* exit = new CUI_Button();
-   exit->Move(0.1, 0.2, 0.3, 0.3 );
+   exit->Move(0.1, 0.095, 0.4, 0.18 );
    exit->SetShader(drawable->GetShader("button"));
    exit->SetHoverShader(drawable->GetShader("button_hover"));
    exit->SetClickShader(drawable->GetShader("button_click"));
    exit->SetFont( drawable->GetFont("raster8w") );
-   exit->SetText("Exit");
+   exit->SetText(" Exit ");
    exit->SetTextScale(0.40f);
    exit->SetFrameID( EXIT );
    mainFrame->AddChild(exit);
-   drawable->AddFrame(exit);    
+   drawable->AddFrame(exit);
+
+   fullScreenCombo->BringToFront();
+   resolutionMenu->BringToFront();
 
    // hook up the main callback
    drawable->SetCallbackFunc("testWin", (CUI_UI::callbackfunc)GuiHandler );
@@ -354,15 +522,7 @@ TestWinApp::GuiHandler( int id, int numparam, void *value )
          System::GetSystem()->Stop();
          break;
       }
-   case FULL_SET:
-      {
-         CUI_ComboBox* fullScreenCombo = static_cast<CUI_ComboBox*>( mUI->GetFrame( FULL_COMBO ) );
-         assert( fullScreenCombo );
-
-         mWindow->SetFullScreenMode( fullScreenCombo->GetSelectedID() - FULL_OFF );
-         break;
-      }
-   case RES_SET:
+   case RES_SET: //Resolution Set
       {
          if( mSelectedRes != -1 )
          {
@@ -371,7 +531,96 @@ TestWinApp::GuiHandler( int id, int numparam, void *value )
 
          break;
       }
-   default:
+   case FULL_SET: //FullScreen set
+      {
+         CUI_ComboBox* fullScreenCombo = dynamic_cast<CUI_ComboBox*>( mUI->GetFrame( FULL_COMBO ) );
+         assert( fullScreenCombo );
+
+         bool fullScreen = fullScreenCombo->GetSelectedID() - FULL_OFF;
+         mWindow->SetFullScreenMode( fullScreen );
+
+         CUI_TextBox* positionTitle = dynamic_cast<CUI_TextBox*>( mUI->GetFrame( POS_TITLE ) );
+         assert(positionTitle);
+         CUI_Button* positionButton = dynamic_cast<CUI_Button*>( mUI->GetFrame( POS_SET ) );
+         assert(positionButton);
+         
+         CUI_EditableTextBox* xEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_X ) );
+         assert(xEdit);
+         CUI_EditableTextBox* yEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_Y ) );
+         assert(yEdit);
+         CUI_EditableTextBox* wEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_W ) );
+         assert(wEdit);
+         CUI_EditableTextBox* hEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_H ) );
+         assert(hEdit);
+
+         CUI_TextBox* xText = dynamic_cast<CUI_TextBox*>( mUI->GetFrame( POS_X_TEXT ) );
+         assert(xText);
+         CUI_TextBox* yText = dynamic_cast<CUI_TextBox*>( mUI->GetFrame( POS_Y_TEXT ) );
+         assert(yText);
+         CUI_TextBox* wText = dynamic_cast<CUI_TextBox*>( mUI->GetFrame( POS_W_TEXT ) );
+         assert(wText);
+         CUI_TextBox* hText = dynamic_cast<CUI_TextBox*>( mUI->GetFrame( POS_H_TEXT ) );
+         assert(hText);
+
+         positionTitle->Activate( !fullScreen );
+         positionButton->Activate( !fullScreen );
+         
+         xEdit->Activate( !fullScreen );
+         yEdit->Activate( !fullScreen );
+         wEdit->Activate( !fullScreen );
+         hEdit->Activate( !fullScreen );
+         
+         xText->Activate( !fullScreen );
+         yText->Activate( !fullScreen );
+         wText->Activate( !fullScreen );
+         hText->Activate( !fullScreen );
+         
+         if( !fullScreen )
+         {
+            //query values
+            int x,y,w,h;
+            mWindow->GetPosition( &x, &y, &w, &h );
+            xEdit->SetText( ToString(x).c_str() );
+            yEdit->SetText( ToString(y).c_str() );
+            wEdit->SetText( ToString(w).c_str() );
+            hEdit->SetText( ToString(h).c_str() );
+
+            xEdit->MoveCursor( ToString(x).length() );
+            yEdit->MoveCursor( ToString(y).length() );
+            wEdit->MoveCursor( ToString(w).length() );
+            hEdit->MoveCursor( ToString(h).length() );
+         }
+         
+         break;
+      }
+   case TITLE_SET: //Window Title Set
+      {
+         CUI_EditableTextBox* titleEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( TITLE_EDIT ) );
+         assert(titleEdit);
+         
+         mWindow->SetWindowTitle( titleEdit->GetText() );
+         break;
+      }
+   case POS_SET: //Position Set
+      {
+         CUI_EditableTextBox* xEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_X ) );
+         assert(xEdit);
+         CUI_EditableTextBox* yEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_Y ) );
+         assert(yEdit);
+         CUI_EditableTextBox* wEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_W ) );
+         assert(wEdit);
+         CUI_EditableTextBox* hEdit = dynamic_cast<CUI_EditableTextBox*>( mUI->GetFrame( POS_H ) );
+         assert(hEdit);
+
+         int x = atoi( xEdit->GetText() );
+         int y = atoi( yEdit->GetText() );
+         int w = atoi( wEdit->GetText() );
+         int h = atoi( hEdit->GetText() );
+
+         mWindow->SetPosition( x, y, w, h );
+         break;
+      }
+   default: //menu selection
       {
          if( id >= DEPTH_ITEMS )
          {
@@ -387,7 +636,7 @@ TestWinApp::GuiHandler( int id, int numparam, void *value )
 
             string selectedString = string(wh) + " " + string(r) + " " + string(d);
 
-            mCurrentRexText->SetText( selectedString.c_str() );
+            mCurrentResText->SetText( selectedString.c_str() );
          }
 
          break;
