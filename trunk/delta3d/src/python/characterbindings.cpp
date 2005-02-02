d30b7de85257e205d291a8ef94a68d1f90d390a3
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-02-02 21:49:21 +0000

    updated for 0.8 mods

--- trunk/delta3d/src/python/characterbindings.cpp
+++ trunk/delta3d/src/python/characterbindings.cpp
@@ -13,19 +13,20 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EA_overloads, ExecuteAction, 1, 3)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EAWS_overloads, ExecuteActionWithSpeed, 2, 4)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EAWA_overloads, ExecuteActionWithAngle, 2, 4)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EAWSAA_overloads, ExecuteActionWithSpeedAndAngle, 3, 5)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LF_overloads, LoadFile, 1, 2)
 
 void initCharacterBindings()
 {
    Character* (*CharacterGI1)(int) = &Character::GetInstance;
    Character* (*CharacterGI2)(std::string) = &Character::GetInstance;
 
-   class_<Character, bases<Transformable>, osg::ref_ptr<Character> >("Character", init<optional<std::string> >())
+   class_<Character, bases<Transformable,Loadable>, osg::ref_ptr<Character> >("Character", init<optional<std::string> >())
       .def("GetInstanceCount", &Character::GetInstanceCount)
       .staticmethod("GetInstanceCount")
       .def("GetInstance", CharacterGI1, return_internal_reference<>())
       .def("GetInstance", CharacterGI2, return_internal_reference<>())
       .staticmethod("GetInstance")
-      .def("LoadFile", &Character::LoadFile)
+      .def("LoadFile", &Character::LoadFile, LF_overloads()[return_internal_reference<>()])
       .def("GetFilename", &Character::GetFilename)
       .def("SetRotation", &Character::SetRotation)
       .def("GetRotation", &Character::GetRotation)
