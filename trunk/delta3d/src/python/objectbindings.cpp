7f4e91f432af7db4d49fb74b65264f84d0487009
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-02-02 23:12:05 +0000

    updated for 0.8

--- trunk/delta3d/src/python/objectbindings.cpp
+++ trunk/delta3d/src/python/objectbindings.cpp
@@ -7,16 +7,15 @@
 
 using namespace boost::python;
 using namespace dtCore;
-/*
+
 class ObjectWrap : public Object
 {
    public:
 
       ObjectWrap(PyObject* self, std::string name = "")
-         : Object(name),
-           mSelf(self)
+         : mSelf(self)
       {}
-
+      /*
       virtual bool LoadFile(std::string filename, bool useCache = true)
       {
          return call_method<bool>(mSelf, "LoadFile", filename, useCache);
@@ -26,12 +25,22 @@ class ObjectWrap : public Object
       {
          return Object::LoadFile(filename, useCache);
       }
+      */  
+      void LoadFileWrapper1(std::string filename, bool useCache)
+      {
+         Object::LoadFile(filename,useCache);
+      }
+   
+      void LoadFileWrapper2(std::string filename)
+      {
+         Object::LoadFile(filename);
+      }
       
    protected:
 
       PyObject* mSelf;
 };
-*/
+
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LF_overloads, LoadFile, 1, 2)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(RGUL_overloads, RecenterGeometryUponLoad, 0, 1)
 
@@ -40,12 +49,14 @@ void initObjectBindings()
    Object* (*ObjectGI1)(int) = &Object::GetInstance;
    Object* (*ObjectGI2)(std::string) = &Object::GetInstance;
 
-   class_<Object, bases<Physical>, osg::ref_ptr<Object>, boost::noncopyable>("Object", init<optional<std::string> >())
+   class_<Object, bases<Physical>, osg::ref_ptr<ObjectWrap>, boost::noncopyable>("Object", init<optional<std::string> >())
       .def("GetInstanceCount", &Object::GetInstanceCount)
       .staticmethod("GetInstanceCount")
       .def("GetInstance", ObjectGI1, return_internal_reference<>())
       .def("GetInstance", ObjectGI2, return_internal_reference<>())
       .staticmethod("GetInstance")
-      .def("LoadFile", &Object::LoadFile, LF_overloads()[return_internal_reference<>()])
+      //.def("LoadFile", &Object::LoadFile, LF_overloads()[return_internal_reference<>()])
+      .def("LoadFile", &ObjectWrap::LoadFileWrapper1 )
+      .def("LoadFile", &ObjectWrap::LoadFileWrapper2 )
       .def("RecenterGeometryUponLoad", &Object::RecenterGeometryUponLoad, RGUL_overloads());
 }
