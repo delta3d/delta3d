217dc6d751f920686bc5b4cf86dbd31c7c1914fc
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-02-04 22:07:46 +0000

    linking problem

--- trunk/delta3d/src/python/install_linux_python.sh
+++ trunk/delta3d/src/python/install_linux_python.sh
@@ -11,8 +11,8 @@ echo 'Linking from dtcharbindings.so -> libdtChar.so'
 ln -sf libdtcharbindings.so dtChar.so
 echo 'Linking from dtaudiobindings.so -> libdtAudio.so'
 ln -sf libdtaudiobindings.so dtAudio.so
-echo 'Linking from dtguiflbindings.so -> libdtGUIFl.so'
-ln -sf libdtguiflbindings.so dtGUIFl.so
+echo 'Linking from gui_flbindings.so -> libdtGUIFl.so'
+ln -sf libgui_flbindings.so dtGUIFl.so
 #echo 'Linking from dthlabindings.so -> libdt.so'
 #ln -sf libdthlabindings.so dtHLA.so
 
