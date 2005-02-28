efb8f632ce97708befd87a984a25187ebe3046f4
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-02-28 19:46:25 +0000

    fixed shader bug on ATIs, will it work on NVIDIA now???

--- trunk/delta3d/src/soarx/soarxterrain.cpp
+++ trunk/delta3d/src/soarx/soarxterrain.cpp
@@ -64,16 +64,15 @@ const char* fragmentShader =
    "  vec4 detail = texture2D(detailGradient, gl_TexCoord[0].st);"
    "  vec4 scale = texture2D(detailScale, gl_TexCoord[1].st);"
    "  vec4 base = texture2D(baseGradient, gl_TexCoord[1].st);"
-//   "  vec4 combined = (detail - 0.5) * scale + (base - 0.5);"
+ //"  vec4 combined = (detail - 0.5) * scale + (base - 0.5);" //original Andrejz
    "  vec4 combined = (detail - 0.5) * scale + 0.5 + (base - 0.5);"
-   "  vec3 normal = normalize(vec3(combined.y, 0.5, combined.z));"
+ //"  vec3 normal = normalize(vec3(combined.y, 0.5, combined.z));"
+   "  vec3 normal = gl_NormalMatrix * normalize(vec3(combined.y, 0.5, combined.z));"
    "  gl_FragColor = texture2D(baseColor, gl_TexCoord[1].st);"
    "  gl_FragColor.rgb *= max(0.0, dot(normal, vec3(gl_LightSource[0].position)));"
    "  float fog;"
    "  const float LOG2E = 1.442695;"   // = 1/log(2)
-//   "  if (gl_FogFragCoord < 0.0) {gl_FogFragCoord = 0.0;}"
-//   "  fog = exp2(-gl_Fog.density * .3 * gl_Fog.density * gl_FogFragCoord * gl_FogFragCoord * LOG2E);"
-"  fog = exp2(-gl_Fog.density * gl_Fog.density * gl_FogFragCoord * gl_FogFragCoord * LOG2E);"
+   "  fog = exp2(-gl_Fog.density * .3 * gl_Fog.density * gl_FogFragCoord * gl_FogFragCoord * LOG2E);"
    "  fog = clamp(fog, 0.0, 1.0);"
    "  gl_FragColor = mix(gl_Fog.color, gl_FragColor, fog);"
    "}";
