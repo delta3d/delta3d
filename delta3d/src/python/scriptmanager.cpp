c4cd1bfdf30a260e5129da3eb9c2c7b742636206
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-03-15 00:04:26 +0000

    now kicks off new interpreter on each Load(), stores thread as a member, and successive Run() executes file object already in memory

--- delta3d/src/python/scriptmanager.cpp
+++ delta3d/src/python/scriptmanager.cpp
@@ -1,32 +1,53 @@
 #include "python/scriptmanager.h"
 
+#include <boost/python/errors.hpp>
+
 using namespace dtScript;
 
 IMPLEMENT_MANAGEMENT_LAYER(ScriptManager)
 
 ScriptManager::ScriptManager()
-: mFileObject(0)
+:  mFileObject(0),
+   mThreadState(0)
 {
+   RegisterInstance( this );
+
    SetName("ScriptManager");
-   Py_Initialize();
+
+   Py_Initialize(); // start python thread
 }
 
 ScriptManager::~ScriptManager()
 {
-   Py_Finalize();
+   if ( mThreadState != NULL ) Py_EndInterpreter( mThreadState );
+
+   Py_Finalize(); // kill python thread
+
+   DeregisterInstance( this );
 }
 
 void 
 ScriptManager::Load( std::string filename )
 {
+   if ( mThreadState != NULL ) Py_EndInterpreter( mThreadState );
+
+   mThreadState = Py_NewInterpreter();
+
    mFilename = filename;
-   mFileObject = PyFile_FromString( const_cast<char*>(mFilename.c_str()), "r" );
+   mFileObject = PyFile_FromString( const_cast<char*>( mFilename.c_str() ), "r" );
 }
 
 void 
 ScriptManager::Run()
 { 
-   PyRun_SimpleFile( PyFile_AsFile(mFileObject), const_cast<char*>(mFilename.c_str()) );
+   try
+   {
+      PyRun_SimpleFile( PyFile_AsFile(mFileObject), const_cast<char*>(mFilename.c_str()) );
+   }
+   catch (...)
+   {
+      PyErr_Print();
+   }
 }
 
 void 
