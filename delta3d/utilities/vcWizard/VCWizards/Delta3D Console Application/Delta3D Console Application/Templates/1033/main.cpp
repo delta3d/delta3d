39d2462f8dda25a911c57392295027ac26c0327a
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-03-30 22:36:23 +0000

    added visual studio project template

---
+++ delta3d/utilities/vcWizard/VCWizards/Delta3D Console Application/Delta3D Console Application/Templates/1033/main.cpp
@@ -0,0 +1,21 @@
+// main.cpp : defines the entry point for the console application.
+
+#include "[!output PROJECT_NAME].h"
+
+using namespace dtCore;
+
+main()
+{
+   //set data search path to parent directory and delta3d/data
+   SetDataFilePathList( "..;" + GetDeltaDataPathList() ); 
+	
+   //Instantiate the application and look for the config file
+   RefPtr<[!output PROJECT_NAME]> app = new [!output PROJECT_NAME]();
+	
+   app->Config(); //configuring the application
+   app->Run(); // running the simulation loop
+   
+   return 0;
+
+}
+
