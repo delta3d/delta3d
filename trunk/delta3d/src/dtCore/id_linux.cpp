512fd4c8d38c33e84c2d1f44843e4ac2a70a1cd5
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-04-05 01:03:34 +0000

    fixed on linux

--- trunk/delta3d/src/dtCore/id_linux.cpp
+++ trunk/delta3d/src/dtCore/id_linux.cpp
@@ -1,38 +1,35 @@
 #include "dtCore/id.h"
-
-//#include <uuid/uuid.h>
+#include "dtCore/notify.h"
+#include <iostream>
 
 using namespace dtCore;
 using namespace std;
    
 Id::Id()
 {
+   uuid_clear( mId );
    uuid_generate( mId );
 }
 
-/*
 Id::Id( const Id& id )
 {
+   uuid_clear( mId );
    uuid_copy( mId, id.mId );
 }
-*/
 
-Id::~Id()
-{
-}
 
 bool Id::operator== ( Id id )
 {
-   if( uuid_compare( mId, id.mId ) == 0 )
-      return true;
-   else
-      return false;
+   return uuid_compare( mId, id.mId ) == 0;
 }
 
 void Id::Set( const std::string& stringId )
 {
-   //uuid_parse
-   //uuid_copy( mId, id.mId );
+   assert( stringId.length() != 36 );
+   
+   uuid_clear( mId );
+
+   assert( uuid_parse( stringId.c_str(), mId ) != 0 );
 }
 
 void Id::Get( std::string& stringId ) const
