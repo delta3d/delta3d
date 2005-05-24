9f189e52b99ac877c8a13205c33ca0bf927ac6b2
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-05-24 18:26:13 +0000

    cleaned up code for public release

--- delta3d/examples/testGUI/testgui.cpp
+++ delta3d/examples/testGUI/testgui.cpp
@@ -3,13 +3,10 @@
 #include "dtABC/dtabc.h"
 
 #include <CEGUI/CEGUI.h>
-#include <CEGUI/CEGUIPropertySet.h>
 
 
 using namespace dtCore;
 using namespace dtABC;
-using namespace std;
-
 
 
 class TestGUIApp : public dtABC::Application
@@ -20,9 +17,7 @@ public:
       {
       }
 
-   ~TestGUIApp()
-   {
-   }
+      ~TestGUIApp() {}
 
    virtual void Config()
    {
@@ -31,9 +26,6 @@ public:
 
       dtABC::Application::Config();
 
-      GetWindow()->SetWindowTitle("TestCEGApp");
-
-      //setup scene here
       ///put something in the background to look at
       RefPtr<Object> helo = new Object( "Helo" );
       helo->LoadFile( "models/uh-1n.ive" );
@@ -47,23 +39,12 @@ public:
       int x,y,w,h;
       GetWindow()->GetPosition(&x, &y, &w, &h);
       GetWindow()->ShowCursor( false );
+
       mGUI = new dtGUI::CEUIDrawable(w, h);
 
       BuildGUI();
 
       GetScene()->AddDrawable(mGUI.get());
-   }
-
-   virtual void KeyPressed( dtCore::Keyboard *keyboard, 
-                            Producer::KeyboardKey key,
-                            Producer::KeyCharacter character )
-   {
-      switch( key )
-      {
-      case Producer::Key_Escape: Quit(); break;
-        
-      default:  break;       
-      }
    }
 
 
@@ -72,33 +53,7 @@ public:
       mLayoutFilename = filename;
    }
 
-
-   static void OutputProperties(CEGUI::Window *window)
-   {
-      // Log all its properties + values
-      CEGUI::PropertySet::PropertyIterator itr = ((CEGUI::PropertySet*)window)->getIterator();
-      while (!itr.isAtEnd()) 
-      {
-         try 
-         {
-            if (!window->isPropertyDefault(itr.getCurrentKey()))
-            {
-               Notify (ALWAYS, "%s, Prop: %s, %s", window->getName().c_str(),
-                  itr.getCurrentKey().c_str(),
-                  window->getProperty(itr.getCurrentKey()).c_str());
-            }
-         }
-         catch (CEGUI::InvalidRequestException& exception) 
-         {
-            // If something goes wrong, show user
-            Notify(WARN, "InvalidRequestException for %s: %s", itr.getCurrentKey().c_str(), exception.getMessage().c_str());
-         }
-         itr++;
-      }
-   }
-
 private:
-   //place member variables here
    RefPtr<dtGUI::CEUIDrawable> mGUI;
    std::string mLayoutFilename;
 
@@ -112,27 +67,27 @@ private:
          CEGUI::System::getSingleton().setDefaultMouseCursor("WindowsLook", "MouseArrow");
          CEGUI::System::getSingleton().setDefaultFont("Tahoma-12");
 
-         CEGUI::Window* sheet = CEGUI::WindowManager::getSingleton().createWindow("DefaultGUISheet", "root_wnd");
+         CEGUI::WindowManager *wm = CEGUI::WindowManager::getSingletonPtr();
+
+         CEGUI::Window* sheet = wm->createWindow("DefaultGUISheet", "root_wnd");
          CEGUI::System::getSingleton().setGUISheet(sheet);
 
          if (!mLayoutFilename.empty())
          {
             //load GUI layout from file
-            CEGUI::Window *w = CEGUI::WindowManager::getSingleton().loadWindowLayout(
-                  mLayoutFilename.c_str() ); 
-
+            CEGUI::Window *w = wm->loadWindowLayout( mLayoutFilename.c_str() );                 
             sheet->addChildWindow(w);
          }
          else
          {
             // background panel
-            CEGUI::StaticImage* panel = (CEGUI::StaticImage*)CEGUI::WindowManager::getSingleton().createWindow("WindowsLook/StaticImage", "Panel 1");
+            CEGUI::StaticImage* panel = (CEGUI::StaticImage*)wm->createWindow("WindowsLook/StaticImage", "Panel 1");
             sheet->addChildWindow(panel);
             panel->setPosition(CEGUI::Point(0.0f, 0.0f));
             panel->setSize(CEGUI::Size(1.f, 1.f));
 
             //Delta3D text
-            CEGUI::StaticText* st = (CEGUI::StaticText*)CEGUI::WindowManager::getSingleton().createWindow("WindowsLook/StaticText","Delta_3D");
+            CEGUI::StaticText* st = (CEGUI::StaticText*)wm->createWindow("WindowsLook/StaticText","Delta_3D");
             panel->addChildWindow(st);
             st->setPosition(CEGUI::Point(0.2f, 0.3f));
             st->setSize(CEGUI::Size(0.6f, 0.2f));
@@ -143,14 +98,14 @@ private:
             st->setHorizontalFormatting(CEGUI::StaticText::HorzCentred);
 
             // Edit box for text entry
-            CEGUI::Editbox* eb = (CEGUI::Editbox*)CEGUI::WindowManager::getSingleton().createWindow("WindowsLook/Editbox", "EditBox");
+            CEGUI::Editbox* eb = (CEGUI::Editbox*)wm->createWindow("WindowsLook/Editbox", "EditBox");
             panel->addChildWindow(eb);
             eb->setPosition(CEGUI::Point(0.3f, 0.55f));
             eb->setSize(CEGUI::Size(0.4f, 0.1f));
             eb->setText("Editable text box");
 
             //slider
-            CEGUI::Scrollbar* slider = (CEGUI::Scrollbar*)CEGUI::WindowManager::getSingleton().createWindow("WindowsLook/HorizontalScrollbar", "slider1");
+            CEGUI::Scrollbar* slider = (CEGUI::Scrollbar*)wm->createWindow("WindowsLook/HorizontalScrollbar", "slider1");
             panel->addChildWindow(slider);
             slider->setPosition(CEGUI::Point(0.12f, 0.1f));
             slider->setSize(CEGUI::Size(0.76f, 0.05f));
@@ -161,7 +116,7 @@ private:
             slider->setScrollPosition(255.f);
 
             // quit button
-            CEGUI::PushButton* btn = (CEGUI::PushButton*)CEGUI::WindowManager::getSingleton().createWindow("WindowsLook/Button", "QuitButton");
+            CEGUI::PushButton* btn = (CEGUI::PushButton*)wm->createWindow("WindowsLook/Button", "QuitButton");
             panel->addChildWindow(btn);
             btn->setText("Exit");
             btn->setPosition( CEGUI::Point(0.4f, 0.7f) );
@@ -169,15 +124,14 @@ private:
          }
 
          //hook up subscribers
-         CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
-
-         wmgr.getWindow("Panel 1")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);      
-         wmgr.getWindow("Delta_3D")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
-         wmgr.getWindow("EditBox")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
-         wmgr.getWindow("slider1")->subscribeEvent(CEGUI::Scrollbar::EventScrollPositionChanged, &sliderHandler);
-         wmgr.getWindow("slider1")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
-         wmgr.getWindow("QuitButton")->subscribeEvent(CEGUI::PushButton::EventClicked, &quitHandler);
-         wmgr.getWindow("QuitButton")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
+         //we'll use the "doubleclick" to print out some Window properties
+         wm->getWindow("Panel 1")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);      
+         wm->getWindow("Delta_3D")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
+         wm->getWindow("EditBox")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
+         wm->getWindow("slider1")->subscribeEvent(CEGUI::Scrollbar::EventScrollPositionChanged, &sliderHandler);
+         wm->getWindow("slider1")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
+         wm->getWindow("QuitButton")->subscribeEvent(CEGUI::PushButton::EventClicked, &quitHandler);
+         wm->getWindow("QuitButton")->subscribeEvent(CEGUI::Window::EventMouseDoubleClick, &OutputHandler);
       }
 
       // catch to prevent exit (errors will be logged).
@@ -187,19 +141,22 @@ private:
       }
    }
 
+   //display all the properties of this Window
    static bool OutputHandler(const CEGUI::EventArgs &e)
    {
       CEGUI::Window *w = (CEGUI::Window*)((const CEGUI::WindowEventArgs&)e).window;
-      TestGUIApp::OutputProperties(w);
+      dtGUI::CEUIDrawable::DisplayProperties(w, false);
       return true;
    }
 
+   //quit!
    static bool quitHandler(const CEGUI::EventArgs& e)
    {
       dtABC::Application::GetInstance(0)->Quit();
       return true;
    }
 
+   //adjust the alpha of the whole "sheet"
    static bool sliderHandler(const CEGUI::EventArgs& e)
    {
       CEGUI::Scrollbar* slider = (CEGUI::Scrollbar*)((const CEGUI::WindowEventArgs&)e).window;
@@ -211,7 +168,6 @@ private:
 
       return true;
    }
-
 };
 
 
@@ -229,11 +185,13 @@ int main( int argc, const char* argv[] )
    }
 
    //Instantiate the application and look for the config file
-   RefPtr<TestGUIApp> app = new TestGUIApp();
-   app->SetLayoutFilename(filename);
+   {
+      RefPtr<TestGUIApp> app = new TestGUIApp();
+      app->SetLayoutFilename(filename);
 
-   app->Config(); //configuring the application
-   app->Run(); // running the simulation loop
+      app->Config(); //configuring the application
+      app->Run(); // running the simulation loop
+   }
 
    return 0;
 }
