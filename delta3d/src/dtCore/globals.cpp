962d5e2d3250c5f2ce475f5d2421bbaed7babd68
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-05-24 17:53:49 +0000

    fixed win32 bug that did not allow absolute pathnames, added GetDataFilePathList

--- delta3d/src/dtCore/globals.cpp
+++ delta3d/src/dtCore/globals.cpp
@@ -1,29 +1,64 @@
+#include <dtCore/globals.h>
 
-#include "dtCore/globals.h"
-#include "osgDB/FileUtils"
-
-
+#include <osgDB/FileUtils>
 
 /*!
  * Set the list of paths that dtCore should use to search for files to load.  Paths
- * are separated with a single ";" on Win32 and a single ":" on Linux.
+ * are separated with a single ";" on Win32 and a single ":" on Linux. Remember to
+ * double-up your backslashes, lest they be escaped.
  *
  * @param pathList : The list of all paths to be used to find data files
  */
-void dtCore::SetDataFilePathList(std::string pathList )
+void dtCore::SetDataFilePathList( std::string pathList )
 {
-   for(std::string::iterator itr = pathList.begin(); itr != pathList.end(); itr++)
+   for( std::string::size_type i = 0; i < pathList.size(); i++ )
    {
       #if defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
-      if( *itr == ':' )
-         *itr = ';';
+      try
+      {
+         if( pathList.at(i) == ':' && pathList.at(i+1) != '\\' )
+         {
+            pathList.at(i) = ';';
+         }
+      }
+      catch( std::out_of_range )
+      {}
       #else
-      if( *itr == ';' )
-         *itr = ':'; 
+      if( pathList[i] == ';' )
+      {
+         pathList[i] = ':'; 
+      }
       #endif
    }
    
    osgDB::setDataFilePathList(pathList);
+}
+
+/*!
+* Get the list of paths that dtCore should use to search for files to load.  Paths
+* are separated with a single ";" on Win32 and a single ":" on Linux.
+*/
+std::string dtCore::GetDataFilePathList()
+{
+   osgDB::FilePathList pathList = osgDB::getDataFilePathList();
+
+   std::string pathString = "";
+   for(std::deque<std::string>::iterator itr = pathList.begin(); itr != pathList.end(); itr++)
+   {
+      pathString += *itr;
+
+      std::deque<std::string>::iterator next = itr + 1;
+      if( next != pathList.end() )
+      {
+         #if defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
+         pathString += ';';
+         #else
+         pathString += ':';
+         #endif
+      }
+   }
+
+   return pathString;
 }
 
 /*!
