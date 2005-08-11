e817280a36fb02a938c8ff5d0bff8e5639192584
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-08-11 02:56:56 +0000

    converted to CEGUI

--- trunk/delta3d/examples/testWin/testwin.h
+++ trunk/delta3d/examples/testWin/testwin.h
@@ -1,8 +1,11 @@
 #ifndef DELTA_TESTWINAPP
 #define DELTA_TESTWINAPP
 
-#include "dtCore/dt.h"
-#include "dtABC/dtabc.h"
+#include <CEGUI/CEGUI.h>
+
+#include "dtABC/application.h"
+#include <dtGUI/ceuidrawable.h>
+#include <dtCore/deltawin.h>
 
 class TestWinApp   :  public   dtABC::Application
 {
@@ -10,60 +13,25 @@ class TestWinApp   :  public   dtABC::Application
 public:
 
    TestWinApp( std::string configFilename = "config.xml" );
+   ~TestWinApp() {};
 
    virtual void Config();
 
-   virtual void              KeyPressed( 
-      dtCore::Keyboard*       keyboard, 
-      Producer::KeyboardKey   key,
-      Producer::KeyCharacter  character );
-
-   static void UpdatePosition();
-   static bool GuiHandler( int id, int numparam, void *value );
+   virtual void PostFrame( const double deltaFrameTime );
 
 private:
+   dtCore::RefPtr<dtGUI::CEUIDrawable> mGUI;
+   dtCore::DeltaWin::ResolutionVec mResolutionVec; ///<vector of available resolutions
 
-   enum FrameIDs
-   {
-      FULL_COMBO     = 100,
-      FULL_SET       = 101,
-      FULL_OFF       = 102,
-      FULL_ON        = 103,
-      EXIT           = 200,
-      RES_MENU       = 300,
-      RES_SET        = 301,
-      WH_MENU        = 302,
-      TITLE_SET      = 303,
-      TITLE_EDIT     = 304,
-      POS_TITLE      = 305,
-      POS_X          = 306, 
-      POS_Y          = 307,
-      POS_W          = 308,
-      POS_H          = 309,
-      POS_X_TEXT     = 310, 
-      POS_Y_TEXT     = 311,
-      POS_W_TEXT     = 312,
-      POS_H_TEXT     = 313,
-      POS_SET        = 314,
-      POS_REFRESH    = 315,
-      REFRESH_MENU_1 = 1000,
-      DEPTH_MENU_1   = 2000
-   };
+   void BuildGUI( dtCore::DeltaWin::ResolutionVec &resolutions,
+                  dtCore::DeltaWin::Resolution &currentRes);
 
-   enum ItemIDs
-   {
-      WH_ITEMS      = 3000,
-      REFRESH_ITEMS = 4000,
-      DEPTH_ITEMS   = 5000,
-   };
+   void UpdateWidgets();
 
-   dtCore::RefPtr<dtCore::UIDrawable> mDrawable;
-
-   static CUI_UI* mUI;
-   static dtCore::RefPtr<dtCore::DeltaWin> mWindow;
-   static dtCore::ResolutionVec mResVec;
-   static int mSelectedRes;
-   static CUI_TextBox* mCurrentResText;
+   static void FullScreenToggleCB(const CEGUI::EventArgs &e);
+   static void WindowPositionCB(const CEGUI::EventArgs &e);
+   static void WindowTitleCB( const CEGUI::EventArgs &e);
+   static void ChangeResolutionCB( const CEGUI::EventArgs &e);
 };
 
 #endif // DELTA_TESTWINAPP
