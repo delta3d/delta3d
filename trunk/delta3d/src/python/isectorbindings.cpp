2e594dee14231c29ffeabeb5a27068c4d6dd904f
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-08-18 18:17:46 +0000

    +fixed bindings

--- trunk/delta3d/src/python/isectorbindings.cpp
+++ trunk/delta3d/src/python/isectorbindings.cpp
@@ -13,17 +13,13 @@ void initIsectorBindings()
    Isector* (*IsectorGI1)(int) = &Isector::GetInstance;
    Isector* (*IsectorGI2)(std::string) = &Isector::GetInstance;
 
-   void (Isector::*GetHitPoint1)( osg::Vec3&, int ) const = &Isector::GetHitPoint;
-   void (Isector::*GetHitPoint2)( sgVec3, int ) const = &Isector::GetHitPoint;
+   void (Isector::*GetHitPoint)( osg::Vec3&, int ) const = &Isector::GetHitPoint;
 
-   void (Isector::*SetDirection1)( const osg::Vec3& ) = &Isector::SetDirection;
-   void (Isector::*SetDirection2)( sgVec3 ) = &Isector::SetDirection;
+   void (Isector::*SetDirection)( const osg::Vec3& ) = &Isector::SetDirection;
 
-   void (Isector::*SetStartPosition1)( const osg::Vec3& ) = &Isector::SetStartPosition;
-   void (Isector::*SetStartPosition2)( sgVec3 ) = &Isector::SetStartPosition;
+   void (Isector::*SetStartPosition)( const osg::Vec3& ) = &Isector::SetStartPosition;
 
-   void (Isector::*SetEndPosition1)( const osg::Vec3& ) = &Isector::SetEndPosition;
-   void (Isector::*SetEndPosition2)( sgVec3 ) = &Isector::SetEndPosition;
+   void (Isector::*SetEndPosition)( const osg::Vec3& ) = &Isector::SetEndPosition;
 
    class_<Isector, bases<Transformable>, dtCore::RefPtr<Isector> >("Isector", init<const osg::Vec3&, const osg::Vec3&>())
       .def("GetInstanceCount", &Isector::GetInstanceCount)
@@ -32,14 +28,10 @@ void initIsectorBindings()
       .def("GetInstance", IsectorGI2, return_internal_reference<>())
       .staticmethod("GetInstance")
       .def("SetLength", &Isector::SetLength)
-      .def("GetHitPoint", GetHitPoint1)
-      .def("GetHitPoint", GetHitPoint2)
-      .def("SetDirection", SetDirection1)
-      .def("SetDirection", SetDirection2)
-      .def("SetStartPosition", SetStartPosition1)
-      .def("SetStartPosition", SetStartPosition2)
-      .def("SetEndPosition", SetEndPosition1)
-      .def("SetEndPosition", SetEndPosition2)
+      .def("GetHitPoint", GetHitPoint)
+      .def("SetDirection", SetDirection)
+      .def("SetStartPosition", SetStartPosition)
+      .def("SetEndPosition", SetEndPosition)
       .def("Update", &Isector::Update)
       .def("SetGeometry", &Isector::SetGeometry);
 }
