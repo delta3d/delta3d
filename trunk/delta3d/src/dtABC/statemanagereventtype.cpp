6d572bbdbf8dc06b7072e6133c7e33e3c39a6ced
rufeo <rufeo@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-08-22 19:40:48 +0000

    + StateManager is not a template class now. + StateManager is not a singleton now. + The StateManagerEventTypes was moved inside of StateManager. + The example, testStateManager is currently broken in Debug, so I am not committing it.

--- trunk/delta3d/src/dtABC/statemanagereventtype.cpp
+++
@@ -1,8 +0,0 @@
-#include "dtABC/statemanagereventtype.h"
-
-using namespace dtABC;
-
-IMPLEMENT_ENUM(StateManagerEventType);
-
-const StateManagerEventType StateManagerEventType::TRANSITION_OCCURRED("TRANSITION_OCCURRED");
-
