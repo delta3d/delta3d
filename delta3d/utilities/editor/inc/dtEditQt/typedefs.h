734dae43f445271b7d55cc6fb37067fce877e6ae
David Guthrie <LEgregius@users.noreply.github.com>
2005-08-24 15:39:05 +0000

    added qt4.0.1 workaround typedefs

---
+++ delta3d/utilities/editor/inc/dtEditQt/typedefs.h
@@ -0,0 +1,7 @@
+#include <osg/ref_ptr>
+#include <dtDAL/actorproxy.h>
+#include <vector>
+
+typedef osg::ref_ptr<dtDAL::ActorProxy>                proxyRefPtr;
+typedef std::vector< osg::ref_ptr<dtDAL::ActorProxy> > proxyRefPtrVector;
+typedef osg::ref_ptr<dtDAL::ActorProperty>             propertyRefPtr;
