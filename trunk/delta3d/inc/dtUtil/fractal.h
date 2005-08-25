4636250deb34d0574c15f3b031cc41a6dd590390
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-08-25 02:06:55 +0000

    +added noise

---
+++ trunk/delta3d/inc/dtUtil/fractal.h
@@ -0,0 +1,97 @@
+#ifndef __FRACTAL_H__
+#define __FRACTAL_H__
+
+/* 
+* Delta3D Open Source Game and Simulation Engine 
+* Copyright (C) 2004 MOVES Institute 
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free 
+* Software Foundation; either version 2.1 of the License, or (at your option) 
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License 
+* along with this library; if not, write to the Free Software Foundation, Inc., 
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+*
+* @author Bradley Anderegg
+*/
+
+
+namespace dtUtil
+{
+
+
+template <class Real, class Vector, class Noise>
+class Fractal
+{
+
+public:
+
+   Real FBM(Vector& vect_in, int octaves = 2, Real freq = 0.5f, Real persistance = 2.0f, Real lacunarity = 0.5f);
+   Real IslandFractal(Vector& vect_in, int octaves = 4, Real freq = 0.25f, Real persistance = 0.65f, Real lacunarity = 2.21731f, Real oscarity = 2.37f);
+   
+private:
+   Vector VecNoise(const Vector& point, Real offset);
+
+public:
+
+   //data members
+   Noise mNoise;
+
+};
+
+
+
+//inline
+
+
+template <class Real, class Vector, class Noise>
+Real Fractal<Real, Vector, Noise>::FBM(Vector& vect_in, int octaves, Real freq, Real persistance, Real lacunarity)
+{
+   Real total = 0.0f;
+   Real amplitude = 1.0f;
+
+   for(int i = 0; i < octaves; i++) 
+   {
+      total += mNoise.GetNoise(vect_in) * amplitude;
+      vect_in *= freq;
+      freq *= lacunarity;
+      amplitude *= persistance;		
+   }
+
+ 
+   return total;
+}
+
+template <class Real, class Vector, class Noise>
+Real Fractal<Real, Vector, Noise>::IslandFractal(Vector& vect_in, int octaves /* = 4 */, Real freq /* = 2.0f */, Real persistance /* = 0.5f */, Real lacunarity /* = 2.21731f */, Real oscarity /* = 2.37f */)
+{
+   Real total = 0.0f;
+   Real pers = 1.0f;
+
+   for(int i = 0; i < octaves; i++) 
+   {
+      total += mNoise.GetNoise(vect_in * freq) * pers;
+
+      freq *= lacunarity;
+      pers *= persistance;
+   }
+
+   total *= (abs(total * persistance) + (total * total * oscarity));
+
+   return abs(total);
+}
+
+
+
+
+}//dtCore
+
+#endif//__FRACTAL_H__
+
