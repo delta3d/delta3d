a7e8b702a40d857c79614149732a81ae5088b51d
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-09 01:15:57 +0000

    fixed bug that would cause the 3rd mouse button to be ignored.

--- delta3d/src/dtCore/mouse.cpp
+++ delta3d/src/dtCore/mouse.cpp
@@ -202,7 +202,7 @@ void Mouse::passiveMouseMotion(float x, float y)
  */
 void Mouse::buttonPress(float x, float y, unsigned int button)
 {
-   if ((int)button >= GetButtonCount() ) return;
+   if ((int)button > GetButtonCount() ) return;
 
    MouseButton mouseButton = (MouseButton)(button-1);
 
@@ -225,7 +225,7 @@ void Mouse::buttonPress(float x, float y, unsigned int button)
  */
 void Mouse::doubleButtonPress(float x, float y, unsigned int button)
 {
-   if ((int)button >= GetButtonCount() ) return;
+   if ((int)button > GetButtonCount() ) return;
 
    MouseButton mouseButton = (MouseButton)(button-1);
 
@@ -248,7 +248,7 @@ void Mouse::doubleButtonPress(float x, float y, unsigned int button)
  */
 void Mouse::buttonRelease(float x, float y, unsigned int button)
 {
-   if ((int)button >= GetButtonCount() ) return;
+   if ((int)button > GetButtonCount() ) return;
 
    MouseButton mouseButton = (MouseButton)(button-1);
 
