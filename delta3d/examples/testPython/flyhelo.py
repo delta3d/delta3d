ba3525ab350702a749d0f7870c6d7323bfe90d08
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-09 22:00:41 +0000

    updated API

--- delta3d/examples/testPython/flyhelo.py
+++ delta3d/examples/testPython/flyhelo.py
@@ -14,7 +14,9 @@ def radians(v):
 while True:
    transform.Set(40*cos(radians(angle)),
                  100 + 40*sin(radians(angle)), 
-                 0, angle, 0, -45)
+                 0, 
+                 angle, 0, -45,
+                 1.0,1.0,1.0)
    helo.SetTransform(transform)
    sleep(0.01)
    angle += 0.45
