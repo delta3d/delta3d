e743a7e6dceb26d3ccbf810e49da130bb18050c7
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-10 01:07:45 +0000

    Made ScriptManager its own thread so it can kick off scripts in the background while your app is unaffected. Also changed testPython to use this design instead of the interactive loop.

--- trunk/delta3d/src/dtScript/scriptmanager.cpp
+++ trunk/delta3d/src/dtScript/scriptmanager.cpp
@@ -5,38 +5,22 @@ using namespace dtScript;
 
 IMPLEMENT_MANAGEMENT_LAYER(ScriptManager)
 
-ScriptManager::ScriptManager()
-:  mFileObject(0),
-   mThreadState(0)
+ScriptManager::ScriptManager( const std::string& name )
+:  Base( name ),
+   mFileObject( 0 )
 {
    RegisterInstance( this );
 
-   SetName("ScriptManager");
-
-   Py_Initialize(); // start python thread
+   Py_Initialize();
 }
 
 ScriptManager::~ScriptManager()
 {
-   if ( mThreadState != 0 )
-   {
-      Py_EndInterpreter( mThreadState );
-   }
-
-   Py_Finalize(); // kill python thread
-
    DeregisterInstance( this );
 }
 
 void ScriptManager::Load( const std::string& filename )
 {
-   if( mThreadState != 0 )
-   {
-      Py_EndInterpreter( mThreadState );
-   }
-
-   mThreadState = Py_NewInterpreter();
-
    mFilename = filename;
    mFileObject = PyFile_FromString( const_cast< char* >( mFilename.c_str() ), "r" );
 }
@@ -45,9 +29,14 @@ void ScriptManager::Run( const std::string& filename )
 { 
    if( osgDB::fileExists( filename ) )
    {
-      Load( filename);
+      Load( filename );
    }
-   
+
+   start();
+}
+
+void ScriptManager::run()
+{
    try
    {
       PyRun_SimpleFile( PyFile_AsFile( mFileObject ), const_cast< char* >( mFilename.c_str() ) );
