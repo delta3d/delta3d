41fb47933addcf2deaa4c339106c1f73c54fae82
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-13 22:00:14 +0000

    add dtNet into the build, fixed minor linking lines in dtnet.h

--- trunk/delta3d/inc/dtNet/dtnet.h
+++ trunk/delta3d/inc/dtNet/dtnet.h
@@ -1,25 +1,49 @@
 #ifndef DTNET_INCLUDE
 #define DTNET_INCLUDE
 
-#include "dtNet/netmgr.h"
-
+#include <dtNet/netmgr.h>
 
 #if defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
 
-/* You may turn off this include message by defining _NOAUTOLIB */
+/* You may turn off this include message by defining _NOAUTOLIBMSG */
 #undef _AUTOLIBNAME1
+#undef _AUTOLIBNAME2
+#undef _AUTOLIBNAME3
 
 #if defined(_DEBUG)
-   #define _AUTOLIBNAME1   "dtNetD.lib"
+
+   #ifndef DT_LIBRARY  
+      #define _AUTOLIBNAME1 "dtNetD.lib"
+   #endif
+
+   #define _AUTOLIBNAME2 "HawkNLD.lib"
+   #define _AUTOLIBNAME2 "gned.lib"
 #else 
-   #define _AUTOLIBNAME1   "dtNet.lib"  
+
+   #ifndef DT_LIBRARY  
+      #define _AUTOLIBNAME1 "dtNet.lib"
+   #endif
+
+   #define _AUTOLIBNAME2 "HawkNL.lib"
+   #define _AUTOLIBNAME2 "gne.lib"
 #endif
 
 #ifndef _NOAUTOLIBMSG
-   #pragma message( "Will automatically link with " _AUTOLIBNAME1 )
+
+   #ifndef DT_LIBRARY
+      #pragma message( "Will automatically link with " _AUTOLIBNAME1 )
+   #endif
+
+   #pragma message( "Will automatically link with " _AUTOLIBNAME2 )
+   #pragma message( "Will automatically link with " _AUTOLIBNAME3 )
 #endif
 
-#pragma  comment( lib, _AUTOLIBNAME1 )
+#ifndef DT_LIBRARY
+   #pragma  comment( lib, _AUTOLIBNAME1 )
+#endif
+
+#pragma  comment( lib, _AUTOLIBNAME2 )
+#pragma  comment( lib, _AUTOLIBNAME3 )
 
 #endif  // defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
 
