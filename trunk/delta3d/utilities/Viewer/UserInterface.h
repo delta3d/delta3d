b1f40c286661826242c04eb1a3a263ce72a87f50
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-14 18:24:46 +0000

    +fixed viewer to allow file filter to catch *.rbody files

--- trunk/delta3d/utilities/Viewer/UserInterface.h
+++ trunk/delta3d/utilities/Viewer/UserInterface.h
@@ -8,7 +8,7 @@
 #include <FL/Fl_File_Chooser.H>
 #include "viewwindow.h"
 #include "viewstate.h"
-#define kDefFltr "Geometry Files (*.{osg,ive,flt,3ds})\tOSG Files (*.osg)\tIVE Files (*.ive)\tFlight Files (*.flt)\t3ds Export Files (*.3ds)"
+#define kDefFltr "Geometry Files (*.{osg,ive,flt,3ds,rbody})\tOSG Files (*.osg)\tIVE Files (*.ive)\tFlight Files (*.flt)\t3ds Export Files (*.3ds)\trbody Files (*.rbody)"
 class ChoicePopUp;   // forward reference
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Menu_Bar.H>
