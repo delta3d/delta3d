f91520392c6f427f4615a785f4a26ef0b8ef1e05
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-15 23:00:43 +0000

    updated std::string to const std::string&

--- trunk/delta3d/src/python/objectbindings.cpp
+++ trunk/delta3d/src/python/objectbindings.cpp
@@ -2,26 +2,28 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-#include "python/dtpython.h"
-#include "dtCore/object.h"
+#include <python/dtpython.h>
+#include <dtCore/object.h>
 
 using namespace boost::python;
 using namespace dtCore;
 
+// We need to wrap Object so LoadFile does not return an osg::Node
+// since Python doesn't know what one is!
 class ObjectWrap : public Object
 {
    public:
 
-      ObjectWrap(PyObject* self, std::string name = "")
+      ObjectWrap(PyObject* self, const std::string& name = "")
          : mSelf(self)
       {}
 
-      void LoadFileWrapper1(std::string filename, bool useCache)
+      void LoadFileWrapper1(const std::string& filename, bool useCache)
       {
          Object::LoadFile(filename,useCache);
       }
    
-      void LoadFileWrapper2(std::string filename)
+      void LoadFileWrapper2(const std::string& filename)
       {
          Object::LoadFile(filename);
       }
@@ -31,7 +33,6 @@ class ObjectWrap : public Object
       PyObject* mSelf;
 };
 
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LF_overloads, LoadFile, 1, 2)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(RGUL_overloads, RecenterGeometryUponLoad, 0, 1)
 
 void initObjectBindings()
@@ -39,7 +40,9 @@ void initObjectBindings()
    Object* (*ObjectGI1)(int) = &Object::GetInstance;
    Object* (*ObjectGI2)(std::string) = &Object::GetInstance;
 
-   class_<Object, bases<Physical>, dtCore::RefPtr<ObjectWrap>, boost::noncopyable>("Object", init<optional<std::string> >())
+   // ObjectWrap* should probably be a dtCore::RefPtr<ObjectWrap>, but that causes a big fat crash
+   // on exit on Linux: *** glibc detected *** python: double free or corruption (out): 0xb64054d0 ***
+   class_<Object, bases<Physical>, ObjectWrap*, boost::noncopyable>("Object", init<optional<const std::string&> >())
       .def("GetInstanceCount", &Object::GetInstanceCount)
       .staticmethod("GetInstanceCount")
       .def("GetInstance", ObjectGI1, return_internal_reference<>())
