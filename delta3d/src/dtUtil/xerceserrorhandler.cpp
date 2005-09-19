42a88ff64e39c7bd6ec6ff7e7aa06eccff43bae4
rufeo <rufeo@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-19 05:05:47 +0000

    + unified log messages.

--- delta3d/src/dtUtil/xerceserrorhandler.cpp
+++ delta3d/src/dtUtil/xerceserrorhandler.cpp
@@ -19,7 +19,7 @@ XercesErrorHandler::~XercesErrorHandler()
 void XercesErrorHandler::warning(const SAXParseException& e)
 {
    char* msg = XMLString::transcode(e.getMessage());
-   std::string line = ToString<XMLSSize_t>( e.getLineNumber() );
+   std::string line = ToString( e.getLineNumber() );
    LOG_WARNING("Xerces parsing warning occurred at line " + line + ", with message: " + std::string(msg) )
    XMLString::release( &msg );
 }
@@ -27,15 +27,16 @@ void XercesErrorHandler::warning(const SAXParseException& e)
 void XercesErrorHandler::error(const SAXParseException& e)
 {
    char* msg = XMLString::transcode(e.getMessage());
-   std::string line = ToString<XMLSSize_t>(e.getLineNumber() );
-   LOG_ERROR("Xerces parsing error occurred at line " + line + ", with message: " + std::string(msg) )
+   std::string line = dtUtil::ToString( e.getLineNumber() );
+   LOG_ERROR("Xerces error occurred while parsing at line, " + line + ", with message: " + std::string(msg) )
    XMLString::release( &msg );
 }
 
 void XercesErrorHandler::fatalError(const SAXParseException& e)
 {
    char* msg = XMLString::transcode(e.getMessage());
-   LOG_ERROR("Xerces parsing fatal error occurred with message: " + std::string(msg) )
+   std::string line = dtUtil::ToString( e.getLineNumber() );
+   LOG_ERROR("Xerces fatal error occurred while parsing at line, " + line + ", with message: " + std::string(msg) )
    XMLString::release( &msg );
 }
 
