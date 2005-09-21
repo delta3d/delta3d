68d5a344e851fd76c4ce0ec4517f336d0effdf36
banderegg <banderegg@users.noreply.github.com>
2005-09-21 19:10:21 +0000

    +fixed texure coordinates

--- delta3d/examples/testBumpMap/cubevertices.h
+++ delta3d/examples/testBumpMap/cubevertices.h
@@ -15,6 +15,14 @@
             where the two additional verts and tex coords are from the 
             opposite and adjacent vertices and the standard triangle.
 
+         As a side note, one may notice this data to be very repetitive.
+            And although I could have done this by offsetting indices and
+            modding them to wrap back around, I decided to list them by hand
+            for the sake of the demo.  This will show better what is happening
+            to the user. In practice I recommend just offsetting the indices of 
+            the original vert and tex coord list.
+
+
 */
 
 typedef struct _CV_
@@ -62,7 +70,7 @@ _CV_::_CV_()
 //////////////////////////////////////////////////////////////////////////
 
 
-//front
+//bottom
    (*mVerts)[0].set(mSize, mSize, -mSize);
    (*mVerts)[1].set(-mSize, mSize, -mSize);
    (*mVerts)[2].set(-mSize, -mSize, -mSize);
@@ -71,7 +79,7 @@ _CV_::_CV_()
    (*mVerts)[4].set (mSize, mSize, -mSize);
    (*mVerts)[5].set (-mSize, -mSize, -mSize);
 
-//back
+//top
    (*mVerts)[6].set (-mSize, mSize, mSize);
    (*mVerts)[7].set (mSize, mSize, mSize);
    (*mVerts)[8].set (-mSize, -mSize, mSize);
@@ -98,7 +106,7 @@ _CV_::_CV_()
    (*mVerts)[22].set (mSize, mSize, mSize);
    (*mVerts)[23].set (mSize, -mSize, -mSize);
 
-//top
+//front
    (*mVerts)[24].set (mSize, mSize, mSize);
    (*mVerts)[25].set (-mSize, mSize, mSize);
    (*mVerts)[26].set (-mSize, mSize, -mSize);
@@ -107,7 +115,7 @@ _CV_::_CV_()
    (*mVerts)[28].set (mSize, mSize, mSize);
    (*mVerts)[29].set (-mSize, mSize, -mSize);
 
-//bottom
+//back
    (*mVerts)[30].set (mSize, -mSize, -mSize);
    (*mVerts)[31].set (-mSize, -mSize, -mSize);
    (*mVerts)[32].set (-mSize, -mSize, mSize);
@@ -120,7 +128,7 @@ _CV_::_CV_()
 //Verts 2
 //////////////////////////////////////////////////////////////////////////
 
-//front
+//bottom
    (*mVerts2)[0].set (-mSize, mSize, -mSize);
    (*mVerts2)[1].set (-mSize, -mSize, -mSize);
    (*mVerts2)[2].set (mSize, mSize, -mSize);
@@ -129,7 +137,7 @@ _CV_::_CV_()
    (*mVerts2)[4].set (-mSize, -mSize, -mSize);
    (*mVerts2)[5].set (mSize, -mSize, -mSize);
 
-//back
+//top
    (*mVerts2)[6].set (mSize, mSize, mSize);
    (*mVerts2)[7].set (-mSize, -mSize, mSize);
    (*mVerts2)[8].set (-mSize, mSize, mSize);
@@ -157,7 +165,7 @@ _CV_::_CV_()
    (*mVerts2)[22].set (mSize, -mSize, -mSize);
    (*mVerts2)[23].set (mSize, -mSize, mSize);
 
-//top
+//front
    (*mVerts2)[24].set (-mSize, mSize, mSize);
    (*mVerts2)[25].set (-mSize, mSize, -mSize);
    (*mVerts2)[26].set (mSize, mSize, mSize);
@@ -166,7 +174,7 @@ _CV_::_CV_()
    (*mVerts2)[28].set (-mSize, mSize, -mSize);
    (*mVerts2)[29].set (mSize, mSize, -mSize);
 
-//bottom
+//back
    (*mVerts2)[30].set (-mSize, -mSize, -mSize);
    (*mVerts2)[31].set (-mSize, -mSize, mSize);
    (*mVerts2)[32].set (mSize, -mSize, -mSize);
@@ -179,7 +187,7 @@ _CV_::_CV_()
 //Verts 3
 //////////////////////////////////////////////////////////////////////////
 
-//front
+//bottom
    (*mVerts3)[0].set (-mSize, -mSize, -mSize);
    (*mVerts3)[1].set (mSize, mSize, -mSize);
    (*mVerts3)[2].set (-mSize, mSize, -mSize);
@@ -188,7 +196,7 @@ _CV_::_CV_()
    (*mVerts3)[4].set (mSize, -mSize, -mSize);
    (*mVerts3)[5].set (mSize, mSize, -mSize);
 
-//back
+//top
    (*mVerts3)[6].set (-mSize, -mSize, mSize);
    (*mVerts3)[7].set (-mSize, mSize, mSize);
    (*mVerts3)[8].set (mSize, mSize, mSize);
@@ -215,7 +223,7 @@ _CV_::_CV_()
    (*mVerts3)[22].set (mSize, -mSize, mSize);
    (*mVerts3)[23].set (mSize, mSize, mSize);
 
-//top
+//front
    (*mVerts3)[24].set (-mSize, mSize, -mSize);
    (*mVerts3)[25].set (mSize, mSize, mSize);
    (*mVerts3)[26].set (-mSize, mSize, mSize);
@@ -224,7 +232,7 @@ _CV_::_CV_()
    (*mVerts3)[28].set (mSize, mSize, -mSize );
    (*mVerts3)[29].set (mSize, mSize, mSize);
 
-//bottom
+//back
    (*mVerts3)[30].set (-mSize, -mSize, mSize);
    (*mVerts3)[31].set (mSize, -mSize, -mSize);
    (*mVerts3)[32].set (-mSize, -mSize, -mSize);
@@ -237,7 +245,7 @@ _CV_::_CV_()
 //Normals
 //////////////////////////////////////////////////////////////////////////
 
-//front
+//bottom
    (*mNormals)[0].set (0.0f, 0.0f, 1.0f);
    (*mNormals)[1].set (0.0f, 0.0f, 1.0f);
    (*mNormals)[2].set (0.0f, 0.0f, 1.0f);
@@ -246,7 +254,7 @@ _CV_::_CV_()
    (*mNormals)[4].set (0.0f, 0.0f, 1.0f);
    (*mNormals)[5].set (0.0f, 0.0f, 1.0f);
 
-//back
+//top
    (*mNormals)[6].set (0.0f, 0.0f, -1.0f);
    (*mNormals)[7].set (0.0f, 0.0f, -1.0f);
    (*mNormals)[8].set (0.0f, 0.0f, -1.0f);
@@ -273,7 +281,7 @@ _CV_::_CV_()
    (*mNormals)[22].set (-1.0f, 0.0f, 0.0f);
    (*mNormals)[23].set (-1.0f, 0.0f, 0.0f);
 
-//top
+//front
    (*mNormals)[24].set (0.0f, -1.0f, 0.0f);
    (*mNormals)[25].set (0.0f, -1.0f, 0.0f);
    (*mNormals)[26].set (0.0f, -1.0f, 0.0f);
@@ -282,7 +290,7 @@ _CV_::_CV_()
    (*mNormals)[28].set (0.0f, -1.0f, 0.0f);
    (*mNormals)[29].set (0.0f, -1.0f, 0.0f);
 
-//bottom
+//back
    (*mNormals)[30].set (0.0f, 1.0f, 0.0f);
    (*mNormals)[31].set (0.0f, 1.0f, 0.0f);
    (*mNormals)[32].set (0.0f, 1.0f, 0.0f);
@@ -295,174 +303,186 @@ _CV_::_CV_()
 // Tex Coords1
 //////////////////////////////////////////////////////////////////////////
 
-//front
-   (*mTexCoords)[0].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[1].set (0.0f, 0.0f);
-   (*mTexCoords)[2].set (0.0f, mTexRepeat);
+//bottom
+   (*mTexCoords)[0].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[1].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords)[2].set (-mTexRepeat, -mTexRepeat);
 
-   (*mTexCoords)[3].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords)[4].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[5].set (0.0f, mTexRepeat);
-
-//back
-   (*mTexCoords)[6].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[7].set (0.0f, 0.0f);
-   (*mTexCoords)[8].set (mTexRepeat, mTexRepeat);
-
-   (*mTexCoords)[9].set (0.0f, 0.0f);
-   (*mTexCoords)[10].set (0.0f, mTexRepeat);
-   (*mTexCoords)[11].set (mTexRepeat, mTexRepeat);
-
-//left
-   (*mTexCoords)[12].set (0.0f, 0.0f);
-   (*mTexCoords)[13].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[14].set (mTexRepeat, mTexRepeat);
-
-   (*mTexCoords)[15].set (0.0f, mTexRepeat);
-   (*mTexCoords)[16].set (0.0f, 0.0f);
-   (*mTexCoords)[17].set (mTexRepeat, mTexRepeat);
-
-//right
-   (*mTexCoords)[18].set (0.0f, 0.0f);
-   (*mTexCoords)[19].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[20].set (mTexRepeat, mTexRepeat);
-
-   (*mTexCoords)[21].set (0.0f, mTexRepeat);
-   (*mTexCoords)[22].set (0.0f, 0.0f);
-   (*mTexCoords)[23].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[3].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[4].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[5].set (-mTexRepeat, -mTexRepeat);
 
 //top
-   (*mTexCoords)[24].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[25].set (0.0f, 0.0f);
-   (*mTexCoords)[26].set (0.0f, mTexRepeat);
 
-   (*mTexCoords)[27].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords)[28].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[29].set (0.0f, mTexRepeat);
+   (*mTexCoords)[6].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[7].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[8].set (-mTexRepeat, mTexRepeat);
 
-//bottom
-   (*mTexCoords)[30].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords)[31].set (0.0f, mTexRepeat);
-   (*mTexCoords)[32].set (0.0f, 0.0f);
+   (*mTexCoords)[9].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[10].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[11].set (-mTexRepeat, mTexRepeat);
 
-   (*mTexCoords)[33].set (mTexRepeat, 0.0f);
-   (*mTexCoords)[34].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords)[35].set (0.0f, 0.0f);
+//left
+   (*mTexCoords)[12].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[13].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[14].set (-mTexRepeat, mTexRepeat);
+
+   (*mTexCoords)[15].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[16].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[17].set (-mTexRepeat, mTexRepeat);
+
+//right
+
+   (*mTexCoords)[18].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords)[19].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[20].set (mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords)[21].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[22].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords)[23].set (mTexRepeat, -mTexRepeat);
+
+//front
+   (*mTexCoords)[24].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[25].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords)[26].set (-mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords)[27].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[28].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords)[29].set (-mTexRepeat, -mTexRepeat);
+
+//back
+   (*mTexCoords)[30].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[31].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[32].set (mTexRepeat, mTexRepeat);
+
+   (*mTexCoords)[33].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords)[34].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords)[35].set (mTexRepeat, mTexRepeat);
 
 //////////////////////////////////////////////////////////////////////////
 // TexCoords 2
 //////////////////////////////////////////////////////////////////////////
-//front
-   (*mTexCoords2)[0].set (0.0f, 0.0f);
-   (*mTexCoords2)[1].set (0.0f, mTexRepeat);
-   (*mTexCoords2)[2].set (mTexRepeat, 0.0f);
+//bottom
 
-   (*mTexCoords2)[3].set (mTexRepeat, 0.0f);
-   (*mTexCoords2)[4].set (0.0f, mTexRepeat);
-   (*mTexCoords2)[5].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[0].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[1].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[2].set (mTexRepeat, mTexRepeat);
 
-//back
-   (*mTexCoords2)[6].set (0.0f, 0.0f);
-   (*mTexCoords2)[7].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[8].set (mTexRepeat, 0.0f);
+   (*mTexCoords2)[3].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[4].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[5].set (mTexRepeat, -mTexRepeat);
 
-   (*mTexCoords2)[9].set  (0.0f, mTexRepeat);
-   (*mTexCoords2)[10].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[11].set  (0.0f, 0.0);
-
-//left
-   (*mTexCoords2)[12].set (mTexRepeat, 0.0f);
-   (*mTexCoords2)[13].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[14].set (0.0f, 0.0f);
-
-   (*mTexCoords2)[15].set (0.0f, 0.0f);
-   (*mTexCoords2)[16].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[17].set (0.0f, mTexRepeat);
-
-//right
-   (*mTexCoords2)[18].set (mTexRepeat, 0.0f);
-   (*mTexCoords2)[19].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[20].set (0.0f, 0.0f);
-
-   (*mTexCoords2)[21].set (0.0f, 0.0f);
-   (*mTexCoords2)[22].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[23].set (0.0f, mTexRepeat);
 
 //top
-   (*mTexCoords2)[24].set (0.0f, 0.0f);
-   (*mTexCoords2)[25].set (0.0f, mTexRepeat);
-   (*mTexCoords2)[26].set (mTexRepeat, 0.0f);
+   
+   (*mTexCoords2)[6].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[7].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[8].set (-mTexRepeat, -mTexRepeat);
 
-   (*mTexCoords2)[27].set (mTexRepeat, 0.0f);
-   (*mTexCoords2)[28].set (0.0f, mTexRepeat);
-   (*mTexCoords2)[29].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[9].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[10].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[11].set (mTexRepeat, -mTexRepeat);
 
-//bottom
-   (*mTexCoords2)[30].set (0.0f, mTexRepeat);
-   (*mTexCoords2)[31].set (0.0f, 0.0f);
-   (*mTexCoords2)[32].set (mTexRepeat, mTexRepeat);
+//left
 
-   (*mTexCoords2)[33].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords2)[34].set (0.0f, 0.0f);
-   (*mTexCoords2)[35].set (mTexRepeat, 0.0f);
+   (*mTexCoords2)[12].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[13].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[14].set (mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords2)[15].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[16].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[17].set (-mTexRepeat, -mTexRepeat);
+   
+   
+//right
+   (*mTexCoords2)[18].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[19].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[20].set (-mTexRepeat, mTexRepeat);
+
+   (*mTexCoords2)[21].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[22].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[23].set (mTexRepeat, mTexRepeat);
+
+//front
+   (*mTexCoords2)[24].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[25].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[26].set (mTexRepeat, mTexRepeat);
+
+   (*mTexCoords2)[27].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[28].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[29].set (mTexRepeat, -mTexRepeat);
+
+//back
+   (*mTexCoords2)[30].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[31].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[32].set (-mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords2)[33].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords2)[34].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords2)[35].set (-mTexRepeat, mTexRepeat);
 
 //////////////////////////////////////////////////////////////////////////
 // Tex Coords 3
 //////////////////////////////////////////////////////////////////////////
 
-//front
-   (*mTexCoords3)[0].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[1].set (mTexRepeat, 0.0f);
-   (*mTexCoords3)[2].set (0.0f, 0.0f);
+//bottom
 
-   (*mTexCoords3)[3].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[4].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[5].set (mTexRepeat, 0.0f);
+   (*mTexCoords3)[0].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[1].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[2].set (-mTexRepeat, mTexRepeat);
+  
+   (*mTexCoords3)[3].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[4].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[5].set (mTexRepeat, mTexRepeat);   
 
-//back
-   (*mTexCoords3)[6].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[7].set (mTexRepeat, 0.0f);
-   (*mTexCoords3)[8].set (0.0f, 0.0f);
-
-   (*mTexCoords3)[9].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[10].set (0.0f, 0.0);
-   (*mTexCoords3)[11].set (0.0f, mTexRepeat);
-
-//left
-   (*mTexCoords3)[12].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[13].set (0.0f, 0.0f);
-   (*mTexCoords3)[14].set (mTexRepeat, 0.0f);
-
-   (*mTexCoords3)[15].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[16].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[17].set (0.0f, 0.0f);
-
-//right
-   (*mTexCoords3)[18].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[19].set (0.0f, 0.0f);
-   (*mTexCoords3)[20].set (mTexRepeat, 0.0f);
-
-   (*mTexCoords3)[21].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[22].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[23].set (0.0f, 0.0f);
 
 //top
-   (*mTexCoords3)[24].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[25].set (mTexRepeat, 0.0f);
-   (*mTexCoords3)[26].set (0.0f, 0.0f);
 
-   (*mTexCoords3)[27].set (0.0f, mTexRepeat);
-   (*mTexCoords3)[28].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[29].set (mTexRepeat, 0.0f);
+   (*mTexCoords3)[6].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[7].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[8].set (mTexRepeat, -mTexRepeat);
 
-//bottom
-   (*mTexCoords3)[30].set (0.0f, 0.0f);
-   (*mTexCoords3)[31].set (mTexRepeat, mTexRepeat);
-   (*mTexCoords3)[32].set (0.0f, mTexRepeat);
+   (*mTexCoords3)[9].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[10].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[11].set (mTexRepeat, mTexRepeat);
+  
 
-   (*mTexCoords3)[33].set (0.0f, 0.0f);
-   (*mTexCoords3)[34].set (mTexRepeat, 0.0f);
-   (*mTexCoords3)[35].set (mTexRepeat, mTexRepeat);
+//left
+   (*mTexCoords3)[12].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[13].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[14].set (mTexRepeat, mTexRepeat);
+   
+   (*mTexCoords3)[15].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[16].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[17].set (mTexRepeat, -mTexRepeat);
+   
+
+//right
+   (*mTexCoords3)[18].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[19].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[20].set (-mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords3)[21].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[22].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[23].set (-mTexRepeat, mTexRepeat);
+
+//front
+   (*mTexCoords3)[24].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[25].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[26].set (-mTexRepeat, mTexRepeat);
+
+   (*mTexCoords3)[27].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[28].set (mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[29].set (mTexRepeat, mTexRepeat);
+
+//back
+   (*mTexCoords3)[30].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[31].set (-mTexRepeat, -mTexRepeat);
+   (*mTexCoords3)[32].set (mTexRepeat, -mTexRepeat);
+
+   (*mTexCoords3)[33].set (mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[34].set (-mTexRepeat, mTexRepeat);
+   (*mTexCoords3)[35].set (-mTexRepeat, -mTexRepeat);
 
 }
 
