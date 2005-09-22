c45616b3833c632c5b514179345613e75ddcb3c7
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-22 22:20:31 +0000

    *** empty log message ***

---
+++ branches/BRANCH_V_1_0/delta3d/src/python/dtutilbindings.cpp
@@ -0,0 +1,14 @@
+// dtutilbindings.cpp: Python bindings for dtUtil library.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include <python/dtpython.h>
+
+#include <dtUtil/dtutil.h>
+
+void init_PUTFUNCTIONNAMEHERE_Bindings();
+
+BOOST_PYTHON_MODULE(PyDtUtil)
+{
+   init_PUTFUNCTIONNAMEHERE_Bindings();
+}
