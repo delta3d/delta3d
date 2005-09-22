a08b3bcba674ed02cadd6b7b4444e7c206bbdf91
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-22 23:15:21 +0000

    updated bindings

--- delta3d/src/python/deltawinbindings.cpp
+++ delta3d/src/python/deltawinbindings.cpp
@@ -10,7 +10,13 @@ using namespace dtCore;
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SC_overloads, ShowCursor, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SFSM_overloads, SetFullScreenMode, 0, 1)
-   
+
+int IVR1( const DeltaWin::ResolutionVec& rv, int w) { return DeltaWin::IsValidResolution(rv, w); }
+int IVR2( const DeltaWin::ResolutionVec& rv, int w, int h) { return DeltaWin::IsValidResolution(rv, w, h); }
+int IVR3( const DeltaWin::ResolutionVec& rv, int w, int h, int r) { return DeltaWin::IsValidResolution(rv, w, h, r); }
+int IVR4( const DeltaWin::ResolutionVec& rv, int w, int h, int r, int d) { return DeltaWin::IsValidResolution(rv, w, h, r, d); }
+
+
 void initDeltaWinBindings()
 {
    DeltaWin* (*DeltaWinGI1)(int) = &DeltaWin::GetInstance;
@@ -18,6 +24,7 @@ void initDeltaWinBindings()
 
    bool (*DeltaWinCSR1)(int,int,int,int) = &DeltaWin::ChangeScreenResolution;
    bool (*DeltaWinCSR2)(DeltaWin::Resolution) = &DeltaWin::ChangeScreenResolution;
+
    
    scope DeltaWinScope = class_<DeltaWin, bases<Base>, dtCore::RefPtr<DeltaWin> >("DeltaWin", init<optional<std::string, int, int, int, int, bool, bool> >())
       .def("GetInstanceCount", &DeltaWin::GetInstanceCount)
@@ -26,10 +33,12 @@ void initDeltaWinBindings()
       .def("GetInstance", DeltaWinGI2, return_internal_reference<>())
       .staticmethod("GetInstance")
       .def("CalcPixelCoords", &DeltaWin::CalcPixelCoords)
+      .def("CalcWindowCoords", &DeltaWin::CalcWindowCoords)
       .def("ShowCursor", &DeltaWin::ShowCursor, SC_overloads())
       .def("GetShowCursor", &DeltaWin::GetShowCursor)
       .def("SetFullScreenMode", &DeltaWin::SetFullScreenMode, SFSM_overloads())
       .def("GetFullScreenMode", &DeltaWin::GetFullScreenMode)
+      .def("KillGLWindow", &DeltaWin::KillGLWindow)
       .def("SetWindowTitle", &DeltaWin::SetWindowTitle)
       .def("GetWindowTitle", &DeltaWin::GetWindowTitle, return_internal_reference<>())
       .def("SetPosition", &DeltaWin::SetPosition)
@@ -40,7 +49,11 @@ void initDeltaWinBindings()
       .def("GetCurrentResolution", &DeltaWin::GetCurrentResolution)
       .def("ChangeScreenResolution", DeltaWinCSR1)
       .def("ChangeScreenResolution", DeltaWinCSR2)
-      .def("IsValidResolution", &DeltaWin::IsValidResolution);
+      .def("IsValidResolution", &DeltaWin::IsValidResolution)
+      .def("IsValidResolution", IVR1)
+      .def("IsValidResolution", IVR2)
+      .def("IsValidResolution", IVR3)
+      .def("IsValidResolution", IVR4);
 
    class_<DeltaWin::Resolution>("Resolution")
       .def_readwrite("width", &DeltaWin::Resolution::width)
