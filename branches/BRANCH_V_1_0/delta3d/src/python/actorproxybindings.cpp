73fddfafae6cb5b86a81130b82749cb03bcc7965
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-09-29 16:21:04 +0000

    Added register_ptr_to_python to do conversion between osg::ref_ptr and * but... doesn't work yet

--- branches/BRANCH_V_1_0/delta3d/src/python/actorproxybindings.cpp
+++ branches/BRANCH_V_1_0/delta3d/src/python/actorproxybindings.cpp
@@ -1,5 +1,6 @@
 #include <python/dtpython.h>
 #include <dtDAL/actorproxy.h>
+#include <boost/python/register_ptr_to_python.hpp>
 
 using namespace boost::python;
 using namespace dtDAL;
@@ -27,11 +28,13 @@ void initActorProxyBindings()
 {
    dtCore::DeltaDrawable* (ActorProxy::*GA1)() = (dtCore::DeltaDrawable* (ActorProxy::*)())&ActorProxy::GetActor;
 
-   class_< ActorProxy, dtCore::RefPtr<ActorProxyWrap>, boost::noncopyable >( "ActorProxy", no_init )
+   class_< ActorProxy, osg::ref_ptr<ActorProxy>, boost::noncopyable >( "ActorProxy", no_init )
       .def( "GetName", &ActorProxy::GetName, return_internal_reference<>() )
       .def( "SetName", &ActorProxy::SetName ) 
       .def( "GetClassName", &ActorProxy::GetClassName, return_internal_reference<>() )
       .def( "IsInstanceOf", &ActorProxy::IsInstanceOf )
-      .def( "GetActor", GA1, &ActorProxyWrap::DefaultGetActor, return_internal_reference<>() )
+      //.def( "GetActor", GA1, &ActorProxyWrap::DefaultGetActor, return_internal_reference<>() )
       ;
+
+   register_ptr_to_python< osg::ref_ptr< ActorProxy > >();
 }
