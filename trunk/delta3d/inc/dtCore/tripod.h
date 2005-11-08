dac4e9cbc95b87414307496e65aeb70e584785d9
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-11-08 19:46:57 +0000

    TripodActorProxy, comin' at ya! Had to change Tripod to be a DeltaDrawable for this to work...

--- trunk/delta3d/inc/dtCore/tripod.h
+++ trunk/delta3d/inc/dtCore/tripod.h
@@ -21,11 +21,10 @@
 #ifndef DELTA_TRIPOD
 #define DELTA_TRIPOD
 
-#include "dtCore/base.h"
-#include "dtCore/camera.h"
-
+#include <dtCore/deltadrawable.h>
+#include <dtCore/camera.h>
+#include <dtUtil/deprecationmgr.h>
 #include <osg/Vec3>
-
 
 namespace dtCore
 {
@@ -35,9 +34,9 @@ namespace dtCore
     *  Transformable.  The Tripod has a built-in offset which allows the Camera
     *  to "stand-off" the parent Transformable.  The user may also supply a scale
     *  valued for the x,y,z, and h,p,r coordinates.  These scale values allow 
-    *  the Camera to "lag" behind.
-    *  Also, the Tripod may be instructed to always point toward another
-    *  Transformable.
+    *  the Camera to "lag" behind. Also, the Tripod may be instructed to always point
+    *  toward another Transformable. The Tripod is a DeltaDrawable solely so we can
+    *  make an actor proxy for it.
     *
     *  Note: To have a Camera keep a fixed, parent-relative distance from the parent,
     *  you can simply use the functionality in Transformable and not use the Tripod.
@@ -47,14 +46,14 @@ namespace dtCore
     *    myCamera->SetTransform( &offset, Transformable::REL_CS );
     *  \endcode
     */
-   class DT_CORE_EXPORT Tripod : public Base
+   class DT_CORE_EXPORT Tripod : public DeltaDrawable
    {
    public:
 
       DECLARE_MANAGEMENT_LAYER(Tripod)
       
       ///Contructor which takes in a optional Camera and Transformable
-      Tripod(Camera *cam = NULL, Transformable *parent = NULL);
+      Tripod(Camera *cam = 0, Transformable *parent = 0);
 
       ///Supply a Camera to connect to this Tripod
       void SetCamera( Camera *cam );
@@ -62,17 +61,38 @@ namespace dtCore
       ///set camera by name
       void SetCamera( const std::string& camName );
 
-      ///Get the currently connected Camera
-      RefPtr<Camera> GetCamera() const {return mCamera;}
+      /*
+      RefPtr<Camera> GetCamera() const
+      {
+         DEPRECATE(  "RefPtr<Camera> GetCamera()",
+                     "Camera* GetCamera()" )
+
+         return mCamera;
+      }
+      */
+
+      ///Get a non-const pointer to the currently connected Camera
+      Camera* GetCamera() {return mCamera.get();}
 
       ///Attach this Tripod to a Transformable
       void SetAttachToTransformable(Transformable *parent);
+
       ///Attach to transformable by name
       void SetAttachToTransformable(const std::string& parentName);
 
-      ///Get the currently connected Transformable
-      RefPtr<Transformable> GetAttachedTransformable() const {return mParentTrans;}
-      
+      /*
+      RefPtr<Transformable> GetAttachedTransformable() const
+      {
+         DEPRECATE(  "RefPtr<Transformable> GetAttachedTransformable()",
+                     "Transformable* GetAttachedTransformable()" )
+
+         return mParentTrans;
+      }
+      */
+
+      ///Get a non-const pointer to the currently connected Transformable
+      Transformable* GetAttachedTransformable() {return mParentTrans.get();}
+
       ///Set the Tripod's offset from the parent Transformable
       void SetOffset(float x, float y, float z, float h, float p, float r)
       { SetOffset( osg::Vec3(x,y,z), osg::Vec3(h,p,r) ); }
@@ -100,10 +120,13 @@ namespace dtCore
 
       ///Set the mode of how the Tripod connects to the parent
       void SetTetherMode( TetherMode mode) {mTetherMode = mode;}
-      TetherMode GetTetherMode()const {return mTetherMode;}
+      TetherMode GetTetherMode() const {return mTetherMode;}
 
       ///Point the Tripod at this Transformable
       void SetLookAtTarget(Transformable *target);
+
+      Transformable* GetLookAtTarget() { return mLookAtTarget.get(); }
+      const Transformable* GetLookAtTarget() const { return mLookAtTarget.get(); }
 
    protected:
 
