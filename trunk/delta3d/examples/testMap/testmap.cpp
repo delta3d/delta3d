941fda3f96a5715a69108ba8796b0b07fb26d182
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-11-09 19:15:14 +0000

    Cleaned up code to fit guidelines, plugged leaks

--- trunk/delta3d/examples/testMap/testmap.cpp
+++ trunk/delta3d/examples/testMap/testmap.cpp
@@ -1,5 +1,6 @@
 /// Demo application illustrating map loading and actor manipulation
 /// @author Eddie Johnson
+/// @author Chris Osborn
 
 #include <dtABC/dtabc.h>
 #include <dtCore/dt.h>
@@ -12,161 +13,176 @@ using namespace dtCore;
 using namespace dtDAL;
 using namespace dtABC; 
 
-class TestApp : public Application
+class TestMap : public Application
 {
-public:
-
-   TestApp() : Application("testMap.xml")
-    {
-        // Set up a motion model so we may move the camera
-        wmm = new WalkMotionModel(GetKeyboard(), GetMouse());
-        wmm->SetTarget(GetCamera());
-
-        // Steps to load a map into Delta3D
-        // 1. Set the project context, just like in the editor
-        // 2. Get a reference to the map
-        // 3. Load the map into the scene through the project class
-        std::string contextName = GetDeltaDataPathList() + "/demoMap";
-        Project::GetInstance().SetContext(contextName);
-        Map &myMap = Project::GetInstance().GetMap("MyCoolMap");
-
-        //Since we are in an Application we can simply call...
-        LoadMap(myMap);
-
-        // But if wanted to load the map into another Scene we would
-        // have to call:
-        // Project::GetInstance().LoadMapIntoScene(myMap, *GetScene());
-
-        // Get the proxies from the map
-        std::vector< osg::ref_ptr<ActorProxy> > proxies;
-        myMap.FindProxies(proxies, "StaticMesh*");
-
-        // Initialization
-        helicopter = tree = NULL;
-        smoke = new ParticleSystem;
-        explosion = new EffectManager;
-        step = -1.0f;
-        bufferExplosion = true;
-        smoke->LoadFile("Particles/smoke.osg");
-        explosion->AddDetonationTypeMapping(HighExplosiveDetonation, "Particles/explosion.osg");
-
-        for(unsigned int i = 0; i < proxies.size(); i++)
-        {
-            // Find our helicopter by name
-            if(proxies[i]->GetName() == "StaticMesh0")
-                helicopter = dynamic_cast<Object*> (proxies[i]->GetActor());
-
-            // Find our tree by name
-            if(proxies[i]->GetName() == "StaticMesh1")
-                tree = dynamic_cast<Object*> (proxies[i]->GetActor());
-        }
-
-        // Error check
-        if(!helicopter)
-        {
-           std::cout << "Failed to locate the helicopter\n";
-            Quit();
-        }
-
-        if(!tree)
-        {
-            std::cout << "Failed to find the tree\n";
-            Quit();
-        }
-
-        // move our tree away a little bit
-        tree->SetTransform(new Transform(-50, 0, -1, 0, 0, 0));
-        smoke->SetEnabled(true);
-        GetScene()->AddDrawable(explosion);
-
-        // translate the camera back some
-        GetCamera()->SetTransform(new Transform(-25, -100, 0, 0, 0, 0));
-
-        Config();
-    }
-
-    virtual void OnMessage(MessageData *data)
-    {
-        Transform tTree, tHeli;
-
-        tree->GetTransform(&tTree);
-        helicopter->GetTransform(&tHeli);
-        
-        osg::Vec3 tTreeTranslation;
-        tTree.GetTranslation(tTreeTranslation);
-        
-        osg::Vec3 tHeliTranslation;
-        tHeli.GetTranslation(tHeliTranslation);
-        
-        // If the helicopter isn't at the tree yet, keep translating...
-        if(tTreeTranslation.x() < tHeliTranslation.x())
-            helicopter->SetTransform(new Transform(step, 0, 1, 90, 0, 0));
-
-        // It's there
-        else if(tTreeTranslation.x() >= tHeliTranslation.x())
-        {
-            float x, y, z;
-            tHeli.GetTranslation(x, y, z);
-            osg::Vec3 heliPos(x, y, z);
-
-            // This is somewhat of a hack. The explosion will loop continuously in here
-            // because of the conditions on which it is invoked. It eats framerate and 
+   public:
+   
+   TestMap() :
+      Application("testMap.xml"),
+      mWmm(0),
+      mHelicopter(0),
+      mTree(0),
+      mSmoke(0),
+      mExplosionManager(0),
+      mStep(-1.0f),
+      mBufferExplosion(true)
+   {
+      // Set up a motion model so we may move the camera
+      mWmm = new WalkMotionModel(GetKeyboard(), GetMouse());
+      mWmm->SetTarget(GetCamera());
+      
+      // Steps to load a map into Delta3D
+      // 1. Set the project context, just like in the editor
+      // 2. Get a reference to the map
+      // 3. Load the map into the scene through the project class
+      std::string contextName = GetDeltaDataPathList() + "/demoMap";
+      Project::GetInstance().SetContext(contextName);
+      Map &myMap = Project::GetInstance().GetMap("MyCoolMap");
+      
+      //Since we are in an Application we can simply call...
+      LoadMap(myMap);
+      
+      // But if wanted to load the map into another Scene we would
+      // have to call:
+      //Project::GetInstance().LoadMapIntoScene(myMap, *GetScene());
+      
+      // Get the proxies from the map
+      std::vector< osg::ref_ptr<ActorProxy> > proxies;
+      myMap.FindProxies(proxies, "StaticMesh*");
+      
+      // Initialization
+      mSmoke = new ParticleSystem;
+      mExplosionManager = new EffectManager;
+      mSmoke->LoadFile("Particles/smoke.osg");
+      mExplosionManager->AddDetonationTypeMapping(HighExplosiveDetonation, "Particles/explosion.osg");
+      
+      for(unsigned int i = 0; i < proxies.size(); i++)
+      {
+         // Find our mHelicopter by name
+         if(proxies[i]->GetName() == "StaticMesh0")
+         {
+            mHelicopter = dynamic_cast<Object*> (proxies[i]->GetActor());
+         }
+         
+         // Find our mTree by name
+         if(proxies[i]->GetName() == "StaticMesh1")
+         {
+            mTree = dynamic_cast<Object*> (proxies[i]->GetActor());
+         }
+      }
+      
+      // Error check
+      if(!mHelicopter.valid())
+      {
+         std::cout << "Failed to locate the helicopter\n";
+         Quit();
+      }
+      
+      if(!mTree.valid())
+      {
+         std::cout << "Failed to find the tree\n";
+         Quit();
+      }
+      
+      // move our tree away a little bit
+      Transform xform(-50.0f, 0.0f, -1.0f);
+      mTree->SetTransform(&xform);
+      mSmoke->SetEnabled(true);
+      GetScene()->AddDrawable(mExplosionManager.get());
+      
+      // translate the camera back some
+      xform.SetTranslation(-25.0f,-100.0f,0.0f);
+      GetCamera()->SetTransform(&xform);
+   }
+   
+   virtual void OnMessage(MessageData *data)
+   {
+      Transform tTree, tHeli;
+      
+      mTree->GetTransform(&tTree);
+      mHelicopter->GetTransform(&tHeli);
+      
+      osg::Vec3 tTreeTranslation;
+      tTree.GetTranslation(tTreeTranslation);
+      
+      osg::Vec3 tHeliTranslation;
+      tHeli.GetTranslation(tHeliTranslation);
+      
+      // If the mHelicopter isn't at the tree yet, keep translating...
+      if(tTreeTranslation.x() < tHeliTranslation.x())
+      {
+         Transform xform(mStep, 0.0f, 1.0f, 90.0f, 0.0f, 0.0f);
+         mHelicopter->SetTransform(&xform);
+      }
+      // It's there
+      else if(tTreeTranslation.x() >= tHeliTranslation.x())
+      {
+         float x, y, z;
+         tHeli.GetTranslation(x, y, z);
+         osg::Vec3 heliPos(x, y, z);
+         
+         // This is somewhat of a hack. The explosion will loop continuously in here
+         // because of the conditions on which it is invoked. It eats framerate and 
             // causes the app to chug, not to mention doesn't even look like an explosion
-            if(bufferExplosion)
-            {
-                explosion->AddDetonation(heliPos);
-                bufferExplosion = false;
-                smoke->SetParent(NULL);
-                helicopter->AddChild(smoke);
-                smoke->SetEnabled(true);
-                // Make sure to orient the new mesh to match the old one
-                helicopter->SetTransform(new Transform(x, y, 0, 90, -30, 0));
-            }
-        }
-
-        // Reset the scene
-        if(GetKeyboard()->GetKeyState(Producer::Key_R))
-            Reset();
-
-        step -= 0.05f;
-    }
-
-    // Re-initialize to default conditions
-    void Reset()
-    {
-        GetScene()->AddDrawable(tree);
-        helicopter->SetTransform(new Transform(0, 0, 1, 90, 0, 0));
-        helicopter->LoadFile("StaticMeshes/uh-1n.ive");
-        smoke->SetEnabled(false);
-        step = -0.00001f;
-        bufferExplosion = true;
-    }
-
-    virtual ~TestApp()
-    {
-        
-    }
-
+         if(mBufferExplosion)
+         {
+            mExplosionManager->AddDetonation(heliPos);
+            mBufferExplosion = false;
+            mSmoke->SetParent(0);
+            mHelicopter->AddChild(mSmoke.get());
+            mSmoke->SetEnabled(true);
+            // Make sure to orient the new mesh to match the old one
+            Transform xform(x,y,0.0f,90.0f,-30.0f,0.0f);
+            mHelicopter->SetTransform(&xform);
+         }
+      }
+      
+      // Reset the scene
+      if(GetKeyboard()->GetKeyState(Producer::Key_R))
+      {
+         Reset();
+      }
+      
+      mStep -= 0.05f;
+   }
+   
+   // Re-initialize to default conditions
+   void Reset()
+   {
+      GetScene()->AddDrawable(mTree.get());
+      Transform xform(0.0f,0.0f,1.0f,90.0f,0.0f,0.0f);
+      mHelicopter->SetTransform(&xform);
+      mHelicopter->LoadFile("StaticMeshes/uh-1n.ive");
+      mSmoke->SetEnabled(false);
+      mStep = -1.0f;
+      mBufferExplosion = true;
+   }
+   
+   virtual ~TestMap()
+   {
+   }
+   
 private:
 
-    WalkMotionModel *wmm;
-    Object *helicopter, *tree;
-    ParticleSystem *smoke;
-    EffectManager  *explosion;
-    float step;
-    bool bufferExplosion;
+   RefPtr<WalkMotionModel> mWmm;
+   RefPtr<Object> mHelicopter;
+   RefPtr<Object> mTree;
+   RefPtr<ParticleSystem> mSmoke;
+   RefPtr<EffectManager>  mExplosionManager;
+   float mStep;
+   bool mBufferExplosion;
 };
 
 int main()
 {
    dtCore::SetDataFilePathList( GetDeltaRootPath() + "/examples/testMap/;" +
-                                 dtCore::GetDeltaDataPathList() + ";" +
-                                 GetDeltaDataPathList()+"/effects/" );
-   RefPtr<TestApp> app;
+                                GetDeltaDataPathList() );
+   RefPtr<TestMap> app;
    
    try
    {
-      app = new TestApp;
+      app = new TestMap;
+      app->Config();
       app->Run();
    }
    catch (const dtUtil::Exception &e)
