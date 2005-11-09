55e28bffc03e3d2cc78e9ba47084f586c2c8eef3
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-11-09 19:16:50 +0000

    Now works with FindProxies

--- trunk/delta3d/examples/testMap/testmap.py
+++ trunk/delta3d/examples/testMap/testmap.py
@@ -18,14 +18,9 @@ class TestMap(Application):
       myMap = Project.GetInstance().GetMap("MyCoolMap")
       self.LoadMap(myMap)
 
-      # This doesn't work! I can't get Boost.Python to properly
-      # translate between the retrn value of this method:
-      # std::vector< osg::_ref< ActorProxy > > and my own wrapper
-      # ActorProxyVec...
-      
       # Get the proxies from the map
-      proxies = ActorProxyVector()
-      #myMap.FindProxies(proxies,"StaticMesh*")
+      self.proxies = ActorProxyVector()
+      myMap.FindProxies(self.proxies,"StaticMesh*")
 
       self.helicopter = None
       self.tree = None
@@ -37,27 +32,27 @@ class TestMap(Application):
       self.smoke.LoadFile("Particles/smoke.osg")
       self.explosion.AddDetonationTypeMapping(HighExplosiveDetonation, "Particles/explosion.osg")
       
-      #for actor in proxies :
-      #   # Find our helicopter by name
-      #   if actor.GetName() == "StaticMesh0" :
-      #      self.helicopter = actor.GetActor()
-      #   # Find our tree by name
-      #   if actor.GetName() == "StaticMesh1" :
-      #      self.tree = actor.GetActor()
+      for actor in self.proxies :
+         # Find our helicopter by name
+         if actor.GetName() == "StaticMesh0" :
+            self.helicopter = actor.GetActor()
+         # Find our tree by name
+         if actor.GetName() == "StaticMesh1" :
+            self.tree = actor.GetActor()
       
       # Error check
-      #if self.helicopter is None :
-      #   print "Failed to locate the helicopter"
-      #   sys.exit()
-      # 
-      #if self.tree is None :
-      #    print "Failed to find the tree"
-      #    sys.exit()
+      if self.helicopter is None :
+         print "Failed to locate the helicopter"
+         sys.exit()
+       
+      if self.tree is None :
+         print "Failed to find the tree"
+         sys.exit()
      
       # move our tree away a little bit
-      #self.tree.SetTransform(Transform(-50, 0, -1, 0, 0, 0))
-      #self.smoke.SetEnabled(1)
-      #self.GetScene().AddDrawable(explosion)
+      self.tree.SetTransform(Transform(-50, 0, -1, 0, 0, 0))
+      self.smoke.SetEnabled(1)
+      self.GetScene().AddDrawable(self.explosion)
 
       # translate the camera back some
       self.GetCamera().SetTransform(Transform(-25, -100, 0, 0, 0, 0))
@@ -67,52 +62,50 @@ class TestMap(Application):
       self.wmm.SetDefaultMappings( self.GetKeyboard(), self.GetMouse() )
       self.wmm.SetTarget( self.GetCamera() )
    
-   #def PreFrame(self, deltaFrameTime) :
-      
-      #tTree = Transform()
-      #tHeli = Transform()
+   def PreFrame(self, deltaFrameTime) :
 
-      #self.tree.GetTransform(tTree)
-      #self.helicopter.GetTransform(tHeli)
+      tTree = Transform()
+      tHeli = Transform()
+
+      self.tree.GetTransform(tTree)
+      self.helicopter.GetTransform(tHeli)
         
-      #tTreeTranslation = Vec3
-      #tTree.GetTranslation(tTreeTranslation)
+      tTreeTranslation = Vec3()
+      tTree.GetTranslation(tTreeTranslation)
        
-      #tHeliTranslation = Vec3
-      #tHeli.GetTranslation(tHeliTranslation)
+      tHeliTranslation = Vec3()
+      tHeli.GetTranslation(tHeliTranslation)
         
       # If the helicopter isn't at the tree yet, keep translating...
-      #if tTreeTranslation.x() < tHeliTranslation.x() :
-      #   self.helicopter.SetTransform(Transform(step, 0, 1, 90, 0, 0))
+      if tTreeTranslation[0] < tHeliTranslation[0] :
+         self.helicopter.SetTransform(Transform(self.step, 0, 1, 90, 0, 0))
       ## It's there
-      #elif tTreeTranslation.x() >= tHeliTranslation.x() :    
-      #   x = 0.0
-      #   y = 0.0
-      #   z = 0.0
-      #   tHeli.GetTranslation(x, y, z)
-      #   heliPos = Vec3(x, y, z)
+      elif tTreeTranslation[0] >= tHeliTranslation[0] :    
+         xyz = Vec3()
+         tHeli.GetTranslation(xyz)
+         heliPos = xyz
 
          # This is somewhat of a hack. The explosion will loop continuously in here
          # because of the conditions on which it is invoked. It eats framerate and 
          # causes the app to chug, not to mention doesn't even look like an explosion
-         #if bufferExplosion :
-         # 
-         #   self.explosion.AddDetonation(heliPos)
-         #   bufferExplosion = 0
-         #   self.smoke.SetParent(None) #?
-         #   self.helicopter.AddChild(smoke)
-         #   self.smoke.SetEnabled(1)
-         #   # Make sure to orient the new mesh to match the old one
-         #   self.helicopter.SetTransform(Transform(x, y, 0, 90, -30, 0))
+         if self.bufferExplosion :
+          
+            self.explosion.AddDetonation(heliPos)
+            self.bufferExplosion = 0
+            self.smoke.SetParent(None) 
+            self.helicopter.AddChild(self.smoke)
+            self.smoke.SetEnabled(1)
+            # Make sure to orient the new mesh to match the old one
+            self.helicopter.SetTransform(Transform(xyz[0], xyz[1], 0, 90, -30, 0))
             
       # Reset the scene
-      #if GetKeyboard().GetKeyState( KeyboardKey.Key_R ) :
-      #    Reset()
+      if self.GetKeyboard().GetKeyState( KeyboardKey.Key_R ) :
+          Reset()
 
-      #step -= 0.05
+      self.step -= 0.05
       
    def Reset(self) :
-      GetScene().AddDrawable(tree);
+      self.GetScene().AddDrawable(tree);
       self.helicopter.SetTransform(Transform(0, 0, 1, 90, 0, 0))
       self.helicopter.LoadFile("StaticMeshes/uh-1n.ive")
       self.smoke.SetEnabled(0)
