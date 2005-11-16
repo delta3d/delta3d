86bc33f2b38fcb96e79c5db448aeb6896cc78a5e
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-11-16 23:35:47 +0000

    Going back to OSG 0.9.9 :(

--- delta3d/src/dtCore/stats.cpp
+++ delta3d/src/dtCore/stats.cpp
@@ -450,7 +450,7 @@ int Stats::WritePrims( const int ypos, osgUtil::Statistics& stats)
     unsigned int totalNumPrimives = 0;
     unsigned int totalNumIndices = 0;
     unsigned int totalNumTriangles = 0;
-    osgUtil::Statistics::PrimitiveValueMap::iterator pItr;   // TODO: In OSG post-0.9.9, this class is
+    osgUtil::Statistics::PrimtiveValueMap::iterator pItr;   // TODO: In OSG post-0.9.9, this class is
                                                             // called PrimitiveValueMap (spelling correction)
     for(pItr=stats._primitiveCount.begin(); pItr!=stats._primitiveCount.end(); ++pItr)
     {
