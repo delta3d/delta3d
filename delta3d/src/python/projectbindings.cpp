a95f336396c3aabc06589e68b87debd221125095
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2005-12-20 01:14:26 +0000

    Updated bindings to match merged BMH code

--- delta3d/src/python/projectbindings.cpp
+++ delta3d/src/python/projectbindings.cpp
@@ -6,14 +6,14 @@ using namespace boost::python;
 using namespace dtDAL;
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SC_overloads, SetContext, 1, 2)
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LMIS_overloads, LoadMapIntoScene, 2, 3)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LMIS_overloads, LoadMapIntoScene, 2, 4)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(CM_overloads, CloseMap, 1, 2)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(DM_overloads, DeleteMap, 1, 2)
 
 void initProjectBindings()
 {
-   Map& ( Project::*LMIS1 )( const std::string&, dtCore::Scene&, bool ) = &Project::LoadMapIntoScene;
-   void ( Project::*LMIS2 )( Map&, dtCore::Scene&, bool ) = &Project::LoadMapIntoScene;
+   Map& ( Project::*LMIS1 )( const std::string&, dtCore::Scene&, bool, bool ) = &Project::LoadMapIntoScene;
+   void ( Project::*LMIS2 )( Map&, dtCore::Scene&, bool, bool ) = &Project::LoadMapIntoScene;
    
    void ( Project::*DM1 )( Map&, bool ) = &Project::DeleteMap;
    void ( Project::*DM2 )( const std::string&, bool ) = &Project::DeleteMap;
