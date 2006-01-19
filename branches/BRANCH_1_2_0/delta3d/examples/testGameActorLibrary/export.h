1ca1f13879c47724a205342a743c734810e37bac
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-01-19 23:57:46 +0000

    comment fix

--- branches/BRANCH_1_2_0/delta3d/examples/testGameActorLibrary/export.h
+++ branches/BRANCH_1_2_0/delta3d/examples/testGameActorLibrary/export.h
@@ -24,9 +24,9 @@
 /**
 * This is modeled from the DT_EXPORT macro found in dtCore/export.h.  
 * We define another due to conflicts with using the DT_EXPORT while  
-* trying to import Delta3D symbols.  The DT_GAMEMANAGER_LIBRARY macro should be used
-* in front of any classes that are to be exported from the terrain library.
-* Also note that DT_GAMEMANAGER_LIBRARY should be defined in the compiler 
+* trying to import Delta3D symbols.  The DT_EXAMPLE_LIBRARY macro should be used
+* in front of any classes that are to be exported from the testGameActorLibrary.
+* Also note that DT_EXAMPLE_LIBRARY should be defined in the compiler 
 * preprocessor #defines.
 */
 #if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
