5b8becda43ce7434e8e334263f3bb7789ee5110f
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-03-13 22:13:09 +0000

    Added more friendly warning message for invalid collision shapes (it's ok if the warning is in STAGE)

--- delta3d/src/dtCore/transformable.cpp
+++ delta3d/src/dtCore/transformable.cpp
@@ -783,7 +783,7 @@ void Transformable::SetCollisionBox( osg::Node* node )
          // This case happens in STAGE when the collision shape property is
          // parsed _before_ the mesh filename. After the mesh has been
          // loaded then the box will be recalculated.
-         LOG_WARNING( "Calculated values for collision box are invalid." )
+         LOG_WARNING( "Calculated values for collision box are invalid. If you are reading this from STAGE, don't worry, it just means the collision shape ActorProperty was parsed from XML before the actual mesh. The collision shape will be recalculated when the mesh is loaded." )
       }
    }
 }
