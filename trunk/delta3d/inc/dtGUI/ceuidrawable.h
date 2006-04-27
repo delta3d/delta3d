769096f9defd1bb6b8615ec480084104ea362d6b
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-04-27 19:29:22 +0000

    +changed the CEGUI Drawable to be compatible with multitexture models

--- trunk/delta3d/inc/dtGUI/ceuidrawable.h
+++ trunk/delta3d/inc/dtGUI/ceuidrawable.h
@@ -64,8 +64,7 @@ namespace dtGUI
 
          virtual void drawImplementation(osg::State& state) const
          { //tell the UI to update and to render
-            if (!mUI) return;
-            state.setActiveTextureUnit(0); //makes it work with other multitexture Objects
+            if (!mUI) return;       
             mUI->getSingletonPtr()->renderGUI();
          }
 
