5c56b05b48bd6065fd68b1513e9a130e7f270aaa
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-06-19 22:39:07 +0000

    Further tweaking for listener wrappers, still not quite there

--- trunk/delta3d/src/python/mousebindings.cpp
+++ trunk/delta3d/src/python/mousebindings.cpp
@@ -8,6 +8,49 @@
 using namespace boost::python;
 using namespace dtCore;
 
+class MouseListenerWrap : public MouseListener, public wrapper<MouseListener>
+{
+   public:
+   MouseListenerWrap()
+   {
+   }
+   protected:
+   virtual ~MouseListenerWrap()
+   {
+   }
+   public:
+   
+   bool HandleButtonPressed(const Mouse* mouse, Mouse::MouseButton button)
+   {
+      return this->get_override( "HandleButtonPressed" )( boost::ref(mouse), button );
+   }
+   
+   bool HandleButtonReleased(const Mouse* mouse, Mouse::MouseButton button)
+   {
+      return this->get_override( "HandleButtonReleased" )( boost::ref(mouse), button );
+   }
+   
+   bool HandleButtonClicked(const Mouse* mouse, Mouse::MouseButton button, int clickCount)
+   {
+      return this->get_override( "HandleButtonClicked" )( boost::ref(mouse), button, clickCount );
+   }
+   
+   bool HandleMouseMoved(const Mouse* mouse, float x, float y)
+   {
+      return this->get_override( "HandleMouseMoved" )( boost::ref(mouse), x, y );
+   }
+   
+   bool HandleMouseDragged(const Mouse* mouse, float x, float y)
+   {
+      return this->get_override( "HandleMouseDragged" )( boost::ref(mouse), x, y );
+   }
+   
+   bool HandleMouseScrolled(const Mouse* mouse, int delta)
+   {
+      return this->get_override( "HandleMouseScrolled" )( boost::ref(mouse), delta );
+   }
+};
+
 void initMouseBindings()
 {
    Mouse* (*MouseGI1)(int) = &Mouse::GetInstance;
@@ -16,7 +59,7 @@ void initMouseBindings()
    typedef void (Mouse::*MouseFloatFloatMemFun)(float&,float&) const;
    MouseFloatFloatMemFun MouseGP1 = &Mouse::GetPosition;
 
-   class_<MouseListener, dtCore::RefPtr<MouseListener>, boost::noncopyable >("MouseListener",no_init)
+   class_<MouseListenerWrap, dtCore::RefPtr<MouseListenerWrap>, boost::noncopyable >("MouseListener")
       .def("HandleButtonPressed", pure_virtual(&MouseListener::HandleButtonPressed))
       .def("HandleButtonReleased", pure_virtual(&MouseListener::HandleButtonReleased))
       .def("HandleButtonClicked", pure_virtual(&MouseListener::HandleButtonClicked))
@@ -33,7 +76,7 @@ void initMouseBindings()
       .def("GetPosition", MouseGP1)
       .def("SetPosition", &Mouse::SetPosition)
       .def("GetButtonState", &Mouse::GetButtonState)
-      .def("AddMouseListener", &Mouse::AddMouseListener)
+      .def("AddMouseListener", &Mouse::AddMouseListener, with_custodian_and_ward<1,2>())
       .def("RemoveMouseListener", &Mouse::RemoveMouseListener);
       
    enum_<Mouse::MouseButton>("MouseButton")
