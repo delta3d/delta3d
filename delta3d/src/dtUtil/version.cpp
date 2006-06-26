a6386783db029cbf6897e956211291c7ddafb2f8
cwosborn <cwosborn@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-06-26 19:55:43 +0000

    Added version utility & tests. This will allow users to grab the working version of Delta3D at compile or run time. Resolves Case 773.

---
+++ delta3d/src/dtUtil/version.cpp
@@ -0,0 +1,33 @@
+/* -*-c++-*-
+ * Delta3D Open Source Game and Simulation Engine 
+ * Copyright (C) 2006 MOVES Institute 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either version 2.1 of the License, or (at your option) 
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ *
+ * @author Robert Osfield (via <osg/Version>)
+ * @author Chris Osborn
+ */
+
+#include <dtUtil/version.h>
+
+const char* Delta3DGetVersion()
+{
+   return "1.3.0";
+}
+
+const char* Delta3DGetLibraryName()
+{
+   return "Delta3D Library";
+}
