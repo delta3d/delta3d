fe21143695474cd2529a09a78560a40458dc30c7
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-07-12 20:32:43 +0000

    *** empty log message ***

---
+++ trunk/delta3d/src/dtDAL/waypointactorproxy.cpp
@@ -0,0 +1,98 @@
+/*
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2004-2006 MOVES Institute
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @author Bradley Anderegg 06/07/2006
+ */
+
+#include <dtDAL/waypointactorproxy.h>
+#include <dtAI/waypointmanager.h>
+#include <dtAI/WaypointActor.h>
+
+#include <dtDAL/actorproxyicon.h>
+#include <dtDAL/enginepropertytypes.h>
+
+using namespace dtAI;
+
+namespace dtDAL
+{
+   unsigned WaypointActorProxy::mNumWaypoints = 0;
+
+   WaypointActorProxy::WaypointActorProxy()
+   {
+      SetClassName("WaypointActorProxy");
+   }
+   
+   WaypointActorProxy::~WaypointActorProxy()
+   {      
+      WaypointManager::GetInstance()->RemoveWaypoint(static_cast<WaypointActor*> (mActor.get()));
+   }
+
+   void WaypointActorProxy::CreateActor()
+   {
+      mActor = new WaypointActor;      
+      WaypointManager::GetInstance()->AddWaypoint(static_cast<WaypointActor*>(GetActor()));    
+
+      std::ostringstream ss;
+      ss << "Waypoint" << mNumWaypoints++;
+      SetName(ss.str());
+   }  
+
+
+   void WaypointActorProxy::BuildPropertyMap()
+   {
+      WaypointActor *pWaypointActor = static_cast<WaypointActor*> (mActor.get());
+      dtDAL::TransformableActorProxy::BuildPropertyMap();
+   }
+
+   const dtDAL::ActorProxy::RenderMode& WaypointActorProxy::GetRenderMode()
+   {
+      return dtDAL::ActorProxy::RenderMode::DRAW_BILLBOARD_ICON;
+   }
+
+
+   dtDAL::ActorProxyIcon *WaypointActorProxy::GetBillBoardIcon()
+   {
+      if(!mBillBoardIcon.valid())
+      {
+         mBillBoardIcon =
+            new dtDAL::ActorProxyIcon(dtDAL::ActorProxyIcon::IconType::GENERIC);
+      }
+
+      return mBillBoardIcon.get();
+   }
+
+
+   void WaypointActorProxy::OnTranslation(const osg::Vec3 &oldValue, const osg::Vec3 &newValue)
+   {
+      WaypointActor *pWaypointActor = static_cast<WaypointActor*> (mActor.get());
+      WaypointManager::GetInstance()->MoveWaypoint(pWaypointActor->GetIndex(), newValue);
+   }
+
+   bool WaypointActorProxy::IsPlaceable() const
+   {
+      return false;
+   }
+   
+   bool WaypointActorProxy::IsGhostProxy() const
+   {
+      return true;
+   }
+
+
+
+}//namespace 
