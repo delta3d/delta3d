b97ea91a471ecf4823a7a08a57b9bd9e5c530a3f
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-07-14 19:02:36 +0000

    +added a function to rotate a character to face a specific point

--- trunk/delta3d/inc/dtChar/character.h
+++ trunk/delta3d/inc/dtChar/character.h
@@ -147,6 +147,29 @@ namespace dtChar
           * axis
           */
          float GetRotation() const;
+
+
+         /**
+         * Sets the rotation speed of this character.
+         *
+         * @param rotation speed in degrees per second         
+         */
+         void SetRotationSpeed(float rotation);
+
+         /**
+         * Returns the rotation speed of this character.
+         *
+         * @return the current rotation speed in degrees per second         
+         */
+         float GetRotationSpeed() const;
+
+         /**
+         * Rotates the character over time so its heading faces a specific point         
+         *
+         * @param the point to rotate to
+         * @param the time step
+         */
+         void RotateCharacterToPoint(const osg::Vec3& pPoint, float deltaTime);
          
          /**
           * Sets the walk/run velocity of this character.
@@ -242,7 +265,12 @@ namespace dtChar
           * The character's current rotation.
           */
          float mRotation;
-         
+
+         /**
+         * The character's current rotation speed.
+         */
+         float mRotationSpeed;
+
          /**
           * The character's current walk/run velocity.
           */
@@ -256,6 +284,7 @@ namespace dtChar
          dtCore::RefPtr< osgUtil::UpdateVisitor > mUpdateVisitor;
          int mPauseFrameNumber;
    };
+
 };
 
 #endif // DELTA_CHARACTER
