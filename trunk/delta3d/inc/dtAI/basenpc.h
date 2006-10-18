fa4ddb188d5ac211df0b20aa4e2aa6f5d78a3b34
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-10-18 22:28:10 +0000

    +made BaseNPC derive from osg::Referenced +added documentation to BaseNPC

--- trunk/delta3d/inc/dtAI/basenpc.h
+++ trunk/delta3d/inc/dtAI/basenpc.h
@@ -31,6 +31,8 @@
 
 #include <dtUtil/functor.h>
 
+#include <osg/Referenced>
+
 #include <map>
 #include <string>
 
@@ -38,9 +40,10 @@
 namespace dtAI
 {
    /**
-    * This represents the base class used for an ai agent having embedded support for the planning architecture
+    * This represents the base delta class used for AI agents having embedded support for the planning architecture
+    * and a generic FSM.
     */
-   class DT_AI_EXPORT BaseNPC
+   class DT_AI_EXPORT BaseNPC: public osg::Referenced
    {
       public:
 
@@ -51,48 +54,167 @@ namespace dtAI
          typedef std::map<std::string, Action> ActionMap;
 
       public:
-   
          BaseNPC(const std::string& pName);
+      protected:
          virtual ~BaseNPC();
-          
+
+      public:
+         /**
+         * InitNPC() must be called before spawn
+         * use OnInitNPC() for derived initialize behavior
+         * calls InitializeFSM() and RegisterActions()
+         */
          void InitNPC();
+
+         /**
+         * SpawnNPC() sets the WorldState of the NPC to be equal to the WorldState template (SetWSTemplate())
+         * it then sets a goal from the goal list if it does not already have one 
+         * and calls Spawn() for derived specific spawning behavoir 
+         */
          void SpawnNPC();
+
+         /**
+         * KillNPC() puts the character in the sleep state so it no longer does processing on update
+         * calls Kill() for derived specific behavoir
+         */
          void KillNPC();
 
+         /**
+         * Sets the NPC into the Sleep state which does nothing on update
+         */
          void SetSleeping(bool pIsSleeping);
 
+         /**
+         * Update should be called every frame
+         * calls ExecutePlan which will generate a plan if it doesnt have one
+         * Updates the state machine which may change states based on input
+         */
          virtual void Update(double dt);
 
+         /**
+         * Loads Operators, Goals and Conditionals from an NPCScript file 
+         * see UnitTestsDtAI for an example
+         */
          bool LoadNPCScript(const std::string& pFileName);
 
+         /**
+         * Sets the current goal by name, the goal is used during GeneratePlan if the user
+         * has not overridden IsDesiredState()
+         */
          bool SetGoal(const std::string& pGoal);
+
+         /**
+         * GeneratePlan() uses the Planner to generate a plan
+         * the plan is a list of Operators which can be added manually or 
+         * added through LoadNPCScript with a valid script file
+         * the plan is created to be of minimal cost (uses RemainingCost()) and satisfy the 
+         * IsDesiredState() function 
+         */
          void GeneratePlan();
 
+         /**
+         * Actions are function pointers called by name during ExecutePlan()
+         * there must be an action registered for every Operator with the same name
+         * returned by the Operators GetName() function
+         */
          void RegisterAction(const std::string& pName, Action pAction);
 
+         /**
+         * Operators define what the NPC can do and how it affects the NPC's state
+         * Operators are used to chain together in order to satisfy a Goal 
+         * they can be added manually or added by LoadNPCScript() 
+         */
          void AddOperator(Operator* pOperator);
+
+         /**
+         * Goals are used to control an NPC's behavior they evaluate a WorldState
+         * and return whether or not the WorldState completes the Goal
+         * Goals can be added manually or added through LoadNPCScript()
+         */
          void AddGoal(const std::string& pName, Goal* pGoal);
 
-         Planner::OperatorList GetPlan() const;
 
+         /**
+         * GetPlan() returns the last plan generated through GeneratePlan()
+         * where a plan is just a list of Operators designed to achieve a specific Goal
+         */
+         Planner::OperatorList GetPlan() const;
+ 
+         /**
+         * SetWSTemplate() sets the template of the WorldState for this NPC
+         * a WorldState Template can be thought of as the initial state of the NPC
+         */
          void SetWSTemplate(const WorldState& pWS);
 
       protected:
 
+         /**
+         * Called on SpawnNPC()
+         */
          virtual void Spawn();
+
+         /**
+         * Called on KillNPC()
+         */
          virtual void Kill();
+
+         /**
+         * Override this to set the initial state of the NPC after it is spawned
+         */
          virtual void SelectState(double dt);
 
+         /**
+         * Runs an action of specifed name, where an action is a function that modifies
+         * the NPC's WorldState
+         */
          bool ExecuteAction(const std::string& pAction, double dt, WorldState* pWS);
+
+         /**
+         * Calls GeneratePlan if no plan exists
+         * Proceeds to run each Action registered with a specific name of an Operator
+         * moving to call the next Action when the current one returns true
+         * called on Update()
+         */
          virtual void ExecutePlan(double dt);
 
+         /**
+         * Sets up the FSM with the basic states DEFAULT, SPAWN, IDLE, DIE
+         * as defined in NPCState.h
+         */
          void InitializeFSM();      
+
+         /**
+         * Override OnInitializeFSM() to add states and transitions specific to your NPC
+         */
          virtual void OnInitializeFSM();    
 
+         /**
+         * Called on InitNPC()
+         */
          virtual void OnInit();
+
+         /**
+         * Override to register function pointers with strings which correspond to
+         * the name of this NPC's Operators using RegisterAction()
+         * NOTE: In order to run a generated plan you must override this and register an 
+         * Action for each potential Operator
+         */
          virtual void RegisterActions();
 
+         /**
+         * Used by the planner on GeneratePlan
+         * should estimate the cost remaining to achieve the desired goal
+         * the more accurate this is, the more optimized the algorithm will 
+         * be able to generate a plan
+         */
          virtual float RemainingCost(const WorldState* pWS) const;
+
+         /**
+         * Used by the planner on GeneratePlan
+         * returns true when the WorldState passed in is desirable
+         * the default implementation uses a specific Goal to evaluate
+         * the plan and determine if certain conditionals are met
+         */
          virtual bool IsDesiredState(const WorldState* pWS) const;
 
          bool mSleeping;
