04793243be4fe437d3581dbbef924bde85a52044
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-11-22 21:08:23 +0000

    Changed all code using osg/observer_ptr to use dtCore/observerptr.h.  Also changed observerptr.h to subclass the osg one because it was necessary for the implementation to work since osg/Referenced requires the object to be passed in so it can track it. Since this turned out to be only about 3 lines of code past the initial class declaration, I decided that it would be best to make dtCore::RefPtr work the same way, and it was once again 3 lines of code plus a stream operator we added.  An added benefit is that osg::ref_ptr's can now be assigned to dtCore ones and vice versa.  Also, we get osg's maintenance of the code for free.

--- trunk/delta3d/inc/dtCore/observerptr.h
+++ trunk/delta3d/inc/dtCore/observerptr.h
@@ -14,94 +14,25 @@
 #ifndef DELTA_OBSERVER_PTR
 #define DELTA_OBSERVER_PTR
 
-// Delta3D ObserverPtr, a wrap of the OSG ObserverPtr but in the dtCore namespace.
+#include <osg/observer_ptr>
+
+// Delta3D ObserverPtr, a wrap of the OSG observer_ptr but in the dtCore namespace.
 //
 //////////////////////////////////////////////////////////////////////
 
-namespace dtCore 
+namespace dtCore
 {
-   class Observer
+   template <class T>
+   class ObserverPtr: public osg::observer_ptr<T>
    {
       public:
-         virtual ~Observer() {}
-         virtual void objectDeleted(void*) {}
+         ObserverPtr(): osg::observer_ptr<T>() {}
+           
+         ObserverPtr(T* t): osg::observer_ptr<T>(t) {}
+           
+         ObserverPtr(const osg::observer_ptr<T>& rp): 
+            osg::observer_ptr<T>(rp) {}
    };
-
-   /** Smart pointer for observed objects, that automatically set pointers to them to null when they deleted.*/
-   template<class T>
-   class ObserverPtr : public Observer
-   {
-
-   public:
-      typedef T element_type;
-
-      ObserverPtr() :_ptr(0L)                            {}
-      ObserverPtr(T* t):_ptr(t)                          { if (_ptr) _ptr->addObserver(this); }
-      ObserverPtr(const ObserverPtr& rp):Observer(), _ptr(rp._ptr)  { if (_ptr) _ptr->addObserver(this); }
-      ~ObserverPtr()                                     { if (_ptr) _ptr->removeObserver(this); _ptr=0; }
-
-      inline ObserverPtr& operator = (const ObserverPtr& rp)
-      {
-         if (_ptr==rp._ptr) return *this;
-         if (_ptr) _ptr->removeObserver(this);
-
-         _ptr = rp._ptr;
-         if (_ptr) _ptr->addObserver(this);
-         return *this;
-      }
-
-      inline ObserverPtr& operator = (T* ptr)
-      {
-         if (_ptr==ptr) return *this;
-         if (_ptr) _ptr->removeObserver(this);
-
-         _ptr = ptr;
-         if (_ptr) _ptr->addObserver(this);
-
-         return *this;
-      }
-
-      virtual void objectDeleted(void* ptr)
-      {
-         if (_ptr==ptr)
-         {
-            _ptr = 0;
-         }
-      }
-
-      // comparison operators for observer_ptr.
-      inline bool operator == (const ObserverPtr& rp) const { return (_ptr==rp._ptr); }
-      inline bool operator != (const ObserverPtr& rp) const { return (_ptr!=rp._ptr); }
-      inline bool operator < (const ObserverPtr& rp) const { return (_ptr<rp._ptr); }
-      inline bool operator > (const ObserverPtr& rp) const { return (_ptr>rp._ptr); }
-
-      // comparison operator for const T*.
-      inline bool operator == (const T* ptr) const { return (_ptr==ptr); }
-      inline bool operator != (const T* ptr) const { return (_ptr!=ptr); }
-      inline bool operator < (const T* ptr) const { return (_ptr<ptr); }
-      inline bool operator > (const T* ptr) const { return (_ptr>ptr); }
-
-
-      inline T& operator*()  { return *_ptr; }
-
-      inline const T& operator*() const { return *_ptr; }
-
-      inline T* operator->() { return _ptr; }
-
-      inline const T* operator->() const   { return _ptr; }
-
-      inline bool operator!() const   { return _ptr==0L; }
-
-      inline bool valid() const       { return _ptr!=0L; }
-
-      inline T* get() { return _ptr; }
-
-      inline const T* get() const { return _ptr; }
-
-   private:
-      T* _ptr;
-   };
-
 }
 
 #endif
--- trunk/delta3d/inc/dtCore/refptr.h
+++ trunk/delta3d/inc/dtCore/refptr.h
@@ -25,92 +25,28 @@
 //
 //////////////////////////////////////////////////////////////////////
 
+#include <osg/ref_ptr>
 #include <ostream>
 
 namespace dtCore 
 {
    /** Smart pointer for handling referenced counted objects.*/
    template<class T>
-   class RefPtr
+   class RefPtr : public osg::ref_ptr<T>
    {
 
    public:
-      typedef T element_type;
-
-      RefPtr() :_ptr(0L) {}
-      RefPtr(T* t):_ptr(t)              { if (_ptr) _ptr->ref(); }
-      RefPtr(const RefPtr& rp):_ptr(rp._ptr)  { if (_ptr) _ptr->ref(); }
-      ~RefPtr()                           { if (_ptr) _ptr->unref(); _ptr=0; }
-
-      inline RefPtr& operator = (const RefPtr& rp)
-      {
-      if (_ptr==rp._ptr) return *this;
-      T* tmp_ptr = _ptr;
-      _ptr = rp._ptr;
-      if (_ptr) _ptr->ref();
-      // unref second to prevent any deletion of any object which might
-      // be referenced by the other object. i.e rp is child of the
-      // original _ptr.
-      if (tmp_ptr) tmp_ptr->unref();
-      return *this;
-      }
-
-      inline RefPtr& operator = (T* ptr)
-      {
-      if (_ptr==ptr) return *this;
-      T* tmp_ptr = _ptr;
-      _ptr = ptr;
-      if (_ptr) _ptr->ref();
-      // unref second to prevent any deletion of any object which might
-      // be referenced by the other object. i.e rp is child of the
-      // original _ptr.
-      if (tmp_ptr) tmp_ptr->unref();
-      return *this;
-      }
-
-      // comparison operators for RefPtr.
-      inline bool operator == (const RefPtr& rp) const { return (_ptr==rp._ptr); }
-      inline bool operator != (const RefPtr& rp) const { return (_ptr!=rp._ptr); }
-      inline bool operator < (const RefPtr& rp) const { return (_ptr<rp._ptr); }
-      inline bool operator > (const RefPtr& rp) const { return (_ptr>rp._ptr); }
-
-      // comparison operator for const T*.
-      inline bool operator == (const T* ptr) const { return (_ptr==ptr); }
-      inline bool operator != (const T* ptr) const { return (_ptr!=ptr); }
-      inline bool operator < (const T* ptr) const { return (_ptr<ptr); }
-      inline bool operator > (const T* ptr) const { return (_ptr>ptr); }
-
-      //added for by Delta3D
+      RefPtr(): osg::ref_ptr<T>()            {}
+      RefPtr(T* t): osg::ref_ptr<T>(t)       {}
+      RefPtr(const RefPtr& rp):osg::ref_ptr<T>(rp) {}     
+ 
+      // added specifically for Delta3D
       friend inline std::ostream &operator<<(std::ostream &os,
          const RefPtr& rp)
       {
-         os << rp._ptr;
+         os << rp.get();
          return os;
       }
-
-      inline T& operator*()  { return *_ptr; }
-
-      inline const T& operator*() const { return *_ptr; }
-
-      inline T* operator->() { return _ptr; }
-
-      inline const T* operator->() const   { return _ptr; }
-
-      inline bool operator!() const	{ return _ptr==0L; }
-
-      inline bool valid() const	{ return _ptr!=0L; }
-
-      inline T* get() { return _ptr; }
-
-      inline const T* get() const { return _ptr; }
-
-      inline T* take() { return release();}
-
-      inline T* release() { T* tmp=_ptr; if (_ptr) _ptr->unref_nodelete(); _ptr=0; return tmp;}
-
-      private:
-      T* _ptr;
-     
    };
 }
 
