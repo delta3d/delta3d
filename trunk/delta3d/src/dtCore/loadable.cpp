65e766f407823aab1ad0464cb6767d41d84fd6c9
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2006-11-28 16:50:42 +0000

    removed buggy CopyOp code in loadable, removed some cruft from the game manager, and added a warning message to stage when libraries can't be loaded from a map.

--- trunk/delta3d/src/dtCore/loadable.cpp
+++ trunk/delta3d/src/dtCore/loadable.cpp
@@ -27,7 +27,7 @@ Loadable::~Loadable(void)
 * @param filename : The name of the file to be loaded
 * @param useCache : If true, use OSG's object cache
 */
-osg::Node* Loadable::LoadFile(const std::string& filename, bool useCache, unsigned int cacheFlags)
+osg::Node* Loadable::LoadFile(const std::string& filename, bool useCache)
 {   
    mFilename = filename;
    Log::GetInstance().LogMessage(Log::LOG_DEBUG, __FUNCTION__,
@@ -44,14 +44,16 @@ osg::Node* Loadable::LoadFile(const std::string& filename, bool useCache, unsign
       options->setObjectCacheHint(osgDB::ReaderWriter::Options::CACHE_NONE);
    }
 
-
    osg::Node *model = osgDB::readNodeFile(mFilename, options.get());
    if (model != 0)
    {
-      if(useCache)
-      {
-         return static_cast<osg::Node*>(model->clone(osg::CopyOp( cacheFlags ) ));
-      }
+      // this crashes - prolly should be called from the Update traversal
+//      if (mDrawableNode.get()->getNumChildren() != 0)
+//      {
+//         mDrawableNode.get()->removeChild(0,mDrawableNode.get()->getNumChildren() );
+//      }
+
+      //mDrawableNode.get()->addChild( model );
       return model;
    }
    else
