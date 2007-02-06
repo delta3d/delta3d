4d1ed864b14d1d9b9d1eed74757452b82ac8c51f
fretlessmayhem <fretlessmayhem@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-02-06 14:37:43 +0000

    Added includes for Linux nightly build

--- trunk/delta3d/tests/dtUtil/fileutilstests.cpp
+++ trunk/delta3d/tests/dtUtil/fileutilstests.cpp
@@ -22,6 +22,8 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <dtUtil/fileutils.h>
+#include <dtUtil/log.h>
+#include <dtDAL/enginepropertytypes.h>
 #include <dtCore/globals.h>
 
 class FileUtilsTests : public CPPUNIT_NS::TestFixture
