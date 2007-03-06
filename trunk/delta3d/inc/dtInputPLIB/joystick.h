28d999e1ac7094af620aabec1a3258631ee24dc5
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-03-06 14:39:13 +0000

    Fixed some comment formatting in the joystick files.  Also fixed a bug where testInput wouldn't link due to a missing library name in the delta3d.py.  It shouldn't have ever worked... Odd.  Also moved allTests back into the tests directory.

--- trunk/delta3d/inc/dtInputPLIB/joystick.h
+++ trunk/delta3d/inc/dtInputPLIB/joystick.h
@@ -1,22 +1,22 @@
 /* 
-* Delta3D Open Source Game and Simulation Engine 
-* Copyright (C) 2004-2005 MOVES Institute 
-*
-* This library is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Lesser General Public License as published by the Free 
-* Software Foundation; either version 2.1 of the License, or (at your option) 
-* any later version.
-*
-* This library is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
-* details.
-*
-* You should have received a copy of the GNU Lesser General Public License 
-* along with this library; if not, write to the Free Software Foundation, Inc., 
-* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
-*
-*/
+ * Delta3D Open Source Game and Simulation Engine 
+ * Copyright (C) 2004-2005 MOVES Institute 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either version 2.1 of the License, or (at your option) 
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ *
+ */
 
 #ifndef DELTA_INPUT_PLIB
 #define DELTA_INPUT_PLIB
@@ -33,56 +33,56 @@
 namespace dtInputPLIB
 {
    /**
-   * A joystick device.
-   */
+    * A joystick device.
+    */
    class DT_INPUT_PLIB_EXPORT Joystick : public dtCore::InputDevice
    {
       DECLARE_MANAGEMENT_LAYER(Joystick)
 
-   public:
+      public:
 
-      /**
-      * Creates instances of Joystick corresponding to each
-      * connected joystick device.
-      */
-      static void CreateInstances();
+         /**
+          * Creates instances of Joystick corresponding to each
+          * connected joystick device.
+          */
+         static void CreateInstances();
 
-      /**
-      * Destroys all Joystick instances.
-      */
-      static void DestroyInstances();
+         /**
+          * Destroys all Joystick instances.
+          */
+         static void DestroyInstances();
 
-      /**
-      * Polls all Joystick instances.
-      */
-      static void PollInstances();
+         /**
+          * Polls all Joystick instances.
+          */
+         static void PollInstances();
 
-      /**
-      * Manually polls the state of this joystick, updating
-      * all of its features.
-      */
-      void Poll();
+         /**
+          * Manually polls the state of this joystick, updating
+          * all of its features.
+          */
+         void Poll();
 
 
-   private:
+      private:
 
-      /**
-      * Constructor.
-      *
-      * @param name the instance name
-      * @param joystick the underlying PLIB joystick object
-      */
-      Joystick(std::string name, jsJoystick* joystick);
+         /**
+          * Constructor.
+          *
+          * @param name the instance name
+          * @param joystick the underlying PLIB joystick object
+          */
+         Joystick(std::string name, jsJoystick* joystick);
 
-      /**
-      * Destructor.
-      */
-      virtual ~Joystick();
+         /**
+          * Destructor.
+          */
+         virtual ~Joystick();
 
-      /**
-      * The underlying PLIB joystick object.
-      */
-      jsJoystick* mJoystick;
+         /**
+          * The underlying PLIB joystick object.
+          */
+         jsJoystick* mJoystick;
    };
 }
 
