5384ff02d624347c61eb9944dd2760378f586017
fretlessmayhem <fretlessmayhem@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-04-12 13:53:54 +0000

    Removed a weird C cast where the code did the following : QString var = (const char*) NULL; which is unnecessary since QStrings are init'd to "" by default. Changed a static global string to to be a static global const std::string&. Changed "" includes to <>.

--- delta3d/utilities/editor/inc/dtEditQt/actortypetreewidget.h
+++ delta3d/utilities/editor/inc/dtEditQt/actortypetreewidget.h
@@ -26,69 +26,69 @@
 #include <QtGui/QTreeWidget>
 #include <dtDAL/actortype.h>
 
-namespace dtEditQt {
-
-
+namespace dtEditQt 
+{
     /**
-    * This class is the tree widget that is used to draw the actor type.  The internal 
-    * nodes represent categories.  The leaf nodes represent actor types.  They should
-    * be drawn differently and only leaf nodes can be created or selected (in theory).
-    */
-    class ActorTypeTreeWidget : public QTreeWidgetItem {
-    public:
-        static std::string CATEGORY_SEPARATOR;
+     * This class is the tree widget that is used to draw the actor type.  The internal 
+     * nodes represent categories.  The leaf nodes represent actor types.  They should
+     * be drawn differently and only leaf nodes can be created or selected (in theory).
+     */
+    class ActorTypeTreeWidget : public QTreeWidgetItem 
+    {
+      public:
+            static const std::string &CATEGORY_SEPARATOR;
 
         /**
-        * Constructor - Use this for leaf nodes.  These should be actor type and can 
-        * be created, selected, etc...
-        *
-        * @param parent The parent of this tree widget 
-        * @param actorType The Actor type of this tree entry. 
-        */
+         * Constructor - Use this for leaf nodes.  These should be actor type and can 
+         * be created, selected, etc...
+         *
+         * @param parent The parent of this tree widget 
+         * @param actorType The Actor type of this tree entry. 
+         */
         ActorTypeTreeWidget(ActorTypeTreeWidget* parent, dtCore::RefPtr<dtDAL::ActorType> actorType);
 
         /**
-        * Constructor - Use this for internal, NON leaf nodes.  These are considered to be 
-        * category placeholders and should not be creatable.
-        *
-        * @param parent The parent of this tree widget 
-        * @param str The display string for this category (ie, 'dtCore', 'Vehicles', 'Trucks')
-        *   You should only pass in the single segement, 'vehicles' not 'stuff.vehicles'
-        */
+         * Constructor - Use this for internal, NON leaf nodes.  These are considered to be 
+         * category placeholders and should not be creatable.
+         *
+         * @param parent The parent of this tree widget 
+         * @param str The display string for this category (ie, 'dtCore', 'Vehicles', 'Trucks')
+         *   You should only pass in the single segement, 'vehicles' not 'stuff.vehicles'
+         */
         ActorTypeTreeWidget(ActorTypeTreeWidget* parent, const QString &str);
 
         /**
-        * Constructor - The root node needs a separate constructor
-        *
-        * @param parent The parent of this tree widget 
-        * @param str The display string for this category (ie, 'dtCore', 'Vehicles', 'Trucks')
-        *   You should only pass in the single segement, 'vehicles' not 'stuff.vehicles'
-        */
+         * Constructor - The root node needs a separate constructor
+         *
+         * @param parent The parent of this tree widget 
+         * @param str The display string for this category (ie, 'dtCore', 'Vehicles', 'Trucks')
+         *   You should only pass in the single segement, 'vehicles' not 'stuff.vehicles'
+         */
         ActorTypeTreeWidget(QTreeWidget* parent, const QString &str);
 
         /**
-        * Gets the actor type for this node.  If it's an internal node, this should be null.
-        * A leaf node should always have an actor type.
-        *
-        * @return The actor type for this leaf node
-        */
+         * Gets the actor type for this node.  If it's an internal node, this should be null.
+         * A leaf node should always have an actor type.
+         *
+         * @return The actor type for this leaf node
+         */
         dtCore::RefPtr<dtDAL::ActorType> getActorType();
 
         /**
-        * Returns true if this is a leaf node. You can check this by checking the actor type for NULL.
-        *
-        * @return True if this is a leaf node and therefore a valid actor type
-        */
+         * Returns true if this is a leaf node. You can check this by checking the actor type for NULL.
+         *
+         * @return True if this is a leaf node and therefore a valid actor type
+         */
         bool isLeafNode();
 
         /**
-        * Gets the category segment.  The category segment is used to draw internal 
-        * nodes.  For leaf nodes, this is null.
-        *
-        * @return The category segment. NULL if this is a leaf node, otherwise, the string segment 
-        *   that represents just the internal section - ie, if this was the node at 
-        *   'stuff.vehicles.cars', then this method would only return 'cars'.
-        */
+         * Gets the category segment.  The category segment is used to draw internal 
+         * nodes.  For leaf nodes, this is null.
+         *
+         * @return The category segment. NULL if this is a leaf node, otherwise, the string segment 
+         *  that represents just the internal section - ie, if this was the node at 
+         *  'stuff.vehicles.cars', then this method would only return 'cars'.
+         */
         const QString &getCategorySegment();
 
         /**
@@ -98,20 +98,20 @@ namespace dtEditQt {
         QString getCategoryOrName();
 
         /**
-        * This recursive function attempts to build up the appropriate parents and 
-        * child nodes to make an actor type and it's categories fit into the tree.
-        * It takes the categories as a QStringList mutable iterator. It pops the top off
-        * and tries to find it.  If it does, then it adds the rest of hte iterators to that 
-        * item, recursively.  If it does not find a match, then it creates a new internal node
-        * and adds the actor to that node recursively.  If the iterator list is empty, then 
-        * it assumes it is at the right level and just adds it to this node.
-        * 
-        * This returns true if it was successfully added.  False means some error occured.
-        * 
-        * @param listIterator The iterator that we're working with.  This WILL be modified
-        * @param actorType The actor type we're trying to insert
-        * @return True if we succcessfully added it, false if some error or already exists.
-        */
+         * This recursive function attempts to build up the appropriate parents and 
+         * child nodes to make an actor type and it's categories fit into the tree.
+         * It takes the categories as a QStringList mutable iterator. It pops the top off
+         * and tries to find it.  If it does, then it adds the rest of hte iterators to that 
+         * item, recursively.  If it does not find a match, then it creates a new internal node
+         * and adds the actor to that node recursively.  If the iterator list is empty, then 
+         * it assumes it is at the right level and just adds it to this node.
+         * 
+         * This returns true if it was successfully added.  False means some error occured.
+         * 
+         * @param listIterator The iterator that we're working with.  This WILL be modified
+         * @param actorType The actor type we're trying to insert
+         * @return True if we succcessfully added it, false if some error or already exists.
+         */
         bool recursivelyAddCategoryAndActorTypeAsChildren(
             QMutableStringListIterator *listIterator, dtCore::RefPtr<dtDAL::ActorType> actorType);
 
