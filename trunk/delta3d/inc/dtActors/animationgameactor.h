0adb6b2f61695c44b775f2b637195fe5b3270f87
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-04-18 20:17:15 +0000

    comment indentation was out of whack, so I reindented the files.  It didn't seem to change TOO much.  Hope this doesn't mess anyone up...

--- trunk/delta3d/inc/dtActors/animationgameactor.h
+++ trunk/delta3d/inc/dtActors/animationgameactor.h
@@ -1,23 +1,23 @@
 /*
-* Delta3D Open Source Game and Simulation Engine
-* Copyright (C) 2006, Alion Science and Technology
-*
-* This library is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Lesser General Public License as published by the Free
-* Software Foundation; either version 2.1 of the License, or (at your option)
-* any later version.
-*
-* This library is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-* details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this library; if not, write to the Free Software Foundation, Inc.,
-* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*
-* Michael Guerrero
-*/
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2006, Alion Science and Technology
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Michael Guerrero
+ */
 #ifndef DELTA_ANIMATIONGAMEACTOR_H
 #define DELTA_ANIMATIONGAMEACTOR_H
 
@@ -36,117 +36,117 @@ namespace osg   { class Geode;         }
 namespace dtActors
 {
    /**
-   * This class is the game actor for an animated model.
-   * @see GameActor
-   * @see AnimationGameActorProxy  
-   */
+    * This class is the game actor for an animated model.
+    * @see GameActor
+    * @see AnimationGameActorProxy  
+    */
    class DT_PLUGIN_EXPORT AnimationGameActor : public dtGame::GameActor
    {
-   public:
-      /// string constants for this actor
-      struct DT_PLUGIN_EXPORT PropertyNames
-      {
-         static const std::string ANIMATION_GROUP;
-         static const std::string ANIMATION_GROUP_LABEL;
-         static const std::string ANIMATION_BLEND_GROUP;
-         static const std::string ANIMATION_BLEND_WEIGHT;
-         static const std::string ANIMATION_BLEND_ID;
-         static const std::string ANIMATION_BLEND_DELAY;
-      };
+      public:
+         /// string constants for this actor
+         struct DT_PLUGIN_EXPORT PropertyNames
+         {
+            static const std::string ANIMATION_GROUP;
+            static const std::string ANIMATION_GROUP_LABEL;
+            static const std::string ANIMATION_BLEND_GROUP;
+            static const std::string ANIMATION_BLEND_WEIGHT;
+            static const std::string ANIMATION_BLEND_ID;
+            static const std::string ANIMATION_BLEND_DELAY;
+         };
 
-      /**
-      * Constructs a AnimationGameActor actor.
-      * @param proxy The actor proxy owning this task actor.     
-      */
-      AnimationGameActor(dtGame::GameActorProxy &proxy);   
+         /**
+          * Constructs a AnimationGameActor actor.
+          * @param proxy The actor proxy owning this task actor.     
+          */
+         AnimationGameActor(dtGame::GameActorProxy &proxy);   
 
-      /**
-      * Loads a model file.
-      * @param modelFile The filename of the model to load.
-      */
-      virtual void SetModel(const std::string &modelFile);
+         /**
+          * Loads a model file.
+          * @param modelFile The filename of the model to load.
+          */
+         virtual void SetModel(const std::string &modelFile);
 
-      /**
-      * Called when the actor has been added to the game manager.
-      */
-      virtual void AddedToScene(dtCore::Scene* scene);     
+         /**
+          * Called when the actor has been added to the game manager.
+          */
+         virtual void AddedToScene(dtCore::Scene* scene);     
 
-      void ApplyAnimationGroup(const dtDAL::NamedGroupParameter& prop);
-      dtCore::RefPtr<dtDAL::NamedGroupParameter> MakeAnimationGroup();
+         void ApplyAnimationGroup(const dtDAL::NamedGroupParameter& prop);
+         dtCore::RefPtr<dtDAL::NamedGroupParameter> MakeAnimationGroup();
 
-      const dtAnim::Cal3DAnimator* GetAnimator() const;
+         const dtAnim::Cal3DAnimator* GetAnimator() const;
 
-      //virtual void TickRemote(const dtGame::Message& tickMessage);
-      //virtual void TickLocal(const dtGame::Message& tickMessage);
-      virtual void OnMessage(dtCore::Base::MessageData *data);
+         //virtual void TickRemote(const dtGame::Message& tickMessage);
+         //virtual void TickLocal(const dtGame::Message& tickMessage);
+         virtual void OnMessage(dtCore::Base::MessageData *data);
 
-   protected:      
+      protected:      
 
-      dtCore::RefPtr<osg::Geode>            mModelGeode;
-      dtAnim::Cal3DLoader                  *mModelLoader;
-      dtCore::RefPtr<dtAnim::Cal3DAnimator> mAnimator;      
+         dtCore::RefPtr<osg::Geode>            mModelGeode;
+         dtAnim::Cal3DLoader                  *mModelLoader;
+         dtCore::RefPtr<dtAnim::Cal3DAnimator> mAnimator;      
 
-      /// Destroys this actor.     
-      virtual ~AnimationGameActor();    
+         /// Destroys this actor.     
+         virtual ~AnimationGameActor();    
 
-   private:
-     
+      private:
+
    };
 
    /**
-   * This class is the proxy for an animated model game object.
-   * @see GameActorProxy
-   * @see AnimationGameActor  
-   */
+    * This class is the proxy for an animated model game object.
+    * @see GameActorProxy
+    * @see AnimationGameActor  
+    */
    class DT_PLUGIN_EXPORT AnimationGameActorProxy : public dtGame::GameActorProxy
    {
-   public:
+      public:
 
-      /**
-      * Constructs the proxy.
-      */
-      AnimationGameActorProxy();
+         /**
+          * Constructs the proxy.
+          */
+         AnimationGameActorProxy();
 
-      /**
-      * Builds the property map for the task actor proxy.  These properties
-      * wrap the specified properties located in the actor.
-      */
-      virtual void BuildPropertyMap();
+         /**
+          * Builds the property map for the task actor proxy.  These properties
+          * wrap the specified properties located in the actor.
+          */
+         virtual void BuildPropertyMap();
 
-      /**
-      * Registers any invokables used by the proxy.  The invokables
-      * allow the actor to hook into the game manager messages system.
-      */
-      virtual void BuildInvokables();
+         /**
+          * Registers any invokables used by the proxy.  The invokables
+          * allow the actor to hook into the game manager messages system.
+          */
+         virtual void BuildInvokables();
 
-      /**
-      * Gets the method by which this static mesh is rendered. This is used by STAGE.
-      * @return If there is no geometry currently assigned, this
-      *  method will return RenderMode::DRAW_BILLBOARD_ICON.  If
-      *  there is geometry assigned to this static mesh, RenderMode::DRAW_ACTOR
-      *  is returned.
-      */
-      virtual const dtDAL::ActorProxy::RenderMode& GetRenderMode();
+         /**
+          * Gets the method by which this static mesh is rendered. This is used by STAGE.
+          * @return If there is no geometry currently assigned, this
+          *  method will return RenderMode::DRAW_BILLBOARD_ICON.  If
+          *  there is geometry assigned to this static mesh, RenderMode::DRAW_ACTOR
+          *  is returned.
+          */
+         virtual const dtDAL::ActorProxy::RenderMode& GetRenderMode();
 
-      /**
-      * Gets the billboard used to represent static mesh if this proxy's
-      * render mode is RenderMode::DRAW_BILLBOARD_ICON. Used by STAGE.
-      * @return
-      */
-      virtual dtDAL::ActorProxyIcon* GetBillBoardIcon();
+         /**
+          * Gets the billboard used to represent static mesh if this proxy's
+          * render mode is RenderMode::DRAW_BILLBOARD_ICON. Used by STAGE.
+          * @return
+          */
+         virtual dtDAL::ActorProxyIcon* GetBillBoardIcon();
 
-   protected:
+      protected:
 
-      /// Destroys the proxy.
-      virtual ~AnimationGameActorProxy();
+         /// Destroys the proxy.
+         virtual ~AnimationGameActorProxy();
 
-      /**
-      * Called by the game manager during creation of the proxy.  This method
-      * creates the real actor and returns it.
-      */
-      virtual void CreateActor();
+         /**
+          * Called by the game manager during creation of the proxy.  This method
+          * creates the real actor and returns it.
+          */
+         virtual void CreateActor();
 
-   private:
+      private:
    };
 
 }
