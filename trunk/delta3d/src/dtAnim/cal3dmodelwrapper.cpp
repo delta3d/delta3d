5f59904da55543ad2113cdca3bfced5e7563ce49
mjguerre <mjguerre@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-05-01 21:02:55 +0000

    fixed bug in GetCoreAnimationKeyframeCount that returned the summed total of keyframes for every track instead of the number of keyframes per track which is the desired functionality

--- trunk/delta3d/src/dtAnim/cal3dmodelwrapper.cpp
+++ trunk/delta3d/src/dtAnim/cal3dmodelwrapper.cpp
@@ -238,9 +238,18 @@ int Cal3DModelWrapper::GetParentBoneID(unsigned int boneID) const
    return NULL_BONE;
 }
 
+
+/// This function assumes that every track in an animation has the same number of keyframes
 unsigned int Cal3DModelWrapper::GetCoreAnimationKeyframeCount( int animID ) const
 {
-   return mCalModel->getCoreModel()->getCoreAnimation(animID)->getTotalNumberOfKeyframes();
+   CalCoreAnimation *pAnimation = mCalModel->getCoreModel()->getCoreAnimation(animID);
+   assert(pAnimation);
+
+   int numberOfTracks = pAnimation->getTrackCount();
+   int totalKeyFrames = pAnimation->getTotalNumberOfKeyframes();   
+   assert((totalKeyFrames % numberOfTracks) == 0);
+
+   return totalKeyFrames / numberOfTracks;
 }
 
 float Cal3DModelWrapper::GetCoreAnimationDuration( int animID ) const
