2a6199f0bcf5f167ca3d5c469dcbad8f02c443fd
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-05-09 19:58:37 +0000

    added to files I missed in the last commit.

---
+++ trunk/delta3d/inc/dtHLAGM/ddmcamerageographiccalculator.h
@@ -0,0 +1,46 @@
+#ifndef DELTA_DDM_CAMERA_GEOGRAPHIC_CALCULATOR
+#define DELTA_DDM_CAMERA_GEOGRAPHIC_CALCULATOR
+
+#include <dtCore/refptr.h>
+#include <dtHLAGM/export.h>
+#include <dtHLAGM/ddmcalculatorgeographic.h>
+
+namespace dtCore
+{
+   class Camera;
+}
+
+namespace dtHLAGM
+{
+   /**
+    * This calculator is intended for generating subscription regions based on the view of the camera.
+    */
+   class DDMCameraGeographicCalculator : public dtHLAGM::DDMCalculatorGeographic
+   {
+      public:
+         
+         typedef dtHLAGM::DDMCalculatorGeographic BaseClass;
+         
+         DDMCameraGeographicCalculator();
+         
+         /// Sets the camera this calculator is using for generating the region.
+         void SetCamera(dtCore::Camera* camera);
+
+         /// @return the camera this calculator is using for generating the region.
+         dtCore::Camera* GetCamera();
+                  
+         /**
+          * Updates the subscription region 
+          * @param region the region to update.
+          */
+         virtual bool UpdateRegionData(DDMRegionData& ddmData) const;
+
+      protected:
+         virtual ~DDMCameraGeographicCalculator();
+         
+      private:
+         dtCore::RefPtr<dtCore::Camera> mCamera;
+   };
+}
+
+#endif /*DELTA_DDM_CAMERA_GEOGRAPHIC_CALCULATOR*/
