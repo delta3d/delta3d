8998cc79561160e86dc9ef6fe44f9472ae45db18
banderegg <banderegg@users.noreply.github.com>
2007-05-23 17:25:28 +0000

    +added performance test for the animation component

--- delta3d/tests/dtAnim/animationcomponenttests.cpp
+++ delta3d/tests/dtAnim/animationcomponenttests.cpp
@@ -20,16 +20,24 @@
 #include <prefix/dtgameprefix-src.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <dtUtil/log.h>
+#include <dtUtil/stringutils.h>
+
 #include <dtAnim/animationcomponent.h>
 #include <dtAnim/animationhelper.h>
 
 #include <dtCore/refptr.h>
 #include <dtCore/system.h>
 #include <dtCore/scene.h>
+#include <dtCore/globals.h>
+#include <dtCore/timer.h>
 
 #include <dtGame/gamemanager.h>
 
+#include <dtDAL/map.h>
+#include <dtDAL/project.h>
 
+#include <dtActors/animationgameactor2.h>
 #include <dtActors/engineactorregistry.h>
 
 #include <string>
@@ -58,6 +66,7 @@ namespace dtAnim
    {
       CPPUNIT_TEST_SUITE( AnimationComponentTests );
          CPPUNIT_TEST( TestAnimationComponent );
+         CPPUNIT_TEST( TestAnimationPerformance );
       CPPUNIT_TEST_SUITE_END();
 
       public:
@@ -65,9 +74,11 @@ namespace dtAnim
          void tearDown();
 
          void TestAnimationComponent(); 
+         void TestAnimationPerformance();
 
       private:
 
+         dtCore::RefPtr<dtUtil::Log> mLogger;
          dtCore::RefPtr<GameManager> mGM;
          dtCore::RefPtr<AnimationComponent> mAnimComp;
          dtCore::RefPtr<GameActorProxy> mTestGameActor;
@@ -79,6 +90,8 @@ namespace dtAnim
 
    void AnimationComponentTests::setUp()
    {
+      mLogger = &dtUtil::Log::GetInstance("animationcomponenttests.cpp");
+
       dtCore::System::GetInstance().SetShutdownOnWindowClose(false);
       dtCore::System::GetInstance().Start();
       mGM = new GameManager(*new dtCore::Scene());
@@ -118,5 +131,129 @@ namespace dtAnim
       CPPUNIT_ASSERT(helper->HasBeenUpdated());
    }
 
+   void AnimationComponentTests::TestAnimationPerformance()
+   {      
+      typedef std::vector<dtCore::RefPtr<dtDAL::ActorProxy> > ProxyContainer;
+      ProxyContainer proxies;
+
+      //load map
+      try
+      {
+         std::string context = dtCore::GetDeltaRootPath() + "/tests/data/ProjectContext";
+         dtDAL::Project::GetInstance().SetContext(context, true);
+         mGM->ChangeMap("AnimationPerformance");
+         
+         dtDAL::Project::GetInstance().GetMap("AnimationPerformance").FindProxies(proxies, "CharacterEntity");      
+      
+      }
+      catch (dtUtil::Exception &e)
+      {
+         CPPUNIT_FAIL(e.ToString());
+      }
+
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Performance testing AnimationComponent with " + dtUtil::ToString(proxies.size()) + " animated entities.");
+      
+      //register animation actors
+      ProxyContainer::iterator iter, end;
+      for(iter = proxies.begin(), end = proxies.end(); iter != end; ++iter)
+      {
+         dtGame::GameActorProxy* gameProxy = dynamic_cast<dtGame::GameActorProxy*>((*iter).get());
+         if(gameProxy)
+         {
+            dtActors::AnimationGameActor2* actor = dynamic_cast<dtActors::AnimationGameActor2*>(&gameProxy->GetGameActor());
+
+            if(actor)
+            { 
+               mAnimComp->RegisterActor(*gameProxy, *actor->GetHelper());               
+            }
+    
+         }
+      }
    
+
+      //lets do some performance testing
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Testing performance of PlayAnimation on AnimationHelper");
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      dtCore::Timer timer;
+      dtCore::Timer_t timerStart, timerEnd;
+
+      timerStart = timer.Tick();
+      for(iter = proxies.begin(), end = proxies.end(); iter != end; ++iter)
+      {
+         dtGame::GameActorProxy* gameProxy = dynamic_cast<dtGame::GameActorProxy*>((*iter).get());
+         if(gameProxy)
+         {
+            dtActors::AnimationGameActor2* actor = dynamic_cast<dtActors::AnimationGameActor2*>(&gameProxy->GetGameActor());
+
+            if(actor)
+            { 
+               actor->GetHelper()->PlayAnimation("Walk");
+            }      
+         } 
+      }
+      timerEnd = timer.Tick();
+
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Time Results for Play Animation: " + dtUtil::ToString(timer.DeltaMil(timerStart, timerEnd)) + " ms");
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      int numUpdates = 60;
+      float updateTime = 1.0f / 60.0f;
+      dtCore::RefPtr<dtGame::Message> message = mGM->GetMessageFactory().CreateMessage(dtGame::MessageType::TICK_LOCAL);
+
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Testing performance of " + dtUtil::ToString(numUpdates) + " updates on AnimationComponent");
+
+      timerStart = timer.Tick();
+      for(int i = 0; i < numUpdates; ++i)
+      {
+         mAnimComp->ProcessMessage(*message.get());
+      }
+      timerEnd = timer.Tick();
+
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Time Results for Update: " + dtUtil::ToString(timer.DeltaMil(timerStart, timerEnd)) + " ms");
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Testing performance of ForceFadeOut()");
+
+      timerStart = timer.Tick();
+      for(iter = proxies.begin(), end = proxies.end(); iter != end; ++iter)
+      {
+         dtGame::GameActorProxy* gameProxy = dynamic_cast<dtGame::GameActorProxy*>((*iter).get());
+         if(gameProxy)
+         {
+            dtActors::AnimationGameActor2* actor = dynamic_cast<dtActors::AnimationGameActor2*>(&gameProxy->GetGameActor());
+
+            if(actor)
+            { 
+               actor->GetHelper()->ClearAnimation("Walk", 0.0);
+            }      
+         }
+      }
+      timerEnd = timer.Tick();
+
+      mLogger->LogMessage(dtUtil::Log::LOG_ALWAYS, __FUNCTION__, __LINE__, "Time Results for Play Animation: " + dtUtil::ToString(timer.DeltaMil(timerStart, timerEnd)) + " ms");
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      
+      //close map
+      try
+      {
+         if(!mGM->GetCurrentMap().empty())
+         {
+            dtDAL::Project::GetInstance().CloseMap(dtDAL::Project::GetInstance().GetMap(mGM->GetCurrentMap()), true);
+            dtDAL::Project::GetInstance().DeleteMap(dtDAL::Project::GetInstance().GetMap(mGM->GetCurrentMap()));
+         }
+      }
+      catch(dtUtil::Exception& e)
+      {
+         CPPUNIT_FAIL(e.ToString());
+      }
+    
 }
+
+}//namespace dtAnim
