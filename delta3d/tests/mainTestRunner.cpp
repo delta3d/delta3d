f1a5686212355f831d77d1f3a66e27a2f11c516a
cmmurphy <cmmurphy@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-05-25 14:54:29 +0000

    Modified the unit test so you can now pass in a suite name and a test name to just run one suite or just a single test.  Run the unit tests and pass in args, like below to run just the TestMachineInfo test.
    GameManagerTests GameManagerTests::TestMachineInfo

--- delta3d/tests/mainTestRunner.cpp
+++ delta3d/tests/mainTestRunner.cpp
@@ -81,6 +81,16 @@ class TimingListener : public CppUnit::TestListener
 int main(int argc, char* argv[])
 {
    const std::string &executable = argv[0];
+   std::string singleSuiteName = "";
+   std::string singleTestName = "";
+
+   // Get the test name from args.
+   if (argc > 1)
+      singleSuiteName = argv[1];
+   if (argc > 2)
+      singleTestName = argv[2];
+
+   // We need to change our directory based on the executable
    dtUtil::FileInfo info = dtUtil::FileUtils::GetInstance().GetFileInfo(executable);
    if(info.fileType == dtUtil::FILE_NOT_FOUND)
    {
@@ -89,6 +99,7 @@ int main(int argc, char* argv[])
    }
    else
    {
+      std::string path = info.path;
       LOG_ALWAYS(std::string("Changing to directory \"") + info.path + "\".");
 
       try 
@@ -102,37 +113,100 @@ int main(int argc, char* argv[])
       }
    }
 
-   dtCore::Timer testsClock;
-   dtCore::Timer_t testsTimerStart = testsClock.Tick(); 
-
-   CPPUNIT_NS::TestResult testResult;
-
    CPPUNIT_NS::TestResultCollector collectedResults;
-   testResult.addListener(&collectedResults);
 
-   TimingListener timelistener;
-   testResult.addListener(&timelistener);
-
-   CPPUNIT_NS::TestRunner testRunner;
-   testRunner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-   testRunner.run(testResult);
-
-   CPPUNIT_NS::CompilerOutputter compilerOutputter(&collectedResults,std::cerr);
-   compilerOutputter.write();
-
-   // print out slow tests and total time.
-   dtCore::Timer_t testsTimerStop = testsClock.Tick();
-   double timeDelta = testsClock.DeltaSec(testsTimerStart, testsTimerStop);
-   timeDelta = (floor(timeDelta * 10000.0)) / 10000.0; // force data truncation
-   if(!mSlowTests.str().empty())
+   try
    {
-      std::cerr << " <<< SLOW TEST RESULTS ::: START >>> " << std::endl << 
-         mSlowTests.str() << " <<< SLOW TEST RESULTS ::: END ::: TotalTime[" << 
-         timeDelta << "] >>> " << std::endl;
-   }
-   else 
-      std::cerr << " <<< SLOW TEST RESULTS ::: ALL TESTS RAN FAST!!! WOOT! ::: TotalTime[" << timeDelta << "] >>> " << std::endl;
+      dtCore::Timer testsClock;
+      dtCore::Timer_t testsTimerStart = testsClock.Tick(); 
 
+      CPPUNIT_NS::TestResult testResult;   
+      testResult.addListener(&collectedResults);
+      TimingListener timelistener;
+      testResult.addListener(&timelistener);
+
+      // setup the test runner - does all the work
+      CPPUNIT_NS::TestRunner testRunner;
+      CPPUNIT_NS::Test *fullTestSuite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+
+      // Check for a specific test suite - if passed in, we only run the one suite
+      if (singleSuiteName != "")
+      {
+         LOG_ALWAYS(std::string("Single suite name detected in arguments. Attempting to load a single test named [") + singleSuiteName + std::string("]."));
+         CPPUNIT_NS::Test *suiteTest = fullTestSuite->findTest(singleSuiteName);
+         if (suiteTest == NULL)
+         {
+            std::cerr << " *** FAILED to find test suite named [" << singleSuiteName << 
+               "]. Please check suite name. The name should match what was used in the registration line, " <<
+               "\'CPPUNIT_TEST_SUITE(MyTests)\' would be \'MyTests\'. Aborting test." <<  std::endl;
+         }
+         // Check for a specific test within the specific suite
+         else if (singleTestName != "")
+         {
+            LOG_ALWAYS(std::string("Individual test name detected in arguments. Attempting to load test [") + singleTestName + std::string("]."));
+            CPPUNIT_NS::Test *individualTest = suiteTest->findTest(singleTestName);
+            if (individualTest == NULL)
+            {
+               std::cerr << " *** FAILED to individual test [" << singleTestName << 
+                  "] inside suite [" << singleSuiteName << "]. Please check suite name. " <<
+                  "The name should match what was used in the registration line, " <<
+                  "\'CPPUNIT_TEST(TestFunction)\' would be \'TestFunction\'. Aborting test." <<  std::endl;
+            }
+            else 
+            {
+               LOG_ALWAYS(std::string("   *** Found test suite and single test[ ") + 
+                  singleTestName + std::string("].  Starting run."));
+               testRunner.addTest(individualTest);
+            }
+         }
+         // We found the single suite, no individual test
+         else
+         {
+            LOG_ALWAYS(std::string("Found single test suite. Starting run."));
+            testRunner.addTest(suiteTest);
+         }
+      }
+      else 
+      {
+         LOG_ALWAYS(std::string("No arguments detected.  Running all tests!  Pass the suite name as 1st arg to run a single suite. For single test, pass test name as 2nd arg."));
+         testRunner.addTest(fullTestSuite);
+      }
+
+      // Go to it!!!  Run this puppy!
+      testRunner.run(testResult);
+
+      CPPUNIT_NS::CompilerOutputter compilerOutputter(&collectedResults,std::cerr);
+      compilerOutputter.write();
+
+      // print out slow tests and total time.
+      dtCore::Timer_t testsTimerStop = testsClock.Tick();
+      double timeDelta = testsClock.DeltaSec(testsTimerStart, testsTimerStop);
+      timeDelta = (floor(timeDelta * 10000.0)) / 10000.0; // force data truncation
+      if(!mSlowTests.str().empty())
+      {
+         std::cerr << " <<< SLOW TEST RESULTS ::: START >>> " << std::endl << 
+            mSlowTests.str() << " <<< SLOW TEST RESULTS ::: END ::: TotalTime[" << 
+            timeDelta << "] >>> " << std::endl;
+      }
+      else 
+         std::cerr << " <<< SLOW TEST RESULTS ::: ALL TESTS RAN FAST!!! WOOT! ::: TotalTime[" << timeDelta << "] >>> " << std::endl;
+   }
+   catch (std::invalid_argument &ie)
+   {
+      std::cerr << " <<< Invalid argument occurred. Likely, the suite name or test name are invalid or not found. " << 
+         " For tests, be sure to include the class name like [MyClass::TestStuff]. Or, see cppunit.sourceforge.net for more info.  Error: [" <<
+         ie.what() << "]. >>> " << std::endl;
+   }
+   catch (std::exception &e)
+   {
+      std::cerr << " <<< Exception occurred. Error: [" << e.what() << "]. >>> " << std::endl;
+   }
+   catch (...)
+   {
+      std::cerr << " <<< Exception occurred while running main.cpp for this unit test. No other info available >>> " << std::endl;
+   }
+
+   //dtAudio::AudioManager::Destroy();
    return collectedResults.wasSuccessful () ? 0 : 1;
 }
 
