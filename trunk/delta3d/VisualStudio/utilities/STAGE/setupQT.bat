25d79bf85bb458021a72dde94b687aaf32037a5d
fretlessmayhem <fretlessmayhem@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-06-06 19:03:19 +0000

    Fixed a bug in setupQT.bat where it was generating Debug moc files in release mode. The mode debug or release can now be specified on the command line. Updated pre build steps in the project file to reflect this.

--- trunk/delta3d/VisualStudio/utilities/STAGE/setupQT.bat
+++ trunk/delta3d/VisualStudio/utilities/STAGE/setupQT.bat
@@ -1,13 +1,16 @@
 @echo off
+:: Import environment variables for Visual Studio.
 
-REM Import environment variables for Visual Studio.
 call vcvars32.bat
 
-REM Tell qmake to build the Makefile which will be used to generate the sources.
+:: Tell qmake to build the Makefile which will be used to generate the sources.
+
 call %QTDIR%\bin\qmake -makefile editor.pro
 
-REM Now, since we will be actually building the application using Visual Studio,
-REM we only use the Makefile to generate the MOC sources.  Since these are added
-REM to the VC++ project, they will get included in the final build.
-call nmake debug-mocables
+:: Now, since we will be actually building the application using Visual Studio,
+:: we only use the Makefile to generate the MOC sources.  Since these are added
+:: to the VC++ project, they will get included in the final build.
+
+if "%1" == "debug" call nmake debug-mocables
+if "%1" == "release" call nmake release-mocables
 
