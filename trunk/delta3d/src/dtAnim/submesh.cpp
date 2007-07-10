e9be46d47d0e80e723beb4f4b21b69aa72c55be5
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-07-10 19:05:16 +0000

    changed line endings to native, and changed a magic string for a shader uniform to a constant in animnodebuilder.

--- trunk/delta3d/src/dtAnim/submesh.cpp
+++ trunk/delta3d/src/dtAnim/submesh.cpp
@@ -10,102 +10,102 @@ using namespace dtAnim;
 class SubMeshDirtyCallback: public osg::Geometry::UpdateCallback 
 {
 public:
-	virtual void update (osg::NodeVisitor *, osg::Drawable *d) 
+   virtual void update (osg::NodeVisitor *, osg::Drawable *d) 
     {
-		d->dirtyBound();
-	}
+      d->dirtyBound();
+   }
 };
 
 SubMeshDrawable::SubMeshDrawable(Cal3DModelWrapper *wrapper, unsigned mesh, unsigned submesh) 
 {
    this->mWrapper   = wrapper;
-	this->mMeshID    = mesh;
-	this->mSubmeshID = submesh;
+   this->mMeshID    = mesh;
+   this->mSubmeshID = submesh;
 
-	setUseDisplayList(false);
-	setUpMaterial();
+   setUseDisplayList(false);
+   setUpMaterial();
 
-	// initialize arrays, just in case
-	mMeshVertices           = 0;
-	mMeshNormals            = 0;
-	mMeshTextureCoordinates = 0;
-	mMeshFaces              = 0;
+   // initialize arrays, just in case
+   mMeshVertices           = 0;
+   mMeshNormals            = 0;
+   mMeshTextureCoordinates = 0;
+   mMeshFaces              = 0;
 
-	// set the model to LOD 1, so that we have the maximal number of
-	// vertices and faces in order to be able to pre-allocate the arrays
-	mWrapper->SetLODLevel(1);
-	
-	if(mWrapper->BeginRenderingQuery())
-	{
-		if(mWrapper->SelectMeshSubmesh(mesh, submesh))
-		{
-			vertexCount             = mWrapper->GetVertexCount();
-			faceCount               = mWrapper->GetFaceCount();
-			mMeshVertices           = new float[vertexCount*3];
-			mMeshNormals            = new float[vertexCount*3];
-			mMeshTextureCoordinates = new float[vertexCount*2];
-			mMeshFaces              = new int[faceCount*3];
+   // set the model to LOD 1, so that we have the maximal number of
+   // vertices and faces in order to be able to pre-allocate the arrays
+   mWrapper->SetLODLevel(1);
+   
+   if(mWrapper->BeginRenderingQuery())
+   {
+      if(mWrapper->SelectMeshSubmesh(mesh, submesh))
+      {
+         vertexCount             = mWrapper->GetVertexCount();
+         faceCount               = mWrapper->GetFaceCount();
+         mMeshVertices           = new float[vertexCount*3];
+         mMeshNormals            = new float[vertexCount*3];
+         mMeshTextureCoordinates = new float[vertexCount*2];
+         mMeshFaces              = new int[faceCount*3];
 
-			mWrapper->GetFaces(mMeshFaces);
-		}
+         mWrapper->GetFaces(mMeshFaces);
+      }
 
-		mWrapper->EndRenderingQuery();
-	}
-	setUpdateCallback(new SubMeshDirtyCallback());
+      mWrapper->EndRenderingQuery();
+   }
+   setUpdateCallback(new SubMeshDirtyCallback());
 }
 
 SubMeshDrawable::~SubMeshDrawable(void)
 {
-	delete [] mMeshVertices;
-	delete [] mMeshNormals;
-	delete [] mMeshFaces;
-	delete [] mMeshTextureCoordinates;
+   delete [] mMeshVertices;
+   delete [] mMeshNormals;
+   delete [] mMeshFaces;
+   delete [] mMeshTextureCoordinates;
 }
 
 void SubMeshDrawable::setUpMaterial() 
 {
-	osg::StateSet *set = this->getOrCreateStateSet();   
-	
+   osg::StateSet *set = this->getOrCreateStateSet();   
+   
    // select mesh and submesh for further data access
    if(mWrapper->SelectMeshSubmesh(mMeshID, mSubmeshID)) 
    {
-	  osg::Material *material = new osg::Material();
-	  //material->setColorMode(osg::Material::AMBIENT_AND_DIFFUSE);
-	  set->setAttributeAndModes(material, osg::StateAttribute::ON);
-	  unsigned char meshColor[4];
-	  osg::Vec4 materialColor;
+     osg::Material *material = new osg::Material();
+     //material->setColorMode(osg::Material::AMBIENT_AND_DIFFUSE);
+     set->setAttributeAndModes(material, osg::StateAttribute::ON);
+     unsigned char meshColor[4];
+     osg::Vec4 materialColor;
 
-	  // set the material ambient color
-	  mWrapper->GetAmbientColor(&meshColor[0]);
-	  materialColor[0] = meshColor[0] / 255.0f;
+     // set the material ambient color
+     mWrapper->GetAmbientColor(&meshColor[0]);
+     materialColor[0] = meshColor[0] / 255.0f;
      materialColor[1] = meshColor[1] / 255.0f;
      materialColor[2] = meshColor[2] / 255.0f;
      materialColor[3] = meshColor[3] / 255.0f;
-	  //if (materialColor[3] == 0) materialColor[3]=1.0f;
-	  material->setAmbient(osg::Material::FRONT_AND_BACK, materialColor);
+     //if (materialColor[3] == 0) materialColor[3]=1.0f;
+     material->setAmbient(osg::Material::FRONT_AND_BACK, materialColor);
 
-	  // set the material diffuse color
-	  mWrapper->GetDiffuseColor( &meshColor[0] );
-	  materialColor[0] = meshColor[0] / 255.0f;
+     // set the material diffuse color
+     mWrapper->GetDiffuseColor( &meshColor[0] );
+     materialColor[0] = meshColor[0] / 255.0f;
      materialColor[1] = meshColor[1] / 255.0f;
      materialColor[2] = meshColor[2] / 255.0f;
      materialColor[3] = meshColor[3] / 255.0f;
-	  //if (materialColor[3] == 0) materialColor[3]=1.0f;
-	  material->setDiffuse(osg::Material::FRONT_AND_BACK, materialColor);
+     //if (materialColor[3] == 0) materialColor[3]=1.0f;
+     material->setDiffuse(osg::Material::FRONT_AND_BACK, materialColor);
 
-	  // set the material specular color
-	  mWrapper->GetSpecularColor(&meshColor[0]);
-	  materialColor[0] = meshColor[0] / 255.0f;
+     // set the material specular color
+     mWrapper->GetSpecularColor(&meshColor[0]);
+     materialColor[0] = meshColor[0] / 255.0f;
      materialColor[1] = meshColor[1] / 255.0f;
      materialColor[2] = meshColor[2] / 255.0f;
      materialColor[3] = meshColor[3] / 255.0f;
-	  //if (materialColor[3] == 0) materialColor[3]=1.0f;
-	  material->setSpecular(osg::Material::FRONT_AND_BACK, materialColor);
+     //if (materialColor[3] == 0) materialColor[3]=1.0f;
+     material->setSpecular(osg::Material::FRONT_AND_BACK, materialColor);
 
-	  // set the material shininess factor
-	  float shininess;
-	  shininess = mWrapper->GetShininess();
-	  material->setShininess(osg::Material::FRONT_AND_BACK, shininess);
+     // set the material shininess factor
+     float shininess;
+     shininess = mWrapper->GetShininess();
+     material->setShininess(osg::Material::FRONT_AND_BACK, shininess);
 
      if (mWrapper->GetMapCount() > 0)
      {
@@ -114,94 +114,94 @@ void SubMeshDrawable::setUpMaterial()
         {
            set->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
         }
-     }		
-	}
+     }      
+   }
 }
 
 void SubMeshDrawable::drawImplementation(osg::State& state) const 
 {
-	// begin the rendering loop
-	if(mWrapper->BeginRenderingQuery())
-	{
-		// select mesh and submesh for further data access
-		if(mWrapper->SelectMeshSubmesh(mMeshID, mSubmeshID))
-		{
-			// get the transformed vertices of the submesh
-			vertexCount = mWrapper->GetVertexCount();
-			vertexCount = mWrapper->GetVertices(mMeshVertices);
+   // begin the rendering loop
+   if(mWrapper->BeginRenderingQuery())
+   {
+      // select mesh and submesh for further data access
+      if(mWrapper->SelectMeshSubmesh(mMeshID, mSubmeshID))
+      {
+         // get the transformed vertices of the submesh
+         vertexCount = mWrapper->GetVertexCount();
+         vertexCount = mWrapper->GetVertices(mMeshVertices);
 
-			// get the transformed normals of the submesh
-			mWrapper->GetNormals(mMeshNormals);
+         // get the transformed normals of the submesh
+         mWrapper->GetNormals(mMeshNormals);
+         
+         // get the texture coordinates of the submesh
+         // this is still buggy, it renders only the first texture.
+         // it should be a loop rendering each texture on its own texture unit
+         unsigned tcount = mWrapper->GetTextureCoords(0, mMeshTextureCoordinates);
 
-			// get the texture coordinates of the submesh
-			// this is still buggy, it renders only the first texture.
-			// it should be a loop rendering each texture on its own texture unit
-			unsigned tcount = mWrapper->GetTextureCoords(0, mMeshTextureCoordinates);
-
-			// flip vertical coordinates
-			for (unsigned int i = 1; i < vertexCount * 2; i += 2)
+         // flip vertical coordinates
+         for (unsigned int i = 1; i < vertexCount * 2; i += 2)
             {
-				mMeshTextureCoordinates[i] = 1.0f - mMeshTextureCoordinates[i];
-			}
+            mMeshTextureCoordinates[i] = 1.0f - mMeshTextureCoordinates[i];
+         }
 
-			// set the vertex and normal buffers
-			state.setVertexPointer(3, GL_FLOAT, 0, mMeshVertices);
-			state.setNormalPointer(GL_FLOAT, 0, mMeshNormals);
+         // set the vertex and normal buffers
+         state.setVertexPointer(3, GL_FLOAT, 0, mMeshVertices);
+         state.setNormalPointer(GL_FLOAT, 0, mMeshNormals);
 
-			// set the texture coordinate buffer and state if necessary
-			if((mWrapper->GetMapCount() > 0) && (tcount > 0))
-			{
-				// set the texture coordinate buffer
-				state.setTexCoordPointer(0, 2, GL_FLOAT, 0, mMeshTextureCoordinates);
-			}
+         // set the texture coordinate buffer and state if necessary
+         if((mWrapper->GetMapCount() > 0) && (tcount > 0))
+         {
+            // set the texture coordinate buffer
+            state.setTexCoordPointer(0, 2, GL_FLOAT, 0, mMeshTextureCoordinates);
+         }
 
             // White 
-			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-			
+         glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+         
 
             // draw the submesh
-			if(sizeof(CalIndex) == sizeof(short))
+         if(sizeof(CalIndex) == sizeof(short))
             {
-				glDrawElements(GL_TRIANGLES, faceCount * 3, GL_UNSIGNED_SHORT, mMeshFaces);
+            glDrawElements(GL_TRIANGLES, faceCount * 3, GL_UNSIGNED_SHORT, mMeshFaces);
             }
-			else
+         else
             {
-				glDrawElements(GL_TRIANGLES, faceCount * 3, GL_UNSIGNED_INT, mMeshFaces);
+            glDrawElements(GL_TRIANGLES, faceCount * 3, GL_UNSIGNED_INT, mMeshFaces);
             }
 
-			// get the faces of the submesh for the next frame
-			faceCount = mWrapper->GetFaces(mMeshFaces);
-		}
+         // get the faces of the submesh for the next frame
+         faceCount = mWrapper->GetFaces(mMeshFaces);
+      }
 
-		// end the rendering
-		mWrapper->EndRenderingQuery();
-	}
+      // end the rendering
+      mWrapper->EndRenderingQuery();
+   }
 }
 
 
 void SubMeshDrawable::accept(osg::PrimitiveFunctor& functor) const
 {
-	functor.setVertexArray(vertexCount, (osg::Vec3f *)(mMeshVertices));
+   functor.setVertexArray(vertexCount, (osg::Vec3f *)(mMeshVertices));
 
-	if(sizeof(CalIndex) == sizeof(short))
-	{
-		osg::ref_ptr<osg::DrawElementsUShort> pset = new osg::DrawElementsUShort(osg::PrimitiveSet::TRIANGLES,
-																				 faceCount*3, (GLushort *) mMeshFaces);
-		pset->accept(functor);
-	} 
+   if(sizeof(CalIndex) == sizeof(short))
+   {
+      osg::ref_ptr<osg::DrawElementsUShort> pset = new osg::DrawElementsUShort(osg::PrimitiveSet::TRIANGLES,
+                                                             faceCount*3, (GLushort *) mMeshFaces);
+      pset->accept(functor);
+   } 
     else
     {
-		osg::ref_ptr<osg::DrawElementsUInt> pset = new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES,
-																			 faceCount*3, (GLuint *) mMeshFaces);
-		pset->accept(functor);
-	}
+      osg::ref_ptr<osg::DrawElementsUInt> pset = new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES,
+                                                          faceCount*3, (GLuint *) mMeshFaces);
+      pset->accept(functor);
+   }
 
-	return;
+   return;
 }
 
 osg::Object* SubMeshDrawable::clone(const osg::CopyOp&) const 
 {
-	return new SubMeshDrawable(mWrapper.get(), mMeshID, mSubmeshID);
+   return new SubMeshDrawable(mWrapper.get(), mMeshID, mSubmeshID);
 }
 
 osg::Object* SubMeshDrawable::cloneType() const
@@ -211,29 +211,29 @@ osg::Object* SubMeshDrawable::cloneType() const
 
 osg::BoundingBox SubMeshDrawable::computeBound() const 
 {
-	osg::BoundingBox bbox;
+   osg::BoundingBox bbox;
     bbox.init();
 
-	// We have to traverse all vertices to recalculate bounds
-	
-	if(mWrapper->BeginRenderingQuery()) 
+   // We have to traverse all vertices to recalculate bounds
+   
+   if(mWrapper->BeginRenderingQuery()) 
     {
-		if(mWrapper->SelectMeshSubmesh(mMeshID, mSubmeshID)) 
+      if(mWrapper->SelectMeshSubmesh(mMeshID, mSubmeshID)) 
         {
-			// get the transformed vertices of the submesh
-			int vertexCount = mWrapper->GetVertexCount();
-			float *meshVertices = new float[vertexCount*3];
-			vertexCount = mWrapper->GetVertices(meshVertices);
+         // get the transformed vertices of the submesh
+         int vertexCount = mWrapper->GetVertexCount();
+         float *meshVertices = new float[vertexCount*3];
+         vertexCount = mWrapper->GetVertices(meshVertices);
 
-			for (int vertex=0;vertex<vertexCount;vertex++) 
+         for (int vertex=0;vertex<vertexCount;vertex++) 
             {
-				float* v=meshVertices+vertex*3;
-				bbox.expandBy(v[0],v[1],v[2]);
-			}
+            float* v=meshVertices+vertex*3;
+            bbox.expandBy(v[0],v[1],v[2]);
+         }
 
-			delete []meshVertices;
-		}
-		mWrapper->EndRenderingQuery();
-	}
-	return bbox;
+         delete []meshVertices;
+      }
+      mWrapper->EndRenderingQuery();
+   }
+   return bbox;
 }
