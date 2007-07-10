c1b5ea2f47b487df02db50ef9b054ce3d80d086e
banderegg <banderegg@users.noreply.github.com>
2007-07-10 13:21:34 +0000

    +added the attachmentcontroller.h/cpp to the vcproj +added support for hardware skinning with hardwaresubmesh +added option for the animnodebuilder to create a hardwaresubmesh

--- delta3d/src/dtAnim/animnodebuilder.cpp
+++ delta3d/src/dtAnim/animnodebuilder.cpp
@@ -22,17 +22,20 @@
 #include <dtAnim/animnodebuilder.h>
 #include <dtAnim/cal3dmodelwrapper.h>
 #include <dtAnim/submesh.h>
-
+#include <dtAnim/hardwaresubmesh.h>
+#include <dtCore/globals.h>
 #include <dtUtil/log.h>
 
 #include <osg/Geode>
+
+#include <cal3d/hardwaremodel.h>
 
 namespace dtAnim
 {
 
 AnimNodeBuilder::AnimNodeBuilder()
 {
-   SetCreate(CreateFunc(this, &AnimNodeBuilder::DefaultCreate));
+   SetCreate(CreateFunc(this, &AnimNodeBuilder::CreateSoftware));
 }
 
 AnimNodeBuilder::AnimNodeBuilder(const CreateFunc& pCreate)
@@ -57,7 +60,7 @@ dtCore::RefPtr<osg::Geode> AnimNodeBuilder::CreateGeode(Cal3DModelWrapper* pWrap
 }
 
 
-dtCore::RefPtr<osg::Geode> AnimNodeBuilder::DefaultCreate(Cal3DModelWrapper* pWrapper)
+dtCore::RefPtr<osg::Geode> AnimNodeBuilder::CreateSoftware(Cal3DModelWrapper* pWrapper)
 {
    if(!pWrapper)
    {
@@ -89,5 +92,177 @@ dtCore::RefPtr<osg::Geode> AnimNodeBuilder::DefaultCreate(Cal3DModelWrapper* pWr
 
    return geode;
 }
+
+
+dtCore::RefPtr<osg::Geode> AnimNodeBuilder::CreateHardware(Cal3DModelWrapper* pWrapper)
+{
+   if(!pWrapper)
+   {
+      LOG_ERROR("Invalid parameter to CreateGeode.");
+      return 0;
+   }
+
+   //TODO: query for the maximum number of bones through opengl
+   static const int MAX_BONES = 72;
+
+   osg::Geode* geode = new osg::Geode();
+
+   pWrapper->SetLODLevel(1);
+   pWrapper->Update(0);
+
+   if(pWrapper && pWrapper->BeginRenderingQuery()) 
+   {
+
+      CalCoreModel* model = pWrapper->GetCalModel()->getCoreModel();
+      
+      unsigned numVerts = 0;
+      unsigned numIndices = 0;
+      unsigned numTextureUnits = 2;
+
+      int meshCount = model->getCoreMeshCount();
+
+      for(int meshId = 0; meshId < meshCount; meshId++) 
+      {
+         CalCoreMesh* calMesh = model->getCoreMesh(meshId);         
+         int submeshCount = calMesh->getCoreSubmeshCount();
+               
+         for(int submeshId = 0; submeshId < submeshCount; submeshId++) 
+         {
+            CalCoreSubmesh* subMesh = calMesh->getCoreSubmesh(submeshId);
+            numVerts += subMesh->getVertexCount();
+            numIndices += 3 * subMesh->getFaceCount();
+         }
+      }
+      
+
+      float* vertArray = new float[numVerts * 3];
+      float* normArray = new float[numVerts * 3];
+      float* tex1Array = new float[numVerts * 2];
+      float* tex2Array = new float[numVerts * 2];
+      float* boneWeightArray = new float[numVerts * 4];
+      float* boneIDArray = new float[numVerts * 4];
+      CalIndex* indexArray = new CalIndex[numIndices];
+
+      CalHardwareModel* hardwareModel = new CalHardwareModel(model);
+      hardwareModel->setVertexBuffer((char*) vertArray, 3 * sizeof(float));
+      hardwareModel->setIndexBuffer(indexArray);
+      hardwareModel->setNormalBuffer((char*) normArray, 3 * sizeof(float));
+      hardwareModel->setWeightBuffer((char*) boneWeightArray, 4 * sizeof(float));
+      hardwareModel->setMatrixIndexBuffer((char*) boneIDArray, 4 * sizeof(float));
+      hardwareModel->setTextureCoordNum(2);
+      hardwareModel->setTextureCoordBuffer(0, (char*) tex1Array, 2 * sizeof(float));
+      hardwareModel->setTextureCoordBuffer(1, (char*) tex2Array, 2 * sizeof(float));
+
+      if(hardwareModel->load(0, 0, MAX_BONES))
+      {
+         numVerts = hardwareModel->getTotalVertexCount();
+         numIndices = 3 * hardwareModel->getTotalFaceCount();
+      
+         osg::Drawable::Extensions* glExt = osg::Drawable::getExtensions(0, true);
+         GLuint vbo[2];
+         glExt->glGenBuffers(2, vbo);
+
+         float* vboVertexAttr = new float[numVerts * 18];
+
+         for(unsigned i = 0; i < numVerts; ++i)
+         {
+            unsigned count = i * 18;
+            vboVertexAttr[count + 0] = vertArray[(i * 3) + 0];
+            vboVertexAttr[count + 1] = vertArray[(i * 3) + 1];
+            vboVertexAttr[count + 2] = vertArray[(i * 3) + 2];
+
+            vboVertexAttr[count + 3] = normArray[(i * 3) + 0];
+            vboVertexAttr[count + 4] = normArray[(i * 3) + 1];
+            vboVertexAttr[count + 5] = normArray[(i * 3) + 2];
+
+            vboVertexAttr[count + 6] = tex1Array[(i * 2) + 0];
+            vboVertexAttr[count + 7] = tex1Array[(i * 2) + 1];
+
+            vboVertexAttr[count + 8] = tex2Array[(i * 2) + 0];
+            vboVertexAttr[count + 9] = tex2Array[(i * 2) + 1];
+
+            vboVertexAttr[count + 10] = boneWeightArray[(i * 4) + 0];
+            vboVertexAttr[count + 11] = boneWeightArray[(i * 4) + 1];
+            vboVertexAttr[count + 12] = boneWeightArray[(i * 4) + 2];
+            vboVertexAttr[count + 13] = boneWeightArray[(i * 4) + 3];
+            
+            vboVertexAttr[count + 14] = boneIDArray[(i * 4) + 0];           
+            vboVertexAttr[count + 15] = boneIDArray[(i * 4) + 1];            
+            vboVertexAttr[count + 16] = boneIDArray[(i * 4) + 2];
+            vboVertexAttr[count + 17] = boneIDArray[(i * 4) + 3];
+         }
+
+         for(int meshCount = 0; meshCount < hardwareModel->getHardwareMeshCount(); ++meshCount)
+         {
+		      hardwareModel->selectHardwareMesh(meshCount);
+
+	         for(int face = 0; face < hardwareModel->getFaceCount(); ++face) 
+            {
+               for(int index = 0; index < 3; ++index)
+               {
+		            indexArray[face * 3 + index + hardwareModel->getStartIndex()] += hardwareModel->getBaseVertexIndex();
+               }
+	         }
+         }
+
+         glExt->glBindBuffer(GL_ARRAY_BUFFER_ARB, vbo[0]);
+         glExt->glBufferData(GL_ARRAY_BUFFER_ARB, 18 * sizeof(float) * numVerts, (const void*) vboVertexAttr, GL_STATIC_DRAW_ARB);
+
+         glExt->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER_ARB, vbo[1]);
+         glExt->glBufferData(GL_ELEMENT_ARRAY_BUFFER_ARB, numIndices * sizeof(CalIndex), (const void*) indexArray, GL_STATIC_DRAW_ARB);
+
+         //todo- pull shader name out of character xml
+         osg::Program* shader = LoadShaders("shaders/HardwareCharacter.vert");
+         osg::Uniform* boneTrans = new osg::Uniform(osg::Uniform::FLOAT_VEC4, "boneTransforms", MAX_BONES);
+
+         for(int meshCount = 0; meshCount < hardwareModel->getHardwareMeshCount(); ++meshCount)
+         {
+            HardwareSubMeshDrawable* drawable = new HardwareSubMeshDrawable(pWrapper, hardwareModel, shader, boneTrans, meshCount, vbo[0], vbo[1]);
+            geode->addDrawable(drawable);
+         }
+      } 
+      else
+      {
+         LOG_ERROR("Unable to create a hardware mesh.");
+      }
+
+
+
+
+      pWrapper->EndRenderingQuery();
+   }
+   
+
+   return geode;
+}
+
+
+osg::Program* AnimNodeBuilder::LoadShaders(const std::string& shaderFile) const
+{
+   //we should allow the user to specify this in the character xml
+   std::string vertFile = dtCore::FindFileInPathList(shaderFile);
+   //std::string fragShader = dtCore::FindFileInPathList("shaders/HardwareCharacter.frag");
+
+   osg::Program* prog = 0;
+    
+   if(!vertFile.empty())// || fragShader.empty())
+   {
+      prog = new osg::Program;
+
+	   dtCore::RefPtr<osg::Shader> vertShader = new osg::Shader(osg::Shader::VERTEX);
+      vertShader->loadShaderSourceFromFile(vertFile);
+
+	   prog->addShader(vertShader.get());
+
+   }
+   else
+   {
+      LOG_ERROR("Unable to load vertex shader '" + shaderFile + "'.");
+   }
+
+   return prog;
+}
+
+
 
 }//namespace dtAnim
---
+++ delta3d/src/dtAnim/hardwaresubmesh.cpp
@@ -0,0 +1,137 @@
+/* -*-c++-*-
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2007, Alion Science and Technology
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Bradley Anderegg 06/27/2007
+ */
+
+#include <dtAnim/hardwaresubmesh.h>
+#include <osg/Material>
+#include <osg/Texture2D>
+#include <osg/PolygonMode>
+#include <dtAnim/cal3dmodelwrapper.h>
+#include <osg/Geometry>
+#include <dtUtil/matrixutil.h>
+#include <cal3d/hardwaremodel.h>
+
+namespace dtAnim
+{
+
+HardwareSubMeshDrawable::HardwareSubMeshDrawable(Cal3DModelWrapper *wrapper, CalHardwareModel* model, osg::Program* shader, osg::Uniform* boneTrans, unsigned mesh, unsigned vertexVBO, unsigned indexVBO)
+: mWrapper(wrapper)
+, mHardwareModel(model)
+, mProgram(shader)
+, mBoneTransforms(boneTrans)
+, mMeshID(mesh)
+, mVertexVBO(vertexVBO)
+, mIndexVBO(indexVBO)
+{ 
+	setUseDisplayList(false);
+   setUseVertexBufferObjects(true);
+
+   osg::StateSet* ss = getOrCreateStateSet();
+   ss->setAttributeAndModes(mProgram.get(), osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+   ss->addUniform(mBoneTransforms.get());
+
+   if (mWrapper->GetMapCount() > 0)
+   {
+      osg::Texture2D *texture = (osg::Texture2D*)mWrapper->GetMapUserData(0);
+      if (texture != 0) 
+      {
+         ss->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
+      }
+   }	
+}
+
+HardwareSubMeshDrawable::~HardwareSubMeshDrawable(void)
+{
+}
+
+void HardwareSubMeshDrawable::drawImplementation(osg::State& state) const 
+{
+   //select the proper hardware mesh
+   mHardwareModel->selectHardwareMesh(mMeshID);
+
+   //spin through the bones in the hardware mesh
+   int numBones = mHardwareModel->getBoneCount();
+   for(int bone = 0; bone < numBones; ++bone)
+   {
+      CalSkeleton* skel = mWrapper->GetCalModel()->getSkeleton();
+      const CalQuaternion& quat = mHardwareModel->getRotationBoneSpace(bone, skel);
+      const CalVector& vec = mHardwareModel->getTranslationBoneSpace(bone, skel);
+
+      //compute matrices
+      osg::Matrix matRot(osg::Quat(quat.x, quat.y, quat.z, quat.w));
+
+      osg::Vec4 rotX, rotY, rotZ;
+      rotX = dtUtil::MatrixUtil::GetRow4(matRot, 0);
+      rotY = dtUtil::MatrixUtil::GetRow4(matRot, 1);
+      rotZ = dtUtil::MatrixUtil::GetRow4(matRot, 2);
+
+      rotX[3] = vec.x;
+      rotY[3] = vec.y;
+      rotZ[3] = vec.z;
+
+      //set data on uniform
+      mBoneTransforms->setElement(bone * 3 + 0, rotX);
+      mBoneTransforms->setElement(bone * 3 + 1, rotY);
+      mBoneTransforms->setElement(bone * 3 + 2, rotZ);
+   }
+   
+   //bind the textures if we are using a fragment shader
+
+   //bind the VBO's
+   state.disableAllVertexArrays();
+
+   const Extensions* glExt = getExtensions(state.getContextID(),true);
+
+   glExt->glBindBuffer(GL_ARRAY_BUFFER_ARB, mVertexVBO);
+
+   unsigned stride = 18 * sizeof(float);
+
+   #define BUFFER_OFFSET(x)((GLvoid*) (0 + (x * sizeof(float))))
+
+   state.setVertexPointer(3, GL_FLOAT, stride, BUFFER_OFFSET(0));
+
+   state.setNormalPointer(GL_FLOAT, stride, BUFFER_OFFSET(3));
+   state.setTexCoordPointer(0, 2, GL_FLOAT, stride, BUFFER_OFFSET(6));
+   state.setTexCoordPointer(1, 2, GL_FLOAT, stride, BUFFER_OFFSET(8));
+
+	state.setTexCoordPointer(2, 4 , GL_FLOAT, stride, BUFFER_OFFSET(10));
+   state.setTexCoordPointer(3, 4 , GL_FLOAT, stride, BUFFER_OFFSET(14));
+
+   //make the call to render
+   glExt->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER_ARB, mIndexVBO);
+
+   glDrawElements(GL_TRIANGLES,  mHardwareModel->getFaceCount() * 3, (sizeof(CalIndex) < 4) ? GL_UNSIGNED_SHORT: GL_UNSIGNED_INT, (void*)(sizeof(CalIndex) * mHardwareModel->getStartIndex()));
+
+   glExt->glBindBuffer(GL_ARRAY_BUFFER_ARB,0);
+   glExt->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER_ARB,0);
+}
+
+osg::Object* HardwareSubMeshDrawable::clone(const osg::CopyOp&) const 
+{
+	return new HardwareSubMeshDrawable(mWrapper.get(), mHardwareModel, mProgram.get(), mBoneTransforms.get(), mMeshID, mVertexVBO, mIndexVBO );
+}
+
+osg::Object* HardwareSubMeshDrawable::cloneType() const
+{
+	return new HardwareSubMeshDrawable(mWrapper.get(), mHardwareModel, mProgram.get(), mBoneTransforms.get(), mMeshID, mVertexVBO, mIndexVBO );
+}
+
+
+} //namespace dtAnim
