16d2effb13b24a821151ed3662e8a752af08b22f
fretlessmayhem <fretlessmayhem@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-07-26 01:25:20 +0000

    Reverting previous change.

--- delta3d/inc/dtAnim/cal3ddatabase.h
+++ delta3d/inc/dtAnim/cal3ddatabase.h
@@ -58,10 +58,9 @@ namespace dtAnim
 
          /// @return the node builder for this database.
          AnimNodeBuilder& GetNodeBuilder();
-
-         Cal3DDatabase();	 
+         
       protected:
-
+         Cal3DDatabase();
          virtual ~Cal3DDatabase();
 
          Cal3DModelData* Find(const std::string& filename);
