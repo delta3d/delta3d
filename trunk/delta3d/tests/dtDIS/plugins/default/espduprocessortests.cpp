9e649e66430aff618352060e06c1eb976ffe9f81
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-10-16 22:51:31 +0000

    incremental checkin verifying dtDIS will create remote actors in response to a received DIS PDU.

--- trunk/delta3d/tests/dtDIS/plugins/default/espduprocessortests.cpp
+++ trunk/delta3d/tests/dtDIS/plugins/default/espduprocessortests.cpp
@@ -7,15 +7,19 @@
 #include <dtDIS/plugins/default/initializepdu.h>            // for usage
 #include <dtDIS/plugins/default/initializeupdatemessage.h>  // for usage
 #include <dtDIS/plugins/default/samplegameactorproxy.h>     // for usage
+#include <dtDIS/sharedstate.h>
 
 #include <dtGame/gamemanager.h>
 #include <dtCore/scene.h>
+
+#include <dtActors/engineactorregistry.h>
 
 #include <cstddef>   // for NULL
 #include <dtGame/gmcomponent.h>
 #include <dtCore/system.h>
 #include <dtGame/messagefactory.h>        // for usage
 #include <dtGame/actorupdatemessage.h>         // for usage
+#include <dtGame/defaultmessageprocessor.h>
 
 
 namespace dtTest
@@ -28,13 +32,13 @@ namespace dtTest
       void teardown(); 
 
       /// see if the pdu values show up on the actor
-      void TestApplyToLocalActor();
+      void CreateRemoteActorFromEntityStatePDU();
 
       /// see if the actor values show up on the pdu
       void TestNoActor();
 
       CPPUNIT_TEST_SUITE( ESPduProcessorTests );
-         CPPUNIT_TEST( TestApplyToLocalActor );
+         CPPUNIT_TEST( CreateRemoteActorFromEntityStatePDU );
          CPPUNIT_TEST( TestNoActor );
       CPPUNIT_TEST_SUITE_END();
 
@@ -74,51 +78,48 @@ namespace dtTest
 using namespace dtTest;
 CPPUNIT_TEST_SUITE_REGISTRATION( ESPduProcessorTests );
 
-void ESPduProcessorTests::TestApplyToLocalActor()
+void ESPduProcessorTests::CreateRemoteActorFromEntityStatePDU()
 {
    dtCore::RefPtr<dtCore::Scene> scene = new dtCore::Scene();
    dtCore::RefPtr<dtGame::GameManager> gm = new dtGame::GameManager( *scene );
+
+   dtCore::RefPtr<dtGame::DefaultMessageProcessor> mp = new dtGame::DefaultMessageProcessor();
+   gm->AddComponent( *mp, dtGame::GameManager::ComponentPriority::NORMAL);
    dtCore::System::GetInstance().Start();
    dtCore::System::GetInstance().Step();
    dtCore::System::GetInstance().Step();
 
-   // locally, add a GameActorProxy
-   dtCore::RefPtr<dtTest::SampleGameActorProxy> sap = new dtTest::SampleGameActorProxy();
-   sap->CreateActor();
-   sap->BuildPropertyMap();
-   dtCore::RefPtr<dtGame::ActorUpdateMessage> created;
-   dtCore::RefPtr<dtGame::MachineInfo> blah = new dtGame::MachineInfo("blah");
-   dtGame::MessageFactory mfact("blah", *blah, "blah");
-   mfact.CreateMessage(dtGame::MessageType::INFO_ACTOR_CREATED,created);
-   gm->SendMessage( *created );
-   gm->AddActor( *sap , true , false );
-
-   ///\todo need to tick the system so that proxy is really added?
-   dtCore::System::GetInstance().Step();
-   dtCore::System::GetInstance().Step();
-
+   //create a test pdu and initialize it
    DIS::EntityStatePdu pdu;
    InitializePdu initpdu;
    initpdu( pdu );
 
-   dtDIS::ESPduProcessor processor( gm.get(), NULL );
-
-   dtCore::RefPtr<dtTest::CatcherComponent> cc = new dtTest::CatcherComponent(&dtGame::MessageType::INFO_ACTOR_UPDATED);
-   gm->AddComponent( *cc , dtGame::GameManager::ComponentPriority::NORMAL );
-
+   //add a new mapping of DIS entity type and ActorType
+   dtDIS::SharedState sharedState;
+   sharedState.GetActorMap().AddActorMapping(pdu.getEntityType(), 
+      dtActors::EngineActorRegistry::GAME_MESH_ACTOR_TYPE.get() );
+ 
+   //create our entity state pdu processor and feed it a PDU to work on.
+   //In this case, it should create a new Actor and apply all the values
+   //of the PDU to the Actor's Properties
+   dtDIS::ESPduProcessor processor( gm.get(), &sharedState );
    processor.Process( pdu );
 
    ///\todo need to tick the system so that message is really applied?
    dtCore::System::GetInstance().Step();
 
-   CPPUNIT_ASSERT( cc->mCaught );
 
-   CPPUNIT_ASSERT_EQUAL( pdu.getEntityAppearance() , sap->mAppearance );
+   //CPPUNIT_ASSERT_EQUAL( pdu.getEntityAppearance() , sap->mAppearance );
    //CPPUNIT_ASSERT_EQUAL( pdu.getEntityLocation() , sap->mLocation );
    //CPPUNIT_ASSERT_EQUAL( pdu.getEntityOrientation() , sap->mOrientation );
-   CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getEntity() , sap->mEntityIDEntity );
-   CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getApplication() , sap->mEntityIDApplication );
-   CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getSite() , sap->mEntityIDSite );
+   //CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getEntity() , sap->mEntityIDEntity );
+   //CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getApplication() , sap->mEntityIDApplication );
+   //CPPUNIT_ASSERT_EQUAL( pdu.getEntityID().getSite() , sap->mEntityIDSite );
+
+   //at this point, there should be a new Actor out there
+   std::vector<dtDAL::ActorProxy*> proxies;
+   gm->FindActorsByType(*dtActors::EngineActorRegistry::GAME_MESH_ACTOR_TYPE, proxies );
+   CPPUNIT_ASSERT_EQUAL_MESSAGE("Should only be one actor created", size_t(1), proxies.size());
 }
 
 void ESPduProcessorTests::TestNoActor()
