df5cebce3988be5411d7134e3400e2129f9140a5
behollis <behollis@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-10-26 18:41:58 +0000

    Added BMH's SimCore base actor properties (string identifiers). These properties need to be supported by dtDIS's PDU-to-ActorUpdateMessage applicator.
    *This addition tries to avoid a 'direct' dependency on SimCore by not including files directly from that code base. However, any updates or changes made to SimCore (and vice-versa for that matter) involving these property names, would also need to be duplicated in dtDIS, in order for continued DIS support in DVTE.*

--- delta3d/src/dtDIS/propertyname.cpp
+++ delta3d/src/dtDIS/propertyname.cpp
@@ -5,32 +5,50 @@ namespace dtDIS
    // -- EntityPropertyName -- //
    // entitytype stuff
    // entityid stuff
-   const char EntityPropertyName::ENTITYID[] = { "DIS_Entity_EntityID\0" };
-   const char EntityPropertyName::APPEARANCE[] = { "DIS_Entity_Appearance\0" };
+   const char EntityPropertyName::ENTITYID   [] = { "DIS_Entity_EntityID\0"   } ;
+   const char EntityPropertyName::APPEARANCE [] = { "DIS_Entity_Appearance\0" } ;
 
 
    // -- EnginePropertyName -- //
-   const char EnginePropertyName::ROTATION[] = { "Rotation\0" };
-   const char EnginePropertyName::TRANSLATION[] = { "Translation\0" };
-   const char EnginePropertyName::LAST_KNOWN_TRANSLATION[] = { "Last Known Translation\0" };
-   const char EnginePropertyName::LAST_KNOWN_ROTATION[] = { "Last Known Rotation\0" };
+   const char EnginePropertyName::ROTATION                  [] = { "Rotation\0"                 } ;
+   const char EnginePropertyName::TRANSLATION               [] = { "Translation\0"              } ;
+   const char EnginePropertyName::LAST_KNOWN_TRANSLATION    [] = { "Last Known Translation\0"   } ;
+   const char EnginePropertyName::LAST_KNOWN_ROTATION       [] = { "Last Known Rotation\0"      } ;
 
-   const char EnginePropertyName::VELOCITY[] = { "Velocity Vector\0" };
-   const char EnginePropertyName::ACCELERATION[] = { "Acceleration Vector\0" };
+   const char EnginePropertyName::VELOCITY                  [] = { "Velocity Vector\0"          } ;
+   const char EnginePropertyName::ACCELERATION              [] = { "Acceleration Vector\0"      } ;
 
-   const char EnginePropertyName::DEAD_RECKONING_ALGORITHM[] = { "Dead Reckoning Algorithm\0" };
+   const char EnginePropertyName::DEAD_RECKONING_ALGORITHM  [] = { "Dead Reckoning Algorithm\0" } ;
 
    ///\todo this only named "Flying" for now because of the dvte::IG::Entity actor.
-   const char EnginePropertyName::GROUND_CLAMP[] = { "Flying\0" };
+   const char EnginePropertyName::GROUND_CLAMP              [] = { "Flying\0"                   } ;
 
-   const char EnginePropertyName::RESOURCE_DAMAGE_OFF[] = { "Non-damaged actor\0" };
-   const char EnginePropertyName::RESOURCE_DAMAGE_ON[] = { "Damaged actor\0" };
-   const char EnginePropertyName::RESOURCE_DAMAGE_DESTROYED[] = { "Destroyed actor\0" };
+   const char EnginePropertyName::RESOURCE_DAMAGE_OFF       [] = { "Non-damaged actor\0"        } ;
+   const char EnginePropertyName::RESOURCE_DAMAGE_ON        [] = { "Damaged actor\0"            } ;
+   const char EnginePropertyName::RESOURCE_DAMAGE_DESTROYED [] = { "Destroyed actor\0"          } ;
 
    ///\todo this only uses the substring 'Parameter' because dvte::IG::Entity needs it.
-   const char EnginePropertyName::ARTICULATION[] = { "ArticulatedParameter\0" };
+   const char EnginePropertyName::ARTICULATION              [] = { "ArticulatedParameter\0"     } ;
 
    /// this value is not a typo, it needs to be this because of the DVTE support.
-   const char EnginePropertyName::DOF_NODE_NAME[] = { "OurName\0" };
+   const char EnginePropertyName::DOF_NODE_NAME             [] = { "OurName\0"                  } ;
+
+   // -- HLABaseEntityPropertyName -- //
+   const char HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_TRANSLATION     [] = { "Last Known Translation"     } ;
+   const char HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_ROTATION        [] = { "Last Known Rotation"        } ;
+   const char HLABaseEntityPropertyName::PROPERTY_VELOCITY_VECTOR            [] = { "Velocity Vector"            } ;
+   const char HLABaseEntityPropertyName::PROPERTY_ACCELERATION_VECTOR        [] = { "Acceleration Vector"        } ;
+   const char HLABaseEntityPropertyName::PROPERTY_ANGULAR_VELOCITY_VECTOR    [] = { "Angular Velocity Vector"    } ;
+   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_POSITION      [] = { "EngineSmokePosition"        } ;
+   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_ON            [] = { "EngineSmokeOn"              } ;
+   const char HLABaseEntityPropertyName::PROPERTY_FLAMES_PRESENT             [] = { "FlamesPresent"              } ;
+   const char HLABaseEntityPropertyName::PROPERTY_SMOKE_PLUME_PRESENT        [] = { "SmokePlumePresent"          } ;
+   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_POSITION            [] = { "Engine Position"            } ;
+   const char HLABaseEntityPropertyName::PROPERTY_FLYING                     [] = { "Flying"                     } ;
+   const char HLABaseEntityPropertyName::PROPERTY_DAMAGE_STATE               [] = { "Damage State"               } ;
+   const char HLABaseEntityPropertyName::PROPERTY_DEFAULT_SCALE              [] = { "Default Scale"              } ;
+   const char HLABaseEntityPropertyName::PROPERTY_SCALE_MAGNIFICATION_FACTOR [] = { "Scale Magnification Factor" } ;
+   const char HLABaseEntityPropertyName::PROPERTY_MODEL_SCALE                [] = { "Model Scale"                } ;
+   const char HLABaseEntityPropertyName::PROPERTY_MODEL_ROTATION             [] = { "Model Rotation"             } ;
 }
 
