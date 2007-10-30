078131b3eef99b65dd1455f5861a760cd246e67d
behollis <behollis@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-10-30 17:11:54 +0000

    New actor property names for ActorUpdateMessages. The added property names use std::strings as opposed to char arrays.

--- delta3d/src/dtDIS/propertyname.cpp
+++ delta3d/src/dtDIS/propertyname.cpp
@@ -12,20 +12,20 @@ namespace dtDIS
    // -- EnginePropertyName -- //
    const char EnginePropertyName::ROTATION                  [] = { "Rotation\0"                 } ;
    const char EnginePropertyName::TRANSLATION               [] = { "Translation\0"              } ;
-   const char EnginePropertyName::LAST_KNOWN_TRANSLATION    [] = { "Last Known Translation\0"   } ;
-   const char EnginePropertyName::LAST_KNOWN_ROTATION       [] = { "Last Known Rotation\0"      } ;
+   const char EnginePropertyName::LAST_KNOWN_TRANSLATION    [] = { "Last_Known_Translation\0"   } ;
+   const char EnginePropertyName::LAST_KNOWN_ROTATION       [] = { "Last_Known_Rotation\0"      } ;
 
-   const char EnginePropertyName::VELOCITY                  [] = { "Velocity Vector\0"          } ;
-   const char EnginePropertyName::ACCELERATION              [] = { "Acceleration Vector\0"      } ;
+   const char EnginePropertyName::VELOCITY                  [] = { "Velocity_Vector\0"          } ;
+   const char EnginePropertyName::ACCELERATION              [] = { "Acceleration' 'Vector\0"      } ;
 
-   const char EnginePropertyName::DEAD_RECKONING_ALGORITHM  [] = { "Dead Reckoning Algorithm\0" } ;
+   const char EnginePropertyName::DEAD_RECKONING_ALGORITHM  [] = { "Dead_Reckoning_Algorithm\0" } ;
 
    ///\todo this only named "Flying" for now because of the dvte::IG::Entity actor.
    const char EnginePropertyName::GROUND_CLAMP              [] = { "Flying\0"                   } ;
 
-   const char EnginePropertyName::RESOURCE_DAMAGE_OFF       [] = { "Non-damaged actor\0"        } ;
-   const char EnginePropertyName::RESOURCE_DAMAGE_ON        [] = { "Damaged actor\0"            } ;
-   const char EnginePropertyName::RESOURCE_DAMAGE_DESTROYED [] = { "Destroyed actor\0"          } ;
+   const char EnginePropertyName::RESOURCE_DAMAGE_OFF       [] = { "Non_damaged_actor\0"        } ;
+   const char EnginePropertyName::RESOURCE_DAMAGE_ON        [] = { "Damaged_actor\0"            } ;
+   const char EnginePropertyName::RESOURCE_DAMAGE_DESTROYED [] = { "Destroyed_actor\0"          } ;
 
    ///\todo this only uses the substring 'Parameter' because dvte::IG::Entity needs it.
    const char EnginePropertyName::ARTICULATION              [] = { "ArticulatedParameter\0"     } ;
@@ -34,21 +34,21 @@ namespace dtDIS
    const char EnginePropertyName::DOF_NODE_NAME             [] = { "OurName\0"                  } ;
 
    // -- HLABaseEntityPropertyName -- //
-   const char HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_TRANSLATION     [] = { "Last Known Translation"     } ;
-   const char HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_ROTATION        [] = { "Last Known Rotation"        } ;
-   const char HLABaseEntityPropertyName::PROPERTY_VELOCITY_VECTOR            [] = { "Velocity Vector"            } ;
-   const char HLABaseEntityPropertyName::PROPERTY_ACCELERATION_VECTOR        [] = { "Acceleration Vector"        } ;
-   const char HLABaseEntityPropertyName::PROPERTY_ANGULAR_VELOCITY_VECTOR    [] = { "Angular Velocity Vector"    } ;
-   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_POSITION      [] = { "EngineSmokePosition"        } ;
-   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_ON            [] = { "EngineSmokeOn"              } ;
-   const char HLABaseEntityPropertyName::PROPERTY_FLAMES_PRESENT             [] = { "FlamesPresent"              } ;
-   const char HLABaseEntityPropertyName::PROPERTY_SMOKE_PLUME_PRESENT        [] = { "SmokePlumePresent"          } ;
-   const char HLABaseEntityPropertyName::PROPERTY_ENGINE_POSITION            [] = { "Engine Position"            } ;
-   const char HLABaseEntityPropertyName::PROPERTY_FLYING                     [] = { "Flying"                     } ;
-   const char HLABaseEntityPropertyName::PROPERTY_DAMAGE_STATE               [] = { "Damage State"               } ;
-   const char HLABaseEntityPropertyName::PROPERTY_DEFAULT_SCALE              [] = { "Default Scale"              } ;
-   const char HLABaseEntityPropertyName::PROPERTY_SCALE_MAGNIFICATION_FACTOR [] = { "Scale Magnification Factor" } ;
-   const char HLABaseEntityPropertyName::PROPERTY_MODEL_SCALE                [] = { "Model Scale"                } ;
-   const char HLABaseEntityPropertyName::PROPERTY_MODEL_ROTATION             [] = { "Model Rotation"             } ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_TRANSLATION     = "Last Known Translation"     ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_LAST_KNOWN_ROTATION        = "Last Known Rotation"        ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_VELOCITY_VECTOR            = "Velocity Vector"            ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_ACCELERATION_VECTOR        = "Acceleration Vector"        ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_ANGULAR_VELOCITY_VECTOR    = "Angular Velocity Vector"    ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_POSITION      = "EngineSmokePosition"        ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_ENGINE_SMOKE_ON            = "EngineSmokeOn"              ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_FLAMES_PRESENT             = "FlamesPresent"              ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_SMOKE_PLUME_PRESENT        = "SmokePlumePresent"          ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_ENGINE_POSITION            = "Engine Position"            ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_FLYING                     = "Flying"                     ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_DAMAGE_STATE               = "Damage State"               ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_DEFAULT_SCALE              = "Default Scale"              ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_SCALE_MAGNIFICATION_FACTOR = "Scale Magnification Factor" ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_MODEL_SCALE                = "Model Scale"                ;
+   const std::string HLABaseEntityPropertyName::PROPERTY_MODEL_ROTATION             = "Model Rotation"             ;
 }
 
