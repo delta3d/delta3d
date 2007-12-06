ab556ab0eb180de64a72c8503225a5d359d049e1
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-12-06 02:29:43 +0000

    Added an option to the VS EntryPoint wizard to generate BaseInputComponent derivative code.

--- trunk/delta3d/VisualStudio/utilities/vcWizard/VCWizards/Delta3DEntryPointWizard/Templates/1033/EntryPoint.cpp
+++ trunk/delta3d/VisualStudio/utilities/vcWizard/VCWizards/Delta3DEntryPointWizard/Templates/1033/EntryPoint.cpp
@@ -4,6 +4,10 @@
 #include <dtGame/defaultmessageprocessor.h>
 [!endif]
 
+[!if INPUT_CHECKBOX]
+#include "[!output PROJECT_NAME]Input.h"
+[!endif]
+
 extern "C" ENTRY_POINT_EXPORT dtGame::GameEntryPoint* CreateGameEntryPoint()
 {
    return new [!output PROJECT_NAME]EntryPoint;
@@ -35,6 +39,11 @@ void [!output PROJECT_NAME]EntryPoint::OnStartup( dtGame::GameApplication &app )
 [!if DMP_CHECKBOX]
    dtGame::DefaultMessageProcessor *messageProc = new dtGame::DefaultMessageProcessor();
    app.GetGameManager()->AddComponent( *messageProc, dtGame::GameManager::ComponentPriority::HIGHEST);
+[!endif]
+
+[!if INPUT_CHECKBOX]
+   [!output PROJECT_NAME]Input *inp = new [!output PROJECT_NAME]Input();
+   app.GetGameManager()->AddComponent( *inp, dtGame::GameManager::ComponentPriority::NORMAL );
 [!endif]
 
 }
