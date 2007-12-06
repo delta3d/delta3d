26d61bd6fbb01237589bba0474b3ac2cc170aa87
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2007-12-06 19:50:07 +0000

    Added DefaultNetworkPublishingComponent option to the VS wizard. Generated code now stores the optional components as ref pointers and removed them from the GameManager on shutdown.

--- delta3d/VisualStudio/utilities/vcWizard/VCWizards/Delta3DEntryPointWizard/Templates/1033/EntryPoint.h
+++ delta3d/VisualStudio/utilities/vcWizard/VCWizards/Delta3DEntryPointWizard/Templates/1033/EntryPoint.h
@@ -4,6 +4,23 @@
 #include "EntryPointExport.h"
 #include <dtGame/gameentrypoint.h>
 
+
+[!if DMP_CHECKBOX || DNPC_CHECKBOX]
+namespace dtGame
+{
+[!if DMP_CHECKBOX]
+   class DefaultMessageProcessor;
+[!endif]
+[!if DNPC_CHECKBOX]
+   class DefaultNetworkPublishingComponent;
+[!endif]
+}
+[!endif]
+
+[!if INPUT_CHECKBOX]
+class [!output PROJECT_NAME]Input;
+[!endif]
+
 class ENTRY_POINT_EXPORT [!output PROJECT_NAME]EntryPoint : public dtGame::GameEntryPoint
 {
 public:
@@ -19,7 +36,15 @@ protected:
    virtual ~[!output PROJECT_NAME]EntryPoint();
 
 private:
-
+[!if DMP_CHECKBOX]
+   dtCore::RefPtr<dtGame::DefaultMessageProcessor> mMessageProc;
+[!endif]
+[!if DNPC_CHECKBOX]
+   dtCore::RefPtr<dtGame::DefaultNetworkPublishingComponent> mNetworkComponent;
+[!endif]
+[!if INPUT_CHECKBOX]
+   dtCore::RefPtr<[!output PROJECT_NAME]Input> mInputComponent;
+[!endif]
 };
 
 #endif // [!output PROJECT_NAME]EntryPoint_h__
