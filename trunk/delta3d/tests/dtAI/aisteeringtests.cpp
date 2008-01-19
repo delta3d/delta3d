539d03662e4d839c2397ef59cbcf906575fc2db9
banderegg <banderegg@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-01-19 19:23:59 +0000

    +a lot of general template cleanup code +added error handling to the steering behavoir (I may pull this out into a base class) +added an error handling unit test to the steering tests +added a templateutility header to dtUtil to avoid copied template code, this is probably a good home for future generic functors as well

--- trunk/delta3d/tests/dtAI/aisteeringtests.cpp
+++ trunk/delta3d/tests/dtAI/aisteeringtests.cpp
@@ -1,6 +1,5 @@
 /*
 * Delta3D Open Source Game and Simulation Engine
-* Copyright (C) 2006, Delta3D
 *
 * This library is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Lesser General Public License as published by the Free
@@ -20,6 +19,8 @@
 */
 #include <prefix/dtgameprefix-src.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <dtUtil/templateutility.h>
+#include <dtAI/sensor.h>
 #include <dtAI/steeringutility.h>
 #include <dtAI/steeringbehavior.h>
 #include <dtCore/refptr.h>
@@ -27,29 +28,91 @@
 namespace dtAI
 {
 
+   template <typename T>
+   struct ErrorHandler: public osg::Referenced
+   {
+      ErrorHandler(){}
+      ErrorHandler(typename dtUtil::TypeTraits<T>::param_type dataValue): mDataType(dataValue){}
 
+      void operator()(typename dtUtil::TypeTraits<T>::param_type p)
+      {
+         mDataType = p;
+      }
+
+      typename dtUtil::TypeTraits<T>::param_type GetValue()
+      {
+         return mDataType;
+      }
+
+      typename dtUtil::TypeTraits<T>::const_param_type GetValue() const
+      {
+         return mDataType;
+      }
+
+      private:
+         T mDataType;
+   };
+
+   struct EvaluateIncrement
+   {
+      void operator()(int& i)
+      {
+         ++i;
+      }
+
+   };
+
+   struct SensorState
+   {
+      SensorState(): mState(false){} 
+      void operator()(bool r)
+      {
+         mState = r;
+      }
+      bool mState;
+   };
+
+
+   struct CompareInt
+   {
+      bool operator()(bool& result, int first, int second)
+      {
+         //std::cerr << "CompareInt-  First:" << first << ", Second:" << second << std::endl << std::endl;
+         return result = (first == second);
+      }
+   };
+
+   struct GenericSensor
+   {
+      typedef Sensor<int, int, EvaluateIncrement, dtUtil::DoNothing<void, int>, CompareInt, SensorState*, bool> CompareSensor;
+   };
+
+   struct Steering 
+   {
+      typedef SteeringBehavior<KinematicGoal, Kinematic, SteeringOutput, GenericSensor::CompareSensor*, bool, ErrorHandler<bool>*> Steering::BehavoirBase;
+   };
 
    /**
-    *   To test the SteeringBehavoir API we create a simple derived class.
-    */
-   class TestSteeringBehavoir: public SteeringBehavior<KinematicGoal, Kinematic, SteeringOutput>
+   *   To test the SteeringBehavoir API we create a simple derived class.
+   */
+   class TestSteeringBehavoir: public Steering::BehavoirBase
    {
    public:
-      typedef SteeringBehavior<KinematicGoal, Kinematic, SteeringOutput> BaseClass;
+      typedef Steering::BehavoirBase BaseClass;
 
+      TestSteeringBehavoir(){}
 
-      /*virtual*/ void Think(float testNumber, BaseClass::ConstKinematicGoalParam current_goal, BaseClass::ConstKinematicParam current_state, BaseClass::SteeringOutByRefParam result)
+      /*virtual*/ void Think(float testNumber, Steering::BehavoirBase::ConstKinematicGoalParam current_goal, Steering::BehavoirBase::ConstKinematicParam current_state, Steering::BehavoirBase::SteeringOutByRefParam result)
       {
          result.mLinearVelocity.set(testNumber, 0.0f, 0.0f);
       }
 
-      /*virtual*/ bool GoalAchieved(BaseClass::ConstKinematicGoalParam current_goal, BaseClass::ConstKinematicParam current_state)
+      /*virtual*/ bool GoalAchieved(Steering::BehavoirBase::ConstKinematicGoalParam current_goal, Steering::BehavoirBase::ConstKinematicParam current_state)
       {
          return current_goal.GetLinearVelocity()[1] == current_state.mLinearVelocity[0];
       }
 
    };
-
 
 
    /// Math unit tests for dtUtil
@@ -58,6 +121,7 @@ namespace dtAI
       CPPUNIT_TEST_SUITE(AISteeringTests);
       CPPUNIT_TEST(TestKinematicGoal);
       CPPUNIT_TEST(TestSteeringBehavoirBaseClass);
+      CPPUNIT_TEST(TestSteeringBehavoirErrorHandling);
       CPPUNIT_TEST_SUITE_END();
 
    public:
@@ -66,15 +130,26 @@ namespace dtAI
 
       void TestKinematicGoal();
       void TestSteeringBehavoirBaseClass();
+      void TestSteeringBehavoirErrorHandling();
 
    private:
+
+      SensorState mState;
+      dtCore::RefPtr<GenericSensor::CompareSensor> mSensor;
+      dtCore::RefPtr<TestSteeringBehavoir> mSteeringBehavoir;
+      dtCore::RefPtr<ErrorHandler<bool> > mErrorHandler;
    };
 
    CPPUNIT_TEST_SUITE_REGISTRATION(AISteeringTests);
 
    void AISteeringTests::setUp()
    {
-   }
+      mSensor = new GenericSensor::CompareSensor(0, 10, EvaluateIncrement(), dtUtil::DoNothing<void, int>(), CompareInt(), &mState);
+      mSteeringBehavoir = new TestSteeringBehavoir();
+      
+      mErrorHandler = new ErrorHandler<bool>(false);
+      mSteeringBehavoir->AddErrorHandler(mSensor.get(), mErrorHandler.get());
+   } 
 
    void AISteeringTests::tearDown()
    {
@@ -83,7 +158,6 @@ namespace dtAI
    void AISteeringTests::TestSteeringBehavoirBaseClass()
    {
 
-      TestSteeringBehavoir tb;
       KinematicGoal kg;
       Kinematic k;
       SteeringOutput steer;
@@ -91,17 +165,17 @@ namespace dtAI
       const float TestNumber = 1.0f;
       kg.SetLinearVelocity(osg::Vec3(TestNumber, 0.0f, 0.0f));
       
-      tb.Think(TestNumber, kg, k, steer);
+      mSteeringBehavoir->Think(TestNumber, kg, k, steer); 
       
       CPPUNIT_ASSERT(steer.mLinearVelocity[0] == TestNumber);
-      CPPUNIT_ASSERT(tb.GoalAchieved(kg, k));
+      CPPUNIT_ASSERT(mSteeringBehavoir->GoalAchieved(kg, k));
    }
 
    void AISteeringTests::TestKinematicGoal()
    {  
       KinematicGoal kg;
 
-      CPPUNIT_ASSERT(!kg.HasPosition());
+      CPPUNIT_ASSERT(!kg.HasPosition()); 
       CPPUNIT_ASSERT(!kg.HasRotation());
       CPPUNIT_ASSERT(!kg.HasLinearVelocity());
       CPPUNIT_ASSERT(!kg.HasAngularVelocity());
@@ -115,5 +189,28 @@ namespace dtAI
       CPPUNIT_ASSERT(kg.HasRotation());
       CPPUNIT_ASSERT(kg.HasLinearVelocity());
       CPPUNIT_ASSERT(kg.HasAngularVelocity());
+   }
+
+   void AISteeringTests::TestSteeringBehavoirErrorHandling()
+   {
+      mErrorHandler->operator()(false);
+
+      for(int i = 0; i < 9; ++i)
+      {
+         mSteeringBehavoir->InvokeErrorHandling();
+         CPPUNIT_ASSERT_MESSAGE("Invoking Error Handling loop, no errors should be triggered until the sensor is triggered",!mErrorHandler->GetValue());
+      }
+
+      mSteeringBehavoir->InvokeErrorHandling();
+      CPPUNIT_ASSERT_MESSAGE("Error should have been triggered by sensors 10th iteration", mErrorHandler->GetValue());
+
+      //remove the error handler and assert that the sensor does not trigger an error
+      mErrorHandler->operator()(false);
+      CPPUNIT_ASSERT_MESSAGE("Resetting error handler to test remove handler", !mErrorHandler->GetValue()); 
+      mSteeringBehavoir->RemoveErrorHandler(mSensor.get(), mErrorHandler.get());
+
+      mSteeringBehavoir->InvokeErrorHandling();
+      CPPUNIT_ASSERT_MESSAGE("Invoking error handling should not trigger an error if the handler was succesfully removed", !mErrorHandler->GetValue()); 
+
    }
 }
