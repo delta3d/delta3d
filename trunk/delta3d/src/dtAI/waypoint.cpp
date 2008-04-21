9008d34b6f0007d7e1334f90c1d6878560df5a1a
mjguerre <mjguerre@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-04-21 21:45:06 +0000

    Added the ability to set custom colors on waypoints - rendering using the render flags still works by setting the custom color

--- trunk/delta3d/src/dtAI/waypoint.cpp
+++ trunk/delta3d/src/dtAI/waypoint.cpp
@@ -25,6 +25,7 @@
 #include <osg/Matrix>
 #include <osg/MatrixTransform>
 #include <dtUtil/mathdefines.h>
+#include <dtUtil/log.h>
 
 namespace dtAI
 {
@@ -91,11 +92,40 @@ namespace dtAI
    unsigned Waypoint::GetID() const
    {
       return mID;
+   }   
+
+   void Waypoint::SetRenderFlag(RenderFlag rf) const
+   {
+      mRenderFlag = rf;
+      SetColorFromRenderFlag();      
+   }
+
+   void Waypoint::SetColor(const osg::Vec3 &color)
+   {
+      mColor = color;
+
+      // Since we are setting a custom color, 
+      // change the render flag to match
+      mRenderFlag = RENDER_CUSTOM;
+   }
+
+   const osg::Vec3& Waypoint::GetColor() const
+   {
+      return mColor;
    }
 
    void Waypoint::SetGradient(float pGradient) const
    {
       mGradient = pGradient;
+
+      if (mRenderFlag != RENDER_CUSTOM)
+      {
+         SetColorFromRenderFlag();
+      }
+      else
+      {
+         LOG_WARNING("Setting gradient with the RENDER_CUSTOM flag has no effect");
+      }
    }
 
    float Waypoint::GetGradient() const
@@ -111,6 +141,40 @@ namespace dtAI
    float Waypoint::GetAlpha() const
    {
       return mAlpha;
+   }
+
+   void Waypoint::SetColorFromRenderFlag() const
+   {
+      switch(mRenderFlag)
+      {
+         case RENDER_DEFAULT:
+         {
+            mColor = osg::Vec3(1.0f, 1.0f, 1.0f) * mGradient;
+            break;
+         }
+         case RENDER_RED:
+         {
+            mColor = osg::Vec3(1.0f, 0.0f, 0.0f) * mGradient;
+            break;
+         }
+         case RENDER_GREEN:
+         {
+            mColor = osg::Vec3(0.0f, 1.0f, 0.0f) * mGradient;
+            break;
+         }
+         case RENDER_BLUE:
+         {
+            mColor = osg::Vec3(0.0f, 0.0f, 1.0f) * mGradient;
+            break;
+         }
+         case RENDER_CUSTOM:
+         {
+            LOG_WARNING("There is no need to manually set the render flag to RENDER_CUSTOM");
+            break;
+         }
+         default:
+            LOG_WARNING("Trying to set unknown render flag.");
+      }
    }
 
 }//namespace 
