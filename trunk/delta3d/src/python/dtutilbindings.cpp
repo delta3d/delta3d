3d84ffc47ebe956c95f024cfef52e450f6821704
djmccue <djmccue@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-04-22 00:13:33 +0000

    added DateTime bindings back in

--- trunk/delta3d/src/python/dtutilbindings.cpp
+++ trunk/delta3d/src/python/dtutilbindings.cpp
@@ -4,11 +4,36 @@
 
 #include <boost/python.hpp>
 
+// for date/time
+#include <python/dtpython.h>
+#include <dtUtil/datetime.h>
+
 void initEnumerationBindings();
 void init_NoiseBindings();
 void init_MatrixUtilBindings();
 void init_PolarDecompBindings();
 void init_LogBindings();
+
+void init_DateTimeBindings()
+{
+   using namespace boost::python;
+
+   // todo -- find a better home for this
+   class_<dtUtil::DateTime>("DateTime")
+      .def("GetYear",   &dtUtil::DateTime::GetYear)
+      .def("SetYear",   &dtUtil::DateTime::SetYear)
+      .def("GetMonth",  &dtUtil::DateTime::GetMonth)
+      .def("SetMonth",  &dtUtil::DateTime::SetMonth)
+      .def("GetDay",    &dtUtil::DateTime::GetDay)
+      .def("SetDay",    &dtUtil::DateTime::SetDay)
+      .def("GetHour",   &dtUtil::DateTime::GetHour)
+      .def("SetHour",   &dtUtil::DateTime::SetHour)
+      .def("GetMinute", &dtUtil::DateTime::GetMinute)
+      .def("SetMinute", &dtUtil::DateTime::SetMinute)
+      .def("GetSecond", &dtUtil::DateTime::GetSecond)
+      .def("SetSecond", &dtUtil::DateTime::SetSecond)
+      ;
+}
 
 BOOST_PYTHON_MODULE(PyDtUtil)
 {
@@ -17,4 +42,5 @@ BOOST_PYTHON_MODULE(PyDtUtil)
    init_MatrixUtilBindings();
    init_PolarDecompBindings();
    init_LogBindings();
+   init_DateTimeBindings();
 }
