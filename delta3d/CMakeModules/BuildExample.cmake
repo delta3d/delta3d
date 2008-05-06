43c2ed50542562eef38f2bf8ce82a9b2c73ff7b3
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-05-06 22:32:53 +0000

    Added new cmake utility to build the console example executables and converted the testApp CMakeLists.txt to use it.

---
+++ delta3d/CMakeModules/BuildExample.cmake
@@ -0,0 +1,23 @@
+#Build a Delta3D console app executable.
+#Expects files to be compiled in TARGET_SRC and TARGET_H.
+#Supply the target name as the first parameter.  Additional
+#parameters will be passed to LINK_WITH_VARIABLES
+MACRO(BUILD_EXE_EXAMPLE TGTNAME)
+  
+  ADD_EXECUTABLE(${TGTNAME} ${TARGET_SRC} ${TARGET_H})
+  
+  FOREACH(varname ${ARGN})
+      LINK_WITH_VARIABLES( ${TGTNAME} ${varname} )
+  ENDFOREACH(varname)
+  
+  #little hack to get the .dll into /bin and .lib into /lib
+  IF (MSVC)
+    SET_TARGET_PROPERTIES(${TGTNAME} PROPERTIES PREFIX "/../../bin/")
+    SET_TARGET_PROPERTIES(${TGTNAME} PROPERTIES IMPORT_PREFIX "../")
+  ENDIF (MSVC)
+  
+  IF (WIN32)
+    SET_TARGET_PROPERTIES(${TGTNAME} PROPERTIES DEBUG_POSTFIX "${CMAKE_DEBUG_POSTFIX}")
+  ENDIF (WIN32)
+
+ENDMACRO(BUILD_EXE_EXAMPLE)
