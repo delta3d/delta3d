c8ecb5caef5b2e6dec62cffd184d698908da90e4
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-05-07 20:11:20 +0000

    Merged in some user contributions to make it a little easier to build dtHLA with CERTI.

---
+++ trunk/delta3d/CMakeModules/FindRTICERTI.cmake
@@ -0,0 +1,46 @@
+# Locate CERTI RTI
+# This module defines
+# RTIS_LIBRARY
+# RTIS_FOUND, if false, do not try to link to OSG
+# RTIS_INCLUDE_DIR, where to find the headers
+
+find_path(RTIS_INCLUDE_DIR
+   NAMES
+      RTI.hh
+   PATHS
+      /usr/local/include
+	$ENV{CERTI_HOME}/include
+)
+
+find_library(RTIS_LIBRARY
+   NAMES
+      RTI
+   PATHS
+      /usr/local/lib 
+	$ENV{CERTI_HOME}/release
+)
+
+find_library(RTIS_RTI_LIBRARY
+   NAMES
+      CERTI
+   PATHS
+      /usr/local/lib 
+	$ENV{CERTI_HOME}/release
+)
+
+if(UNIX)
+find_library(RTIS_PARSER_LIBRARY
+   NAMES
+      xml2
+   PATHS
+      /usr/local/lib 
+)
+endif(UNIX)
+
+set(RTIS_FOUND "NO")
+if(RTIS_LIBRARY AND RTIS_INCLUDE_DIR)
+    set(RTIS_FOUND "YES")
+    SET(RTIS_LIBRARIES
+           RTIS_LIBRARY
+           RTIS_RTI_LIBRARY)
+endif(RTIS_LIBRARY AND RTIS_INCLUDE_DIR)
