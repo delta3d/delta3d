5194ca7aa4e3044c41d097dc3eb20cc74e39381b
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-06-05 02:07:26 +0000

    Had to change the CEGUI data file paths to support both dtGUI::HUD and dtGUI::CEUIDrawable at the same time.
    Note: if you're relying on the examples/data/gui files, you will have to add additional search paths.  See testgui.cpp for an example.

--- delta3d/examples/testGUI/testgui.cpp
+++ delta3d/examples/testGUI/testgui.cpp
@@ -209,6 +209,10 @@ int main( int argc, const char* argv[] )
    std::string dataPath = dtCore::GetDeltaDataPathList();
    dtCore::SetDataFilePathList(dataPath + ";" +
                                dtCore::GetDeltaRootPath() + "/examples/data;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/imagesets;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/schemes;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/fonts;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/looknfeel;" +
                                dtCore::GetDeltaRootPath() + "/examples/testGUI;");
 
    std::string filename;
