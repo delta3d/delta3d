01a7b2bc319777ada6e935cc3cd9100892dde1b8
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-06-09 19:31:36 +0000

    Now better handles the generation of debug Python bindings.  The bindings are now generated to bin/release and bin/debug and use the same filename regardless of the configuration.  Users will have to specify which configuration of the bindings they wish to use by supplying a different path to the PYTHON_LIB_PATH (PATH?) environment.
    Refactored the CMake python files by adding a new CMake macro to aid in building the binding projects.

---
+++ trunk/delta3d/CMakeModules/BuildBinding.cmake
@@ -0,0 +1,23 @@
+#Build a Delta3D Python binding.
+#Expects files to be compiled in TARGET_SRC and TARGET_H.
+#Supply the target name as the first parameter.  Additional
+#parameters will be passed to LINK_WITH_VARIABLES
+MACRO (BUILD_PYTHON_BINDING TGTNAME)
+  
+  ADD_LIBRARY( ${TGTNAME} SHARED ${TARGET_SRC} ${TARGET_H} ) 
+  
+  FOREACH(varname ${ARGN})
+      LINK_WITH_VARIABLES( ${TGTNAME} ${varname} )
+  ENDFOREACH(varname)
+
+  IF (WIN32)
+    SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES SUFFIX ".pyd")
+    SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES DEBUG_POSTFIX "")
+  ENDIF (WIN32)
+  
+  #little hack to get the .dll into /bin and .lib into /lib
+  IF (MSVC)
+    SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES PREFIX "/../../bin/$(ConfigurationName)/")
+  ENDIF (MSVC)
+
+ENDMACRO(BUILD_PYTHON_BINDING)
