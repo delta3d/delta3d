efa6911957309e096fc4dbb61f7cd5c499958af4
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-06-12 16:10:08 +0000

    Fixed broken build by patching up the flymotionmodelbindings.cpp to match the changed C++ API.

--- delta3d/src/python/flymotionmodelbindings.cpp
+++ delta3d/src/python/flymotionmodelbindings.cpp
@@ -18,7 +18,16 @@ void initFlyMotionModelBindings()
    FlyMotionModel* (*FlyMotionModelGI1)(int) = &FlyMotionModel::GetInstance;
    FlyMotionModel* (*FlyMotionModelGI2)(std::string) = &FlyMotionModel::GetInstance;
 
-   class_<FlyMotionModel, bases<MotionModel>, dtCore::RefPtr<FlyMotionModel> >("FlyMotionModel", init<optional<Keyboard*, Mouse*> >())
+   Axis* (FlyMotionModel::*GetTurnUpDownAxis1)() = &FlyMotionModel::GetTurnUpDownAxis;
+   const Axis* (FlyMotionModel::*GetTurnUpDownAxis2)() const = &FlyMotionModel::GetTurnUpDownAxis;
+
+   Axis* (FlyMotionModel::*GetForwardBackwardAxis1)() = &FlyMotionModel::GetFlyForwardBackwardAxis;
+   const Axis* (FlyMotionModel::*GetForwardBackwardAxis2)() const = &FlyMotionModel::GetFlyForwardBackwardAxis;
+
+   Axis* (FlyMotionModel::*GetTurnLeftRightAxis1)() = &FlyMotionModel::GetTurnLeftRightAxis;
+   const Axis* (FlyMotionModel::*GetTurnLeftRightAxis2)() const = &FlyMotionModel::GetTurnLeftRightAxis;
+
+   class_<FlyMotionModel, bases<MotionModel>, dtCore::RefPtr<FlyMotionModel> >("FlyMotionModel", init<optional<Keyboard*, Mouse*, FlyMotionModel::BehaviorOptions> >())
       .def("GetInstanceCount", &FlyMotionModel::GetInstanceCount)
       .staticmethod("GetInstanceCount")
       .def("GetInstance", FlyMotionModelGI1, return_internal_reference<>())
@@ -26,13 +35,25 @@ void initFlyMotionModelBindings()
       .staticmethod("GetInstance")
       .def("SetDefaultMappings", &FlyMotionModel::SetDefaultMappings)
       .def("SetFlyForwardBackwardAxis", &FlyMotionModel::SetFlyForwardBackwardAxis)
-      .def("GetFlyForwardBackwardAxis", &FlyMotionModel::GetFlyForwardBackwardAxis, return_internal_reference<>())
+      .def("GetFlyForwardBackwardAxis", GetForwardBackwardAxis1, return_internal_reference<>())
+      .def("GetFlyForwardBackwardAxis", GetForwardBackwardAxis2, return_internal_reference<>())
       .def("SetTurnLeftRightAxis", &FlyMotionModel::SetTurnLeftRightAxis)
-      .def("GetTurnLeftRightAxis", &FlyMotionModel::GetTurnLeftRightAxis, return_internal_reference<>())
+      .def("GetTurnLeftRightAxis", GetTurnLeftRightAxis1, return_internal_reference<>())
+      .def("GetTurnLeftRightAxis", GetTurnLeftRightAxis2, return_internal_reference<>())
       .def("SetTurnUpDownAxis", &FlyMotionModel::SetTurnUpDownAxis)
-      .def("GetTurnUpDownAxis", &FlyMotionModel::GetTurnUpDownAxis, return_internal_reference<>())
+      .def("GetTurnUpDownAxis", GetTurnUpDownAxis1, return_internal_reference<>())
+      .def("GetTurnUpDownAxis", GetTurnUpDownAxis2, return_internal_reference<>())
       .def("SetMaximumFlySpeed", &FlyMotionModel::SetMaximumFlySpeed)
       .def("GetMaximumFlySpeed", &FlyMotionModel::GetMaximumFlySpeed)
       .def("SetMaximumTurnSpeed", &FlyMotionModel::SetMaximumTurnSpeed)
       .def("GetMaximumTurnSpeed", &FlyMotionModel::GetMaximumTurnSpeed);
+
+   enum_<FlyMotionModel::BehaviorOptions>("BehaviorOptions")
+      .value("OPTIONS_NONE", FlyMotionModel::OPTION_NONE)
+      .value("OPTION_USE_SIMTIME_FOR_SPEED", FlyMotionModel::OPTION_USE_SIMTIME_FOR_SPEED)
+      .value("OPTION_REQUIRE_MOUSE_DOWN", FlyMotionModel::OPTION_REQUIRE_MOUSE_DOWN)
+      .value("OPTION_RESET_MOUSE_CURSOR", FlyMotionModel::OPTION_RESET_MOUSE_CURSOR)
+      .value("OPTION_USE_CURSOR_KEYS", FlyMotionModel::OPTION_USE_CURSOR_KEYS)
+      .value("OPTION_DEFAULT", FlyMotionModel::OPTION_DEFAULT)
+      .export_values();
 }
