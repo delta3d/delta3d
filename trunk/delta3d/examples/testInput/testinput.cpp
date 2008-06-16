77098a68637d11625639728043f37f376f050fbe
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-06-16 18:00:49 +0000

    Fixed a problem that would cause testInput to not find the CEGUI files. Added the CEGUI layout file to the project.

--- trunk/delta3d/examples/testInput/testinput.cpp
+++ trunk/delta3d/examples/testInput/testinput.cpp
@@ -381,6 +381,10 @@ int main( int argc, char **argv )
    std::string dataPath = dtCore::GetDeltaDataPathList();
    dtCore::SetDataFilePathList(dataPath + ";" + 
                                dtCore::GetDeltaRootPath() + "/examples/data" + ";" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/imagesets;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/schemes;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/fonts;" +
+                               dtCore::GetDeltaRootPath() + "/examples/data/gui/looknfeel;" +
                                dtCore::GetDeltaRootPath() + "/examples/testInput");
 
    app = new TestInputApp( "config.xml" );
