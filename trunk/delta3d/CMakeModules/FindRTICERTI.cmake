e92630ee6f025958bcfcad60d43a0a5177c2de50
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-06-27 19:34:47 +0000

    Added more formal certi support.  it's now a build option.  I also changed the varibles to not be RTIS_... but just RTI_...

--- trunk/delta3d/CMakeModules/FindRTICERTI.cmake
+++ trunk/delta3d/CMakeModules/FindRTICERTI.cmake
@@ -1,34 +1,38 @@
 # Locate CERTI RTI
 # This module defines
-# RTIS_LIBRARY
-# RTIS_FOUND, if false, do not try to link to OSG
-# RTIS_INCLUDE_DIR, where to find the headers
+# RTI_LIBRARY
+# RTI_FOUND, if false, do not try to link to OSG
+# RTI_INCLUDE_DIR, where to find the headers
 
-find_path(RTIS_INCLUDE_DIR
+find_path(RTI_INCLUDE_DIR
    NAMES
       RTI.hh
    PATHS
+      ${DELTA3D_EXT_DIR}/inc/CERTI
       /usr/local/include
       /usr/include
-	$ENV{CERTI_HOME}/include
+   	$ENV{CERTI_HOME}/include
 )
 
-find_library(RTIS_LIBRARY
+SET(CERTI_PATHS       
+      ${DELTA3D_EXT_DIR}/lib
+      /usr/local/lib 
+      /usr/lib 
+  	   $ENV{CERTI_HOME}/release)
+
+
+find_library(RTI_LIBRARY
    NAMES
       RTI
    PATHS
-      /usr/local/lib 
-      /usr/lib 
-	$ENV{CERTI_HOME}/release
+      ${CERTI_PATHS}
 )
 
-find_library(RTIS_RTI_LIBRARY
+find_library(RTI_RTI_LIBRARY
    NAMES
       CERTI
    PATHS
-      /usr/local/lib 
-      /usr/lib 
-	$ENV{CERTI_HOME}/release
+      ${CERTI_PATHS}
 )
 
 if(UNIX)
@@ -41,12 +45,12 @@ find_library(RTIS_PARSER_LIBRARY
 )
 endif(UNIX)
 
-set(RTIS_FOUND "NO")
-if(RTIS_LIBRARY AND RTIS_INCLUDE_DIR)
-    set(RTIS_FOUND "YES")
-    SET(RTIS_LIBRARIES
-           ${RTIS_LIBRARY}
-           ${RTIS_RTI_LIBRARY}
-           ${RTIS_PARSER_LIBRARY}
+set(RTI_FOUND "NO")
+if(RTI_LIBRARY AND RTI_INCLUDE_DIR)
+    set(RTI_FOUND "YES")
+    SET(RTI_LIBRARIES
+           ${RTI_LIBRARY}
+           ${RTI_RTI_LIBRARY}
+           ${RTI_PARSER_LIBRARY}
     )
-endif(RTIS_LIBRARY AND RTIS_INCLUDE_DIR)
+endif(RTI_LIBRARY AND RTI_INCLUDE_DIR)
