7ffe976e1161e8e45674a9342fca99b08e5e5cb7
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-07-25 00:05:09 +0000

    Cleaned up the Camera unit tests for readability.  Shaved a couple seconds of compile time off by removing unused #includes and dead code.  Unit test are now more localized and don't mess with the GlobalApplication unless required.
    No functionality change, just code cleanup.

--- trunk/delta3d/tests/dtCore/cameratests.cpp
+++ trunk/delta3d/tests/dtCore/cameratests.cpp
@@ -20,18 +20,11 @@
  */
 #include <prefix/dtgameprefix-src.h>
 
-#include <dtUtil/log.h>
 #include <dtUtil/fileutils.h>
-#include <dtUtil/exception.h>
-#include <dtUtil/macros.h>
 
 #include <dtCore/refptr.h>
-#include <dtCore/view.h>
-#include <dtCore/scene.h>
 #include <dtCore/system.h>
-#include <dtCore/globals.h>
 #include <dtCore/camera.h>
-#include <dtCore/deltawin.h>
 #include <dtABC/application.h>
 
 #include <cppunit/extensions/HelperMacros.h>
@@ -40,13 +33,6 @@
 
 extern dtABC::Application& GetGlobalApplication();
 
-#ifdef DELTA_WIN32
-   #include <Windows.h>
-   #define SLEEP(milliseconds) Sleep((milliseconds))
-#else
-   #include <unistd.h>
-   #define SLEEP(milliseconds) usleep(((milliseconds) * 1000))
-#endif
 
 class CameraTests : public CPPUNIT_NS::TestFixture
 {
@@ -55,141 +41,33 @@ class CameraTests : public CPPUNIT_NS::TestFixture
          CPPUNIT_TEST(TestSaveScreenShot);
          CPPUNIT_TEST(TestPerspective);
          CPPUNIT_TEST(TestFrustum);
+         CPPUNIT_TEST(TestEnabled);
+         CPPUNIT_TEST(TestSettingTheCullingMode);
          CPPUNIT_TEST(TestSupplyingOSGCameraToConstructor);
    
       CPPUNIT_TEST_SUITE_END();
    
    public:
 
-      void setUp()
-      {
-         try
-         {
-            dtCore::SetDataFilePathList(dtCore::GetDeltaDataPathList());
-
-            mApp = &GetGlobalApplication();
-            mScene = mApp->GetScene();
-            mCamera = mApp->GetCamera();
-            mWin = mApp->GetWindow();
-            
-            dtCore::System::GetInstance().SetShutdownOnWindowClose(false);
-            dtCore::System::GetInstance().Start();            
-         }
-         catch (const dtUtil::Exception& e)
-         {
-            CPPUNIT_FAIL((std::string("Error: ") + e.What()).c_str());
-         }
-         catch (const std::exception& ex)
-         {
-            CPPUNIT_FAIL(std::string("Error: ") + ex.what());
-         }
-      }
+      void setUp() {}
       
       void tearDown()
-      {
-         dtCore::System::GetInstance().Stop();
-         mApp = NULL;
-         mScene = NULL;
-         mCamera = NULL;
-         mWin = NULL;
-      
+      {      
          dtUtil::FileUtils& fileUtils = dtUtil::FileUtils::GetInstance();
          
          if (fileUtils.DirExists(SCREEN_SHOT_DIR))
             fileUtils.DirDelete(SCREEN_SHOT_DIR, true);         
       }
       
-      void TestSaveScreenShot()
-      {
-         dtUtil::FileUtils& fileUtils = dtUtil::FileUtils::GetInstance();
-         fileUtils.MakeDirectory(SCREEN_SHOT_DIR);
-         const std::string prefix = SCREEN_SHOT_DIR + "/Mojo";
-         const std::string result = mCamera->TakeScreenShot(prefix);
-
-         CPPUNIT_ASSERT_MESSAGE("The resulting string should be longer.", result.size() > prefix.size());
-         
-         CPPUNIT_ASSERT_MESSAGE("The result should begin with the prefix.", 
-            prefix == result.substr(0,prefix.size()));
-         
-         CPPUNIT_ASSERT_MESSAGE(result + " should not exist yet.", !fileUtils.FileExists(result));
-         
-         dtCore::System::GetInstance().Step();
-         CPPUNIT_ASSERT_MESSAGE(result + " should exist.  Check for the jpeg osg plugin.", fileUtils.FileExists(result));
-   
-         fileUtils.DirDelete(SCREEN_SHOT_DIR, true);
-
-         dtCore::System::GetInstance().Step();
-         CPPUNIT_ASSERT_MESSAGE("A screenshot should not have been written again.", !fileUtils.FileExists(result));
-      }
-
-      void TestPerspective()
-      {
-         double vfovSet = 60.0;
-         double aspectSet = 1.33;
-         double nearSet = 1.0;
-         double farSet = 10000.0;
-         mCamera->SetPerspectiveParams(vfovSet, aspectSet, nearSet, farSet);
-
-         double vfov, aspectRatio, nearClip, farClip;
-         mCamera->GetPerspectiveParams(vfov, aspectRatio, nearClip, farClip);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The vertical field of view should be the same as the one set", 
-                  vfovSet, vfov, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The aspect ratio should be the same as the one set", 
-                  aspectSet, aspectRatio, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The near plane should be the same as the one set", 
-                  nearSet, nearClip, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The far plane should be the same as the one set", 
-                  farSet, farClip, 0.01);
-         
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The aspect ratio method should return the one set", 
-                  aspectSet, mCamera->GetAspectRatio(), 0.01);
-
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The vertical fov method should return the one set", 
-                  vfovSet, mCamera->GetVerticalFov(), 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The horizontal fov method should return vertical fov * aspectRatio.", 
-                  vfovSet * aspectSet, mCamera->GetHorizontalFov(), 0.01);
-      }
-
-      void TestFrustum()
-      {
-         double leftSet = 0.8, rightSet = -0.8;
-         double topSet = 0.8, bottomSet = -0.8;
-         double nearSet = 1.0;
-         double farSet = 10000.0;
-         mCamera->SetFrustum(leftSet, rightSet, bottomSet, topSet, nearSet, farSet);
-
-         double left, right, bottom, top, nearClip, farClip;
-         mCamera->GetFrustum(left, right, bottom, top, nearClip, farClip);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The left plane should be the same as the one set", 
-                  leftSet, left, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The right plane should be the same as the one set", 
-                  rightSet, right, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The top plane should be the same as the one set", 
-                  topSet, top, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The bottom plane should be the same as the one set", 
-                  bottomSet, bottom, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The near plane should be the same as the one set", 
-                  nearSet, nearClip, 0.01);
-         CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The far plane should be the same as the one set", 
-                  farSet, farClip, 0.01);
-      }
-
-      void TestSupplyingOSGCameraToConstructor()
-      {
-         osg::ref_ptr<osg::Camera> osgCam = new osg::Camera();
-         dtCore::RefPtr<dtCore::Camera> camera = new dtCore::Camera(*osgCam);
-
-         CPPUNIT_ASSERT_EQUAL_MESSAGE("Camera didn't return back the supplied OSG Camera",
-                                       osgCam.get(), camera->GetOSGCamera() );
-      }
+      void TestSaveScreenShot();
+      void TestPerspective();
+      void TestFrustum();
+      void TestSupplyingOSGCameraToConstructor();
+      void TestEnabled();
+      void TestSettingTheCullingMode();
 
    private:
-      static const std::string SCREEN_SHOT_DIR;
-      
-      dtCore::RefPtr<dtCore::Scene> mScene;
-      dtCore::RefPtr<dtCore::Camera> mCamera;
-      dtCore::RefPtr<dtCore::DeltaWin> mWin;
-      dtCore::RefPtr<dtABC::Application> mApp;
+      static const std::string SCREEN_SHOT_DIR;      
 };
 
 const std::string CameraTests::SCREEN_SHOT_DIR("TestScreenShot");
@@ -198,32 +76,115 @@ const std::string CameraTests::SCREEN_SHOT_DIR("TestScreenShot");
 CPPUNIT_TEST_SUITE_REGISTRATION(CameraTests);
 
 
-
-class CameraTests2 : public CPPUNIT_NS::TestFixture
+void CameraTests::TestSaveScreenShot()
 {
-   CPPUNIT_TEST_SUITE(CameraTests2);
-      CPPUNIT_TEST(TestEnabled);
-      CPPUNIT_TEST(TestSettingTheCullingMode);
-   CPPUNIT_TEST_SUITE_END();
-public:
-   void setup() {}
-   void tearDown() {}
-   void TestEnabled();
-   void TestSettingTheCullingMode();
+   //uses the GlobalApplication.
 
-};
+   dtUtil::FileUtils& fileUtils = dtUtil::FileUtils::GetInstance();
+   fileUtils.MakeDirectory(SCREEN_SHOT_DIR);
+   const std::string prefix = SCREEN_SHOT_DIR + "/Mojo";
+   const std::string result = GetGlobalApplication().GetCamera()->TakeScreenShot(prefix);
 
-// Registers the fixture into the 'registry'
-CPPUNIT_TEST_SUITE_REGISTRATION(CameraTests2);
+   CPPUNIT_ASSERT_MESSAGE("The resulting string should be longer.", result.size() > prefix.size());
+
+   CPPUNIT_ASSERT_MESSAGE("The result should begin with the prefix.", 
+      prefix == result.substr(0,prefix.size()));
+
+   CPPUNIT_ASSERT_MESSAGE(result + " should not exist yet.", !fileUtils.FileExists(result));
+
+   const bool wasStarted = dtCore::System::GetInstance().IsRunning();
+   if (wasStarted == false)
+   {
+      dtCore::System::GetInstance().Start();
+   }
+
+   dtCore::System::GetInstance().Step();
+   CPPUNIT_ASSERT_MESSAGE(result + " should exist.  Check for the jpeg osg plugin.", fileUtils.FileExists(result));
+
+   fileUtils.DirDelete(SCREEN_SHOT_DIR, true);
+
+   dtCore::System::GetInstance().Step();
+   CPPUNIT_ASSERT_MESSAGE("A screenshot should not have been written again.", !fileUtils.FileExists(result));
+
+   if (wasStarted == false)
+   {
+      dtCore::System::GetInstance().Stop();
+   }
+}
+
+void CameraTests::TestPerspective()
+{
+   double vfovSet = 60.0;
+   double aspectSet = 1.33;
+   double nearSet = 1.0;
+   double farSet = 10000.0;
+
+   dtCore::RefPtr<dtCore::Camera> camera = new dtCore::Camera();
+   camera->SetPerspectiveParams(vfovSet, aspectSet, nearSet, farSet);
+
+   double vfov, aspectRatio, nearClip, farClip;
+   camera->GetPerspectiveParams(vfov, aspectRatio, nearClip, farClip);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The vertical field of view should be the same as the one set", 
+      vfovSet, vfov, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The aspect ratio should be the same as the one set", 
+      aspectSet, aspectRatio, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The near plane should be the same as the one set", 
+      nearSet, nearClip, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The far plane should be the same as the one set", 
+      farSet, farClip, 0.01);
+
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The aspect ratio method should return the one set", 
+      aspectSet, camera->GetAspectRatio(), 0.01);
+
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The vertical fov method should return the one set", 
+      vfovSet, camera->GetVerticalFov(), 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The horizontal fov method should return vertical fov * aspectRatio.", 
+      vfovSet * aspectSet, camera->GetHorizontalFov(), 0.01);
+}
+
+void CameraTests::TestFrustum()
+{
+   double leftSet = 0.8, rightSet = -0.8;
+   double topSet = 0.8, bottomSet = -0.8;
+   double nearSet = 1.0;
+   double farSet = 10000.0;
+
+   dtCore::RefPtr<dtCore::Camera> camera = new dtCore::Camera();
+   camera->SetFrustum(leftSet, rightSet, bottomSet, topSet, nearSet, farSet);
+
+   double left, right, bottom, top, nearClip, farClip;
+   camera->GetFrustum(left, right, bottom, top, nearClip, farClip);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The left plane should be the same as the one set", 
+      leftSet, left, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The right plane should be the same as the one set", 
+      rightSet, right, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The top plane should be the same as the one set", 
+      topSet, top, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The bottom plane should be the same as the one set", 
+      bottomSet, bottom, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The near plane should be the same as the one set", 
+      nearSet, nearClip, 0.01);
+   CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("The far plane should be the same as the one set", 
+      farSet, farClip, 0.01);
+}
+
+void CameraTests::TestSupplyingOSGCameraToConstructor()
+{
+   osg::ref_ptr<osg::Camera> osgCam = new osg::Camera();
+   dtCore::RefPtr<dtCore::Camera> camera = new dtCore::Camera(*osgCam);
+
+   CPPUNIT_ASSERT_EQUAL_MESSAGE("Camera didn't return back the supplied OSG Camera",
+      osgCam.get(), camera->GetOSGCamera() );
+}
 
 
-void CameraTests2::TestEnabled()
+void CameraTests::TestEnabled()
 {
    using namespace dtCore;
    RefPtr<Camera> cam = new Camera();
    CPPUNIT_ASSERT_EQUAL_MESSAGE("Camera should be enabled", true, cam->GetEnabled());
    CPPUNIT_ASSERT_MESSAGE("Node mask should not be 0x0", unsigned(0x0) != cam->GetOSGCamera()->getNodeMask() );
-   
+
    cam->SetEnabled(false);
    CPPUNIT_ASSERT_EQUAL_MESSAGE("Camera should be disabled", false, cam->GetEnabled());
 
@@ -238,25 +199,10 @@ void CameraTests2::TestEnabled()
    cam->SetEnabled(true);
 
    CPPUNIT_ASSERT_EQUAL_MESSAGE("Node mask should be what it was set to before being disabled",
-                                 nodeMask, cam->GetOSGCamera()->getNodeMask());
-
-   RefPtr<DeltaWin> win = new DeltaWin();
-   cam->SetWindow(win.get());
-
-   RefPtr<View> view = new View();
-   view->SetCamera(cam.get());
-   
-   RefPtr<Scene> scene = new Scene();
-   view->SetScene(scene.get());
-
-   
-   System::GetInstance().Config();
-   System::GetInstance().Start();
-   System::GetInstance().Step();
-
+      nodeMask, cam->GetOSGCamera()->getNodeMask());
 }
 
-void CameraTests2::TestSettingTheCullingMode()
+void CameraTests::TestSettingTheCullingMode()
 {
    dtCore::RefPtr<dtCore::Camera> cam = new dtCore::Camera();
    osg::Camera *osgCam = cam->GetOSGCamera();
@@ -264,20 +210,18 @@ void CameraTests2::TestSettingTheCullingMode()
    cam->SetNearFarCullingMode(dtCore::Camera::NO_AUTO_NEAR_FAR);
 
    CPPUNIT_ASSERT_EQUAL_MESSAGE("should be DO_NOT_COMPUTE_NEAR_FAR", 
-                                 osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR, 
-                                 osgCam->getComputeNearFarMode() );
+      osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR, 
+      osgCam->getComputeNearFarMode() );
 
    cam->SetNearFarCullingMode(dtCore::Camera::BOUNDING_VOLUME_NEAR_FAR);
 
    CPPUNIT_ASSERT_EQUAL_MESSAGE("should be COMPUTE_NEAR_FAR_USING_BOUNDING_VOLUMES", 
-                                 osg::CullSettings::COMPUTE_NEAR_FAR_USING_BOUNDING_VOLUMES, 
-                                 osgCam->getComputeNearFarMode() );
+      osg::CullSettings::COMPUTE_NEAR_FAR_USING_BOUNDING_VOLUMES, 
+      osgCam->getComputeNearFarMode() );
 
    cam->SetNearFarCullingMode(dtCore::Camera::PRIMITIVE_NEAR_FAR);
 
    CPPUNIT_ASSERT_EQUAL_MESSAGE("should be COMPUTE_NEAR_FAR_USING_PRIMITIVES", 
-                                 osg::CullSettings::COMPUTE_NEAR_FAR_USING_PRIMITIVES, 
-                                 osgCam->getComputeNearFarMode() );
+      osg::CullSettings::COMPUTE_NEAR_FAR_USING_PRIMITIVES, 
+      osgCam->getComputeNearFarMode() );
 }
-
-
