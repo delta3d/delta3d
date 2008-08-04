09c15578b0a130618a44b5f8b00bc1a5e82a4be4
David Guthrie <LEgregius@users.noreply.github.com>
2008-08-04 22:09:12 +0000

    fixed overqualification compile error

--- delta3d/inc/dtAnim/characterfilehandler.h
+++ delta3d/inc/dtAnim/characterfilehandler.h
@@ -1,21 +1,21 @@
-/* 
- * Delta3D Open Source Game and Simulation Engine 
- * Copyright (C) 2007 MOVES Institute 
+/*
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2007 MOVES Institute
  *
  * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either version 2.1 of the License, or (at your option) 
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this library; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
- * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
  * Erik Johnson
  * Bradley G Anderegg
  */
@@ -90,7 +90,7 @@ namespace dtAnim
       static const std::string SEQUENCE_ELEMENT;
       static const std::string POSEMESH_ELEMENT;
       static const std::string NAME_ELEMENT;
-      static const std::string FILENAME_ELEMENT;      
+      static const std::string FILENAME_ELEMENT;
       static const std::string ANIMATION_NAME_ELEMENT;
       static const std::string START_DELAY_ELEMENT;
       static const std::string FADE_IN_ELEMENT;
@@ -101,7 +101,7 @@ namespace dtAnim
       static const std::string IS_LOOPING_ELEMENT;
       static const std::string IS_ACTION_ELEMENT;
       static const std::string CHILD_ELEMENT;
-      
+
       static const std::string SKINNING_SHADER_ELEMENT;
       static const std::string SHADER_GROUP_ELEMENT;
       static const std::string SHADER_NAME_ELEMENT;
@@ -112,8 +112,8 @@ namespace dtAnim
       static const std::string LOD_END_DISTANCE_ELEMENT;
       static const std::string MAX_VISIBLE_DISTANCE_ELEMENT;
 
-	  static const std::string CharacterFileHandler::SCALE_ELEMENT;
-	  static const std::string CharacterFileHandler::SCALE_FACTOR_ELEMENT;
+	  static const std::string SCALE_ELEMENT;
+	  static const std::string SCALE_FACTOR_ELEMENT;
 
 
    public:
@@ -143,13 +143,13 @@ namespace dtAnim
       //need these definitions to properly export a std::vector<std::string>
       template class DT_ANIM_EXPORT std::allocator<std::string>;
       template class DT_ANIM_EXPORT std::vector<std::string>;
-	
+
 	  // disable warning for stl classes "needs to have dll-interface to be used by clients of class"
 	  #pragma warning(disable : 4251)
 #endif
 
       ///structure to contain all info related to an animation
-      struct AnimationStruct 
+      struct AnimationStruct
       {
          std::string mFileName; ///<The filename of the cal3D animation
          std::string mName;     ///<The user friendly name of this animation
@@ -185,7 +185,7 @@ namespace dtAnim
          bool mIsLooping, mIsAction;
       };
 
-      struct AnimationSequenceStruct : public AnimatableStruct 
+      struct AnimationSequenceStruct : public AnimatableStruct
       {
          AnimationSequenceStruct();
 
@@ -204,9 +204,9 @@ namespace dtAnim
       double mLODStartDistance, mLODEndDistance, mLODMaxVisibleDistance;
       bool mFoundLODOptions;
 
-	  bool  mFoundScale; 
+	  bool  mFoundScale;
 	  float mScale; ///< The scaling factor
-      
+
       std::vector<AnimationChannelStruct> mAnimationChannels; ///<The preconfigured playbable animations
       std::vector<AnimationSequenceStruct> mAnimationSequences; ///<The preconfigured playbable animations
       std::string mSkeletonFilename;                ///<The one skeleton filename
@@ -223,7 +223,7 @@ namespace dtAnim
 
       typedef std::stack<std::string> ElementStack;
       ElementStack mElements;
-      
+
       bool mInSkinningShader;
       bool mInLOD;
 	  bool mInScale;
