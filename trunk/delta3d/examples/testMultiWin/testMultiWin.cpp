f7172ce90a0012987a1cc4bb82b7951c16a0fe71
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-09-04 01:22:26 +0000

    Freshened up the TestMultiWin example by adding motion models to all the cameras, changed the clear color of the cameras for clarity, and fixed a couple little quirks.

--- trunk/delta3d/examples/testMultiWin/testMultiWin.cpp
+++ trunk/delta3d/examples/testMultiWin/testMultiWin.cpp
@@ -29,78 +29,57 @@ void TestMultiWin::Config()
    //change the title of the pre-built Window 
    //(this already has a Camera and Scene assignApped to it)
    GetWindow()->SetWindowTitle("testMultWin - Window 1");
-   GetWindow()->SetPosition(0, 0, 640, 480);
+   GetWindow()->SetPosition(10, 30, 640, 480);
 
    //create a new Window and Camera
    mWin2 = new DeltaWin("testMultWin - Window 2");
-   mWin2->SetPosition(640, 0, 640, 480);
+   mWin2->SetPosition(660, 30, 640, 960);
 
-   Transform transform( 0.0f, 0.0f, 5.0f );
+   //set the first Camera position
+   Transform transform( 0.0f, -30.0f, 5.0f );
    GetCamera()->SetTransform( transform );
+   GetCamera()->SetAspectRatio(1.0);
 
+   //hook up a motion to the first camera, using the Application's Keyboard/Mouse
+   mMotion1 = new OrbitMotionModel(GetKeyboard(), GetMouse());
+   mMotion1->SetTarget(GetCamera());
 
+   //Create second view, using the Application's Scene
    mView2 = new View("View 2");
    mView2->SetScene( GetScene() ); //use the default, pre-built Scene
    this->AddView( *mView2 );
-   
+
+   //create second Camera, added to second View, second Window
+   mCam2 = new Camera("Camera 2");
+   mCam2->SetWindow( mWin2.get() );
+   mCam2->GetOSGCamera()->setViewport( new osg::Viewport(0.0, 0.0, 640.0, 480.0));
+   mCam2->SetTransform( transform );
+   mCam2->SetClearColor(1.f, 0.f, 0.f, 1.f);
+   mCam2->SetAspectRatio(1.0);
+   mView2->SetCamera( mCam2.get() );
+   mMotion2 = new OrbitMotionModel(mView2->GetKeyboard(), mView2->GetMouse());
+   mMotion2->SetTarget(mCam2.get());
+
+   //Create a third View, using the Application's Scene
    mView3 = new View("View 3");
    mView3->SetScene( GetScene() ); //use the default, pre-built Scene
    this->AddView( *mView3 );
-   
+
+   //create a third Camera, added to third View, sharing the second Window
+   mCam3 = new Camera("Camera 3");
+   mView3->SetCamera( mCam3.get() );
+   mCam3->SetWindow( mWin2.get() );
+   mCam3->GetOSGCamera()->setViewport( new osg::Viewport(0.0, 480.0, 640.0, 480.0));
+   mCam3->SetTransform( transform );
+   mCam3->SetClearColor(0.f, 1.f, 0.f, 1.f);
+   mCam3->SetAspectRatio(1.0);
+   mMotion3 = new OrbitMotionModel(mView3->GetKeyboard(), mView3->GetMouse());
+   mMotion3->SetTarget(mCam3.get());
    
    //setup scene here
    RefPtr<Object> terr = new Object();
    terr->LoadFile("models/terrain_simple.ive");
    GetScene()->AddDrawable( terr.get() );
-   
-   mCam2 = new Camera("Camera 2");
-   mCam2->SetWindow( mWin2.get() );
-   mCam2->GetOSGCamera()->setViewport( new osg::Viewport(0.0, 0.0, 640.0, 240.0));
-   mCam2->SetTransform( transform );
-   mView2->SetCamera( mCam2.get() );
-   
-   mCam3 = new Camera("Camera 3");
-   mView3->SetCamera( mCam3.get() );
-   mCam3->SetWindow( mWin2.get() );
-   mCam3->GetOSGCamera()->setViewport( new osg::Viewport(0.0, 240.0, 640.0, 480.0));
-   mCam3->SetTransform( transform );
-   
-   
-   
-//   //setup scene here
-//   RefPtr<Object> terr = new Object();
-//   terr->LoadFile("models/terrain_simple.ive");
-//   GetScene()->AddDrawable( terr.get() );
-
-   // make sure any camera's which share the same render surface also share the same osg::State.
-   // use a std::map to keep track of what render surfaces are associated with what state.
-//   typedef std::map<Producer::RenderSurface*,osg::State*> RenderSurfaceStateMap;
-//   RenderSurfaceStateMap _renderSurfaceStateMap;
-//   unsigned int contextID = 0;
-
-//   for( int i = 0; i < Camera::GetInstanceCount(); i++ )
-//   {
-//      Producer::Camera *cam = Camera::GetInstance(i)->GetCamera();
-//      Producer::RenderSurface* rs = cam->getRenderSurface();
-//
-//      // get or create the scene handler.
-//      Camera::_SceneHandler *sh;
-//      sh = dynamic_cast<Camera::_SceneHandler*>(cam->getSceneHandler());
-//      if(sh == NULL)
-//         sh = new Camera::_SceneHandler();
-//
-//      osgUtil::SceneView* sv = sh->GetSceneView();
-//
-//      if (_renderSurfaceStateMap.count(rs)==0)
-//      {
-//         _renderSurfaceStateMap[rs] = sv->getState();
-//         sv->getState()->setContextID(contextID++);
-//      }
-//      else
-//      {
-//         sv->setState(_renderSurfaceStateMap[rs]);
-//      }
-//   }
 }
 
 int main()
