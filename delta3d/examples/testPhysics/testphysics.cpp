57660c614c7255510c80ff203c08cb51e3a2f000
djmccue <djmccue@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-10-14 22:39:23 +0000

    minor syntax fix-ups in code files for testPhysics, testRecorder, testShaders, testSound, testStateManager, testTerrain, testTrigger, testWeather, testWidget, and testWin examples

--- delta3d/examples/testPhysics/testphysics.cpp
+++ delta3d/examples/testPhysics/testphysics.cpp
@@ -1,24 +1,25 @@
 /* -*-c++-*-
-* testNetwork - testphysics (.h & .cpp) - Using 'The MIT License'
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * testNetwork - testphysics (.h & .cpp) - Using 'The MIT License'
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <dtABC/application.h>
 #include <dtCore/object.h>
 #include <dtCore/odebodywrap.h>
@@ -41,25 +42,25 @@ class Updater : public Base
 {
 
 public:
-   Updater(Scene* scene) :
-      mScene(scene)
+   Updater(Scene* scene)
+      : mScene(scene)
    {
-      assert( mScene.valid() );
-      AddSender( mScene.get() );
+      assert(mScene.valid());
+      AddSender(mScene.get());
    }
 protected:
    virtual ~Updater()
    {
-      RemoveSender( mScene.get() );
+      RemoveSender(mScene.get());
    }
 
 public:
 
-   virtual void OnMessage( MessageData* data )
+   virtual void OnMessage(MessageData* data)
    {
-      if( data->message == "collision" )
+      if (data->message == "collision")
       {
-         
+
          //Scene::CollisionData* cd = static_cast< Scene::CollisionData* >( data->userData );
 
          //Log::GetInstance().LogMessage( Log::LOG_INFO, __FUNCTION__,
@@ -81,9 +82,9 @@ class TestPhysicsApp : public Application
 
 public:
 
-   TestPhysicsApp( const std::string& configFile = "config.xml" )
-      :  Application( configFile ),
-         mLimit(50)
+   TestPhysicsApp(const std::string& configFile = "config.xml")
+      : Application(configFile)
+      , mLimit(50)
    {
       RefPtr<Object> obj1 = new Object("Ground");
       RefPtr<Object> obj2 = new Object("FallingCrate");
@@ -95,15 +96,15 @@ public:
 
       //position the camera
       Transform position;
-      position.Set( 0.0f, -20.0f, 7.0f, 0.0f, 0.0f, 0.0f);
+      position.Set(0.0f, -20.0f, 7.0f, 0.0f, 0.0f, 0.0f);
       GetCamera()->SetTransform(position);
 
       //position first falling crate
-      position.Set( 0.55f, 0.0f, 7.0f, 0.0f, 40.0f, 0.0f);
+      position.Set(0.55f, 0.0f, 7.0f, 0.0f, 40.0f, 0.0f);
       obj2->SetTransform(position);
 
       //position the crate on the ground
-      position.Set( 0.0f, 0.0f, 4.f, 0.0f, 0.0f, 0.0f);
+      position.Set(0.0f, 0.0f, 4.f, 0.0f, 0.0f, 0.0f);
       obj3->SetTransform(position);
 
       double lx = 1.0;
@@ -111,13 +112,13 @@ public:
       double lz = 1.0;
 
       //create collision meshes
-      obj1->SetCollisionMesh(); 
+      obj1->SetCollisionMesh();
       obj2->SetCollisionBox();
       obj3->SetCollisionBox();
 
       //set the mass for objects
       dMass mass;
-      dMassSetBox( &mass, 1.0, lx, ly, lz ) ;
+      dMassSetBox(&mass, 1.0, lx, ly, lz);
       obj2->SetMass(&mass);
       obj3->SetMass(&mass);
 
@@ -126,19 +127,19 @@ public:
       obj3->EnableDynamics();
 
       //Add the objects to the Scene to be rendered
-      GetScene()->AddDrawable( obj1.get() );
-      GetScene()->AddDrawable( obj2.get() );
-      GetScene()->AddDrawable( obj3.get() );
+      GetScene()->AddDrawable(obj1.get());
+      GetScene()->AddDrawable(obj2.get());
+      GetScene()->AddDrawable(obj3.get());
 
       //put the falling crate in the vector of dropped objects
       mObjects.push_back(obj2);
 
-      GetScene()->SetGravity( 0.0f, 0.0f, -15.0f );
+      GetScene()->SetGravity(0.0f, 0.0f, -15.0f);
 
-      updater = new Updater( GetScene() );
+      updater = new Updater(GetScene());
 
-      omm = new OrbitMotionModel( GetKeyboard(), GetMouse() );
-      omm->SetTarget( GetCamera() );
+      omm = new OrbitMotionModel(GetKeyboard(), GetMouse());
+      omm->SetTarget(GetCamera());
 
       //calculate and set focal distance for orbit motion model (origin -> camera)
       Transform trans;
@@ -147,99 +148,99 @@ public:
       osg::Vec3 camLoc;
       trans.GetTranslation(camLoc);
 
-      osg::Vec3 origin( 0.0f, 0.0f, 0.0f );
-      omm->SetDistance( (camLoc - origin).length() );
+      osg::Vec3 origin(0.0f, 0.0f, 0.0f);
+      omm->SetDistance((camLoc - origin).length());
    }
 
 protected:
 
    virtual ~TestPhysicsApp() {}
 
-   virtual void PreFrame( const double deltaFrameTiime )
+   virtual void PreFrame(const double deltaFrameTiime)
    {
-      while( !mToAdd.empty() )
+      while (!mToAdd.empty())
       {
-         GetScene()->AddDrawable( mToAdd.front().get() );
-         mObjects.push_back( mToAdd.front() );
+         GetScene()->AddDrawable(mToAdd.front().get());
+         mObjects.push_back(mToAdd.front());
          mToAdd.pop();
       }
-      
-      while( !mToRemove.empty() )
+
+      while (!mToRemove.empty())
       {
-         GetScene()->RemoveDrawable( mToRemove.front().get() );
+         GetScene()->RemoveDrawable(mToRemove.front().get());
          mObjects.pop_front();
          mToRemove.pop();
-      }   
+      }
    }
 
    //apply some linear and angular velocity dampening
-   void DampenBody( Object *obj, float vScale, float aScale )
+   void DampenBody(Object* obj, float vScale, float aScale)
    {
       if (obj->DynamicsEnabled())
       {
-         ODEBodyWrap* body = obj->GetBodyWrapper(); 
+         ODEBodyWrap* body = obj->GetBodyWrapper();
          body->ApplyForce(body->GetLinearVelocity() * vScale);
          body->ApplyTorque(body->GetAngularVelocity() * aScale);
       }
    }
 
-   virtual void PostFrame( const double deltaFrameTime )
+   virtual void PostFrame(const double deltaFrameTime)
    {
-      for(  std::deque< RefPtr<Object> >::iterator i = mObjects.begin(); 
-            i!=mObjects.end(); 
-            ++i )
+      for (std::deque< RefPtr<Object> >::iterator i = mObjects.begin();
+           i != mObjects.end();
+           ++i)
       {
-         DampenBody( i->get(), -0.10f, -0.04f );
+         DampenBody(i->get(), -0.10f, -0.04f);
       }
    }
 
    virtual bool KeyPressed(const dtCore::Keyboard* keyboard, int key)
    {
-	  // SetCollision____ must be called before setTransform so
-	  // that a dGeomID will exist before a prePhysicsUpdate, otherwise
-	  // the scale will never be applied to the physics geometry
+     // SetCollision____ must be called before setTransform so
+     // that a dGeomID will exist before a prePhysicsUpdate, otherwise
+     // the scale will never be applied to the physics geometry
       bool verdict(false);
-      switch( key )
+      switch (key)
       {
          case 'p':
          {
-            System::GetInstance().SetPause( !System::GetInstance().GetPause() );
+            System::GetInstance().SetPause(!System::GetInstance().GetPause());
             verdict = true;
             break;
          }
-         case osgGA::GUIEventAdapter::KEY_Escape :
+         case osgGA::GUIEventAdapter::KEY_Escape:
          {
-            while( !mToAdd.empty() )
+            while (!mToAdd.empty())
             {
                mToAdd.pop();
             }
 
-            while( !mToRemove.empty() )
+            while (!mToRemove.empty())
             {
                mToRemove.pop();
             }
 
-            while( !mObjects.empty() )
+            while (!mObjects.empty())
             {
                mObjects.pop_front();
             }
-            
+
             Quit();
             verdict = true;
             break;
          }
-         case 'b' :
+         case 'b':
             {
-               if( mObjects.size() < mLimit )
+               if (mObjects.size() < mLimit)
                {
                   RefPtr<Object> box = new Object("box");
-                  box->LoadFile( "models/physics_crate.ive" );
+                  box->LoadFile("models/physics_crate.ive");
 
-                  float randomScale = RandFloat( 0.5f, 2.0f );
-                  box->SetScale( osg::Vec3(randomScale, randomScale, randomScale) );
+                  float randomScale = RandFloat(0.5f, 2.0f);
+                  box->SetScale(osg::Vec3(randomScale, randomScale, randomScale));
 
                   box->SetCollisionBox();
-                  box->SetTransform( GetStartTransform() );
+                  box->SetTransform(GetStartTransform());
                   //box->RenderCollisionGeometry();
 
                   double lx = 1.0;
@@ -247,7 +248,7 @@ protected:
                   double lz = 1.0;
 
                   dMass mass;
-                  dMassSetBox( &mass, 1.0, lx, ly, lz );
+                  dMassSetBox(&mass, 1.0, lx, ly, lz);
                   box->SetMass(&mass);
 
                   box->EnableDynamics();
@@ -256,29 +257,29 @@ protected:
                }
                else
                {
-                  mToRemove.push( mObjects.front() );
+                  mToRemove.push(mObjects.front());
                }
                verdict = true;
                break;
          }
-         case 's' :
+         case 's':
          {
-            if( mObjects.size() < mLimit )
+            if (mObjects.size() < mLimit)
             {
                RefPtr<Object> sphere = new Object("sphere");
-               sphere->LoadFile( "models/physics_happy_sphere.ive" );
+               sphere->LoadFile("models/physics_happy_sphere.ive");
 
-               float randomScale = RandFloat( 0.5f, 2.0f );
-               sphere->SetScale( osg::Vec3(randomScale, randomScale, randomScale) );
+               float randomScale = RandFloat(0.5f, 2.0f);
+               sphere->SetScale(osg::Vec3(randomScale, randomScale, randomScale));
 
                sphere->SetCollisionSphere();
-               sphere->SetTransform( GetStartTransform() );
+               sphere->SetTransform(GetStartTransform());
                //sphere->RenderCollisionGeometry(true);
 
                double radius = 0.5;
 
                dMass mass;
-               dMassSetSphere( &mass, 1.0, radius );
+               dMassSetSphere(&mass, 1.0, radius);
                sphere->SetMass(&mass);
                sphere->EnableDynamics();
 
@@ -286,26 +287,26 @@ protected:
             }
             else
             {
-               mToRemove.push( mObjects.front() );
+               mToRemove.push(mObjects.front());
             }
             verdict = true;
             break;
          }
-         case 'c' :
+         case 'c':
          {
-            if( mObjects.size() < mLimit )
+            if (mObjects.size() < mLimit)
             {
                RefPtr<Object> cyl = new Object("cylinder");
-               cyl->LoadFile( "models/physics_barrel.ive" );
+               cyl->LoadFile("models/physics_barrel.ive");
 
-               float randomScale = RandFloat( 0.5f, 2.0f );
-               cyl->SetScale( osg::Vec3(randomScale, randomScale, randomScale) );
+               float randomScale = RandFloat(0.5f, 2.0f);
+               cyl->SetScale(osg::Vec3(randomScale, randomScale, randomScale));
 
                cyl->SetCollisionCappedCylinder();
-               cyl->SetTransform( GetStartTransform() );
+               cyl->SetTransform(GetStartTransform());
                //cyl->RenderCollisionGeometry(true);
 
-               double radius = 0.321; 
+               double radius = 0.321;
                double length = 1.0;
 
                dMass mass;
@@ -318,11 +319,11 @@ protected:
             }
             else
             {
-               mToRemove.push( mObjects.front() );
+               mToRemove.push(mObjects.front());
             }
             verdict = true;
             break;
-         } 
+         }
          default:
          {
             break;
@@ -333,18 +334,18 @@ protected:
    }
 
    //If a physical object is picked, apply a little spinning force to it
-   virtual bool MouseButtonPressed(const dtCore::Mouse *mouse, dtCore::Mouse::MouseButton button)
+   virtual bool MouseButtonPressed(const dtCore::Mouse* mouse, dtCore::Mouse::MouseButton button)
    {
-      if (button != 0) {return false;}
+      if (button != 0) { return false; }
 
       dtCore::DeltaDrawable* drawable = GetView()->GetMousePickedObject();
-      if (drawable == NULL) {return false;} //nothing picked
+      if (drawable == NULL) { return false; } //nothing picked
 
-      dtCore::Physical *phys = dynamic_cast<dtCore::Physical *>(drawable);
-      if (phys == NULL) {return false;} //didn't pick a Physical
+      dtCore::Physical* phys = dynamic_cast<dtCore::Physical*>(drawable);
+      if (phys == NULL) { return false; } //didn't pick a Physical
 
-      if (phys->DynamicsEnabled() == false) {return false;} //not enabled
-      
+      if (phys->DynamicsEnabled() == false) { return false; } //not enabled
+
       phys->GetBodyWrapper()->ApplyForce(osg::Vec3(0.f, 0.f, 120.f));
       phys->GetBodyWrapper()->ApplyTorque(osg::Vec3(0.f, 0.f, 60.f));
       return true;
@@ -359,9 +360,9 @@ protected:
          Transform xform;
          xyz[2] += 1.f; //bump up the z so objects aren't buried underground
          xform.SetTranslation(xyz);
-         xform.SetRotation(RandFloat( 0.0f, 180.0f ),
-                           RandFloat( 0.0f, 90.0f ),
-                           RandFloat( 0.0f, 90.0f ) );
+         xform.SetRotation(RandFloat(0.0f, 180.0f),
+                           RandFloat(0.0f,  90.0f),
+                           RandFloat(0.0f,  90.0f));
 
          return xform;
       }
@@ -382,11 +383,11 @@ protected:
 };
 
 
-int main( int argc, char **argv )
+int main(int argc, char** argv)
 {
    dtCore::SetDataFilePathList( dtCore::GetDeltaRootPath() + "/examples/data" + ";");
 
-   RefPtr<TestPhysicsApp> app = new TestPhysicsApp( "config.xml" );
+   RefPtr<TestPhysicsApp> app = new TestPhysicsApp("config.xml");
 
    app->Config();
    app->Run();
