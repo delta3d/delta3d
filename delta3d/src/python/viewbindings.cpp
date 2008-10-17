089621de38f0a4d2ffe4e2df4aaf9e57faf16e56
cjdarken <cjdarken@2b6b6893-201a-0410-b3c1-b7434885eee4>
2008-10-17 04:38:34 +0000

    Added several additional Python bindings for dtCore::View.

--- delta3d/src/python/viewbindings.cpp
+++ delta3d/src/python/viewbindings.cpp
@@ -1,6 +1,9 @@
 #include <python/dtpython.h>
-#include <dtCore/deltadrawable.h>
 #include <dtCore/view.h>
+#include <dtCore/camera.h>
+#include <dtCore/scene.h>
+#include <dtCore/keyboard.h>
+#include <dtCore/mouse.h>
 
 using namespace boost::python;
 using namespace dtCore;
@@ -11,9 +14,38 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(GPP_overloads, GetPickPosition, 2, 3)
 
 void initViewBindings()
 {
-   class_< View, bases<dtCore::Base>, dtCore::RefPtr<View>, boost::noncopyable >( "View", init< optional< const std::string& > >() )
-	  .def( "GetMousePickedObject", &View::GetMousePickedObject, GMPO_overloads()[ return_internal_reference<>() ] )
-	  .def( "GetMousePickPosition", &View::GetMousePickPosition, GMPP_overloads() )
-	  .def( "GetPickPosition", &View::GetPickPosition, GPP_overloads() )
+   Camera* (View::*GetCamera1)() = &View::GetCamera;
+   const Camera* (View::*GetCamera2)() const = &View::GetCamera;
+
+   Scene* (View::*GetScene1)() = &View::GetScene;
+   const Scene* (View::*GetScene2)() const = &View::GetScene;
+
+   Keyboard* (View::*GetKeyboard1)() = &View::GetKeyboard;
+   const Keyboard* (View::*GetKeyboard2)() const = &View::GetKeyboard;
+
+   Mouse* (View::*GetMouse1)() = &View::GetMouse;
+   const Mouse* (View::*GetMouse2)() const = &View::GetMouse;
+
+   class_<View, bases<Base>, dtCore::RefPtr<View>, boost::noncopyable>("View", no_init)
+      .def("SetFrameBin", &View::SetFrameBin)
+      .def("GetFrameBin", &View::GetFrameBin)
+      .def("AddSlave", &View::AddSlave)
+      .def("SetCamera", &View::SetCamera)
+      .def("GetCamera", GetCamera1, return_internal_reference<>())
+      .def("GetCamera", GetCamera2, return_internal_reference<>())
+      .def("SetScene", &View::SetScene)
+      .def("GetScene", GetScene1, return_internal_reference<>())
+      .def("GetScene", GetScene2, return_internal_reference<>())
+      // omitting GetOsgViewerView...what could you do with it if you got it?
+      .def("SetKeyboard", &View::SetKeyboard)
+      .def("GetKeyboard", GetKeyboard1, return_internal_reference<>())
+      .def("GetKeyboard", GetKeyboard2, return_internal_reference<>())
+      .def("SetMouse", &View::SetMouse)
+      .def("GetMouse", GetMouse1, return_internal_reference<>())
+      .def("GetMouse", GetMouse2, return_internal_reference<>())
+	   .def( "GetMousePickedObject", &View::GetMousePickedObject, GMPO_overloads()[ return_internal_reference<>() ] )
+	   .def( "GetMousePickPosition", &View::GetMousePickPosition, GMPP_overloads() )
+	   .def( "GetPickPosition", &View::GetPickPosition, GPP_overloads() )
+      // omitted Get and SetDatabasePager
       ;
 }
