a205dd1e30e097b5685bfbddd65d833eb1ab2ef4
mday299 <mday299@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-01-29 21:54:15 +0000

    Continued refactoring of dtAudio.  Removed redundant object ListenerObj and moved it's functionality into Listener so as to reduce redundancy.  Also removed the member variables that were keeping track of the Listener's gain and velocity.  The Setter and Getter methods now directly ask/update OpenAL for those values (removing the possibility of the member variables getting out of sync with actual OpenAL values).
    Also removed some unnecessary pragma pollution.

--- delta3d/inc/dtAudio/dtaudio.h
+++ delta3d/inc/dtAudio/dtaudio.h
@@ -44,42 +44,4 @@ bool CheckForError(const std::string& userMessage,
 
 } //namespace dtAudio
 
-#if defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
-
-/* You may turn off this include message by defining _NOAUTOLIB */
-#undef _AUTOLIBNAME1
-#undef _AUTOLIBNAME2
-#undef _AUTOLIBNAME3
-
-#if   defined(_DEBUG)
-   #ifndef  DT_AUDIO_LIBRARY
-      #define  _AUTOLIBNAME1  "dtAudiod.lib"
-   #endif
-#else
-   #ifndef  DT_AUDIO_LIBRARY
-      #define  _AUTOLIBNAME1  "dtAudio.lib"
-   #endif
-#endif
-
-#define  _AUTOLIBNAME2  "OpenAL32.lib" // Note: Not Debug
-#define  _AUTOLIBNAME3  "ALut.lib"     // Note: Not Debug
-
-#if   !  defined(_NOAUTOLIBMSG)
-   #ifndef  DT_AUDIO_LIBRARY
-      #pragma message( "Will automatically link with " _AUTOLIBNAME1 )
-   #endif
-
-   #pragma message( "Will automatically link with " _AUTOLIBNAME2 )
-   #pragma message( "Will automatically link with " _AUTOLIBNAME3 )
-#endif
-
-#ifndef  DT_AUDIO_LIBRARY
-   #pragma  comment( lib, _AUTOLIBNAME1 )
-#endif
-
-#pragma  comment( lib, _AUTOLIBNAME2 )
-#pragma  comment( lib, _AUTOLIBNAME3 )
-
-#endif  // defined(_WIN32) || defined(WIN32) || defined(__WIN32__)
-
 #endif   // DELTA_DTAUDIO
