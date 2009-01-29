2d2e3ab20c528b974038fcbc1e77cd853c7ada00
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-01-29 18:06:21 +0000

    Refactored the ScreenShotCallback to avoid some questionable variable name clobbering.

--- delta3d/src/dtCore/screenshotcallback.cpp
+++ delta3d/src/dtCore/screenshotcallback.cpp
@@ -1,4 +1,5 @@
 #include <dtCore/screenshotcallback.h>
+#include <dtCore/camera.h>
 #include <dtUtil/log.h>
 #include <osg/RenderInfo>
 #include <osgDB/WriteFile>
@@ -28,16 +29,16 @@ void dtCore::ScreenShotCallback::operator()(const dtCore::Camera& camera,
       mTakeScreenShotNextFrame = false;
       osg::ref_ptr<osg::Image> image = new osg::Image;
 
-      const osg::Camera *camera = renderInfo.getCurrentCamera();
-      if (camera == NULL)
+      const osg::Camera *osgCamera = camera.GetOSGCamera();
+      if (osgCamera == NULL)
       {
          return;
       }
 
-      int x = static_cast<int>(camera->getViewport()->x());
-      int y = static_cast<int>(camera->getViewport()->y());
-      unsigned int width = static_cast<unsigned int>(camera->getViewport()->width());
-      unsigned int height = static_cast<unsigned int>(camera->getViewport()->height());
+      int x = static_cast<int>(osgCamera->getViewport()->x());
+      int y = static_cast<int>(osgCamera->getViewport()->y());
+      unsigned int width = static_cast<unsigned int>(osgCamera->getViewport()->width());
+      unsigned int height = static_cast<unsigned int>(osgCamera->getViewport()->height());
 
       image->allocateImage(width, height, 1, GL_RGB, GL_UNSIGNED_BYTE);
       image->readPixels(x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE);
