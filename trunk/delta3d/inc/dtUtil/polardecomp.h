2d21fe01e106f3d13cc18dc06a31b095f1246b11
mjguerre <mjguerre@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-02-18 19:48:59 +0000

    Cleaned up code style to better reflect the style guide.  Eliminated several compiler warnings.

--- trunk/delta3d/inc/dtUtil/polardecomp.h
+++ trunk/delta3d/inc/dtUtil/polardecomp.h
@@ -39,43 +39,42 @@ namespace dtUtil
    {
    public:
       /// Find Polar Decomposition of Matrix M: Q=Rotation, S=Scale/Stretch, T=Translation
-      static float Decompose( const osg::Matrix& M, osg::Matrix& Q, osg::Matrix& S, osg::Vec3& T );
+      static float Decompose(const osg::Matrix& M, osg::Matrix& Q, osg::Matrix& S, osg::Vec3& T);
 
    private:
       
       /// Copy nxn matrix A to C using "gets" for assignment
-      static void MatCopyMinusEqual( osg::Matrix& C, const osg::Matrix& A );
+      static void MatCopyMinusEqual(osg::Matrix& C, const osg::Matrix& A);
       
       /// Assign nxn matrix C the element-wise combination of A and B using "op"
-      static void MatBinOpEqualPlus( osg::Matrix& C, const float g1, const osg::Matrix& A, const float g2, const osg::Matrix& B );
+      static void MatBinOpEqualPlus(osg::Matrix& C, const float g1, const osg::Matrix& A, const float g2, const osg::Matrix& B);
       
       /// Set MadjT to transpose of inverse of M times determinant of M
-      static void AdjointTranspose( const osg::Matrix& M, osg::Matrix& MadjT );
+      static void AdjointTranspose(const osg::Matrix& M, osg::Matrix& MadjT);
       
-      static float NormInf( const osg::Matrix& M );
-      static float NormOne( const osg::Matrix& M );
+      static float NormInf(const osg::Matrix& M);
+      static float NormOne(const osg::Matrix& M);
 
       /// Return index of column of M containing maximum abs entry, or -1 if M=0
-      static int FindMaxCol( const osg::Matrix& M );
+      static int FindMaxCol(const osg::Matrix& M);
 
       /// Setup u for Household reflection to zero all v components but first
-      static void MakeReflector( const osg::Vec3& v, osg::Vec3& u );
+      static void MakeReflector(const osg::Vec3& v, osg::Vec3& u);
 
       /// Apply Householder reflection represented by u to column vectors of M
-      static void ReflectCols( osg::Matrix& M, const osg::Vec3& u );
+      static void ReflectCols(osg::Matrix& M, const osg::Vec3& u);
 
       /// Apply Householder reflection represented by u to row vectors of M
-      static void ReflectRows( osg::Matrix& M, const osg::Vec3& u );
+      static void ReflectRows(osg::Matrix& M, const osg::Vec3& u);
 
       // Compute either the 1 or infinity norm of M, depending on tpose
-      static float MatNorm( const osg::Matrix& M, const int tpose );
+      static float MatNorm(const osg::Matrix& M, const int tpose);
 
       /// Find orthogonal factor Q of rank 1 (or less) M
-      static void DoRank1( osg::Matrix& M, osg::Matrix& Q );
+      static void DoRank1(osg::Matrix& M, osg::Matrix& Q);
 
       /// Find orthogonal factor Q of rank 2 (or less) M using adjoint transpose
-      static void DoRank2( osg::Matrix& M, const osg::Matrix& MadjT, osg::Matrix& Q );
-
+      static void DoRank2(osg::Matrix& M, const osg::Matrix& MadjT, osg::Matrix& Q);
    };
 }
 #endif // DELTA_POLAR_DECOMP
