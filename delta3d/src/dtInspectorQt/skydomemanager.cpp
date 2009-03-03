62b71f0b27857a2a385401001c443f288f66c5ec
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-03-03 05:16:52 +0000

    Hooked up the SkyDomeManager to the UI in dtInspectorQt

--- delta3d/src/dtInspectorQt/skydomemanager.cpp
+++ delta3d/src/dtInspectorQt/skydomemanager.cpp
@@ -20,4 +20,37 @@ void dtInspectorQt::SkyDomeManager::OperateOn(dtCore::Base* b)
    dtCore::SkyDome *skydome = dynamic_cast<dtCore::SkyDome*>(b);
 
    mOperateOn = skydome;
+   Update();
+}
+
+//////////////////////////////////////////////////////////////////////////
+void dtInspectorQt::SkyDomeManager::Update()
+{
+   if (mOperateOn.valid())
+   {
+      mUI->skyDomeGroupBox->show();
+
+      osg::Vec3 color;
+      mOperateOn->GetBaseColor(color);
+      mUI->envSkyRedEdit->setValue(color[0]);
+      mUI->envSkyGreenEdit->setValue(color[1]);
+      mUI->envSkyBlueEdit->setValue(color[2]);
+   }
+   else
+   {
+      mUI->skyDomeGroupBox->hide();
+   }
+}
+
+//////////////////////////////////////////////////////////////////////////
+void dtInspectorQt::SkyDomeManager::OnColorChanged(double val)
+{
+   if (mOperateOn.valid())
+   {
+      osg::Vec3 color(mUI->skyDomeRedEdit->value(),
+                      mUI->skyDomeGreenEdit->value(),
+                      mUI->skyDomeBlueEdit->value());
+
+      mOperateOn->SetBaseColor(color);
+   }
 }
