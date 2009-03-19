5cdd57e1b62e606ec24200330e36e70bbb02d4a7
djmccue <djmccue@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-03-19 17:30:40 +0000

    cleaned up code style to better reflect the style guide in STAGE cpp files starting with: O through R

--- delta3d/utilities/STAGE/src/projectcontextdialog.cpp
+++ delta3d/utilities/STAGE/src/projectcontextdialog.cpp
@@ -1,31 +1,31 @@
 /* -*-c++-*-
-* Delta3D Simulation Training And Game Editor (STAGE)
-* STAGE - This source file (.h & .cpp) - Using 'The MIT License'
-* Copyright (C) 2005-2008, Alion Science and Technology Corporation
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-* 
-* This software was developed by Alion Science and Technology Corporation under
-* circumstances in which the U. S. Government may have rights in the software.
-*
-* William E. Johnson II
-*/
+ * Delta3D Simulation Training And Game Editor (STAGE)
+ * STAGE - This source file (.h & .cpp) - Using 'The MIT License'
+ * Copyright (C) 2005-2008, Alion Science and Technology Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * This software was developed by Alion Science and Technology Corporation under
+ * circumstances in which the U. S. Government may have rights in the software.
+ *
+ * William E. Johnson II
+ */
 #include <prefix/dtstageprefix-src.h>
 #include "dtEditQt/projectcontextdialog.h"
 
@@ -51,76 +51,80 @@
 
 namespace dtEditQt
 {
-    ProjectContextDialog::ProjectContextDialog(QWidget *parent) : QDialog(parent)
-    {
-        QVBoxLayout *vLay   = new QVBoxLayout(this);
-        //vLay->setSizeConstraint(QLayout::SetMinimumSize);
 
-        setWindowTitle(tr("Select a project context"));
-        
-        QGroupBox   *group  = new QGroupBox(tr("Properties"),this);
-        QGridLayout *grid   = new QGridLayout(group);
-        QHBoxLayout *buttons= new QHBoxLayout;
-        QPushButton *select = new QPushButton(tr("..."),group);
-        QPushButton *cancel = new QPushButton(tr("Cancel"),this);
-        pathEdit            = new QLineEdit(group);
-        applyButton         = new QPushButton(tr("Apply"),this);
+   ProjectContextDialog::ProjectContextDialog(QWidget* parent)
+      : QDialog(parent)
+   {
+      QVBoxLayout* vLay   = new QVBoxLayout(this);
+      //vLay->setSizeConstraint(QLayout::SetMinimumSize);
 
-        QLabel      *desc   = new QLabel(tr("A project holds all related maps, files, and "
-            "resources.  Please select a directory where your project files will be "
-            "stored."),group);
+      setWindowTitle(tr("Select a project context"));
 
-        desc->setWordWrap(true);
-        grid->addWidget(new QLabel(tr("Path:")), 0, 0);
-        grid->addWidget(pathEdit,0,1);
-        grid->addWidget(select,0,2);
-        grid->addWidget(desc,1,0,1,3,Qt::AlignTop);
-        vLay->addWidget(group);
+      QGroupBox*   group  = new QGroupBox(tr("Properties"),this);
+      QGridLayout* grid   = new QGridLayout(group);
+      QHBoxLayout* buttons= new QHBoxLayout;
+      QPushButton* select = new QPushButton(tr("..."),group);
+      QPushButton* cancel = new QPushButton(tr("Cancel"),this);
+      pathEdit            = new QLineEdit(group);
+      applyButton         = new QPushButton(tr("Apply"),this);
 
-        buttons->addStretch(1);
-        buttons->addWidget(applyButton);
-        buttons->addWidget(cancel);
-        buttons->addStretch(1);
+      QLabel*      desc   = new QLabel(tr("A project holds all related maps, files, and "
+                                          "resources.  Please select a directory where your project files will be "
+                                          "stored."),group);
 
-        vLay->addLayout(buttons);
+      desc->setWordWrap(true);
+      grid->addWidget(new QLabel(tr("Path:")), 0, 0);
+      grid->addWidget(pathEdit, 0, 1);
+      grid->addWidget(select, 0, 2);
+      grid->addWidget(desc, 1, 0, 1, 3, Qt::AlignTop);
+      vLay->addWidget(group);
 
-        connect(select, SIGNAL(clicked()), this, SLOT(spawnFileBrowser()));
-        connect(applyButton, SIGNAL(clicked()), this, SLOT(accept()));
-        connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
+      buttons->addStretch(1);
+      buttons->addWidget(applyButton);
+      buttons->addWidget(cancel);
+      buttons->addStretch(1);
 
-        applyButton->setEnabled(false);
-        pathEdit->setDisabled(true);
-        pathEdit->setText(tr(EditorData::GetInstance().getCurrentProjectContext().c_str()));
-        setMinimumSize(530, 100);
-    }
+      vLay->addLayout(buttons);
 
-    ProjectContextDialog::~ProjectContextDialog()
-    {
+      connect(select,      SIGNAL(clicked()), this, SLOT(spawnFileBrowser()));
+      connect(applyButton, SIGNAL(clicked()), this, SLOT(accept()));
+      connect(cancel,      SIGNAL(clicked()), this, SLOT(reject()));
 
-    }
+      applyButton->setEnabled(false);
+      pathEdit->setDisabled(true);
+      pathEdit->setText(tr(EditorData::GetInstance().getCurrentProjectContext().c_str()));
+      setMinimumSize(530, 100);
+   }
 
-    QString ProjectContextDialog::getProjectPath() const 
-    {
-        return pathEdit->text();
-    }
+   ProjectContextDialog::~ProjectContextDialog()
+   {
 
-    ///////////////////////// SLOTS ////////////////////////////////////
-    void ProjectContextDialog::spawnFileBrowser()
-    {
-        QString dir = QFileDialog::getExistingDirectory(this, tr("Select a project context"));
+   }
 
-        if(dir.isEmpty()) 
-        {
-            applyButton->setEnabled(false);
-            return;
-        }
+   QString ProjectContextDialog::getProjectPath() const 
+   {
+      return pathEdit->text();
+   }
 
-        std::string strippedName = dir.toStdString();
-        if((*strippedName.rbegin()) == '\\' || (*strippedName.rbegin()) == '/')
-            strippedName = strippedName.substr(0, strippedName.size() - 1);
+   ///////////////////////// SLOTS ////////////////////////////////////
+   void ProjectContextDialog::spawnFileBrowser()
+   {
+      QString dir = QFileDialog::getExistingDirectory(this, tr("Select a project context"));
 
-        pathEdit->setText(tr(strippedName.c_str()));
-        applyButton->setEnabled(true);
-    }
+      if (dir.isEmpty()) 
+      {
+         applyButton->setEnabled(false);
+         return;
+      }
 
-}
+      std::string strippedName = dir.toStdString();
+      if ((*strippedName.rbegin()) == '\\' || (*strippedName.rbegin()) == '/')
+      {
+         strippedName = strippedName.substr(0, strippedName.size() - 1);
+      }
+
+      pathEdit->setText(tr(strippedName.c_str()));
+      applyButton->setEnabled(true);
+   }
+
+} // namespace dtEditQt
