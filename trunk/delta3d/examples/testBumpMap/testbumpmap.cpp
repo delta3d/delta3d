349975d4812a4fca9db5bd55b64987ed7a6e8fb2
erheine <erheine@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-03-31 15:41:16 +0000

    Disable lighting on the help labels for the various test apps.

--- trunk/delta3d/examples/testBumpMap/testbumpmap.cpp
+++ trunk/delta3d/examples/testBumpMap/testbumpmap.cpp
@@ -294,6 +294,7 @@ void TestBumpMapApp::CreateHelpLabel()
    mLabel->SetTextAlignment(dtABC::LabelActor::AlignmentEnum::LEFT_CENTER);
    mLabel->SetText(CreateHelpLabelText());
    mLabel->SetEnableDepthTesting(false);
+   mLabel->SetEnableLighting(false);
 
    GetCamera()->AddChild(mLabel.get());
    dtCore::Transform labelOffset(-17.0f, 50.0f, 11.25f, 0.0f, 90.0f, 0.0f);
