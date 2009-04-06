adc170e93da36da856abeb0141efbbe44cf2d308
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-04-06 15:16:56 +0000

    Forced a view and projection update on the main camera to fix a problem where the TestOnScreen unit test in cameratests would fail depending on the order the unit tests are run.

--- trunk/delta3d/tests/dtCore/cameratests.cpp
+++ trunk/delta3d/tests/dtCore/cameratests.cpp
@@ -99,9 +99,12 @@ class CameraTests : public CPPUNIT_NS::TestFixture
       void TestOnScreen()
       {
          dtCore::Camera& camera = *GetGlobalApplication().GetCamera();
+         camera.SetPerspectiveParams(60.0, 1.33, 1.0, 2000.0);
          dtCore::Transform xform;
-         xform.SetTranslation(osg::Vec3(0.0, 0.0, 0.0));
+         xform.MakeIdentity();
          camera.SetTransform(xform);
+         camera.UpdateViewMatrixFromTransform();
+
          osg::Vec3 testPos(0.0, 5.0, 0.0);
 
          osg::Vec3d screenPos;
@@ -334,7 +337,7 @@ void CameraTests::TestSettingTheCullingMode()
 void CameraTests::TestInitialCallbackConditions()
 {
    dtCore::RefPtr<dtCore::Camera> cam = new dtCore::Camera();
-   
+
    osg::Camera::DrawCallback *osgCallback = cam->GetOSGCamera()->getPostDrawCallback();
 
    CPPUNIT_ASSERT_MESSAGE("Camera should not have any Post Draw callbacks added by default.",
@@ -410,7 +413,7 @@ void CameraTests::TestTriggeringCallback()
    dtCore::System::GetInstance().Start();
    dtCore::System::GetInstance().Step();
    dtCore::System::GetInstance().Stop();
- 
+
    CPPUNIT_ASSERT_EQUAL_MESSAGE("The Camera postdraw callback didn't trigger.",
                                 true, cb1->mTriggered);
    CPPUNIT_ASSERT_EQUAL_MESSAGE("The second Camera postdraw callback didn't trigger.",
