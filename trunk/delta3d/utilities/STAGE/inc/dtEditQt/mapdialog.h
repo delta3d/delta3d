c2af095cdab4f246fd203e38af339826b58b73da
djmccue <djmccue@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-04-14 01:39:35 +0000

    cleaned up code style to better reflect the style guide in STAGE h files starting with: G through O

--- trunk/delta3d/utilities/STAGE/inc/dtEditQt/mapdialog.h
+++ trunk/delta3d/utilities/STAGE/inc/dtEditQt/mapdialog.h
@@ -1,31 +1,31 @@
 /* -*-c++-*-
-* Delta3D Simulation Training And Game Editor (STAGE)
-* STAGE - This source file (.h & .cpp) - Using 'The MIT License'
-* Copyright (C) 2005-2008, Alion Science and Technology Corporation
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-* 
-* This software was developed by Alion Science and Technology Corporation under
-* circumstances in which the U. S. Government may have rights in the software.
-*
-* William E. Johnson II
-*/
+ * Delta3D Simulation Training And Game Editor (STAGE)
+ * STAGE - This source file (.h & .cpp) - Using 'The MIT License'
+ * Copyright (C) 2005-2008, Alion Science and Technology Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This software was developed by Alion Science and Technology Corporation under
+ * circumstances in which the U. S. Government may have rights in the software.
+ *
+ * William E. Johnson II
+ */
 
 #ifndef DELTA_MAP_DIALOG
 #define DELTA_MAP_DIALOG
@@ -37,7 +37,7 @@ class QCloseEvent;
 class QPushButton;
 class QTextEdit;
 
-namespace dtDAL 
+namespace dtDAL
 {
    class Map;
 }
@@ -48,31 +48,30 @@ namespace dtEditQt
    {
       Q_OBJECT
 
-      public:
+   public:
+      /// Constructor
+      MapDialog(QWidget* parent = NULL);
 
-         /// Constructor
-         MapDialog(QWidget *parent = NULL);
+      /// Destructor
+      virtual ~MapDialog() { }
 
-         /// Destructor
-         virtual ~MapDialog() { }
+      inline dtDAL::Map* getFinalizedMap() { return myMap; }
 
-         inline dtDAL::Map* getFinalizedMap() { return myMap; } 
+   public slots:
+      /// slot for receiving the text changing signal
+      void edited(const QString& newText);
 
-      public slots:
+      /// slot for applying the changes made
+      void applyChanges();
 
-         /// slot for receiving the text changing signal
-         void edited(const QString &newText);
-
-         /// slot for applying the changes made
-         void applyChanges();
-
-      private:
-         QPushButton *okButton;
-         QLineEdit *nameEdit;
-         QTextEdit *descEdit;
-         QLineEdit *fileEdit;
-         dtDAL::Map *myMap;
+   private:
+      QPushButton* okButton;
+      QLineEdit*   nameEdit;
+      QTextEdit*   descEdit;
+      QLineEdit*   fileEdit;
+      dtDAL::Map*  myMap;
    };
 
-}
-#endif
+} // namespace dtEditQt
+
+#endif // DELTA_MAP_DIALOG
--- trunk/delta3d/utilities/STAGE/inc/dtEditQt/orthoviewport.h
+++ trunk/delta3d/utilities/STAGE/inc/dtEditQt/orthoviewport.h
@@ -1,31 +1,32 @@
 /* -*-c++-*-
-* Delta3D Simulation Training And Game Editor (STAGE)
-* STAGE - This source file (.h & .cpp) - Using 'The MIT License'
-* Copyright (C) 2005-2008, Alion Science and Technology Corporation
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-* 
-* This software was developed by Alion Science and Technology Corporation under
-* circumstances in which the U. S. Government may have rights in the software.
-*
-* Matthew W. Campbell
-*/
+ * Delta3D Simulation Training And Game Editor (STAGE)
+ * STAGE - This source file (.h & .cpp) - Using 'The MIT License'
+ * Copyright (C) 2005-2008, Alion Science and Technology Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This software was developed by Alion Science and Technology Corporation under
+ * circumstances in which the U. S. Government may have rights in the software.
+ *
+ * Matthew W. Campbell
+ */
+
 #ifndef DELTA_ORTHO_VIEWPORT
 #define DELTA_ORTHO_VIEWPORT
 
@@ -45,200 +46,206 @@ namespace dtEditQt
    {
       Q_OBJECT
 
+   public:
+      /**
+       * An enumeration of the different types of views into the scene an
+       * orthographic viewport can render.
+       */
+      class OrthoViewType : public dtUtil::Enumeration
+      {
+         DECLARE_ENUM(OrthoViewType);
+
       public:
+         /**
+          * Top or birds eye view.  This renders the scene along the XY plane looking
+          * down the -Z axis.
+          */
+         static const OrthoViewType TOP;
 
          /**
-          * An enumeration of the different types of views into the scene an
-          * orthographic viewport can render.
+          * Front view.  This renders the scene along the XZ plane looking down the
+          * +Y axis.
           */
-         class OrthoViewType : public dtUtil::Enumeration {
-            DECLARE_ENUM(OrthoViewType);
-            public:
-               /**
-                * Top or birds eye view.  This renders the scene along the XY plane looking
-                * down the -Z axis.
-                */
-               static const OrthoViewType TOP;
-
-               /**
-                * Front view.  This renders the scene along the XZ plane looking down the
-                * +Y axis.
-                */
-               static const OrthoViewType FRONT;
-
-               /**
-                * Side view.  This renders the scene along the XY plane looking down the
-                * -X axis.
-                */
-               static const OrthoViewType SIDE;
-
-            private:
-               OrthoViewType(const std::string &name) : dtUtil::Enumeration(name)
-               {
-                  AddInstance(this);
-               }
-         };
+         static const OrthoViewType FRONT;
 
          /**
-          * Enumerates the specific types of interactions an orthographic viewport
-          * supports.  These extend the interactions of the base viewport.  For example,
-          * when the overall mode is camera mode, the orthographic viewport supports
-          * more specific behavior.
+          * Side view.  This renders the scene along the XY plane looking down the
+          * -X axis.
           */
-         class CameraMode : public dtUtil::Enumeration
+         static const OrthoViewType SIDE;
+
+      private:
+         OrthoViewType(const std::string& name)
+            : dtUtil::Enumeration(name)
          {
-            DECLARE_ENUM(CameraMode);
-            public:
-               static const CameraMode CAMERA_PAN;
-               static const CameraMode CAMERA_ZOOM;
-               static const CameraMode NOTHING;
-
-            private:
-               CameraMode(const std::string &name) : dtUtil::Enumeration(name)
-               {
-                  AddInstance(this);
-               }
-         };
-
-         /**
-          * Sets this orthographic viewport's current view type.
-          * @param type The new view type.
-          */
-         void setViewType(const OrthoViewType &type, bool refreshView=true);
-
-         /**
-          * Gets the type of view currently in use by the viewport.
-          * @return
-          */
-         const OrthoViewType &getViewType() const {
-            return *(this->viewType);
+            AddInstance(this);
          }
+      };
 
-         /**
-          * Moves the camera.
-          * @par
-          *  The camera's movement is based on the current camera mode.<br>
-          *      CAMERA_PAN - Pans the camera along the plane the
-          *          viewport is looking at.<br>
-          *      CAMERA_ZOOM - Zooms the camera in and out.
-          *
-          * @param dx
-          * @param dy
-          */
-         void moveCamera(float dx, float dy);
+      /**
+       * Enumerates the specific types of interactions an orthographic viewport
+       * supports.  These extend the interactions of the base viewport.  For example,
+       * when the overall mode is camera mode, the orthographic viewport supports
+       * more specific behavior.
+       */
+      class CameraMode : public dtUtil::Enumeration
+      {
+         DECLARE_ENUM(CameraMode);
 
-         protected:
-            /**
-             * Constructs the orthographic viewport.
-             */
-            OrthoViewport(const std::string &name, QWidget *parent = NULL,
-                  QGLWidget *shareWith = NULL);
+      public:
+         static const CameraMode CAMERA_PAN;
+         static const CameraMode CAMERA_ZOOM;
+         static const CameraMode NOTHING;
 
-            /**
-             * Destroys the viewport.
-             */
-            virtual ~OrthoViewport() { }
+      private:
+         CameraMode(const std::string& name)
+            : dtUtil::Enumeration(name)
+         {
+            AddInstance(this);
+         }
+      };
 
-            /**
-             * Initializes the viewport.  This just sets the current render style
-             * to be wireframe and the view type to be OrthoViewType::TOP.
-             */
-            void initializeGL();
+      /**
+       * Sets this orthographic viewport's current view type.
+       * @param type The new view type.
+       */
+      void setViewType(const OrthoViewType& type, bool refreshView = true);
 
-            /**
-             * Sets the orthographic projection parameters of the current camera.
-             * @param width The width of the viewport.
-             * @param height The height of the viewport.
-             */
-            void resizeGL(int width, int height);
+      /**
+       * Gets the type of view currently in use by the viewport.
+       * @return
+       */
+      const OrthoViewType& getViewType() const
+      {
+         return *(this->viewType);
+      }
 
-            /**
-             * Called when the user presses a key on the keyboard in the viewport.
-             * Based on the combination of keys pressed, the viewport's current
-             * mode will be set.
-             * @param e
-             */
-            void keyPressEvent(QKeyEvent *e);
+      /**
+       * Moves the camera.
+       * @par
+       *  The camera's movement is based on the current camera mode.<br>
+       *      CAMERA_PAN - Pans the camera along the plane the
+       *          viewport is looking at.<br>
+       *      CAMERA_ZOOM - Zooms the camera in and out.
+       *
+       * @param dx
+       * @param dy
+       */
+      void moveCamera(float dx, float dy);
 
-            /**
-             * Called when the user releases a key on the keyboard in the viewport.
-             * Based on the keys released, the viewport's current mode is
-             * updated accordingly.
-             * @param e
-             */
-            void keyReleaseEvent(QKeyEvent *e);
+   protected:
+      /**
+       * Constructs the orthographic viewport.
+       */
+      OrthoViewport(const std::string& name, QWidget* parent = NULL,
+            QGLWidget* shareWith = NULL);
 
-            /**
-             * Called when the user releases a mouse button in the viewport.  Based on
-             * the buttons released, the viewport's current mode is updated
-             * accordingly.
-             * @param e
-             */
-            void mouseReleaseEvent(QMouseEvent *e);
+      /**
+       * Destroys the viewport.
+       */
+      virtual ~OrthoViewport() { }
 
-            /**
-             * Called when the user presses a mouse button in the viewport.  Based on
-             * the combination of buttons pressed, the viewport's current mode will
-             * be set.
-             * @param e
-             * @see ModeType
-             */
-            void mousePressEvent(QMouseEvent*);
+      /**
+       * Initializes the viewport.  This just sets the current render style
+       * to be wireframe and the view type to be OrthoViewType::TOP.
+       */
+      void initializeGL();
 
-            /**
-             * Called when the user moves the mouse while pressing any combination of
-             * mouse buttons.  Based on the current mode, the camera is updated.
-             * @param dx the adjusted change in x that the mouse moved.
-             * @param dy the adjusted change in y that the mouse moved.
-             */
-            virtual void onMouseMoveEvent(QMouseEvent *e, float dx, float dy);
+      /**
+       * Sets the orthographic projection parameters of the current camera.
+       * @param width The width of the viewport.
+       * @param height The height of the viewport.
+       */
+      void resizeGL(int width, int height);
 
-            /**
-             * Called when the user moves the wheel on a mouse containing a scroll wheel.
-             * This causes the scene to be zoomed in and out.
-             * @param e
-             */
-            void wheelEvent(QWheelEvent *e);
+      /**
+       * Called when the user presses a key on the keyboard in the viewport.
+       * Based on the combination of keys pressed, the viewport's current
+       * mode will be set.
+       * @param e
+       */
+      void keyPressEvent(QKeyEvent* e);
 
-            /**
-             * Called from the mousePressEvent handler.  This sets the viewport state
-             * to properly respond to mouse movement events when in camera mode.
-             * @param e
-             */
-            void beginCameraMode(QMouseEvent *e);
+      /**
+       * Called when the user releases a key on the keyboard in the viewport.
+       * Based on the keys released, the viewport's current mode is
+       * updated accordingly.
+       * @param e
+       */
+      void keyReleaseEvent(QKeyEvent* e);
 
-            /**
-             * Called from the mouseReleaseEvent handler.  This restores the state of
-             * the viewport to it was before camera mode was entered.
-             * @param e
-             */
-            void endCameraMode(QMouseEvent *e);
+      /**
+       * Called when the user releases a mouse button in the viewport.  Based on
+       * the buttons released, the viewport's current mode is updated
+       * accordingly.
+       * @param e
+       */
+      void mouseReleaseEvent(QMouseEvent* e);
 
-            /**
-             * Called from the mousePressEvent handler.  Depending on what modifier
-             * key is pressed, this puts the viewport state into a mode that enables
-             * actor manipulation.
-             * @param e
-             */
-            void beginActorMode(QMouseEvent *e);
+      /**
+       * Called when the user presses a mouse button in the viewport.  Based on
+       * the combination of buttons pressed, the viewport's current mode will
+       * be set.
+       * @param e
+       * @see ModeType
+       */
+      void mousePressEvent(QMouseEvent*);
 
-            /**
-             * Called from the mouseReleaseEvent handler.  This restores the state of
-             * the viewport as it was before actor mode was entered.
-             * @param e
-             */
-            void endActorMode(QMouseEvent *e);
+      /**
+       * Called when the user moves the mouse while pressing any combination of
+       * mouse buttons.  Based on the current mode, the camera is updated.
+       * @param dx the adjusted change in x that the mouse moved.
+       * @param dy the adjusted change in y that the mouse moved.
+       */
+      virtual void onMouseMoveEvent(QMouseEvent* e, float dx, float dy);
 
-            void warpWorldCamera(int x, int y);
+      /**
+       * Called when the user moves the wheel on a mouse containing a scroll wheel.
+       * This causes the scene to be zoomed in and out.
+       * @param e
+       */
+      void wheelEvent(QWheelEvent* e);
 
-         private:
-            ///Allow the ViewportManager access to it can create perspective viewports.
-            friend class ViewportManager;
+      /**
+       * Called from the mousePressEvent handler.  This sets the viewport state
+       * to properly respond to mouse movement events when in camera mode.
+       * @param e
+       */
+      void beginCameraMode(QMouseEvent* e);
 
-            const CameraMode *cameraMode;
-            const OrthoViewType *viewType;
-            osg::Vec3 zoomToPosition;
+      /**
+       * Called from the mouseReleaseEvent handler.  This restores the state of
+       * the viewport to it was before camera mode was entered.
+       * @param e
+       */
+      void endCameraMode(QMouseEvent* e);
+
+      /**
+       * Called from the mousePressEvent handler.  Depending on what modifier
+       * key is pressed, this puts the viewport state into a mode that enables
+       * actor manipulation.
+       * @param e
+       */
+      void beginActorMode(QMouseEvent* e);
+
+      /**
+       * Called from the mouseReleaseEvent handler.  This restores the state of
+       * the viewport as it was before actor mode was entered.
+       * @param e
+       */
+      void endActorMode(QMouseEvent* e);
+
+      void warpWorldCamera(int x, int y);
+
+   private:
+      ///Allow the ViewportManager access to it can create perspective viewports.
+      friend class ViewportManager;
+
+      const CameraMode*    cameraMode;
+      const OrthoViewType* viewType;
+      osg::Vec3 zoomToPosition;
    };
-}
 
-#endif
+} // namespace dtEditQt
+
+#endif // DELTA_ORTHO_VIEWPORT
