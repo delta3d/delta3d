75dc1d8cbfd5bad869de5b9ded14e61e53bc7f36
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-04-30 16:13:23 +0000

    Just a little cleanup with the particleEditor code.  Now 10k bytes lighter in memory.

--- delta3d/utilities/ParticleEditor/src/main.cpp
+++ delta3d/utilities/ParticleEditor/src/main.cpp
@@ -1,35 +1,7 @@
 #include <delta3dthread.h>
 #include <mainwindow.h>
 
-#include <dtCore/camera.h>
-#include <dtCore/globals.h>
-#include <dtCore/scene.h>
-#include <dtCore/compass.h>
-#include <dtCore/system.h>
-#include <dtUtil/fileutils.h>
-
 #include <QtGui/QApplication>
-#include <QtGui/QMessageBox>
-
-#include <sstream>
-#include <string.h>
-#include <math.h>
-
-////////////////////////////////////////////////////////////////////////////////
-
-#ifdef DELTA_WIN32
-#include <Windows.h>
-#define SLEEP(milliseconds) Sleep((milliseconds))
-#else
-#include <unistd.h>
-#define SLEEP(milliseconds) usleep(((milliseconds) * 1000))
-#endif
-
-/////The Editor preferences - gets saved automatically when app exits
-
-// Previous loaded file history
-char absoluteHistory[5][1024];
-char relativeHistory[5][1024];
 
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
