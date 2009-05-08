d15cc60583406754f4c0cd9fb16f722a18212025
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-05-08 13:57:45 +0000

    Second commit for trying to clean up windows.h includes.  (sorry, forgot a comment on the first commit)
    These add an mswin.h to dtUtil that wrapps up the windows include, and also tried to clean up some of the undef stuff, and tried to only include windows.h when necessary.  The theory is that delta compiles so much faster on linux and osx partly because windows.h was being included pretty much everywhere.

--- trunk/delta3d/src/dtUtil/deprecationmgr.cpp
+++ trunk/delta3d/src/dtUtil/deprecationmgr.cpp
@@ -6,11 +6,7 @@
    // disable warning for stl classes "needs to have dll-interface to be used by clients of class"
    #pragma warning(disable : 4251)
 
-   #define WIN32_LEAN_AND_MEAN
-   #include <windows.h>
-   #undef GetClassName
-   #undef SendMessage
-   #undef CreateFont
+   #include <dtUtil/mswin.h>
 #else
    #include <csignal>
    #include <iostream>
