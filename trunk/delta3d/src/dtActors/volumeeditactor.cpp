595a52af7552974f457866d1782765e3174d3ba2
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-06-22 16:39:31 +0000

    fixed a compile error with OSG 2.6, also made the pointer the proxy an observer ptr, otherwise there is a circular reference and actor will never get deleted.
    also fixed a few minor space things to match the coding standard.

--- trunk/delta3d/src/dtActors/volumeeditactor.cpp
+++ trunk/delta3d/src/dtActors/volumeeditactor.cpp
@@ -24,13 +24,13 @@ VolumeEditActor::VolumeShapeType VolumeEditActor::VolumeShapeType::CONE("CONE");
 ////////////////////////////////////////////////////////////////////////////////
 VolumeEditActor::VolumeEditActor(VolumeEditActorProxy *prox)
    : dtCore::Transformable("VolumeEditActor")
+   , mProxy(prox)
    , mVolumeGeode(new osg::Geode())
    , mModel(new dtCore::Model())
-   , mProxy(prox)  
 {
    //For volume to get added to scene, make the Geode a child of the
    //Transformable's OSGNode (Transformable Actor is already in scene)
-   osg::Group *g = this->GetOSGNode()->asGroup();   
+   osg::Group *g = this->GetOSGNode()->asGroup();
    g->addChild(&mModel->GetMatrixTransform());
 
    g = mModel->GetMatrixTransform().asGroup();
@@ -47,7 +47,7 @@ VolumeEditActor::~VolumeEditActor()
 
 VolumeEditActorProxy* VolumeEditActor::GetProxy()
 {
-   return mProxy;
+   return mProxy.get();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -61,7 +61,7 @@ osg::Vec3 VolumeEditActor::GetScale() const
 ////////////////////////////////////////////////////////////////////////////////
 VolumeEditActor::VolumeShapeType& VolumeEditActor::GetShape()
 {
-   if(mVolumeShape == NULL)
+   if (mVolumeShape == NULL)
    {
       //Defaults back to BOX
       return VolumeShapeType::BOX;
@@ -69,30 +69,30 @@ VolumeEditActor::VolumeShapeType& VolumeEditActor::GetShape()
          __FUNCTION__, __LINE__, "No Shape Set");
    }
 
-   if(strcmp(mVolumeShape->className(), "Box") == 0)
+   if (strcmp(mVolumeShape->className(), "Box") == 0)
    {
       return VolumeShapeType::BOX;
    }
 
-   if(strcmp(mVolumeShape->className(), "Sphere") == 0)
+   if (strcmp(mVolumeShape->className(), "Sphere") == 0)
    {
       return VolumeShapeType::SPHERE;
    }
 
-   if(strcmp(mVolumeShape->className(), "Cylinder") == 0)
+   if (strcmp(mVolumeShape->className(), "Cylinder") == 0)
    {
       return VolumeShapeType::CYLINDER;
    }
 
-   if(strcmp(mVolumeShape->className(), "Capsule") == 0)
+   if (strcmp(mVolumeShape->className(), "Capsule") == 0)
    {
       return VolumeShapeType::CAPSULE;
    }
 
-   if(strcmp(mVolumeShape->className(), "Cone") == 0)
+   if (strcmp(mVolumeShape->className(), "Cone") == 0)
    {
       return VolumeShapeType::CONE;
-   }   
+   }
 
    dtUtil::Log::GetInstance().LogMessage(dtUtil::Log::LOG_ERROR,
       __FUNCTION__, __LINE__, "No Shape Set");
@@ -102,30 +102,30 @@ VolumeEditActor::VolumeShapeType& VolumeEditActor::GetShape()
 
 ////////////////////////////////////////////////////////////////////////////////
 void VolumeEditActor::SetScale(const osg::Vec3& xyz)
-{  
-   mModel->SetScale(xyz);   
+{
+   mModel->SetScale(xyz);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void VolumeEditActor::SetShape(VolumeShapeType& shape)
 {
-   if(shape == VolumeShapeType::BOX)
+   if (shape == VolumeShapeType::BOX)
    {
       mVolumeShape = new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), 10.0f, 10.0f, 10.0f);
    }
-   else if(shape == VolumeShapeType::SPHERE)
+   else if (shape == VolumeShapeType::SPHERE)
    {
       mVolumeShape = new osg::Sphere(osg::Vec3(0.0f, 0.0f, 0.0f), 10.0f);
    }
-   else if(shape == VolumeShapeType::CYLINDER)
+   else if (shape == VolumeShapeType::CYLINDER)
    {
       mVolumeShape = new osg::Cylinder(osg::Vec3(0.0f, 0.0f, 0.0f), 10.0f, 10.0f);
    }
-   else if(shape == VolumeShapeType::CAPSULE)
+   else if (shape == VolumeShapeType::CAPSULE)
    {
       mVolumeShape = new osg::Capsule(osg::Vec3(0.0f, 0.0f, 0.0f), 10.0f, 10.0f);
    }
-   else if(shape == VolumeShapeType::CONE)
+   else if (shape == VolumeShapeType::CONE)
    {
       mVolumeShape = new osg::Cone(osg::Vec3(0.0f, 0.0f, 0.0f), 10.0f, 10.0f);
    }
@@ -135,15 +135,15 @@ void VolumeEditActor::SetShape(VolumeShapeType& shape)
             __FUNCTION__, __LINE__, "Unrecognized Shape");
       return;
    }
-   
+
    dtCore::RefPtr<osg::TessellationHints> tessHints = new osg::TessellationHints();
 
-   mVolumeDrawable = new osg::ShapeDrawable(mVolumeShape.get(), tessHints);   
+   mVolumeDrawable = new osg::ShapeDrawable(mVolumeShape.get(), tessHints);
    mVolumeDrawable->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
 
    //Volume should only ever contain one shape drawable,
    //so remove the old one, if there is one
-   mVolumeGeode->removeDrawables(0, 1);   
+   mVolumeGeode->removeDrawables(0, 1);
    mVolumeGeode->addDrawable(mVolumeDrawable.get());
 
    osg::StateSet* stateSet = mVolumeGeode->getOrCreateStateSet();
@@ -163,7 +163,7 @@ void VolumeEditActor::SetShape(VolumeShapeType& shape)
 
       //osg::BlendFunc* blend = new osg::BlendFunc(osg::BlendFunc::SRC_ALPHA, osg::BlendFunc::ONE_MINUS_SRC_ALPHA);
       //stateSet->setAttribute(blend, osg::StateAttribute::ON);
-   }   
+   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -206,7 +206,7 @@ void VolumeEditActorProxy::BuildPropertyMap()
       dtDAL::EnumActorProperty<VolumeEditActor::VolumeShapeType>::SetFuncType(this, &VolumeEditActorProxy::SetShape),
       dtDAL::EnumActorProperty<VolumeEditActor::VolumeShapeType>::GetFuncType(this, &VolumeEditActorProxy::GetShape),
       "Sets the shape for this Volume Editor", groupName));
-   
+
    //Volume actors need to be scalable
    AddProperty(new dtDAL::Vec3ActorProperty("Scale", "Scale",
       dtDAL::MakeFunctor(*actor, &VolumeEditActor::SetScale),
