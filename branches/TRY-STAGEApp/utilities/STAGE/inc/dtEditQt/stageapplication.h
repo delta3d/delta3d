fb3c11acf3404b14ca077c90c82dd80cb12461a3
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-06-24 01:12:16 +0000

    added some missing files (just a barebones dtABC::Application to get STAGE started).

---
+++ branches/TRY-STAGEApp/utilities/STAGE/inc/dtEditQt/stageapplication.h
@@ -0,0 +1,18 @@
+#ifndef stageapplication_h__
+#define stageapplication_h__
+
+#include <dtABC/application.h>
+
+namespace dtEditQt
+{
+   class STAGEApplication : public dtABC::Application
+   {
+   public:
+   	STAGEApplication();
+   protected:
+      virtual ~STAGEApplication();
+   	
+   private:
+   };
+}
+#endif // stageapplication_h__
