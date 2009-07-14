d3a6a00f63b7bb014463bc33fb62e6e5d5cd549d
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-07-14 19:47:15 +0000

    Fixed a bug with the way dtDIS compares DIS::EntityID types.

--- trunk/delta3d/src/dtDIS/entityidcompare.cpp
+++ trunk/delta3d/src/dtDIS/entityidcompare.cpp
@@ -5,7 +5,12 @@ using namespace dtDIS::details;
 
 bool EntityIDCompare::operator ()(const DIS::EntityID& lhs, const DIS::EntityID& rhs) const
 {
-   return( lhs.getEntity()      < rhs.getEntity()
-        || lhs.getSite()        < rhs.getSite()
-        || lhs.getApplication() < rhs.getApplication() );
+   if (lhs.getApplication() != rhs.getApplication())
+      return lhs.getApplication() < rhs.getApplication();
+   else if (lhs.getSite() != rhs.getSite())
+      return lhs.getSite() < rhs.getSite();
+   else if (lhs.getEntity() != rhs.getEntity())
+      return lhs.getEntity() < rhs.getEntity();
+   else
+      return false;
 }
