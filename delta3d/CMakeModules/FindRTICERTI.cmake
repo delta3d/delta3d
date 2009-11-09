2ab95ca6bdb39f21109a326d930d775768552f23
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-11-09 23:03:30 +0000

    Added cmake support for finding (on Windows at least) and using CERTI 3.3.2 RTI.

--- delta3d/CMakeModules/FindRTICERTI.cmake
+++ delta3d/CMakeModules/FindRTICERTI.cmake
@@ -4,53 +4,56 @@
 # RTI_FOUND, if false, do not try to link to OSG
 # RTI_INCLUDE_DIR, where to find the headers
 
-find_path(RTI_INCLUDE_DIR
+FIND_PATH(RTI_INCLUDE_DIR
    NAMES
       RTI.hh
    PATHS
       ${DELTA3D_EXT_DIR}/inc/CERTI
       /usr/local/include
       /usr/include
-   	$ENV{CERTI_HOME}/include
+      "$ENV{PROGRAMFILES}/certi 3.3.2/include"
+      $ENV{CERTI_HOME}/include
 )
 
 SET(CERTI_PATHS       
       ${DELTA3D_EXT_DIR}/lib
       /usr/local/lib 
       /usr/lib 
-  	   $ENV{CERTI_HOME}/release)
+      "$ENV{PROGRAMFILES}/certi 3.3.2/lib"
+      $ENV{CERTI_HOME}/release)
 
 
-find_library(RTI_LIBRARY
+FIND_LIBRARY(RTI_LIBRARY
    NAMES
       RTI
+      RTI-NG
    PATHS
       ${CERTI_PATHS}
 )
 
-find_library(RTI_RTI_LIBRARY
+FIND_LIBRARY(RTI_RTI_LIBRARY
    NAMES
       CERTI
    PATHS
       ${CERTI_PATHS}
 )
 
-if(UNIX)
-find_library(RTI_PARSER_LIBRARY
+IF(UNIX)
+FIND_LIBRARY(RTI_PARSER_LIBRARY
    NAMES
       xml2
    PATHS
       /usr/local/lib 
       /usr/lib 
 )
-endif(UNIX)
+ENDIF(UNIX)
 
-set(RTI_FOUND "NO")
-if(RTI_LIBRARY AND RTI_INCLUDE_DIR)
-    set(RTI_FOUND "YES")
+SET(RTI_FOUND "NO")
+IF(RTI_LIBRARY AND RTI_INCLUDE_DIR)
+    SET(RTI_FOUND "YES")
     SET(RTI_LIBRARIES
            ${RTI_LIBRARY}
            ${RTI_RTI_LIBRARY}
            ${RTI_PARSER_LIBRARY}
     )
-endif(RTI_LIBRARY AND RTI_INCLUDE_DIR)
+ENDIF(RTI_LIBRARY AND RTI_INCLUDE_DIR)
