2b7b10b889b6c0f5e3fc1055d420a1cafbd24bf8
jhoude <jhoude@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-11-24 07:23:04 +0000

    Extracted base functionality of the xml parsing from the map loading system so they can be used elsewhere. Created an ActorPropertySerializer class that handles xml reading/writing of actor properties.

--- branches/dtDirector/inc/dtDAL/mapxml.h
+++ branches/dtDirector/inc/dtDAL/mapxml.h
@@ -22,45 +22,28 @@
 #ifndef DELTA_MAPXML
 #define DELTA_MAPXML
 
-#include <string>
-#include <stack>
-#include <map>
-#include <set>
-#include <vector>
-
-#include <osg/Referenced>
-
-#include <dtCore/refptr.h>
 #include <dtDAL/export.h>
-
-#include <xercesc/util/XercesDefs.hpp>
-#include <xercesc/framework/XMLFormatter.hpp>
-#include <xercesc/sax2/SAX2XMLReader.hpp>
-
-namespace dtUtil
-{
-   class Log;
-}
+#include <dtDAL/basexml.h>
 
 namespace dtDAL
 {
    class Map;
-   class ActorProxy;
-   class AbstractParameter;
-   class ActorProperty;
-   class DataType;
-   class NamedParameter;
    class MapContentHandler;
-   class ArrayActorPropertyBase;
-   class ContainerActorProperty;   
+   class ActorProxy;
+   class ActorPropertySerializer;
    
    /**
     * @class MapParser
     * @brief front end class for converting an XML map into a map instance.
     */
-   class DT_DAL_EXPORT MapParser: public osg::Referenced
+   class DT_DAL_EXPORT MapParser: public BaseXMLParser
    {
       public:
+
+         /**
+          * Constructor.
+          */
+         MapParser();
 
          /**
           * Completely parses a map file.  Be sure store an dtCore::RefPtr to the map immediately, otherwise
@@ -70,7 +53,7 @@ namespace dtDAL
           * @return A pointer to the loaded map.
           * @throws MapLoadParseError if a fatal error occurs in the parsing.
           */
-         Map* Parse(const std::string& path);
+         bool Parse(const std::string& path, Map** map);
 
          /**
          * Parses a prefab resource and places it in the given map
@@ -96,9 +79,6 @@ namespace dtDAL
           */
          const std::string ParseMapName(const std::string& path);
 
-         ///@return true if the map is currently being parsed.
-         bool IsParsing() const { return mParsing; }
-
          /**
           * This method exists to allow actors being parsed to access their map.  It's to help 
           * with certain features of the loading process.  Using the meth
@@ -114,31 +94,15 @@ namespace dtDAL
 
          bool HasDeprecatedProperty() const;
 
-         MapParser();
-
    protected:
 
          virtual ~MapParser();
 
-   public:
+   private:
+      MapParser(const MapParser& copyParser);
+      MapParser& operator=(const MapParser& assignParser);
 
-         /**
-          * this is called automatically on startup.
-          */
-         static void StaticInit();
-         
-         /**
-          * this is called automatically on shutdown.
-          */
-         static void StaticShutdown();
-
-      private:
-         MapParser(const MapParser& copyParser);
-         MapParser& operator=(const MapParser& assignParser);
-         dtCore::RefPtr<MapContentHandler> mHandler;
-         xercesc::SAX2XMLReader* mXercesParser;
-         dtUtil::Log* mLogger;
-         bool mParsing;
+      dtCore::RefPtr<MapContentHandler> mMapHandler;
    };
 
 
@@ -146,91 +110,44 @@ namespace dtDAL
     * @class MapWriter
     * @brief Writes a map out to an XML file
     */
-   class DT_DAL_EXPORT MapWriter: public osg::Referenced
+   class DT_DAL_EXPORT MapWriter: public BaseXMLWriter
    {
-      public:
-         MapWriter();
-         /**
-         * Saves the map to an XML file.
-         * The create time will be set on the map if this is the first time it has been saved.
-         * @param map the map to save.
-         * @param filePath the path to the file to save.  The map has a file name property,
-         *  but it does not include any directories needed or the extension.
-         * @throws ExceptionEnum::MapSaveError if any errors occur saving the file.
-         */
-         void Save(Map& map, const std::string& filePath);
+   public:
 
-         /**
-         * Saves a number of given actor proxies into a prefab resource.
-         */
-         void SavePrefab(std::vector<dtCore::RefPtr<ActorProxy> > proxyList,
-                         const std::string& filePath, const std::string& description,
-                         const std::string& iconFile = "");
+      /**
+       * Constructor.
+       */
+      MapWriter();
+
+      /**
+       * Saves the map to an XML file.
+       * The create time will be set on the map if this is the first time it has been saved.
+       * @param map the map to save.
+       * @param filePath the path to the file to save.  The map has a file name property,
+       *  but it does not include any directories needed or the extension.
+       * @throws ExceptionEnum::MapSaveError if any errors occur saving the file.
+       */
+      void Save(Map& map, const std::string& filePath);
+
+      /**
+       * Saves a number of given actor proxies into a prefab resource.
+       */
+      void SavePrefab(std::vector<dtCore::RefPtr<ActorProxy> > proxyList,
+                      const std::string& filePath, const std::string& description,
+                      const std::string& iconFile = "");
          
-      protected:
-         virtual ~MapWriter(); ///Protected destructor so that this could be subclassed.
-      private:
+   protected:
+      virtual ~MapWriter(); ///Protected destructor so that this could be subclassed.
 
-         class MapFormatTarget: public xercesc::XMLFormatTarget
-         {
-            public:
-               MapFormatTarget();
-               virtual ~MapFormatTarget();
-
-               void SetOutputFile(FILE* newFile);
-               const FILE* GetOutputFile(FILE* newFile) const { return mOutFile; }
-
-               virtual void writeChars(
-                  const XMLByte* const toWrite,
-                  const unsigned int count,
-                  xercesc::XMLFormatter* const formatter);
-
-               virtual void flush();
-
-            private:
-               FILE* mOutFile;
-               dtUtil::Log*  mLogger;
-         };
+   private:
          
-         typedef std::basic_string<XMLCh> xmlCharString;
-         static const int indentSize = 4;
-         dtUtil::Log* mLogger;
+      //disable copy constructor
+      MapWriter(const MapWriter& toCopy): BaseXMLWriter(toCopy) {}
+      //disable operator =
+      MapWriter& operator=(const MapWriter& assignTo) { return *this;}
 
-         bool mLastCharWasLF;
-         std::stack<xmlCharString> mElements;
-
-         MapFormatTarget mFormatTarget;
-         xercesc::XMLFormatter mFormatter;
-
-         //writes out the open tags for a new element including indentation.
-         void BeginElement(const XMLCh* const name, const XMLCh* const attributes = NULL);
-         //writes out the end element tag including indentation if necessary.
-         void EndElement();
-
-         void AddCharacters(const xmlCharString& string);
-         void AddCharacters(const std::string& string);
-
-         void AddIndent();
-
-         template <typename VecType>
-         void WriteVec(const VecType& vec, char* numberConversionBuffer, const size_t bufferMax);
-            
-         template <typename Type>
-         void WriteColorRGBA(const Type& holder, char* numberConversionBuffer, size_t bufferMax);
-         void WriteArray(const ArrayActorPropertyBase& arrayProp, char* numberConversionBuffer, size_t bufferMax);
-         void WriteContainer(const ContainerActorProperty& arrayProp, char* numberConversionBuffer, size_t bufferMax);
-         
-         void WriteSimple(const AbstractParameter& holder);
-
-         void WriteProperty(const ActorProperty& property);
-         void WriteParameter(const NamedParameter& param);
-
-         //disable copy constructor
-         MapWriter(const MapWriter& toCopy): mFormatter("UTF-8", NULL, &mFormatTarget, xercesc::XMLFormatter::NoEscapes, xercesc::XMLFormatter::DefaultUnRep) {}
-         //disable operator =
-         MapWriter& operator=(const MapWriter& assignTo) { return *this;}
+      ActorPropertySerializer* mPropSerializer;
    };
-
 }
 
 #endif
