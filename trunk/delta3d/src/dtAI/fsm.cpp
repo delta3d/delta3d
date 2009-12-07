4a4553805f7974aec4240b1aa05c27b903819f77
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-12-07 23:04:08 +0000

    Added option to dtAI::FSM to disable calling OnExit/OnEntry when transitioning to the same state.  Default behavior remains the same.  (User submitted, Mantis #110)

--- trunk/delta3d/src/dtAI/fsm.cpp
+++ trunk/delta3d/src/dtAI/fsm.cpp
@@ -24,26 +24,32 @@
 
 namespace dtAI
 {
+   //////////////////////////////////////////////////////////////////////////
    FSM::FSM()
       : mCurrentState(new NPCState(&NPCStateTypes::NPC_STATE_DEFAULT))
       , mFactory(0)
+      , mProcessSelfTransitions(true)
    {
       SetupDefaultFactory();
    }
 
+   //////////////////////////////////////////////////////////////////////////
    FSM::FSM(FactoryType* pFactory)
       : mCurrentState(new NPCState(&NPCStateTypes::NPC_STATE_DEFAULT))
       , mFactory(pFactory)
+      , mProcessSelfTransitions(true)
    {
 
    }
 
+   //////////////////////////////////////////////////////////////////////////
    FSM::~FSM()
    {
       FreeMem();
    }
 
 
+   //////////////////////////////////////////////////////////////////////////
    void FSM::FreeMem()
    {
       mCurrentState = 0;
@@ -51,6 +57,7 @@ namespace dtAI
       mTransitions.clear();
    }
 
+   //////////////////////////////////////////////////////////////////////////
    NPCState* FSM::AddState(const NPCState::Type* state)
    {
       NPCState* newState = NULL;
@@ -72,102 +79,128 @@ namespace dtAI
          LOG_ERROR("ObjectFactory has not been initialized, returning NULL pointer.");
       }
 
-   return newState;
-}
-
-//Release note: lazy state addition removed
-void FSM::AddTransition(const NPCEvent* eventType, const NPCState::Type* from, const NPCState::Type* to)
-{
-   //lazy state addition
-   //AddState(from);
-   //AddState(to);
-
-   // checking the set of States
-   NPCState* realFrom = GetState(from);
-   NPCState* realTo = GetState(to);
-
-   if (realFrom != NULL && realTo != NULL)
-   {
-      // checking the transition map's keys
-      TransitionMap::key_type key(eventType, realFrom);
-      mTransitions.insert(TransitionMap::value_type(key , realTo));
+      return newState;
    }
-   else
+
+   //////////////////////////////////////////////////////////////////////////
+   //Release note: lazy state addition removed
+   void FSM::AddTransition(const NPCEvent* eventType, const NPCState::Type* from, const NPCState::Type* to)
    {
-      LOG_ERROR("Unable to add transition- Event:"
-         + eventType->GetName() + ", from state: " + from->GetName() +
-         ", to state: " + to->GetName());
-   }
-}
+      //lazy state addition
+      //AddState(from);
+      //AddState(to);
 
-NPCState* FSM::GetCurrentState()
-{
-   return mCurrentState.get();
-}
+      // checking the set of States
+      NPCState* realFrom = GetState(from);
+      NPCState* realTo = GetState(to);
 
-NPCState* FSM::GetState(const NPCState::Type* pStateType)
-{
-   StateSet::iterator iter      = mStates.begin();
-   StateSet::iterator endOfList = mStates.end();
-
-   while (iter != endOfList)
-   {
-      if ((*iter)->GetName() == pStateType->GetName())
+      if (realFrom != NULL && realTo != NULL)
       {
-         return const_cast<NPCState*>((*iter).get());
+         // checking the transition map's keys
+         TransitionMap::key_type key(eventType, realFrom);
+         mTransitions.insert(TransitionMap::value_type(key , realTo));
       }
-      ++iter;
+      else
+      {
+         LOG_ERROR("Unable to add transition- Event:"
+            + eventType->GetName() + ", from state: " + from->GetName() +
+            ", to state: " + to->GetName());
+      }
    }
 
-   LOG_ERROR("State Type: " + pStateType->GetName() + " not found.");
-   return 0;
-}
-
-/** Forces the given State to now be the 'current' State.*/
-void FSM::MakeCurrent(const NPCState::Type* pStateType)
-{
-   OnStateChange(GetState(pStateType));
-}
-
-void FSM::OnStateChange(NPCState* pState)
-{
-   if (pState == NULL)
+   //////////////////////////////////////////////////////////////////////////
+   NPCState* FSM::GetCurrentState()
    {
-      LOG_ERROR("FSM::OnStateChange, Invalid State: NULL");
-      return;
+      return mCurrentState.get();
    }
-   mCurrentState->OnExit();
-   mCurrentState = pState;
-   mCurrentState->OnEntry();
-}
 
-void FSM::Update(double dt)
-{
-   if (mCurrentState.valid() && mCurrentState->GetUpdate().valid())
+   //////////////////////////////////////////////////////////////////////////
+   NPCState* FSM::GetState(const NPCState::Type* pStateType)
    {
-      mCurrentState->GetUpdate()(dt);
+      StateSet::iterator iter      = mStates.begin();
+      StateSet::iterator endOfList = mStates.end();
+
+      while (iter != endOfList)
+      {
+         if ((*iter)->GetName() == pStateType->GetName())
+         {
+            return const_cast<NPCState*>((*iter).get());
+         }
+         ++iter;
+      }
+
+      LOG_ERROR("State Type: " + pStateType->GetName() + " not found.");
+      return 0;
    }
-}
 
-bool FSM::HandleEvent(const NPCEvent* pEvent)
-{
-   TransitionMap::key_type key(pEvent, mCurrentState.get());
-   TransitionMap::iterator iter = mTransitions.find(key);
-
-   if (iter != mTransitions.end())
+   //////////////////////////////////////////////////////////////////////////
+   /** Forces the given State to now be the 'current' State.*/
+   void FSM::MakeCurrent(const NPCState::Type* pStateType)
    {
-      NPCState* to = (*iter).second.get();
-      OnStateChange(to);
-      return true;
+      OnStateChange(GetState(pStateType));
    }
 
-   LOG_ERROR("Unable to handle event: " + pEvent->GetName() + " from state: " + mCurrentState->GetName());
-   return false;
-}
+   //////////////////////////////////////////////////////////////////////////
+   void FSM::OnStateChange(NPCState* pState)
+   {
+      if (pState == NULL)
+      {
+         LOG_ERROR("FSM::OnStateChange, Invalid State: NULL");
+         return;
+      }
 
-void FSM::SetupDefaultFactory()
-{
-   mFactory = new FactoryType();
-}
+      //if it's a new state or we're handling transitions to the same state
+      if (mCurrentState != pState || mProcessSelfTransitions == true)
+      {
+         mCurrentState->OnExit();
+         mCurrentState = pState;
+         mCurrentState->OnEntry();
+      }
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   void FSM::Update(double dt)
+   {
+      if (mCurrentState.valid() && mCurrentState->GetUpdate().valid())
+      {
+         mCurrentState->GetUpdate()(dt);
+      }
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   bool FSM::HandleEvent(const NPCEvent* pEvent)
+   {
+      TransitionMap::key_type key(pEvent, mCurrentState.get());
+      TransitionMap::iterator iter = mTransitions.find(key);
+
+      if (iter != mTransitions.end())
+      {
+         NPCState* to = (*iter).second.get();
+         OnStateChange(to);
+         return true;
+      }
+
+      LOG_ERROR("Unable to handle event: " + pEvent->GetName() + " from state: " + mCurrentState->GetName());
+      return false;
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   void FSM::SetupDefaultFactory()
+   {
+      mFactory = new FactoryType();
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   void FSM::SetProcessSelfTransitions(bool handle)
+   {
+      mProcessSelfTransitions = handle;
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   bool FSM::GetProcessSelfTransitions() const
+   {
+      return mProcessSelfTransitions;
+   }
+
 
 } // namespace dtAI
