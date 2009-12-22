b375240e6577e64bcac2f12afebc2850eca6d4ee
mjguerre <mjguerre@2b6b6893-201a-0410-b3c1-b7434885eee4>
2009-12-22 20:25:21 +0000

    Initial checkin for the director example.  Doesn't do much yet.

---
+++ trunk/delta3d/examples/testDirector/directorcomponent.cpp
@@ -0,0 +1,172 @@
+/*
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2009 MOVES Institute
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * MG
+ */
+
+#include "directorcomponent.h"
+
+#include <dtABC/application.h>
+#include <dtGame/basemessages.h>
+#include <dtCore/deltawin.h>
+#include <dtCore/transform.h>
+#include <dtCore/collisionmotionmodel.h>
+#include <dtCore/collisioncategorydefaults.h>
+#include <dtActors/engineactorregistry.h>
+#include <dtDirector/director.h>
+
+const std::string& DirectorComponent::NAME = "DirectorComponent";
+
+////////////////////////////////////////////////////////////////////////////////
+
+DirectorComponent::DirectorComponent(const std::string& name)
+   : dtGame::BaseInputComponent(name)
+   , mMotionModel(NULL)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+DirectorComponent::~DirectorComponent()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void DirectorComponent::ProcessMessage(const dtGame::Message& message)
+{
+   if (message.GetMessageType() == dtGame::MessageType::TICK_LOCAL)
+   {
+      const dtGame::TickMessage& tickMessage = static_cast<const dtGame::TickMessage&>(message);
+
+      // Update Director with both sim and real time
+      mDirector->Update(tickMessage.GetDeltaSimTime(), tickMessage.GetDeltaRealTime());
+   }
+   else if (message.GetMessageType() == dtGame::MessageType::INFO_MAP_LOADED)
+   {
+      OnMapLoaded();
+   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool DirectorComponent::HandleKeyPressed(const dtCore::Keyboard* keyBoard, int key)
+{
+   bool handled = true;
+
+   switch (key)
+   {
+   case osgGA::GUIEventAdapter::KEY_Escape:
+      {
+         GetGameManager()->GetApplication().Quit();
+         return true;
+      }
+   case '~':
+      {
+         GetGameManager()->GetApplication().SetNextStatisticsType();
+         return false;
+      }
+   default:
+      break;
+   };
+
+   if (!handled)
+   {
+      return GetGameManager()->GetApplication().KeyPressed(keyBoard, key);
+   }
+
+   return handled;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void DirectorComponent::OnAddedToGM()
+{
+   // Must call this to actually receive input
+   BaseInputComponent::OnAddedToGM();
+
+   mDirector = new dtDirector::Director();
+   mDirector->Init();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+void DirectorComponent::OnMapLoaded()
+{
+   dtGame::GameManager* gm = GetGameManager();
+   dtABC::Application& app = gm->GetApplication();
+   dtCore::Camera* camera  = gm->GetApplication().GetCamera();
+
+   // Allow the player to walk around the level and collide with objects
+   dtCore::CollisionMotionModel* motionModel = 
+      new dtCore::CollisionMotionModel(1.5f, 0.25f, 0.3f, 0.1f, app.GetScene(), app.GetKeyboard(), app.GetMouse());
+
+   motionModel->SetScene(&gm->GetScene());
+   motionModel->SetTarget(camera);
+
+   mMotionModel = motionModel;
+
+   app.GetWindow()->ShowCursor(false);
+
+   std::vector<dtDAL::ActorProxy*> proxyList;
+   gm->FindActorsByType(*dtActors::EngineActorRegistry::STATIC_MESH_ACTOR_TYPE, proxyList);
+
+   // Try to eliminate interpenetration overhead (ODE is the bottleneck here)
+   for (size_t proxyIndex = 0; proxyIndex < proxyList.size(); ++proxyIndex)
+   {
+      dtCore::Transformable* actor = 
+         dynamic_cast<dtCore::Transformable*>(proxyList[proxyIndex]->GetActor());
+
+      actor->SetCollisionCategoryBits(COLLISION_CATEGORY_MASK_CAMERA);
+   }
+
+   // Get the player start position
+   dtDAL::ActorProxy* playerStartProxy = NULL;
+   gm->FindActorByType(*dtActors::EngineActorRegistry::PLAYER_START_ACTOR_TYPE, playerStartProxy);
+
+   dtCore::Transformable* playerStart = 
+      dynamic_cast<dtCore::Transformable*>(playerStartProxy->GetActor());
+
+   dtCore::Transform startTransform;
+   playerStart->GetTransform(startTransform);
+   camera->SetTransform(startTransform);
+
+   LoadDirectorScript();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void DirectorComponent::LoadDirectorScript()
+{
+   mDirector->LoadScript("test");
+
+   std::vector<dtDirector::Node*> nodes;
+   mDirector->GetNodes("Remote Event", "Core", nodes);
+  
+   // Trigger initial events
+   for (size_t index = 0; index < nodes.size(); index++)
+   {
+      dtDirector::EventNode* event = dynamic_cast<dtDirector::EventNode*>(nodes[index]);
+      if (event)
+      {
+         dtDAL::ActorProperty* prop = event->GetProperty("EventName");
+         if (prop && prop->ToString() == "First")
+         {
+            event->Trigger();
+            break;
+         }
+      }
+   }   
+}
+
+////////////////////////////////////////////////////////////////////////////////
