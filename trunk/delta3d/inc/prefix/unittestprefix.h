de53368627ff01331a0be1e3920fa0cec0e10b22
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2010-03-05 18:07:55 +0000

    ifdefd out a few headers in the tests for g++

--- trunk/delta3d/inc/prefix/unittestprefix.h
+++ trunk/delta3d/inc/prefix/unittestprefix.h
@@ -2,15 +2,17 @@
 #define UNITTESTPREFIX_H__
 
 #ifdef DT_USE_PCH
+#ifdef _MSC_VER
 #include <dtUtil/librarysharingmanager.h>
 #include <dtUtil/stringutils.h>
-#include <dtDAL/export.h>
 #include <dtDAL/enginepropertytypes.h>
-#include <osg/gl>
+#include <dtABC/application.h>
+#endif
+#include <dtDAL/export.h>
+#include <osg/GL>
 #include <osg/Referenced>
 #include <algorithm>
 #include <cppunit/extensions/HelperMacros.h>
-#include <dtABC/application.h>
 #endif
 
 #endif // UNITTESTPREFIX_H__
