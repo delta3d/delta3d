a94c3033ee9e69f917b7159b9d82ff2e7e0f9cbb
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2010-06-16 23:41:44 +0000

    Further refinements to the dtActors::CameraDataActor. Now uses Property macros; split up translation/rotation properties. (user submitted)

--- delta3d/inc/dtActors/cameradataactor.h
+++ delta3d/inc/dtActors/cameradataactor.h
@@ -24,6 +24,7 @@
 #include <dtDAL/plugin_export.h>
 #include <osg/Vec4>
 #include <dtDAL/transformableactorproxy.h>
+#include <dtUtil/getsetmacros.h>
 
 namespace dtCore
 {
@@ -60,48 +61,12 @@ namespace dtActors
            * been enabled (e.g., SetChangeClearColor(true)).
            * @param camera The Camera to apply the data to
            */
-         void ApplyDataToCamera(dtCore::Camera& camera);
-
-         // Clear color
-         bool GetChangeClearColor();
-         void SetChangeClearColor(bool value);
-         const osg::Vec4& GetClearColor();
-         void SetClearColor(const osg::Vec4& color);
-
-         // ModelView (Position / Rotation)
-         bool GetChangeModelView();
-         void SetChangeModelView(bool value);
-         const dtCore::Transform GetModelViewTransform();
+         void ApplyDataTo(dtCore::Camera& camera);
 
          // Projection
-         bool GetChangeProjection();
-         void SetChangeProjection(bool value);
          const osg::Matrix& GetProjectionMatrix();
          ProjectionMode& GetProjectionMode();
          void SetProjectionMode(ProjectionMode& value);
-         void SetProjectionRect(const osg::Vec4& value);
-         const osg::Vec4& GetProjectionRect();
-         void SetProjectionPersp(const osg::Vec4& value);
-         const osg::Vec4& GetProjectionPersp();
-         
-         // LODScale
-         bool GetChangeLODScale();
-         void SetChangeLODScale(bool value);
-         void SetLODScale(float value);
-         float GetLODScale();
-
-         // Enabled
-         bool GetChangeEnabled();;
-         void SetChangeEnabled(bool value);
-         bool GetEnabled();
-         void SetEnabled(bool value);
-
-         // Viewport
-         bool GetChangeViewport();
-         void SetChangeViewport(bool value);
-         void SetViewport(const osg::Vec4& value);
-         const osg::Vec4& GetViewport();
-
 
          // TODO NearFarCulling - enum?
          //bool GetChangeNearFarCulling(){ return mChangeNearFarCulling; };
@@ -113,23 +78,22 @@ namespace dtActors
 
       private:
          ProjectionMode* mProjectionMode;
-         osg::Vec4 mProjectionRect;
-         osg::Vec4 mProjectionPersp;
 
-         osg::Vec4 mViewport;
-         osg::Vec4 mClearColor;
-         float mLODScale;
+      DECLARE_PROPERTY_INLINE(bool, Enabled)
+      DECLARE_PROPERTY_INLINE(osg::Vec4, ClearColor)
+      DECLARE_PROPERTY_INLINE(float, LODScale)
+      DECLARE_PROPERTY_INLINE(osg::Vec4, Viewport)
+      DECLARE_PROPERTY_INLINE(osg::Vec4, ProjectionRect)
+      DECLARE_PROPERTY_INLINE(osg::Vec4, ProjectionPersp)
 
-         bool mEnabled;
 
-         bool mChangeEnabled;
-         bool mChangeClearColor;
-         bool mChangeLODScale;
-         bool mChangeProjection;
-         bool mChangeModelView;
-         bool mChangeViewport;
-         //bool mChangeNearFarCulling;
-         //bool mChangeProjectionRP;
+      DECLARE_PROPERTY_INLINE(bool, ChangeEnabled)
+      DECLARE_PROPERTY_INLINE(bool, ChangeClearColor)
+      DECLARE_PROPERTY_INLINE(bool, ChangeLODScale)
+      DECLARE_PROPERTY_INLINE(bool, ChangeProjection)
+      DECLARE_PROPERTY_INLINE(bool, ChangeTranslation)
+      DECLARE_PROPERTY_INLINE(bool, ChangeRotation)
+      DECLARE_PROPERTY_INLINE(bool, ChangeViewport)
    };
 
    class DT_PLUGIN_EXPORT CameraDataActorProxy : public dtDAL::TransformableActorProxy
@@ -139,13 +103,11 @@ namespace dtActors
 
         virtual void BuildPropertyMap();
 
-        virtual bool IsPlaceable() const;
-
         virtual const dtDAL::ActorProxy::RenderMode& GetRenderMode();
          
         dtDAL::ActorProxyIcon* GetBillBoardIcon();
 
-        void ApplyDataToCamera(dtCore::Camera& camera);
+        void ApplyDataTo(dtCore::Camera& camera);
 
       protected:
         virtual void CreateActor();
