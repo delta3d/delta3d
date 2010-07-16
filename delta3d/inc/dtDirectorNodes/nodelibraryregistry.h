08eca8f7c318419eca05c7f32f868735cfe6b964
jhoude <jhoude@2b6b6893-201a-0410-b3c1-b7434885eee4>
2010-07-16 21:05:36 +0000

    dtDirectorNodes: Created a new "Start" Event node that immediately triggers upon the start of script execution.

--- delta3d/inc/dtDirectorNodes/nodelibraryregistry.h
+++ delta3d/inc/dtDirectorNodes/nodelibraryregistry.h
@@ -38,6 +38,7 @@ namespace dtDirector
 
       /// Events
       static dtCore::RefPtr<dtDirector::NodeType> INPUT_NODE_TYPE;
+      static dtCore::RefPtr<dtDirector::NodeType> START_EVENT_NODE_TYPE;
       static dtCore::RefPtr<dtDirector::NodeType> REMOTE_EVENT_NODE_TYPE;
       static dtCore::RefPtr<dtDirector::NodeType> TRIGGER_VOLUME_EVENT_NODE_TYPE;
       static dtCore::RefPtr<dtDirector::NodeType> GAME_MESSAGE_EVENT_NODE_TYPE;
