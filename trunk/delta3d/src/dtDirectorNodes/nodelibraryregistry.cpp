64c0b0c790c18921307aaa1b2b731c13ab493288
jhoude <jhoude@2b6b6893-201a-0410-b3c1-b7434885eee4>
2010-08-11 21:07:55 +0000

    dtDirectorNodes: Created a new Animate Actor Action node that will play animations on a skeletal mesh actor.

--- trunk/delta3d/src/dtDirectorNodes/nodelibraryregistry.cpp
+++ trunk/delta3d/src/dtDirectorNodes/nodelibraryregistry.cpp
@@ -49,6 +49,7 @@
 #include <dtDirectorNodes/lerpactorrotationaction.h>
 #include <dtDirectorNodes/lerpactorscaleaction.h>
 #include <dtDirectorNodes/lerpactortranslationaction.h>
+#include <dtDirectorNodes/animateactoraction.h>
 #include <dtDirectorNodes/sendmessageaction.h>
 #include <dtDirectorNodes/sendeventmessageaction.h>
 
@@ -119,6 +120,7 @@ namespace dtDirector
    RefPtr<NodeType> NodeLibraryRegistry::LERP_ACTOR_ROTATION_ACTION_NODE_TYPE(   new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Lerp Actor Rotation",       "Cinematic",   "Cinematic",   "Linearly interpolates the rotation of an actor."));
    RefPtr<NodeType> NodeLibraryRegistry::LERP_ACTOR_SCALE_ACTION_NODE_TYPE(      new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Lerp Actor Scale",          "Cinematic",   "Cinematic",   "Linearly interpolates the scale of an actor."));
    RefPtr<NodeType> NodeLibraryRegistry::LERP_ACTOR_TRANSLATION_ACTION_NODE_TYPE(new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Lerp Actor Translation",    "Cinematic",   "Cinematic",   "Linearly interpolates the position of an actor."));
+   RefPtr<NodeType> NodeLibraryRegistry::ANIMATE_ACTOR_ACTION_NODE_TYPE(         new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Animate Actor",             "Cinematic",   "Cinematic",   "Animates a skeletal mesh actor."));
    RefPtr<NodeType> NodeLibraryRegistry::SEND_MESSAGE_ACTION_NODE_TYPE(          new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Send Message Action",       "Messages",    "Messages",    "Sends a Game Message."));
    RefPtr<NodeType> NodeLibraryRegistry::SEND_EVENT_MESSAGE_ACTION_NODE_TYPE(    new dtDirector::NodeType(dtDirector::NodeType::ACTION_NODE, "Send Event Message Action", "Messages",    "Messages",    "Sends a Game Event Message."));
 
@@ -190,6 +192,7 @@ namespace dtDirector
       mNodeFactory->RegisterType<LerpActorRotationAction>(LERP_ACTOR_ROTATION_ACTION_NODE_TYPE.get());
       mNodeFactory->RegisterType<LerpActorScaleAction>(LERP_ACTOR_SCALE_ACTION_NODE_TYPE.get());
       mNodeFactory->RegisterType<LerpActorTranslationAction>(LERP_ACTOR_TRANSLATION_ACTION_NODE_TYPE.get());
+      mNodeFactory->RegisterType<AnimateActorAction>(ANIMATE_ACTOR_ACTION_NODE_TYPE.get());
       mNodeFactory->RegisterType<SendMessageAction>(SEND_MESSAGE_ACTION_NODE_TYPE.get());
       mNodeFactory->RegisterType<SendEventMessageAction>(SEND_EVENT_MESSAGE_ACTION_NODE_TYPE.get());
 
