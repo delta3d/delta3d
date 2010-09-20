040b483c5b45a6e14045c631daf72bd68e0b1e54
mjguerre <mjguerre@2b6b6893-201a-0410-b3c1-b7434885eee4>
2010-09-20 20:51:45 +0000

    Added an accessor for posemesh root id's.

--- delta3d/inc/dtAnim/posemesh.h
+++ delta3d/inc/dtAnim/posemesh.h
@@ -1,23 +1,23 @@
-/* 
-* Delta3D Open Source Game and Simulation Engine 
-* Copyright (C) 2004-2008 MOVES Institute 
-*
-* This library is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Lesser General Public License as published by the Free 
-* Software Foundation; either version 2.1 of the License, or (at your option) 
-* any later version.
-*
-* This library is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
-* details.
-*
-* You should have received a copy of the GNU Lesser General Public License 
-* along with this library; if not, write to the Free Software Foundation, Inc., 
-* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
-*
-* Michael Guerrero
-*/
+/*
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2004-2008 MOVES Institute
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Michael Guerrero
+ */
 
 #ifndef _POSE_MESH_H_
 #define _POSE_MESH_H_
@@ -37,9 +37,9 @@
 
 #define TRIANGLE_NOT_FOUND -1
 
-namespace dtAnim 
-{ 
-   class Cal3DModelWrapper; 
+namespace dtAnim
+{
+   class Cal3DModelWrapper;
    struct PoseMeshData;
 
    class DT_ANIM_EXPORT PoseMesh
@@ -52,19 +52,19 @@ namespace dtAnim
          int   mTriangleID;
          float mAzimuth;
          float mElevation;
-      };   
+      };
 
       struct DT_ANIM_EXPORT Vertex
       {
          Vertex(const osg::Vec3& data, unsigned int animID);
 
          osg::Vec3 mData;
-         unsigned int mAnimID; 
+         unsigned int mAnimID;
 
          float     mDebugPrecision;
          osg::Vec3 mDebugData;
          osg::Quat mDebugRotation;
-         
+
       };
 
       struct DT_ANIM_EXPORT Triangle
@@ -106,6 +106,7 @@ namespace dtAnim
       const std::string& GetName() const                 { return mName;            }
       const std::string& GetEffectorName() const         { return mBoneName;        }
       int GetEffectorID() const                          { return mEffectorID;      }
+      int GetRootID() const                              { return mRootID;          }
       const VertexVector& GetVertices() const            { return mVertices;        }
       const Barycentric2DVector& GetBarySpaces() const   { return mBarySpaces;      }
       const TriangleVector& GetTriangles() const         { return mTriangles;       }
@@ -113,11 +114,11 @@ namespace dtAnim
       const osg::Vec3& GetRootForwardAxis() const        { return mRootForward;     }
       const osg::Vec3& GetEffectorForwardAxis() const    { return mEffectorForward; }
 
-      /**  
-      *  GetTargetTriangleData Finds the triangle in the mesh for the given azimuth elevation 
+      /**
+      *  GetTargetTriangleData Finds the triangle in the mesh for the given azimuth elevation
       *                        if it exists, otherwise it returns the closest triangle and its coordinates
       *  @param azimuth the horizontal angle between our forward and our target
-      *  @param elevation the vertical angle between our forward and our target  
+      *  @param elevation the vertical angle between our forward and our target
       *  @return outTriangle struct containing the nearest triangle and it's location
       */
       void GetTargetTriangleData(const float azimuth,
@@ -132,14 +133,14 @@ namespace dtAnim
       int FindPoseTriangleID(float azimuth, float elevation) const;
 
       /**
-      * GetIndexPairsForTriangle Look up the indices for a triangle and 
+      * GetIndexPairsForTriangle Look up the indices for a triangle and
       *                          create pairs corresponding to its edges
       * @param triangleID the triangle to extract pairs from
       * @param pair0 the first pair(edge)
       * @param pair1 the second pair(edge)
       * @param pair2 the thris pair(edge)
       */
-      void GetIndexPairsForTriangle(int triangleID, 
+      void GetIndexPairsForTriangle(int triangleID,
                                     MeshIndexPair& pair0,
                                     MeshIndexPair& pair1,
                                     MeshIndexPair& pair2) const;
@@ -161,7 +162,7 @@ namespace dtAnim
       VertexVector mVertices;
 
       /// the number of spaces is equal to the number of Triangles.
-      Barycentric2DVector mBarySpaces;  
+      Barycentric2DVector mBarySpaces;
 
       // the model should be made const later
       void GetAnimationIDsByName(const dtAnim::Cal3DModelWrapper* model,
