d46c3838abf15ee0121f7375e42bd236f6966428
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-01-21 00:53:04 +0000

    Deprecated the master header files; will generate a compile warning in GCC and a compile message in MSVC.  Users should use the individual header files instead.

--- trunk/delta3d/inc/dtDAL/dtdal.h
+++ trunk/delta3d/inc/dtDAL/dtdal.h
@@ -21,6 +21,15 @@
 #ifndef DTDAL_INCLUDED
 #define DTDAL_INCLUDED
 
+#if defined (WIN32) || defined(__BORLANDC__)
+   #define __STR2__(x) #x
+   #define __STR1__(x) __STR2__(x)
+   #define __LOC__ __FILE__ "("__STR1__(__LINE__)") : Warning Msg: "
+   #pragma message (__LOC__"This header is deprecated. Please #include the individual header files.")
+#elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) || defined(__IBMCPP__)
+   #warning "This header is deprecated. Please #include the individual header files."
+#endif
+
 #include <dtDAL/actorpluginregistry.h>
 #include <dtDAL/actorproperty.h>
 #include <dtDAL/actorproxy.h>
--- trunk/delta3d/inc/dtGame/dtgame.h
+++ trunk/delta3d/inc/dtGame/dtgame.h
@@ -1,6 +1,15 @@
 #ifndef DELTA_GAME
 #define DELTA_GAME
 
+#if defined (WIN32) || defined(__BORLANDC__)
+   #define __STR2__(x) #x
+   #define __STR1__(x) __STR2__(x)
+   #define __LOC__ __FILE__ "("__STR1__(__LINE__)") : Warning Msg: "
+   #pragma message (__LOC__"This header is deprecated. Please #include the individual header files.")
+#elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) || defined(__IBMCPP__)
+   #warning "This header is deprecated. Please #include the individual header files."
+#endif
+
 #include <dtGame/actorupdatemessage.h>
 #include <dtGame/basemessages.h>
 #include <dtGame/binarylogstream.h>
