afa059c21ad72508323b678271f0ee300f739a2d
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-03-08 19:24:21 +0000

    Extracted the AutoLODScaleCameraCallback class into it's own files; no functionality changes.

--- delta3d/src/dtCore/camera.cpp
+++ delta3d/src/dtCore/camera.cpp
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////
 #include <prefix/dtcoreprefix.h>
 #include <dtCore/camera.h>
-
+#include <dtCore/autolodscalecameracallback.h>
 #include <dtCore/deltawin.h>
 #include <dtCore/scene.h>
 #include <dtCore/system.h>
@@ -14,17 +14,12 @@
 #include <dtUtil/datetime.h>
 #include <dtUtil/log.h>
 #include <dtUtil/functor.h>
-#include <dtUtil/mathdefines.h>
 
 #include <osg/Matrix>
-#include <osg/MatrixTransform>
 
-#include <osg/Version>
 #include <osg/GraphicsContext>
 #include <osgViewer/GraphicsWindow>
 
-#include <cassert>
-#include <algorithm>
 #include <cmath>
 
 using namespace dtUtil;
@@ -638,73 +633,7 @@ namespace dtCore
       return true;
    }
 
-   //////////////////////////////////////////////////////////////
-   AutoLODScaleCameraCallback::AutoLODScaleCameraCallback(dtCore::Camera* camera)
-   : mTargetFrameTimeMS(33.333)
-   , mTargetFrameTimeEpsilon(0.3)
-   , mMinLODScale(1.0f)
-   , mMaxLODScale(2.0f)
-   , mChangeFactor(0.001f)
-   , mCamera(camera)
-   , mCameraWasNull(camera == NULL)
-   {
-   }
-
-   //////////////////////////////////////////////////////////////
-   AutoLODScaleCameraCallback::~AutoLODScaleCameraCallback()
-   {
-   }
-
-   //////////////////////////////////////////////////////////////
-   void AutoLODScaleCameraCallback::Update(Camera& camera)
-   {
-      if (mCameraWasNull || mCamera == &camera)
-      {
-         double frameTime = GetFrameTimeMS();
-         // Check to see if they are different by more than a tenth of a millisecond
-         if (!osg::equivalent(mTargetFrameTimeMS, frameTime, mTargetFrameTimeEpsilon))
-         {
-            float oldScale = camera.GetLODScale();
-            float newScale = oldScale;
-
-            if (frameTime < mTargetFrameTimeMS)
-            {
-               newScale = dtUtil::Max(camera.GetLODScale() / (1 + mChangeFactor), mMinLODScale);
-            }
-            else // frame time is greater
-            {
-               newScale = dtUtil::Min(camera.GetLODScale() * (1 + mChangeFactor), mMaxLODScale);
-            }
-
-            if (!dtUtil::Equivalent(oldScale, newScale))
-            {
-               dtUtil::Log& log = dtUtil::Log::GetInstance("camera.cpp");
-               if (log.IsLevelEnabled(dtUtil::Log::LOG_DEBUG))
-               {
-                  log.LogMessage(dtUtil::Log::LOG_DEBUG, __FUNCTION__, __LINE__,
-                           "Auto-LOD: Changing LOD scale from \"%f\" to \"%f\".  FrameTime \"%lf\" ", oldScale, newScale, frameTime);
-               }
-               camera.SetLODScale(newScale);
-            }
-         }
-      }
-   }
-
-   //////////////////////////////////////////////////////////////
-   double AutoLODScaleCameraCallback::GetFrameTimeMS() const
-   {
-      return dtCore::System::GetInstance().GetSystemStageTime(System::STAGES_ALL);
-   }
-
-   //////////////////////////////////////////////////////////////
-   //////////////////////////////////////////////////////////////
-   DT_IMPLEMENT_ACCESSOR(AutoLODScaleCameraCallback, double, TargetFrameTimeMS);
-   DT_IMPLEMENT_ACCESSOR(AutoLODScaleCameraCallback, double, TargetFrameTimeEpsilon)
-   DT_IMPLEMENT_ACCESSOR(AutoLODScaleCameraCallback, float, MinLODScale);
-   DT_IMPLEMENT_ACCESSOR(AutoLODScaleCameraCallback, float, MaxLODScale);
-   DT_IMPLEMENT_ACCESSOR(AutoLODScaleCameraCallback, float, ChangeFactor);
 
 }
 
 /////////////////////////////////////////////////////////////////////////////
-
