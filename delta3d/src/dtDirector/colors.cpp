0bff4b809b9f6a00086d05cbbda38af5f4157f04
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-03-26 00:15:56 +0000

    Added a few more basic colors for dtDirector editor nodes. (user submitted, mantis #199)

--- delta3d/src/dtDirector/colors.cpp
+++ delta3d/src/dtDirector/colors.cpp
@@ -5,6 +5,8 @@ using namespace dtDirector;
 
 
 const osg::Vec3 Colors::GRAY(150, 150, 150);
+const osg::Vec3 Colors::GRAY2(220, 220, 220);
+const osg::Vec3 Colors::GRAY3(100, 100, 100);
 const osg::Vec3 Colors::RED(185, 85, 77);
 const osg::Vec3 Colors::GREEN(118, 152, 74);
 const osg::Vec3 Colors::GREEN2(74, 153, 72);
@@ -19,3 +21,5 @@ const osg::Vec3 Colors::ORANGE(176, 137, 98);
 const osg::Vec3 Colors::MANGENTA(168, 81, 181);
 const osg::Vec3 Colors::MANGENTA2(185, 77, 139);
 const osg::Vec3 Colors::BEIGE(153, 149, 101);
+const osg::Vec3 Colors::BLACK(0, 0, 0);
+const osg::Vec3 Colors::WHITE(255, 255, 255);
