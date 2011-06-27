8a04fdade8e0a09fbb1f899b2e4a5be691743243
jhoude <jhoude@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-06-27 22:29:36 +0000

    dtDirectorQt: Zooming is now smooth.

--- delta3d/src/dtDirectorQt/editorview.cpp
+++ delta3d/src/dtDirectorQt/editorview.cpp
@@ -35,6 +35,7 @@ namespace dtDirector
       , mMinScale(0.01f)
       , mMaxScale(1.5f)
       , mCurrentScale(1.0f)
+      , mGoalScale(1.0f)
    {
       setObjectName("Graph Tab");
       setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
@@ -44,6 +45,12 @@ namespace dtDirector
       // Always hide the scroll bars.
       setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
       setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+      mTimer.setParent(this);
+      mTimer.setInterval(10);
+      connect(&mTimer, SIGNAL(timeout()), this, SLOT(TimerTick()));
+
+      mTimer.start();
    }
 
    ////////////////////////////////////////////////////////////////////////////////
@@ -58,38 +65,56 @@ namespace dtDirector
       float inc = (numberOfSteps * 0.1f) + 1.0f;
 
       // Clamp the scale to the min and max range.
-      if (mCurrentScale * inc > mMaxScale)
+      if (mGoalScale * inc > mMaxScale)
       {
-         inc = mMaxScale / mCurrentScale;
+         inc = mMaxScale / mGoalScale;
       }
-      else if (mCurrentScale * inc < mMinScale)
+      else if (mGoalScale * inc < mMinScale)
       {
-         inc = mMinScale / mCurrentScale;
+         inc = mMinScale / mGoalScale;
       }
 
-      QPointF mousePos = event->pos();
-      mousePos = mapToScene(mousePos.x(), mousePos.y());
+      mFocusPos = event->pos();
 
-      mCurrentScale *= inc;
-      scale(inc, inc);
+      mGoalScale *= inc;
+   }
 
-      // Translate the view towards the mouse cursor.
-      QPointF newMousePos = event->pos();
-      newMousePos = mapToScene(newMousePos.x(), newMousePos.y());
-
-      QPointF translation = mousePos - newMousePos;
-      mScene->GetTranslationItem()->setPos(
-         mScene->GetTranslationItem()->pos() - translation);
-
-      // Refresh all group box items.
-      QList<QGraphicsItem*> itemList = mScene->GetTranslationItem()->childItems();
-      int count = itemList.size();
-      for (int index = 0; index < count; ++index)
+   ////////////////////////////////////////////////////////////////////////////////
+   void EditorView::TimerTick()
+   {
+      // calculate the amount of zoom to increment based on current and goal scales.
+      if (mCurrentScale != 0.0f && abs(mCurrentScale - mGoalScale) > 0.02f)
       {
-         NodeItem* item = dynamic_cast<NodeItem*>(itemList[index]);
-         if (item)
+         float inc = mGoalScale / mCurrentScale;
+
+         if (abs(inc) > 0.02f)
          {
-            item->DrawComment();
+            inc = 1.0f + ((inc - 1.0f) * 0.33f);
+         }
+
+         mCurrentScale *= inc;
+
+         // Translate the view towards the mouse cursor.
+         QPointF oldFocus = mapToScene(mFocusPos.x(), mFocusPos.y());
+
+         scale(inc, inc);
+
+         QPointF newFocus = mapToScene(mFocusPos.x(), mFocusPos.y());
+
+         QPointF translation = oldFocus - newFocus;
+         mScene->GetTranslationItem()->setPos(
+            mScene->GetTranslationItem()->pos() - translation);
+
+         // Refresh all group box items.
+         QList<QGraphicsItem*> itemList = mScene->GetTranslationItem()->childItems();
+         int count = itemList.size();
+         for (int index = 0; index < count; ++index)
+         {
+            NodeItem* item = dynamic_cast<NodeItem*>(itemList[index]);
+            if (item)
+            {
+               item->DrawComment();
+            }
          }
       }
    }
