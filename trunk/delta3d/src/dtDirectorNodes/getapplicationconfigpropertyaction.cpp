b3e4c6a5fa4604fcf66526ee0077c0813768c820
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-06-27 16:43:17 +0000

    fixed include case.

--- trunk/delta3d/src/dtDirectorNodes/getapplicationconfigpropertyaction.cpp
+++ trunk/delta3d/src/dtDirectorNodes/getapplicationconfigpropertyaction.cpp
@@ -19,7 +19,7 @@
  * Author: Eric R. Heine
  */
 #include <prefix/dtdirectornodesprefix.h>
-#include <dtDirectorNodes/GetApplicationConfigPropertyAction.h>
+#include <dtDirectorNodes/getapplicationconfigpropertyaction.h>
 
 #include <dtABC/application.h>
 #include <dtDAL/stringactorproperty.h>
