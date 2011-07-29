e65f4c31804f960967b5bfd1375fbed487b05efe
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-07-29 16:59:23 +0000

    re-added dtDAL dll export macro to generate the dtDAL .lib file.

--- trunk/delta3d/src/dtDAL/warndtdal.cpp
+++ trunk/delta3d/src/dtDAL/warndtdal.cpp
@@ -1,13 +1,9 @@
-
+#include <dtDAL/warndtdal.h>
 #include <dtUtil/log.h>
 
-class WarnDTDAL
-{
-public:
-   WarnDTDAL()
-   {
-      LOGN_WARNING("dtDAL", "The dtDAL library is deprecated, and will eventually be removed.  You should change your build system not to link to this library.");
-   }         
-};
 
-static WarnDTDAL instance;
+//////////////////////////////////////////////////////////////////////////
+WarnDTDAL::WarnDTDAL()
+{
+   LOGN_WARNING("dtDAL", "The dtDAL library is deprecated, and will eventually be removed.  You should change your build system not to link to this library.");
+}
