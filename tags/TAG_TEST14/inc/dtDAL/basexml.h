ccdb4e8666d185ab87e04f7e104147c10a8fbf0d
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-08-04 20:43:05 +0000

    Deleting some temporary tags

--- tags/TAG_TEST14/inc/dtDAL/basexml.h
+++
@@ -1,1 +0,0 @@
-#include <dtCore/basexml.h>
--- tags/TAG_TEST14/src/dtDAL/warndtdal.cpp
+++
@@ -1,9 +0,0 @@
-#include <dtDAL/warndtdal.h>
-#include <dtUtil/log.h>
-
-
-//////////////////////////////////////////////////////////////////////////
-WarnDTDAL::WarnDTDAL()
-{
-   LOGN_WARNING("dtDAL", "The dtDAL library is deprecated, and will eventually be removed.  You should change your build system not to link to this library.");
-}
