530a0bd2c8460f3ec3d426efb55e02c8c9946eab
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-10-07 00:19:57 +0000

    Fixed the output of the STAGE plugins (from previous revision's changes).

--- trunk/delta3d/CMakeModules/delta3d_common.cmake
+++ trunk/delta3d/CMakeModules/delta3d_common.cmake
@@ -159,4 +159,39 @@ function(DELTA3D_FIND_LIBRARY module library)
    endif()
 endfunction(DELTA3D_FIND_LIBRARY module library)
 
+MACRO (SETUP_PLUGIN_OUTPUT_DIRS SUBFOLDER)
+    #put the binary into a "STAGE plugins" folder
+    SET(OUTPUT_BINDIR ${PROJECT_BINARY_DIR}/bin/${SUBFOLDER})
+    SET(OUTPUT_LIBDIR ${PROJECT_BINARY_DIR}/lib)
+    
+    SET (CMAKE_ARCHIVE_OUTPUT_DIRECTORY  ${OUTPUT_LIBDIR})
+    SET (CMAKE_RUNTIME_OUTPUT_DIRECTORY  ${OUTPUT_BINDIR})
+    IF(WIN32)
+      SET (CMAKE_LIBRARY_OUTPUT_DIRECTORY  ${OUTPUT_BINDIR})
+    ELSE(WIN32)
+      SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${OUTPUT_LIBDIR}) 
+    ENDIF(WIN32)
+    
+    # For each configuration (Debug, Release, MinSizeRel... and/or anything the user chooses) 
+    FOREACH(CONF ${CMAKE_CONFIGURATION_TYPES})
+       # Go uppercase (DEBUG, RELEASE...)
+       STRING(TOUPPER "${CONF}" CONF)
+        
+       #Handle the debug differently by putting the output into a debug subfolder
+       IF (${CONF} STREQUAL "DEBUG")
+         SET(DEBUG_FOLDER "Debug")
+       ELSE (${CONF} STREQUAL "DEBUG")
+         SET(DEBUG_FOLDER "")
+       ENDIF (${CONF} STREQUAL "DEBUG")
+
+       SET("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_LIBDIR}")
+       SET("CMAKE_RUNTIME_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_BINDIR}/${DEBUG_FOLDER}")
+       IF(WIN32)
+         SET("CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_BINDIR}/${DEBUG_FOLDER}")
+       ELSE()
+         SET("CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_LIBDIR}")
+       ENDIF()
+    ENDFOREACH()
+ENDMACRO()
+
 endif()
