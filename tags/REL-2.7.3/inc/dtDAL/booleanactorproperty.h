478c4386e6ab708d95b4f9b47dd80dc9f4235dae
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-11-19 01:13:31 +0000

    ..and deleting another failed 2.7.3 attempt.

--- tags/REL-2.7.3/inc/dtDAL/booleanactorproperty.h
+++
@@ -1,1 +0,0 @@
-#include <dtCore/booleanactorproperty.h>
--- tags/REL-2.7.3/utilities/LMS/Java/JavaLMS/org/delta3d/lms/MessageApplet/MessageType.java
+++
@@ -1,9 +0,0 @@
-package org.delta3d.lms.MessageApplet;
-
-public enum MessageType
-{
-	SIMULATION,
-	LAUNCH_PAGE,
-	OBJECTIVE_COMPLETION,
-	OBJECTIVE_SCORE
-}
