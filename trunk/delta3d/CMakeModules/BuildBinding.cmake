ba23d49b5f510243dbd6e250e71eb9945aa7b348
cowboycoder <cowboycoder@2b6b6893-201a-0410-b3c1-b7434885eee4>
2011-11-28 22:12:12 +0000

    Fixed a cmake problem that would generate the Python bindings into the wrong folders (now outputs to build/bin/Release and build/bin/Debug).

--- trunk/delta3d/CMakeModules/BuildBinding.cmake
+++ trunk/delta3d/CMakeModules/BuildBinding.cmake
@@ -3,6 +3,40 @@
 #Supply the target name as the first parameter.  Additional
 #parameters will be passed to TARGET_LINK_LIBRARIES()
 MACRO (BUILD_PYTHON_BINDING TGTNAME)
+
+    #release goes to the bin folder
+    #debug goes to the bin/debug folder
+    SET(OUTPUT_BINDIR ${PROJECT_BINARY_DIR}/bin/)
+    SET(OUTPUT_LIBDIR ${PROJECT_BINARY_DIR}/lib)
+    
+    SET (CMAKE_ARCHIVE_OUTPUT_DIRECTORY  ${OUTPUT_LIBDIR})
+    SET (CMAKE_RUNTIME_OUTPUT_DIRECTORY  ${OUTPUT_BINDIR})
+    IF(WIN32)
+      SET (CMAKE_LIBRARY_OUTPUT_DIRECTORY  ${OUTPUT_BINDIR})
+    ELSE(WIN32)
+      SET(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${OUTPUT_LIBDIR}) 
+    ENDIF(WIN32)
+    
+    # For each configuration (Debug, Release, MinSizeRel... and/or anything the user chooses) 
+    FOREACH(CONF ${CMAKE_CONFIGURATION_TYPES})
+       # Go uppercase (DEBUG, RELEASE...)
+       STRING(TOUPPER "${CONF}" CONF)
+        
+       #Handle the debug differently by putting the output into a debug subfolder
+       IF (${CONF} STREQUAL "DEBUG")
+         SET(DEBUG_FOLDER "Debug")
+       ELSE (${CONF} STREQUAL "DEBUG")
+         SET(DEBUG_FOLDER "Release")
+       ENDIF (${CONF} STREQUAL "DEBUG")
+
+       SET("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_LIBDIR}")
+       SET("CMAKE_RUNTIME_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_BINDIR}/${DEBUG_FOLDER}")
+       IF(WIN32)
+         SET("CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_BINDIR}/${DEBUG_FOLDER}")
+       ELSE()
+         SET("CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONF}" "${OUTPUT_LIBDIR}")
+       ENDIF()
+    ENDFOREACH()
   
   #With Cmake2.6.x and CMP003 NEW, if user sets BOOST_PYTHON_LIBRARY
   #to be the non-versioned file ("boost_python-vc90-mt.lib"), the compiler
@@ -38,10 +72,6 @@ MACRO (BUILD_PYTHON_BINDING TGTNAME)
     SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES SUFFIX ".pyd")
     SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES DEBUG_POSTFIX "")
   ENDIF (WIN32)
-  
-  #little hack to get the .dll into /bin and .lib into /lib
-  IF (MSVC)
-    SET_TARGET_PROPERTIES( ${TGTNAME} PROPERTIES PREFIX "/../../bin/$(ConfigurationName)/")
-  ENDIF (MSVC)
+
 
 ENDMACRO(BUILD_PYTHON_BINDING)
