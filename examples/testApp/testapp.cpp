6bd81a7a775254f133a8cff1082a71b06ada5b4f
banderegg <banderegg@users.noreply.github.com>
2014-08-09 18:15:47 +0000

    Setting culling mode in test app Adding various properties to config Turning on multi threaded osg

--- examples/testApp/testapp.cpp
+++ examples/testApp/testapp.cpp
@@ -246,8 +246,12 @@ void TestApp::OnStartup(dtABC::BaseABC& app, dtGame::GameManager& gameManager)
    //double vfov, aspect, nearClip, farClip;
    dtCore::DeltaWin::Resolution vec = gameManager.GetApplication().GetWindow()->GetCurrentResolution();
    
+   dtCore::Camera* cam = gameManager.GetApplication().GetCamera();
    //gameManager.GetApplication().GetCamera()->GetPerspectiveParams(vfov, aspect, nearClip, farClip);
-   gameManager.GetApplication().GetCamera()->SetPerspectiveParams(75.0, vec.width / vec.height, 0.5f, 25000.0f);
+   cam->SetPerspectiveParams(75.0, vec.width / vec.height, 0.5f, 25000.0f);
+
+   cam->GetOSGCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR);   
+   cam->GetOSGCamera()->setCullingMode(osg::CullSettings::ENABLE_ALL_CULLING);
 
    // Load the map for this application.
    const std::string BASE_MAP("BaseMap");
