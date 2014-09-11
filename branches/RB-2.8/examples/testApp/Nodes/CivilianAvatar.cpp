5235008e634a43e1551b49043c4b128ddc39c5e7
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2014-09-11 21:27:37 +0000

    Test app changes. + The civilian avatar node initializes properly and ticks properly + minor changes to the civilial actor and mesh object actor. + set the max time between draws higher so that it makes it easier for the app to catch up.

--- branches/RB-2.8/examples/testApp/Nodes/CivilianAvatar.cpp
+++ branches/RB-2.8/examples/testApp/Nodes/CivilianAvatar.cpp
@@ -18,137 +18,137 @@ namespace dtExample
 {
 
 
-/////////////////////////////////////////////////////////////////////////////
-CivilianAvatar::CivilianAvatar()
-  : BaseClass()
-{
-  AddAuthor("Bradley Anderegg");
-  AddAuthor("LEgregius");
-}
-
-/////////////////////////////////////////////////////////////////////////////
-CivilianAvatar::~CivilianAvatar()
-{
-}
-
-/////////////////////////////////////////////////////////////////////////////
-DT_IMPLEMENT_ACCESSOR(CivilianAvatar, std::string, NewActorName)
-DT_IMPLEMENT_ACCESSOR(CivilianAvatar, osg::Vec3, NewActorPos)
-DT_IMPLEMENT_ACCESSOR(CivilianAvatar, dtCore::ResourceDescriptor, NewActorPrefab)
-DT_IMPLEMENT_ACCESSOR(CivilianAvatar, dtCore::UniqueId, ExistingActor)
-
-DT_IMPLEMENT_ACCESSOR(CivilianAvatar, std::string, DestinationName)
-
-
-/////////////////////////////////////////////////////////////////////////////
-void CivilianAvatar::Init(const dtDirector::NodeType& nodeType, dtDirector::DirectorGraph* graph)
-{
-  BaseClass::Init(nodeType, graph);
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
-dtGame::GameActorProxy* CivilianAvatar::CreatePrefab(dtGame::GameManager& gm)
-{
-//   AIComponent* comp = NULL;
-   dtGame::GameActorProxy* act = NULL;
-   std::vector<dtCore::RefPtr<dtCore::BaseActorObject> > actors;
-   gm.CreateActorsFromPrefab(GetNewActorPrefab(), actors);
-
-   std::vector<dtCore::RefPtr<dtCore::BaseActorObject> >::iterator i, iend;
-   i = actors.begin();
-   iend = actors.end();
-   for (; i != iend; ++i)
+   /////////////////////////////////////////////////////////////////////////////
+   CivilianAvatar::CivilianAvatar()
+   : BaseClass()
    {
-      dtCore::RefPtr<dtCore::BaseActorObject> actor = *i;
-      if (actor->IsGameActor())
+      AddAuthor("Bradley Anderegg");
+      AddAuthor("LEgregius");
+   }
+
+   /////////////////////////////////////////////////////////////////////////////
+   CivilianAvatar::~CivilianAvatar()
+   {
+   }
+
+   /////////////////////////////////////////////////////////////////////////////
+   DT_IMPLEMENT_ACCESSOR(CivilianAvatar, std::string, NewActorName)
+   DT_IMPLEMENT_ACCESSOR(CivilianAvatar, osg::Vec3, NewActorPos)
+   DT_IMPLEMENT_ACCESSOR(CivilianAvatar, dtCore::ResourceDescriptor, NewActorPrefab)
+   DT_IMPLEMENT_ACCESSOR(CivilianAvatar, dtCore::UniqueId, ExistingActor)
+
+   DT_IMPLEMENT_ACCESSOR(CivilianAvatar, std::string, DestinationName)
+
+
+   /////////////////////////////////////////////////////////////////////////////
+   void CivilianAvatar::Init(const dtDirector::NodeType& nodeType, dtDirector::DirectorGraph* graph)
+   {
+      BaseClass::Init(nodeType, graph);
+
+   }
+
+   /////////////////////////////////////////////////////////////////////////////
+   dtGame::GameActorProxy* CivilianAvatar::CreatePrefab(dtGame::GameManager& gm)
+   {
+      //   AIComponent* comp = NULL;
+      dtGame::GameActorProxy* act = NULL;
+      std::vector<dtCore::RefPtr<dtCore::BaseActorObject> > actors;
+      gm.CreateActorsFromPrefab(GetNewActorPrefab(), actors);
+
+      std::vector<dtCore::RefPtr<dtCore::BaseActorObject> >::iterator i, iend;
+      i = actors.begin();
+      iend = actors.end();
+      for (; i != iend; ++i)
       {
-         act = static_cast<dtGame::GameActorProxy*>(actor.get());
-         act->SetTranslation(GetNewActorPos());
-         if (act->HasComponent(CivilianAIActorComponent::TYPE))
+         dtCore::RefPtr<dtCore::BaseActorObject> actor = *i;
+         if (actor->IsGameActor())
          {
-            gm.AddActor(*act, false, false);
-            InitActor(act);
-            break;
+            act = static_cast<dtGame::GameActorProxy*>(actor.get());
+            act->SetTranslation(GetNewActorPos());
+            if (act->HasComponent(CivilianAIActorComponent::TYPE))
+            {
+               gm.AddActor(*act, false, false);
+               break;
+            }
          }
       }
+      return act;
    }
-   return act;
-}
 
 
-/////////////////////////////////////////////////////////////////////////////
-void CivilianAvatar::InitActor(dtGame::GameActorProxy* actor)
-{
-   CivilianAIActorComponent* civAI = actor->GetComponent<CivilianAIActorComponent>();
-   if (civAI == NULL)
+   /////////////////////////////////////////////////////////////////////////////
+   void CivilianAvatar::InitActor(dtGame::GameActorProxy* actor)
    {
-      LOG_ERROR("Actor")
+      CivilianAIActorComponent* civAI = actor->GetComponent<CivilianAIActorComponent>();
+      if (civAI == NULL)
+      {
+         LOG_ERROR("Actor")
+      }
+      civAI->Initialize();
    }
-   civAI->Initialize();
-}
 
-/////////////////////////////////////////////////////////////////////////////
-void CivilianAvatar::BuildPropertyMap()
-{
-   BaseClass::BuildPropertyMap();
-
-   dtUtil::RefString GROUP("Avatar");
-
-   typedef dtCore::PropertyRegHelper<CivilianAvatar&, CivilianAvatar> RegHelperType;
-   RegHelperType propReg(*this, this, GROUP);
-
-   DT_REGISTER_PROPERTY(NewActorName, "The name of the new Actor", RegHelperType, propReg);
-   DT_REGISTER_PROPERTY(NewActorPos, "The pos of the new Actor", RegHelperType, propReg);
-   DT_REGISTER_ACTOR_ID_PROPERTY("dtExample.CivilianActor", ExistingActor, "Existing Actor", "The id of the Actor", RegHelperType, propReg);
-   DT_REGISTER_RESOURCE_PROPERTY(dtCore::DataType::PREFAB, NewActorPrefab, "New Actor Prefab", "A prefab for the new Actor", RegHelperType, propReg);
-
-   DT_REGISTER_PROPERTY(DestinationName, "The name of the person's destination.", RegHelperType, propReg);
-}
-
-/////////////////////////////////////////////////////////////////////////////
-void CivilianAvatar::OnStart()
-{
-   dtGame::GameActorProxy* actor = NULL;
-   dtGame::GameManager* gm = GetDirector()->GetGameManager();
-   if (gm == NULL) return;
-
-   if (!GetExistingActor().ToString().empty())
+   /////////////////////////////////////////////////////////////////////////////
+   void CivilianAvatar::BuildPropertyMap()
    {
-      gm->FindGameActorById(GetExistingActor(), actor);
+      BaseClass::BuildPropertyMap();
+
+      dtUtil::RefString GROUP("Avatar");
+
+      typedef dtCore::PropertyRegHelper<CivilianAvatar&, CivilianAvatar> RegHelperType;
+      RegHelperType propReg(*this, this, GROUP);
+
+      DT_REGISTER_PROPERTY(NewActorName, "The name of the new Actor", RegHelperType, propReg);
+      DT_REGISTER_PROPERTY(NewActorPos, "The pos of the new Actor", RegHelperType, propReg);
+      DT_REGISTER_ACTOR_ID_PROPERTY("", ExistingActor, "Existing Actor", "The id of the Actor", RegHelperType, propReg);
+      DT_REGISTER_RESOURCE_PROPERTY(dtCore::DataType::PREFAB, NewActorPrefab, "New Actor Prefab", "A prefab for the new Actor", RegHelperType, propReg);
+
+      DT_REGISTER_PROPERTY(DestinationName, "The name of the person's destination.", RegHelperType, propReg);
    }
 
-   if (actor == NULL && !GetNewActorPrefab().IsEmpty())
+   /////////////////////////////////////////////////////////////////////////////
+   void CivilianAvatar::OnStart()
    {
-      actor = CreatePrefab(*gm);
+      dtGame::GameActorProxy* actor = NULL;
+      dtGame::GameManager* gm = GetDirector()->GetGameManager();
+      if (gm == NULL) return;
+
+      if (!GetExistingActor().ToString().empty())
+      {
+         gm->FindGameActorById(GetExistingActor(), actor);
+      }
+
+      if (actor == NULL && !GetNewActorPrefab().IsEmpty())
+      {
+         actor = CreatePrefab(*gm);
+      }
+
+      if (actor == NULL)
+      {
+         LOG_ERROR("Unable to create or link to a Civilian Actor.");
+      }
+      else
+      {
+         InitActor(actor);
+
+         // TODO verify it has the civ ai actor component.  In theory, one could be added.
+         SetActorOnChildNodes(actor, *gm);
+
+         BaseClass::OnStart();
+      }
    }
 
-   if (actor == NULL)
+
+   /////////////////////////////////////////////////////////////////////////////
+   bool CivilianAvatar::Update(float simDelta, float delta, int input, bool firstUpdate)
    {
-      LOG_ERROR("Unable to create or link to a Civilian Actor.");
+
+      return BaseClass::Update(simDelta, delta, input, firstUpdate);
    }
-   else
+
+   ///////////////////////////////////////////////////////////////////////////////
+
+   void CivilianAvatar::SetActorOnChildNodes(dtGame::GameActorProxy* actor, dtGame::GameManager& gm)
    {
-      // TODO verify it has the civ ai actor component.  In theory, one could be added.
-      SetActorOnChildNodes(actor, *gm);
-
-      BaseClass::OnStart();
-   }
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-bool CivilianAvatar::Update(float simDelta, float delta, int input, bool firstUpdate)
-{
-
-  return BaseClass::Update(simDelta, delta, input, firstUpdate);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-void CivilianAvatar::SetActorOnChildNodes(dtGame::GameActorProxy* actor, dtGame::GameManager& gm)
-{
-
       typedef std::vector<dtDirector::Node*> NodeArray;
       NodeArray nodes;
 
@@ -173,17 +173,17 @@ void CivilianAvatar::SetActorOnChildNodes(dtGame::GameActorProxy* actor, dtGame:
             (*iter)->AsValueNode()->SetActorID(actor->GetId(), "Value");
          }
       }
-   
-}
 
-///////////////////////////////////////////////////////////////////////////////
-bool CivilianAvatar::CanConnectValue(dtDirector::ValueLink* link, dtDirector::ValueNode* value)
-{
-  return true;
-}
+   }
+
+   ///////////////////////////////////////////////////////////////////////////////
+   bool CivilianAvatar::CanConnectValue(dtDirector::ValueLink* link, dtDirector::ValueNode* value)
+   {
+      return true;
+   }
 
 
-//---------------------
+   //---------------------
 }//namespace dtExample
 
 
--- branches/RB-2.8/examples/testApp/meshobjectactor.cpp
+++ branches/RB-2.8/examples/testApp/meshobjectactor.cpp
@@ -16,6 +16,7 @@ namespace dtExample
    MeshObjectActor::MeshObjectActor()
    {
       SetHideDTCorePhysicsProps(true);
+
    }
 
    MeshObjectActor::~MeshObjectActor()
