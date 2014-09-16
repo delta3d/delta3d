7f87e621cf1746345c8a62e0ff400b540bbe0523
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2014-09-16 15:56:14 +0000

    Added proper headers to the files and made a few fixes.

--- branches/RB-2.8/examples/testApp/Nodes/SelectTarget.h
+++ branches/RB-2.8/examples/testApp/Nodes/SelectTarget.h
@@ -1,10 +1,25 @@
-// ---------------------------------------------------------------------------------------------------------------------------------
-// dtExample
-// Originally created on 03/11/2014 by Bradley G Anderegg
-// Copyright (C) 2013-2014 Caper Holdings LLC, all rights reserved.
-//
-// SelectTarget.h
-// ---------------------------------------------------------------------------------------------------------------------------------
+/* -*-c++-*-
+ * testAPP - Using 'The MIT License'
+ * Copyright (C) 2014, Caper Holdings LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #ifndef __BLACKLIGHT_SELECTTARGET_H__
 #define __BLACKLIGHT_SELECTTARGET_H__
@@ -13,6 +28,7 @@
 #include "../export.h"
 
 #include "StateNode.h"  
+#include <dtUtil/getsetmacros.h>
 
 
 namespace dtExample
@@ -41,6 +57,8 @@ public:
   virtual bool Update(float simDelta, float delta, int input, bool firstUpdate);
 
   virtual bool CanConnectValue(dtDirector::ValueLink* link, dtDirector::ValueNode* value);
+
+  DT_DECLARE_ACCESSOR(float, Distance);
 
 protected:
 
