1e3688e4feeb027e16ff62a33665b36959fe6c1d
crodgers <crodgers@2b6b6893-201a-0410-b3c1-b7434885eee4>
2014-09-26 20:20:05 +0000

    * Removed the interpolation over particle size.

--- branches/RB-2.8/examples/testApp/surfacevesselactorcomponent.cpp
+++ branches/RB-2.8/examples/testApp/surfacevesselactorcomponent.cpp
@@ -663,7 +663,6 @@ namespace dtExample
    void SurfaceVesselActorComponent::InterpolateParticleSystem(DynamicParticles& particles, float ratio)
    {
       particles.InterpolateAllLayers(dtCore::ParticlePropertyEnum::PS_EMIT_SPEED, ratio);
-      particles.InterpolateAllLayers(dtCore::ParticlePropertyEnum::PS_PARTICLE_SIZE, ratio);
    }
 
    //////////////////////////////////////////////////////////
