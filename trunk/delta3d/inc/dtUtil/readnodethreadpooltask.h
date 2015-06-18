516085d26063a3e17c3ddf00967cfcf76c884bcb
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2015-06-18 15:19:13 +0000

    factored out a thread pool task for loading osg data in the background.

---
+++ trunk/delta3d/inc/dtUtil/readnodethreadpooltask.h
@@ -0,0 +1,60 @@
+/*
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2015 Caper Holdings, LLC
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#ifndef DELTA_READ_NODE_THREAD_POOL_TASK_
+#define DELTA_READ_NODE_THREAD_POOL_TASK_
+
+#include <dtUtil/export.h>
+#include <dtUtil/getsetmacros.h>
+#include <dtUtil/threadpool.h>
+#include <osg/Node>
+#include <osg/ref_ptr>
+#include <osgDB/Options>
+
+namespace dtUtil
+{
+      class DT_UTIL_EXPORT ReadNodeThreadPoolTask : public dtUtil::ThreadPoolTask
+      {
+      public:
+         ReadNodeThreadPoolTask();
+
+         virtual void operator()();
+
+         osg::Node* GetLoadedNode();
+         const osg::Node* GetLoadedNode() const;
+
+         /// Check to see if the loading is complete.  If it returns true, call WaitUntilComplete() to make sure.
+         bool IsComplete() const;
+
+         virtual void ResetData();
+
+         DT_DECLARE_ACCESSOR(bool, UseFileCaching);
+         DT_DECLARE_ACCESSOR(std::string, FileToLoad);
+         DT_DECLARE_ACCESSOR(osg::ref_ptr<osgDB::Options>, LoadOptions);
+
+      protected:
+         virtual ~ReadNodeThreadPoolTask();
+      private:
+         osg::ref_ptr<osg::Node> mLoadedNode;
+         volatile bool mComplete;
+      };
+}
+
+#endif
