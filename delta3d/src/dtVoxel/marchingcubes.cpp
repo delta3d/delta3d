8aa080a7bb0ffa53dc223b34aa6daf6d2e93fb54
banderegg <banderegg@users.noreply.github.com>
2015-08-03 13:36:26 +0000

    Fixing performance problem in marching cubes

--- delta3d/src/dtVoxel/marchingcubes.cpp
+++ delta3d/src/dtVoxel/marchingcubes.cpp
@@ -93,7 +93,7 @@ namespace dtVoxel
       |/       |/    |/       |/
       3--------2     *---2----*
       */
-      int edgeTable[256] = {
+      static const int edgeTable[256] = {
          0x0, 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c,
          0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
          0x190, 0x99, 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c,
@@ -150,7 +150,7 @@ namespace dtVoxel
       Remember, each intersected edge contains only one surface vertex.  The
       vertex triples are listed in counter clockwise order for proper facing.
       */
-      int triTable[256][16] =
+      static const int triTable[256][16] =
       { { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
       { 0, 8, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
       { 0, 1, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
