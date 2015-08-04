8d080288e34bd8bec1afe7be90f15219424c8d3c
David Guthrie <LEgregius@users.noreply.github.com>
2015-08-04 15:44:41 +0000

    Added code to gradually degrade the number tiles updated.

--- delta3d/src/dtVoxel/voxelgrid.cpp
+++ delta3d/src/dtVoxel/voxelgrid.cpp
@@ -51,7 +51,6 @@ namespace dtVoxel
       , mWSDimensions()
       , mBlockDimensions()
       , mCellDimensions()
-      , mMaxCellsToUpdatePerFrame(1)
       , mInitialized(false)
       , mRes0()
       , mRes1()
@@ -200,9 +199,9 @@ namespace dtVoxel
 
    }
 
-   void VoxelGrid::BeginNewUpdates(const osg::Vec3& newCameraPos)
+   void VoxelGrid::BeginNewUpdates(const osg::Vec3& newCameraPos, unsigned maxCellsToUpdate)
    {
-      unsigned runCount = mMaxCellsToUpdatePerFrame;
+      unsigned runCount = maxCellsToUpdate;
       for (auto iter = mDirtyCells.begin(); runCount > 0 && iter != mDirtyCells.end(); ++iter)
       {
          VoxelCellUpdateInfo& updateInfo = *iter;
@@ -227,8 +226,6 @@ namespace dtVoxel
       
       mVoxelActor = &voxelActor;
       
-      mMaxCellsToUpdatePerFrame = mVoxelActor->GetMaxCellsToUpdatePerFrame();
-
       //std::cout << std::endl << "Creating Voxel Grid with " << mNumBlocks << " Voxel Blocks" << std::endl;
 
       GenerateCacheString();
