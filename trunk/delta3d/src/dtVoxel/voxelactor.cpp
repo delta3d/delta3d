f2b71f63cfe9ba70414bb7966f277f2ffe3454b8
legregius <legregius@2b6b6893-201a-0410-b3c1-b7434885eee4>
2015-12-11 21:06:42 +0000

    Merged in a change from the branch for the voxel actor where it does the voxel deleting first.

--- trunk/delta3d/src/dtVoxel/voxelactor.cpp
+++ trunk/delta3d/src/dtVoxel/voxelactor.cpp
@@ -286,6 +286,51 @@ namespace dtVoxel
 
       openvdb::Vec3d lastVector;
       osg::BoundingBox bb;
+      for (unsigned i = 0, iend = indicesDeactivated->GetSize(); i < iend; ++i)
+      {
+         const dtCore::NamedParameter* indexP = indicesDeactivated->GetParameter(i);
+         if (indexP != nullptr && indexP->GetDataType() == dtCore::DataType::VEC3)
+         {
+            auto indexVp = static_cast<const dtCore::NamedVec3Parameter*>(indexP);
+            osg::Vec3 idxVec = indexVp->GetValue();
+            openvdb::Vec3d idxOVDBVec(idxVec.x(), idxVec.y(), idxVec.z());
+            openvdb::Vec3d worldVec = grid->transform().indexToWorld(idxOVDBVec);
+            if (first)
+            {
+               lastVector = worldVec;
+
+               first = false;
+            }
+            else
+            {
+               openvdb::Vec3d diff = lastVector - worldVec;
+               double lengthSqr = diff.lengthSqr();
+               if (lengthSqr > rangeToRecomputeBound)
+               {
+                  //markDirtyCounter++;
+                  MarkVisualDirty(bb, 0);
+                  bb = osg::BoundingBox();
+               }
+            }
+
+            bb.expandBy(osg::Vec3(worldVec.x(), worldVec.y(), worldVec.z()));
+            if (!updateVisualOnly)
+            {
+               openvdb::Coord c(openvdb::Coord::round(idxOVDBVec));
+               accessor.setValueOff(c, grid->background());
+            }
+
+            lastVector = worldVec;
+         }
+         else
+         {
+            LOGN_ERROR("voxelactor.cpp", "Received a VolumeUpdateMessage, but the indices deactivated are not Vec3 parameters.");
+         }
+      }
+
+      first = true;
+      MarkVisualDirty(bb, 0);
+
       for (unsigned i = 0, iend = indices->GetSize(); i < iend; ++i)
       {
          const dtCore::NamedParameter* indexP = indices->GetParameter(i);
@@ -331,51 +376,6 @@ namespace dtVoxel
          else
          {
             LOGN_ERROR("voxelactor.cpp", "Received a VolumeUpdateMessage, but the indices are not Vec3 parameters.");
-         }
-      }
-
-      first = true;
-      MarkVisualDirty(bb, 0);
-
-      for (unsigned i = 0, iend = indicesDeactivated->GetSize(); i < iend; ++i)
-      {
-         const dtCore::NamedParameter* indexP = indicesDeactivated->GetParameter(i);
-         if (indexP != nullptr && indexP->GetDataType() == dtCore::DataType::VEC3)
-         {
-            auto indexVp = static_cast<const dtCore::NamedVec3Parameter*>(indexP);
-            osg::Vec3 idxVec = indexVp->GetValue();
-            openvdb::Vec3d idxOVDBVec(idxVec.x(), idxVec.y(), idxVec.z());
-            openvdb::Vec3d worldVec = grid->transform().indexToWorld(idxOVDBVec);
-            if (first)
-            {
-               lastVector = worldVec;
-
-               first = false;
-            }
-            else
-            {
-               openvdb::Vec3d diff = lastVector - worldVec;
-               double lengthSqr = diff.lengthSqr();
-               if (lengthSqr > rangeToRecomputeBound)
-               {
-                  //markDirtyCounter++;
-                  MarkVisualDirty(bb, 0);
-                  bb = osg::BoundingBox();
-               }
-            }
-
-            bb.expandBy(osg::Vec3(worldVec.x(), worldVec.y(), worldVec.z()));
-            if (!updateVisualOnly)
-            {
-               openvdb::Coord c(openvdb::Coord::round(idxOVDBVec));
-               accessor.setValueOff(c, grid->background());
-            }
-
-            lastVector = worldVec;
-         }
-         else
-         {
-            LOGN_ERROR("voxelactor.cpp", "Received a VolumeUpdateMessage, but the indices deactivated are not Vec3 parameters.");
          }
       }
       
